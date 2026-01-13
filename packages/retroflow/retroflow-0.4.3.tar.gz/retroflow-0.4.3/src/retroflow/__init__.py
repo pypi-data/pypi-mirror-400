fdace270cfec651556e2065e09a4ad828c7d8441
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 23:05:32 +0000

    Add package retroflow/0.4.3/retroflow-0.4.3.tar.gz

---
+++ packages/retroflow/retroflow-0.4.3.tar.gz/retroflow-0.4.3/src/retroflow/__init__.py
@@ -0,0 +1,38 @@
+"""
+RetroFlow - Beautiful ASCII Flowcharts
+
+A Python library for generating ASCII flowcharts with intelligent Sugiyama layout.
+
+Example:
+    >>> from retroflow import FlowchartGenerator
+    >>> generator = FlowchartGenerator()
+    >>> flowchart = generator.generate('''
+    ...     A -> B
+    ...     B -> C
+    ... ''')
+    >>> print(flowchart)
+"""
+
+from .generator import FlowchartGenerator
+from .layout import LayoutResult, NodeLayout, SugiyamaLayout
+from .parser import ParseError, Parser, parse_flowchart
+from .renderer import BoxRenderer, Canvas, LineRenderer
+from .router import BoxInfo, EdgeRoute, EdgeRouter
+
+__version__ = "0.4.3"
+
+__all__ = [
+    "FlowchartGenerator",
+    "Parser",
+    "ParseError",
+    "parse_flowchart",
+    "SugiyamaLayout",
+    "LayoutResult",
+    "NodeLayout",
+    "Canvas",
+    "BoxRenderer",
+    "LineRenderer",
+    "EdgeRouter",
+    "EdgeRoute",
+    "BoxInfo",
+]
