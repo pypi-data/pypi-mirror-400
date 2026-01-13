28896cfe25b90261f11900f1f6907af44b92266f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 15:08:03 +0000

    Add package w2n-n2w/0.1.0/w2n_n2w-0.1.0-py3-none-any.whl

---
+++ packages/w2n-n2w/w2n_n2w-0.1.0-py3-none-any.whl/w2n_n2w/test.py
@@ -0,0 +1,15 @@
+# from w2n_n2w import Convert
+# c = Convert()
+# print(c.convert("two hundred"))
+# #
+# print(c.convert("twenty two", "thirty three", "two hundred"))
+# print(c.convert(222))
+# from n2w.units import units,tens,scales
+# units_reverse = {v: k for k, v in units.items()}
+# # my = {v:k for k,v in units.items()}
+# # print(my)
+# (222)
+# 222/100
+from w2n_n2w import converter
+c = converter.Convert()
+print(c.convert("two hundred"))
