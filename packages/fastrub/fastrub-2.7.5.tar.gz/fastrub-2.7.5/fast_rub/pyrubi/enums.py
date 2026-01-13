9759ae2742a5e6fff42b4498ce65d23707cb5ba2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 19:07:08 +0000

    Add package fastrub/2.7.5/fastrub-2.7.5.tar.gz

---
+++ packages/fastrub/fastrub-2.7.5.tar.gz/fastrub-2.7.5/fast_rub/pyrubi/enums.py
@@ -0,0 +1,21 @@
+class SetAdminAccessList:
+    set_admin:str = "SetAdmin"
+    ban_member:str = "BanMember"
+    change_info:str = "ChangeInfo"
+    pin_messages:str = "PinMessages"
+    delete_messages:str = "DeleteGlobalAllMessages"
+    edit_messages:str = "EditMessages"
+    set_join_link:str = "SetJoinLink"
+    set_member_access:str = "SetMemberAccess"
+    delete_global_all_messages:str = "DeleteGlobalAllMessages"
+
+class SetGroupDefaultAccessList:
+    send_messages:str = "SendMessages"
+    add_member:str = "AddMember"
+    view_admins:str = "ViewAdmins"
+    view_members:str = "ViewMembers"
+
+class ChatActivities:
+    typing:str = "Typing"
+    recording:str = "Recording"
+    uploading:str = "Uploading"
