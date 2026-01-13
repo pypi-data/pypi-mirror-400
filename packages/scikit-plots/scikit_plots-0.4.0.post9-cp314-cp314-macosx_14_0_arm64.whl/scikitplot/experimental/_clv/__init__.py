296f29437c60800e071b0b3c1fd095f5e0446769
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 18:10:47 +0000

    Add package scikit-plots/0.4.0.post9/scikit_plots-0.4.0.post9-cp314-cp314-macosx_14_0_arm64.whl

---
+++ packages/scikit-plots/scikit_plots-0.4.0.post9-cp314-cp314-macosx_14_0_arm64.whl/scikitplot/experimental/_clv/__init__.py
@@ -0,0 +1,17 @@
+# fmt: off
+# ruff: noqa
+# ruff: noqa: PGH004
+# flake8: noqa
+# pylint: skip-file
+# mypy: ignore-errors
+# type: ignore
+
+# This module was copied from the lifetimes project.
+# https://github.com/CamDavidsonPilon/lifetimes
+# https://github.com/CamDavidsonPilon/lifelines
+
+"""
+CLV (Customer Lifetime Value)
+"""
+
+__version__ = "0.1b3"
