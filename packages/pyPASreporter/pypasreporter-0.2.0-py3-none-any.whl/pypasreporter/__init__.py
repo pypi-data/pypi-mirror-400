ac74862f680ef46b88d59ec098b5f4fa0d875d55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 21:05:12 +0000

    Add package pyPASreporter/0.2.0/pypasreporter-0.2.0-py3-none-any.whl

---
+++ packages/pyPASreporter/pypasreporter-0.2.0-py3-none-any.whl/pypasreporter/__init__.py
@@ -0,0 +1,36 @@
+"""
+pyPASreporter: CyberArk PAM reporting and analytics toolkit.
+
+This is the main package that brings together the pyPASreporter ecosystem.
+"""
+
+__version__ = "0.2.0"
+__author__ = "itpamltd"
+__email__ = "itpamltd@proton.me"
+
+# Re-export from sub-packages for convenience
+try:
+    from pypasreporter_evdparser import parse_evd, EVDParser
+except ImportError:
+    parse_evd = None  # type: ignore
+    EVDParser = None  # type: ignore
+
+try:
+    from pypasreporter_pacliparser import parse_pacli, parse_policies, PACLIParser
+except ImportError:
+    parse_pacli = None  # type: ignore
+    parse_policies = None  # type: ignore
+    PACLIParser = None  # type: ignore
+
+from .core import load_evd, load_pacli
+
+__all__ = [
+    "load_evd",
+    "load_pacli",
+    "parse_evd",
+    "parse_pacli",
+    "parse_policies",
+    "EVDParser",
+    "PACLIParser",
+    "__version__",
+]
