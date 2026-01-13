4ebc12b58ac05cd5660f78d4e61db296ad1a65f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 15:08:12 +0000

    Add package cpymad/1.19.0/cpymad-1.19.0-cp311-cp311-macosx_15_0_arm64.whl

---
+++ packages/cpymad/cpymad-1.19.0-cp311-cp311-macosx_15_0_arm64.whl/cpymad/__init__.py
@@ -0,0 +1,51 @@
+from __future__ import unicode_literals
+import sys
+import warnings
+
+
+__title__ = 'cpymad'
+__version__ = '1.19.0'
+
+__summary__ = 'Cython binding to MAD-X'
+__uri__ = 'https://github.com/hibtc/cpymad'
+
+__credits__ = """
+Current cpymad maintainer:
+
+    - Thomas Gläßle <t_glaessle@gmx.de>
+
+Initial pymad creators:
+
+    - Yngve Inntjore Levinsen <Yngve.Inntjore.Levinsen@cern.ch>
+    - Kajetan Fuchsberger <Kajetan.Fuchsberger@cern.ch>
+"""
+
+
+try:
+    # For some unknown reason, in recent versions of numpy on linux, numpy
+    # must be pre-imported before importing cpymad.libmadx to avoid segfault.
+    # See https://github.com/hibtc/cpymad/issues/155.
+    __import__("numpy", None, None, "*")
+except ImportError:
+    pass
+
+
+if sys.version_info < (3, 9):
+    _unsupported_version = (
+        "Support for python 3.8 and below will be removed in a future release!\n"
+        "If you need continued support for an older version, let us know at:\n"
+        "  https://github.com/hibtc/cpymad/issues")
+    warnings.warn(_unsupported_version, DeprecationWarning)
+
+
+def _read_text(package, resource, **kwargs):
+    try:
+        from importlib.resources import files
+        return files(package).joinpath(resource).read_text(**kwargs)
+    except ImportError:
+        from importlib.resources import read_text
+        return read_text(package, resource, **kwargs)
+
+
+def get_copyright_notice() -> str:
+    return _read_text('cpymad.COPYING', 'cpymad.rst', encoding='utf-8')
