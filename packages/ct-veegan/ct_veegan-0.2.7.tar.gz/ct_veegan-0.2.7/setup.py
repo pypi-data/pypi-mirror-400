e56ed01bfa0c216223bdac33cc903be0b372ecc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 18:11:16 +0000

    Add package ct-veegan/0.2.7/ct_veegan-0.2.7.tar.gz

---
+++ packages/ct-veegan/ct_veegan-0.2.7.tar.gz/ct_veegan-0.2.7/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ct_veegan',
+    version='0.2.7',
+    packages=find_packages(),
+    include_package_data=True,  # penting untuk aktifkan MANIFEST.in
+    package_data={
+        "ct_veegan": [
+            "*.model", "*.bin", "*.pt", "*.txt"
+        ]
+    },
+    install_requires=[
+        'torch>=2.0.0',
+        'numpy>=1.20.0',
+        'requests>=2.0.0',
+        'scikit-learn>=1.4.2',
+        'imbalanced-learn>=0.14.0',
+        'gensim>=4.0.0',
+        'datasets>=2.0.0'
+    ],
+    python_requires='>=3.8',
+    description='GAN package for sequence vector generation and classification',
+    author='Laode Hidayat',
+    author_email='your_email@example.com',
+    url='https://github.com/username/ct_veegan',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
