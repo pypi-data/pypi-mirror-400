3701dc164d1086fe819c011ea42022a6a04ca772
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 03:31:27 +0000

    Add package mm-ptx/1.0.1/mm_ptx-1.0.1-cp311-cp311-musllinux_1_2_x86_64.whl

---
+++ packages/mm-ptx/mm_ptx-1.0.1-cp311-cp311-musllinux_1_2_x86_64.whl/mm_ptx/__init__.py
@@ -0,0 +1,11 @@
+# SPDX-FileCopyrightText: 2026 MetaMachines LLC
+#
+# SPDX-License-Identifier: MIT
+
+from .include import get_include_dir, get_ptx_inject_header, get_stack_ptx_header
+
+__all__ = [
+    "get_include_dir",
+    "get_ptx_inject_header",
+    "get_stack_ptx_header",
+]
