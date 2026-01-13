d90656464734201927f21d1ca1964ed541964521
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 22:05:58 +0000

    Add package True-Tiktok-Uploader/2026.1.7/true_tiktok_uploader-2026.1.7.tar.gz

---
+++ packages/True-Tiktok-Uploader/true_tiktok_uploader-2026.1.7.tar.gz/true_tiktok_uploader-2026.1.7/True_Tiktok_Uploader/config.py
@@ -0,0 +1,11 @@
+short_wait = 5  # seconds
+long_wait = 60 # seconds
+uploading_wait = 180 # seconds
+
+main_path = "https://www.tiktok.com/"
+login_path = "https://www.tiktok.com/login/phone-or-email/email"
+upload_path = "https://www.tiktok.com/creator-center/upload?lang=en"
+
+upload_video = "//input[@type='file']"
+description = "//div[@contenteditable='true']"
+post = "//button[@data-e2e='post_video_button']"
