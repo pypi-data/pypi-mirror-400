fe13c964ad03e940f406cb86b06d22e49dbf4f6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 12:05:13 +0000

    Add package ni.measurementlink.discovery.v1.proto/1.1.0/ni_measurementlink_discovery_v1_proto-1.1.0-py3-none-any.whl

---
+++ packages/ni.measurementlink.discovery.v1.proto/ni_measurementlink_discovery_v1_proto-1.1.0-py3-none-any.whl/ni/measurementlink/discovery/v1/annotations/__init__.py
@@ -0,0 +1,13 @@
+"""Constants for discovery service annotations."""
+
+SERVICE_COLLECTION_KEY = "ni/service.collection"
+"""Key for an annotation indicating the hierarchical collection the service belongs to."""
+
+SERVICE_DESCRIPTION_KEY = "ni/service.description"
+"""Key for an annotation containing a description of the service."""
+
+SERVICE_PROGRAMMINGLANGUAGE_KEY = "ni/service.programminglanguage"
+"""Key for an annotation indicating the programming language in which the service is implemented."""
+
+SERVICE_TAGS_KEY = "ni/service.tags"
+"""Key for an annotation containing the tags for the service."""
