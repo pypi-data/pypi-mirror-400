b4f1abc8e02aa98e7e71c84a1cfc58ea21ee6203
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 05:07:59 +0000

    Add package mlrl-boomer/0.15.1/mlrl_boomer-0.15.1-cp314-cp314-win_amd64.whl

---
+++ packages/mlrl-boomer/mlrl_boomer-0.15.1-cp314-cp314-win_amd64.whl/mlrl/boosting/__init__.py
@@ -0,0 +1,12 @@
+"""
+This module provides rule learners that make using of the gradient boosting framework.
+"""
+from mlrl.boosting.learners import BoomerClassifier
+
+try:
+    from mlrl.boosting.testbed.runnables import BoomerRunnable as Runnable
+except ImportError:
+    # Dependency 'mlrl-testbed' is not available, but that's okay since it's optional.
+    pass
+
+Boomer = BoomerClassifier
