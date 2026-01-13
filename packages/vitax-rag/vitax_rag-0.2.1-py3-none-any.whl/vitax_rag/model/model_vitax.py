17dbdaebe7de19823ca26bbff2e35a13f77debb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 09:10:57 +0000

    Add package vitax-rag/0.2.1/vitax_rag-0.2.1-py3-none-any.whl

---
+++ packages/vitax-rag/vitax_rag-0.2.1-py3-none-any.whl/vitax_rag/model/model_vitax.py
@@ -0,0 +1,28 @@
+import torch.nn as nn
+from .model_hyena import HyenaDNAModel
+import json
+
+
+class Encoder(nn.Module):
+    def __init__(self):
+        super(Encoder, self).__init__()
+        config = json.load(open("./data/model_save/config.json"))
+        self.model = HyenaDNAModel(**config, use_head=False)
+    def forward(self, dna):
+        dna_embed = self.get_dna(dna)
+        return dna_embed
+    def get_dna(self,dna_inputs):
+        dna_embed = self.model(dna_inputs)[:,-1,:]
+        return dna_embed
+    def get_dna2(self,dna):
+        dna2 = torch.flip(dna,dims = [1])
+        dna2[:,0] = dna[:,0]
+        dna2[:,-1] = dna[:,-1]
+        dna_embed1 = self.model(dna)[:,-1,:]
+        dna_embed2 = self.model(dna2)[:,-1,:]
+        dna_embed = (dna_embed1 + dna_embed2) / 2
+        return dna_embed
+    
+
+        
+        
