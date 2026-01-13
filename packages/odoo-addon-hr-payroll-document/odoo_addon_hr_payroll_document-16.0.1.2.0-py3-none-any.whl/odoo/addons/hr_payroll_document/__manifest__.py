a6cd7ab06333a7e574bdbf978fbcf8ee951c2f76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 09:11:07 +0000

    Add package odoo-addon-hr-payroll-document/16.0.1.2.0/odoo_addon_hr_payroll_document-16.0.1.2.0-py3-none-any.whl

---
+++ packages/odoo-addon-hr-payroll-document/odoo_addon_hr_payroll_document-16.0.1.2.0-py3-none-any.whl/odoo/addons/hr_payroll_document/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "HR - Payroll Document",
+    "summary": "Manage payroll for each employee",
+    "author": "APSL, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/payroll",
+    "license": "AGPL-3",
+    "category": "Payrolls",
+    "version": "16.0.1.2.0",
+    "depends": ["hr", "base_vat"],
+    "maintainers": ["peluko00"],
+    "external_dependencies": {"python": ["pypdf"]},
+    "data": [
+        "wizard/payroll_management_wizard.xml",
+        "security/ir.model.access.csv",
+        "data/email_payroll_employee.xml",
+        "views/hr_employee_views.xml",
+        "views/res_users_views.xml",
+    ],
+}
