7538e7b562ed77808f6e528f449d29008e5b03fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 22:06:11 +0000

    Add package baihu-logging/1.1.0/baihu_logging-1.1.0-py3-none-any.whl

---
+++ packages/baihu-logging/baihu_logging-1.1.0-py3-none-any.whl/baihu_logging/constants.py
@@ -0,0 +1,21 @@
+from colorama import Fore
+
+LOG_COLORS = {
+    "DEBUG": 0x95A5A6,
+    "INFO": 0x5865F2,
+    "WARNING": 0xF1C40F,
+    "ERROR": 0xE74C3C,
+    "CRITICAL": 0x9B59B6
+}
+
+COLORAMA_COLORS = {
+    "DEBUG": Fore.LIGHTBLACK_EX,
+    "INFO": Fore.BLUE,
+    "WARNING": Fore.YELLOW,
+    "ERROR": Fore.RED,
+    "CRITICAL": Fore.MAGENTA
+}
+
+LOG_LEVELS = list(LOG_COLORS.keys())
+
+BASE_URL = "https://discord.com/api/v10"
