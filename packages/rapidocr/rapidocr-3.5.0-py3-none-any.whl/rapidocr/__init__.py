a4ea43b9a9fc42d1cf8109899103e411b774488e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 18:09:32 +0000

    Add package rapidocr/3.5.0/rapidocr-3.5.0-py3-none-any.whl

---
+++ packages/rapidocr/rapidocr-3.5.0-py3-none-any.whl/rapidocr/__init__.py
@@ -0,0 +1,7 @@
+# -*- encoding: utf-8 -*-
+# @Author: SWHL
+# @Contact: liekkaskono@163.com
+from .main import RapidOCR
+from .utils.load_image import LoadImageError
+from .utils.typings import EngineType, LangCls, LangDet, LangRec, ModelType, OCRVersion
+from .utils.vis_res import VisRes
