ff1fe2a2b584fb2a593ba9d4a9592566ffa5be66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 04:18:06 +0000

    Add package liblaf-melon/1.0.0/liblaf_melon-1.0.0.tar.gz

---
+++ packages/liblaf-melon/liblaf_melon-1.0.0.tar.gz/liblaf_melon-1.0.0/src/liblaf/melon/typing/__init__.pyi
@@ -0,0 +1,8 @@
+from ._geometry import MeshLike, PointSetLike, PolyDataLike, UnstructuredGridLike
+
+__all__ = [
+    "MeshLike",
+    "PointSetLike",
+    "PolyDataLike",
+    "UnstructuredGridLike",
+]
---
+++ packages/liblaf-melon/liblaf_melon-1.0.0.tar.gz/liblaf_melon-1.0.0/src/liblaf/melon/typing/_geometry.py
@@ -0,0 +1,6 @@
+from typing import Any
+
+type MeshLike = Any
+type PointSetLike = Any
+type PolyDataLike = Any
+type UnstructuredGridLike = Any
