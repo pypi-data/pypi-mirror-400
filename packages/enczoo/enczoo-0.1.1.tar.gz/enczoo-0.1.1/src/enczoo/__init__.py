c256a9dd6d6fdac91ade91ea2fa4568da890894e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 20:06:42 +0000

    Add package enczoo/0.1.1/enczoo-0.1.1.tar.gz

---
+++ packages/enczoo/enczoo-0.1.1.tar.gz/enczoo-0.1.1/src/enczoo/__init__.py
@@ -0,0 +1,6 @@
+__all__ = ["ImageEncoding", "Pixels", "ResNet50", "AlexNet"]
+
+
+from enczoo.base import ImageEncoding
+from enczoo.neural_networks.pretrained import AlexNet, ResNet50
+from enczoo.classic.pixels import Pixels
