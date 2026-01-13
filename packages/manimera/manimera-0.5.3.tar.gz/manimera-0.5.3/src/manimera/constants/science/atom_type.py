c269a5cf72cfe3c2203e1501702dd09e0fce23f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 22:06:41 +0000

    Add package manimera/0.5.3/manimera-0.5.3.tar.gz

---
+++ packages/manimera/manimera-0.5.3.tar.gz/manimera-0.5.3/src/manimera/constants/science/atom_type.py
@@ -0,0 +1,147 @@
+# ======================================================================================================================
+# IMPORTS
+# ======================================================================================================================
+
+# STANDARD IMPORTS =====================================================================================================
+
+from enum import Enum
+
+# THIRD PARTY IMPORTS ==================================================================================================
+
+# <None>
+
+# MANIMERA IMPORTS =====================================================================================================
+
+# <None>
+
+# ======================================================================================================================
+# ATOM TYPE CLASS
+# ======================================================================================================================
+
+
+class AtomType(Enum):
+    """Chemical elements with Z (atomic number) and A (most common/stable isotope mass number)."""
+
+    Hydrogen = (1, 1)
+    Helium = (2, 4)
+    Lithium = (3, 7)
+    Beryllium = (4, 9)
+    Boron = (5, 11)
+    Carbon = (6, 12)
+    Nitrogen = (7, 14)
+    Oxygen = (8, 16)
+    Fluorine = (9, 19)
+    Neon = (10, 20)
+    Sodium = (11, 23)
+    Magnesium = (12, 24)
+    Aluminium = (13, 27)
+    Silicon = (14, 28)
+    Phosphorus = (15, 31)
+    Sulfur = (16, 32)
+    Chlorine = (17, 35)
+    Argon = (18, 40)
+    Potassium = (19, 39)
+    Calcium = (20, 40)
+    Scandium = (21, 45)
+    Titanium = (22, 48)
+    Vanadium = (23, 51)
+    Chromium = (24, 52)
+    Manganese = (25, 55)
+    Iron = (26, 56)
+    Cobalt = (27, 59)
+    Nickel = (28, 58)
+    Copper = (29, 63)
+    Zinc = (30, 64)
+    Gallium = (31, 69)
+    Germanium = (32, 74)
+    Arsenic = (33, 75)
+    Selenium = (34, 80)
+    Bromine = (35, 79)
+    Krypton = (36, 84)
+    Rubidium = (37, 85)
+    Strontium = (38, 88)
+    Yttrium = (39, 89)
+    Zirconium = (40, 90)
+    Niobium = (41, 93)
+    Molybdenum = (42, 98)
+    Technetium = (43, 98)
+    Ruthenium = (44, 102)
+    Rhodium = (45, 103)
+    Palladium = (46, 106)
+    Silver = (47, 107)
+    Cadmium = (48, 112)
+    Indium = (49, 115)
+    Tin = (50, 120)
+    Antimony = (51, 121)
+    Tellurium = (52, 128)
+    Iodine = (53, 127)
+    Xenon = (54, 132)
+    Caesium = (55, 133)
+    Barium = (56, 138)
+    Lanthanum = (57, 139)
+    Cerium = (58, 140)
+    Praseodymium = (59, 141)
+    Neodymium = (60, 142)
+    Promethium = (61, 145)
+    Samarium = (62, 152)
+    Europium = (63, 153)
+    Gadolinium = (64, 158)
+    Terbium = (65, 159)
+    Dysprosium = (66, 164)
+    Holmium = (67, 165)
+    Erbium = (68, 166)
+    Thulium = (69, 169)
+    Ytterbium = (70, 174)
+    Lutetium = (71, 175)
+    Hafnium = (72, 180)
+    Tantalum = (73, 181)
+    Tungsten = (74, 184)
+    Rhenium = (75, 187)
+    Osmium = (76, 192)
+    Iridium = (77, 193)
+    Platinum = (78, 195)
+    Gold = (79, 197)
+    Mercury = (80, 202)
+    Thallium = (81, 205)
+    Lead = (82, 208)
+    Bismuth = (83, 209)
+    Polonium = (84, 209)
+    Astatine = (85, 210)
+    Radon = (86, 222)
+    Francium = (87, 223)
+    Radium = (88, 226)
+    Actinium = (89, 227)
+    Thorium = (90, 232)
+    Protactinium = (91, 231)
+    Uranium = (92, 238)
+    Neptunium = (93, 237)
+    Plutonium = (94, 244)
+    Americium = (95, 243)
+    Curium = (96, 247)
+    Berkelium = (97, 247)
+    Californium = (98, 251)
+    Einsteinium = (99, 252)
+    Fermium = (100, 257)
+    Mendelevium = (101, 258)
+    Nobelium = (102, 259)
+    Lawrencium = (103, 262)
+    Rutherfordium = (104, 267)
+    Dubnium = (105, 268)
+    Seaborgium = (106, 269)
+    Bohrium = (107, 270)
+    Hassium = (108, 269)
+    Meitnerium = (109, 278)
+    Darmstadtium = (110, 281)
+    Roentgenium = (111, 282)
+    Copernicium = (112, 285)
+    Nihonium = (113, 286)
+    Flerovium = (114, 289)
+    Moscovium = (115, 289)
+    Livermorium = (116, 293)
+    Tennessine = (117, 294)
+    Oganesson = (118, 294)
+
+
+# ======================================================================================================================
+# ATOM TYPE CLASS END
+# ======================================================================================================================
