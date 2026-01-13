00ebac5cac5ce613726d21c56f7d4f69c0cd32e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 19:07:22 +0000

    Add package solarlog-cli/0.7.0/solarlog_cli-0.7.0.tar.gz

---
+++ packages/solarlog-cli/solarlog_cli-0.7.0.tar.gz/solarlog_cli-0.7.0/src/solarlog_cli/solarlog_client.py
@@ -0,0 +1,296 @@
+"""Client to access Solar-Log."""
+
+from __future__ import annotations
+
+import asyncio
+from datetime import datetime
+import json
+import logging
+from typing import Any
+
+from aiohttp import ClientResponse, ClientSession, ClientTimeout
+import bcrypt
+
+from .solarlog_exceptions import (
+    SolarLogAuthenticationError,
+    SolarLogConnectionError,
+    SolarLogUpdateError,
+)
+
+from .solarlog_models import EnergyData, SolarlogData
+
+SOLARLOG_REQUEST_PAYLOAD = '{ "801": { "170": null } }'
+_LOGGER = logging.getLogger(__name__)
+
+
+class Client:
+    """Client class to access Solar-Log."""
+
+    # pylint: disable=too-many-positional-arguments
+
+    def __init__(self, host: str, session: ClientSession | None, password: str = "") -> None:
+        self.host: str = host
+        self.password: str = password
+
+        self.request_timeout = 30
+
+        if not session:
+            self.session = ClientSession()
+        else:
+            self.session = session
+
+        self._hashed_pwd: bool = False
+        self._close_session: bool = True
+
+    async def test_connection(self) -> bool:
+        """Test the connection to Solar-Log."""
+        if self.session is None:
+            self.session = ClientSession()
+            self._close_session = True
+
+        url = f"{self.host}/getjp"
+
+        response = await self.session.post(
+            url, json=SOLARLOG_REQUEST_PAYLOAD,
+            timeout=ClientTimeout(total=self.request_timeout)
+        )
+
+        if response.status == 200:
+            return True
+
+        return False
+
+    async def login(self) -> bool:
+        """Test the connection to Solar-Log."""
+
+        if self.password == "":
+            return False
+
+        payload: str = f"u=user&p={self.password}"
+
+        response = await self.execute_http_request(payload, "login")
+
+        text = await response.text()
+        _LOGGER.debug("Response: %s", text)
+        if text.count("FAILED - User was wrong"):
+            # Response means, that no password is required
+            self.password = ""
+            return False
+
+        if text.count("FAILED - Password was wrong"):
+            # For newer firmware, login with encrypted PWD is required.
+            # Therefore test with encrypted PWD and only raise authentication error,
+            # if login with encrypted PWD fails as well.
+
+            payload = '{ "550": None }'
+
+            response = await self.execute_http_request(payload)
+
+            text = await response.text()
+            _LOGGER.debug("Response to request for user salts: %s", text)
+            r_dict: dict[str, Any] = json.loads(text)
+
+            salt: str = r_dict.get('550', {}).get('104')
+            _LOGGER.debug("Salt to hash pwd: %s", salt)
+
+            try:
+                if salt != 'QUERY IMPOSSIBLE 000' and salt is not None:
+                    hashed_password = bcrypt.hashpw(
+                        self.password.encode(), salt.encode())
+                    payload = f"u=user&p={hashed_password.decode('utf-8')}"
+                    response = await self.execute_http_request(payload, "login")
+                    text = await response.text()
+                    _LOGGER.debug(
+                        "Response of login with hashed pwd: %s", text)
+                    if text.count("FAILED - Password was wrong"):
+                        _LOGGER.debug("Wrong password (hashed)")
+                        raise SolarLogAuthenticationError
+                    self.password = hashed_password.decode('utf-8')
+            except Exception as exception:
+                raise SolarLogAuthenticationError from exception
+
+            self._hashed_pwd = True
+
+        self.session.cookie_jar.update_cookies(
+            {"SolarLog": response.cookies["SolarLog"].value})
+
+        _LOGGER.debug("response: %s", text)
+        _LOGGER.debug("cookies: %s", response.cookies)
+        _LOGGER.debug("Login successful, token: %s",
+                      response.cookies["SolarLog"].value)
+
+        return True
+
+    async def execute_http_request(self, body: str, path: str = "getjp", timeout: float | None = None) -> ClientResponse: # pylint: disable=line-too-long
+        """Helper function to process the HTTP Get call."""
+        if self.session is None:
+            self.session = ClientSession()
+            self._close_session = True
+
+        url = f"{self.host}/{path}"
+
+        header = {"Content-Type": "text/html", "X-SL-CSRF-PROTECTION": "1"}
+
+        _LOGGER.debug("HTTP-request header: %s", header)
+        _LOGGER.debug("HTTP-request body: %s", body)
+
+        try:
+            response = await self.session.post(
+                url=url,
+                headers=header,
+                data=body,
+                timeout=ClientTimeout(total=timeout or self.request_timeout),
+            )
+        except asyncio.TimeoutError as exception:
+            msg = f"Timeout occurred while connecting to Solar-Log at {self.host}"
+            raise SolarLogConnectionError(msg) from exception
+
+        content_type = response.headers.get("Content-Type", "")
+
+        if response.status != 200:
+            # pylint: disable-next=line-too-long
+            msg = f"The server responded with error code {response.status} while fetching data from Solar-Log at {self.host}.\n{url}\n{header}\n{body}"
+            text = await response.text()
+            raise SolarLogUpdateError(
+                msg,
+                {"Content-Type": content_type, "response": text},
+            )
+
+        _LOGGER.debug("HTTP-request successful: %s", response)
+        return response
+
+    async def parse_http_response(self, response: ClientResponse) -> dict[str, Any]:
+        """Helper function to parse the HTTP response."""
+
+        text = await response.text(errors="replace")
+        _LOGGER.debug("Parsing http response: %s", text)
+
+        if text.count('{"QUERY IMPOSSIBLE 000"}'):
+            raise SolarLogUpdateError(f"Server response: {text}")
+
+        if text.count("ACCESS DENIED") and not text.startswith('{"550":{'):
+            raise SolarLogAuthenticationError(f"Server response: {text}")
+
+        try:
+            json_response = json.loads(text)
+        except ValueError as err:
+            msg = f"Value error while decoding response: {err}."
+            raise SolarLogUpdateError(
+                msg,
+                {"Server response": text},
+            ) from err
+
+        return json_response
+
+    async def get_basic_data(self) -> SolarlogData:
+        """Get basic data from Solar-Log."""
+
+        raw_data: dict[str, Any] = await self.parse_http_response(
+            await self.execute_http_request(SOLARLOG_REQUEST_PAYLOAD)
+        )
+        raw_data = raw_data["801"]["170"]
+
+        data = SolarlogData(
+            last_updated=datetime.strptime(
+                raw_data["100"], "%d.%m.%y %H:%M:%S"),
+            power_ac=raw_data["101"],
+            power_dc=raw_data["102"],
+            voltage_ac=raw_data["103"],
+            voltage_dc=raw_data["104"],
+            yield_day=raw_data["105"],
+            yield_yesterday=raw_data["106"],
+            yield_month=raw_data["107"],
+            yield_year=raw_data["108"],
+            yield_total=raw_data["109"],
+            consumption_ac=raw_data["110"],
+            consumption_day=raw_data["111"],
+            consumption_yesterday=raw_data["112"],
+            consumption_month=raw_data["113"],
+            consumption_year=raw_data["114"],
+            consumption_total=raw_data["115"],
+            total_power=raw_data["116"],
+        )
+
+        return data
+
+    async def get_battery_data(self, timeout: float | None = None) -> list[float]:
+        """Get battery data from Solar-Log"""
+
+        raw_data: dict = await self.parse_http_response(
+            await self.execute_http_request('{ "858": null }', timeout=timeout)
+        )
+
+        data: list[float] = raw_data["858"]
+
+        return data
+
+    async def get_power_per_inverter(self, timeout: float | None = None) -> dict[int, float]:
+        """Get power data from Solar-Log"""
+
+        raw_data: dict = await self.parse_http_response(
+            await self.execute_http_request('{ "782": null }', timeout=timeout)
+        )
+
+        data = {int(key): val for key,
+                val in raw_data["782"].items() if val != "0"}
+
+        return data
+
+    async def get_energy_per_inverter(self, timeout: float | None = None) -> dict[int, float]:
+        """Get power data from Solar-Log"""
+
+        raw_data: dict = await self.parse_http_response(
+            await self.execute_http_request('{ "854": null }', timeout=timeout)
+        )
+        data_list = raw_data["854"][-1][-1]
+
+        data: dict[int, float] = {}
+
+        for item in data_list:
+            if item != 0:
+                data |= {int(data_list.index(item)): item}
+
+        return data
+
+    async def get_energy(self, timeout: float | None = None) -> EnergyData | None:
+        """Get energy data from Solar-Log"""
+
+        raw_data: dict = await self.parse_http_response(
+            await self.execute_http_request('{ "878": null }', timeout=timeout)
+        )
+
+        if raw_data["878"] != "QUERY IMPOSSIBLE 000":
+            return EnergyData(
+                production=raw_data["878"][-1][1],
+                self_consumption=raw_data["878"][-1][3]
+            )
+
+        return None
+
+    async def get_device_list(self, timeout: float | None = None) -> dict[int, str]:
+        """Get list of all connected devices."""
+
+        # get list of all inverters connected to Solar-Log
+        raw_data: dict = await self.parse_http_response(
+            await self.execute_http_request('{ "740": null }', timeout=timeout)
+        )
+        raw_data = raw_data["740"]
+
+        device_list: dict[int, str] = {}
+
+        for key, value in raw_data.items():
+            if value != "Err":
+                # get name of the inverter
+                raw_data = await self.parse_http_response(
+                    await self.execute_http_request(
+                        f"""{{ "141": {{ "{key}": {{ "119": null }} }} }}""", timeout=timeout
+                    )
+                )
+                device_list |= {int(key): raw_data["141"][key]["119"]}
+
+        return device_list
+
+    async def close(self) -> None:
+        """Close open client session."""
+        if self.session and self._close_session:
+            await self.session.close()
---
+++ packages/solarlog-cli/solarlog_cli-0.7.0.tar.gz/solarlog_cli-0.7.0/tests/test_solarlog_cli.py
@@ -0,0 +1,476 @@
+"""Tests for solarlog_cli."""
+
+from aioresponses import aioresponses
+from aiohttp import ClientSession
+
+import pytest
+from syrupy.assertion import SnapshotAssertion
+
+from solarlog_cli.solarlog_connector import SolarLogConnector
+from solarlog_cli.solarlog_exceptions import (
+    SolarLogAuthenticationError,
+    SolarLogConnectionError,
+    SolarLogError,
+    SolarLogUpdateError,
+)
+
+from . import load_fixture
+
+
+@pytest.mark.parametrize(
+    ("response_status", "return_value"),
+    [
+        (200, True),
+        (400, False),
+    ],
+)
+async def test_connection(
+    responses: aioresponses,
+    response_status: str,
+    return_value: bool,
+) -> None:
+    """Test connection."""
+    responses.post(
+        "localhost/getjp",
+        status=response_status,
+    )
+
+    solarlog_connector = SolarLogConnector("localhost")
+
+    await solarlog_connector.client.close()
+    solarlog_connector.client.session = None  # type: ignore [assignment]
+    assert await solarlog_connector.test_connection() == return_value
+
+    assert solarlog_connector.host == "localhost"
+
+    assert solarlog_connector.client.session is not None
+    assert not solarlog_connector.client.session.closed
+    await solarlog_connector.client.close()
+    assert solarlog_connector.client.session.closed
+
+
+async def test_existing_session(
+    responses: aioresponses,
+) -> None:
+    """Test connection."""
+    responses.post(
+        "localhost/getjp",
+        status=200,
+    )
+
+    solarlog_connector = SolarLogConnector(
+        "localhost", session=ClientSession())
+
+    assert await solarlog_connector.test_connection()
+
+    assert solarlog_connector.host == "localhost"
+
+    assert solarlog_connector.client.session is not None
+    assert not solarlog_connector.client.session.closed
+    await solarlog_connector.client.close()
+    assert solarlog_connector.client.session.closed
+
+
+async def test_extended_data_available(
+    responses: aioresponses,
+) -> None:
+    """Test extended data available."""
+
+    solarlog_connector = SolarLogConnector(
+        "http://solarlog.com", password="pwd")
+
+    responses.post(
+        "http://solarlog.com/getjp",
+        timeout=True,
+    )
+    assert not await solarlog_connector.test_extended_data_available()
+
+    responses.post(
+        "http://solarlog.com/getjp",
+        status=400,
+    )
+    assert not await solarlog_connector.test_extended_data_available()
+
+    responses.post(
+        "http://solarlog.com/getjp",
+        body=load_fixture("device_list_access_denied.json"),
+    )
+    responses.post(
+        "http://solarlog.com/login",
+        body="FAILED - User was wrong",
+    )
+    assert not await solarlog_connector.test_extended_data_available()
+    assert solarlog_connector.password == ""
+
+    responses.post(
+        "http://solarlog.com/getjp",
+        body=load_fixture("device_list_access_denied.json"),
+    )
+    responses.post(
+        "http://solarlog.com/login",
+        body="FAILED - Password was wrong",
+    )
+    responses.post(
+        "http://solarlog.com/getjp",
+        body=load_fixture("user_salts.json"),
+    )
+    responses.post(
+        "http://solarlog.com/login",
+        exception=SolarLogAuthenticationError('test'),
+    )
+    solarlog_connector.client.password = "pwd"
+    # type: ignore [call-overload]
+    with pytest.raises(SolarLogAuthenticationError):
+        await solarlog_connector.test_extended_data_available()
+
+    responses.post(
+        "http://solarlog.com/getjp",
+        body=load_fixture("device_list.json"),
+    )
+    assert await solarlog_connector.test_extended_data_available()
+
+    await solarlog_connector.client.close()
+    assert solarlog_connector.client.session.closed
+
+
+async def test_login_and_data_retreival(responses: aioresponses) -> None:
+    """Test login into Solar-Log."""
+    responses.post(
+        "http://solarlog.com/login",
+        headers={"Set-Cookie": "SolarLog=token"},
+        body="SUCCESS - Password was correct, you are now logged in",
+    )
+    solarlog_connector = SolarLogConnector(
+        "http://solarlog.com", password="pwd")
+
+    assert await solarlog_connector.login()
+
+    responses.post(
+        "http://solarlog.com/getjp",
+        body=load_fixture("basic_data.json"),
+    )
+    await solarlog_connector.update_data()
+
+    await solarlog_connector.client.close()
+    assert solarlog_connector.client.session.closed
+
+
+async def test_login_hashed_pwd(responses: aioresponses) -> None:
+    """Test login into Solar-Log."""
+
+    responses.post(
+        "http://solarlog.com/login",
+        body="FAILED - Password was wrong",
+    )
+    responses.post(
+        "http://solarlog.com/getjp",
+        body=load_fixture("user_salts.json"),
+    )
+    responses.post(
+        "http://solarlog.com/login",
+        headers={"Set-Cookie": "SolarLog=token"},
+        body="SUCCESS - Password was correct, you are now logged in",
+    )
+    solarlog_connector = SolarLogConnector(
+        "http://solarlog.com", password="pwd")
+
+    assert await solarlog_connector.login()
+
+    await solarlog_connector.client.close()
+    assert solarlog_connector.client.session.closed
+
+
+async def test_login_exceptions(responses: aioresponses) -> None:
+    """Test exceptions at login into Solar-Log."""
+    solarlog_connector = SolarLogConnector(
+        "http://solarlog.com", password="pwd")
+
+    responses.post(
+        "http://solarlog.com/login",
+        body="FAILED - Password was wrong",
+    )
+    responses.post(
+        "http://solarlog.com/getjp",
+        body=load_fixture("user_salts.json"),
+    )
+    responses.post(
+        "http://solarlog.com/login",
+        body="FAILED - Password was wrong",
+    )
+    # type: ignore [call-overload]
+    with pytest.raises(SolarLogAuthenticationError):
+        await solarlog_connector.client.login()
+
+    responses.post(
+        "http://solarlog.com/login",
+        body="FAILED - User was wrong",
+    )
+    assert not await solarlog_connector.client.login()
+
+    responses.post(
+        "http://solarlog.com/getjp",
+        body=load_fixture("device_list.json"),
+    )
+    assert not await solarlog_connector.client.login()
+
+    await solarlog_connector.client.close()
+    assert solarlog_connector.client.session.closed
+
+
+async def test_update_data(
+    responses: aioresponses,
+    snapshot: SnapshotAssertion
+) -> None:
+    """Test update data."""
+    responses.post(
+        "http://solarlog.com/getjp",
+        body=load_fixture("basic_data.json"),
+    )
+    responses.post(
+        "http://solarlog.com/getjp",
+        body=load_fixture("extended_data.json"),
+    )
+    responses.post(
+        "http://solarlog.com/getjp",
+        body=load_fixture("power_per_inverter.json"),
+    )
+    responses.post(
+        "http://solarlog.com/getjp",
+        body=load_fixture("energy_per_inverter.json"),
+    )
+    responses.post(
+        "http://solarlog.com/getjp",
+        body=load_fixture("battery_data.json"),
+    )
+
+    solarlog_connector = SolarLogConnector(
+        "http://solarlog.com",
+        True,
+        "UTC",
+        {0: True, 1: True, 2: False, 3: True},
+    )
+
+    data = await solarlog_connector.update_data()
+
+    assert data == snapshot
+
+    await solarlog_connector.client.close()
+    assert solarlog_connector.client.session.closed
+
+
+async def test_update_data_without_battery(
+    responses: aioresponses,
+) -> None:
+    """Test update data."""
+    responses.post(
+        "http://solarlog.com/getjp",
+        body=load_fixture("basic_data.json"),
+    )
+    responses.post(
+        "http://solarlog.com/getjp",
+        body=load_fixture("extended_data.json"),
+    )
+    responses.post(
+        "http://solarlog.com/getjp",
+        body=load_fixture("power_per_inverter.json"),
+    )
+    responses.post(
+        "http://solarlog.com/getjp",
+        body=load_fixture("energy_per_inverter.json"),
+    )
+    responses.post(
+        "http://solarlog.com/getjp",
+        body=load_fixture("battery_data_without_battery.json"),
+    )
+
+    solarlog_connector = SolarLogConnector(
+        "http://solarlog.com",
+        True,
+        "UTC",
+        {0: True, 1: True, 2: False, 3: True},
+    )
+
+    data = await solarlog_connector.update_data()
+
+    assert data.battery_data is None
+
+    await solarlog_connector.client.close()
+    assert solarlog_connector.client.session.closed
+
+async def test_update_energy_data(
+    responses: aioresponses,
+    snapshot: SnapshotAssertion
+) -> None:
+    """Test update energy data."""
+
+    responses.post(
+        "http://solarlog.com/getjp",
+        body=load_fixture("extended_data.json"),
+    )
+    responses.post(
+        "http://solarlog.com/getjp",
+        body='{"878": "QUERY IMPOSSIBLE 000"}',
+    )
+
+    solarlog_connector = SolarLogConnector(
+        "http://solarlog.com",
+        True,
+        "UTC",
+        {0: True, 1: True, 2: False, 3: True},
+    )
+
+    data = await solarlog_connector.update_energy_data()
+
+    assert data == snapshot
+
+    data = await solarlog_connector.update_energy_data()
+
+    assert data is None
+
+    await solarlog_connector.client.close()
+    assert solarlog_connector.client.session.closed
+
+@pytest.mark.parametrize(
+    ("status", "request_timeout", "error"),
+    [
+        (200, True, SolarLogConnectionError),
+        (400, False, SolarLogUpdateError),
+    ],
+)
+async def test_update_data_exceptions(
+    responses: aioresponses,
+    status: int,
+    request_timeout: bool,
+    error: SolarLogError,
+) -> None:
+    """Test update data with exceptions."""
+    responses.post(
+        "http://solarlog.com/getjp",
+        timeout=request_timeout,
+        status=status,
+    )
+
+    solarlog_connector = SolarLogConnector(
+        "http://solarlog.com",
+        True,
+        "UTC",
+        {0: True, 1: False, 2: False, 3: True},
+    )
+    await solarlog_connector.client.close()
+    solarlog_connector.client.session = None  # type: ignore [assignment]
+
+    with pytest.raises(error):  # type: ignore [call-overload]
+        await solarlog_connector.update_data()
+
+    await solarlog_connector.client.close()
+    assert solarlog_connector.client.session.closed
+
+
+async def test_update_data_with_data_exceptions(
+    responses: aioresponses,
+) -> None:
+    """Test update data with exceptions due to data."""
+    responses.post(
+        "http://solarlog.com/getjp",
+        body="",
+    )
+    responses.post(
+        "http://solarlog.com/getjp",
+        body='{"QUERY IMPOSSIBLE 000"}',
+    )
+    responses.post(
+        "http://solarlog.com/getjp",
+        body=load_fixture("basic_data_during_update.json"),
+    )
+    responses.post(
+        "http://solarlog.com/getjp",
+        body=load_fixture("basic_data_no_power.json"),
+    )
+
+    solarlog_connector = SolarLogConnector("http://solarlog.com")
+
+    with pytest.raises(SolarLogUpdateError):
+        await solarlog_connector.update_data()
+
+    with pytest.raises(SolarLogUpdateError):
+        await solarlog_connector.update_data()
+
+    with pytest.raises(SolarLogUpdateError):
+        await solarlog_connector.update_data()
+
+    data = await solarlog_connector.update_data()
+
+    assert data.usage == 0
+    assert data.power_available == 0
+
+    await solarlog_connector.client.close()
+    assert solarlog_connector.client.session.closed
+
+
+async def test_update_device_list(
+    responses: aioresponses,
+    snapshot: SnapshotAssertion
+) -> None:
+    """Test update device list."""
+
+    responses.post(
+        "http://solarlog.com/getjp",
+        body=load_fixture("device_list.json"),
+    )
+    responses.post(
+        "http://solarlog.com/getjp",
+        body=load_fixture("device_data_1.json"),
+    )
+    responses.post(
+        "http://solarlog.com/getjp",
+        body=load_fixture("device_data_2.json"),
+    )
+    responses.post(
+        "http://solarlog.com/getjp",
+        body=load_fixture("device_data_3.json"),
+    )
+    responses.post(
+        "http://solarlog.com/getjp",
+        body=load_fixture("device_data_4.json"),
+    )
+
+    solarlog_connector = SolarLogConnector(
+        "http://solarlog.com",
+        True,
+        "UTC",
+        {0: True, 1: False, 2: False, 3: True},
+    )
+
+    await solarlog_connector.update_device_list()
+    data = solarlog_connector.device_list
+
+    assert data == snapshot
+
+    assert solarlog_connector.device_name(0) == "Device 1"
+    assert solarlog_connector.device_name(4) == ""
+
+    await solarlog_connector.client.close()
+    assert solarlog_connector.client.session.closed
+
+
+async def test_enabled_devices(responses: aioresponses) -> None:
+    """Test enabled devices."""
+    responses.post(
+        "http://solarlog.com/getjp",
+        body=load_fixture("device_list.json"),
+    )
+
+    solarlog_connector = SolarLogConnector(
+        "http://solarlog.com", device_enabled={})
+    assert solarlog_connector.device_enabled() == {}
+    assert await solarlog_connector.update_device_list() == {}
+
+    solarlog_connector.set_enabled_devices(
+        {0: True, 1: False, 2: False, 3: True})
+
+    assert solarlog_connector.device_enabled(0)
+    assert not solarlog_connector.device_enabled(1)
+    assert solarlog_connector.device_enabled(3)
+
+    await solarlog_connector.client.close()
+    assert solarlog_connector.client.session.closed
