97f67e8e3790c560483bd2751584c411b5fdf178
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 16:06:55 +0000

    Add package ncrystal-core/4.2.12/ncrystal_core-4.2.12-py3-none-manylinux2014_aarch64.manylinux_2_17_aarch64.whl

---
+++ packages/ncrystal-core/ncrystal_core-4.2.12-py3-none-manylinux2014_aarch64.manylinux_2_17_aarch64.whl/_ncrystal_core/data/lib/cmake/NCrystal/NCrystalTargets-release.cmake
@@ -0,0 +1,19 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "NCrystal::NCrystal" for configuration "Release"
+set_property(TARGET NCrystal::NCrystal APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(NCrystal::NCrystal PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/_ncrystal_core/data/lib/libNCrystal.so"
+  IMPORTED_SONAME_RELEASE "libNCrystal.so"
+  )
+
+list(APPEND _cmake_import_check_targets NCrystal::NCrystal )
+list(APPEND _cmake_import_check_files_for_NCrystal::NCrystal "${_IMPORT_PREFIX}/_ncrystal_core/data/lib/libNCrystal.so" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
