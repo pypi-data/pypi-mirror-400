2a2114b365a50149b97935d2a4d2f8d577cb6cc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 06:04:09 +0000

    Add package pyjolt/0.111.2/pyjolt-0.111.2.tar.gz

---
+++ packages/pyjolt/pyjolt-0.111.2.tar.gz/pyjolt-0.111.2/src/pyjolt/admin/static/index.js
@@ -0,0 +1,16 @@
+/**
+ * Custom JS for admin dashboard
+ */
+import TagsInput from "./custom_js/tags_input.js";
+import RecipientsInput from "./custom_js/email_recipients_input.js";
+import FilesInput from "./custom_js/file_upload_input.js"
+import FileElement from "./custom_js/file_explorer_file.js";
+import FileExplorer from "./custom_js/file_explorer.js";
+import FilePicker from "./custom_js/file_picker.js";
+
+customElements.define("tags-input", TagsInput);
+customElements.define("recipients-input", RecipientsInput);
+customElements.define("files-input", FilesInput);
+customElements.define("file-element", FileElement);
+customElements.define("file-explorer", FileExplorer);
+customElements.define("file-picker", FilePicker);
