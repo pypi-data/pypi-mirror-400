33b5e83c7466cbe5c392b1184f757062221bffe4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 12:08:19 +0000

    Add package eccodeslib/2.45.1.dev9/eccodeslib-2.45.1.dev9-cp312-cp312-manylinux_2_28_aarch64.whl

---
+++ packages/eccodeslib/eccodeslib-2.45.1.dev9-cp312-cp312-manylinux_2_28_aarch64.whl/eccodeslib/include/eccodes_ecbuild_config.h
@@ -0,0 +1,72 @@
+/*
+ * (C) Copyright 2011- ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation nor
+ * does it submit to any jurisdiction.
+ */
+
+#ifndef ECCODES_ecbuild_config_h
+#define ECCODES_ecbuild_config_h
+
+/* ecbuild info */
+
+#ifndef ECBUILD_VERSION_STR
+#define ECBUILD_VERSION_STR "3.13.0"
+#endif
+#ifndef ECBUILD_VERSION
+#define ECBUILD_VERSION "3.13.0"
+#endif
+#ifndef ECBUILD_MACROS_DIR
+#define ECBUILD_MACROS_DIR  "/src/ecbuild/cmake"
+#endif
+
+/* config info */
+
+#define ECCODES_OS_NAME          "Linux-6.14.0-1014-azure"
+#define ECCODES_OS_BITS          64
+#define ECCODES_OS_BITS_STR      "64"
+#define ECCODES_OS_STR           "linux.64"
+#define ECCODES_OS_VERSION       "6.14.0-1014-azure"
+#define ECCODES_SYS_PROCESSOR    "aarch64"
+
+#define ECCODES_BUILD_TIMESTAMP  "20260105150940"
+#define ECCODES_BUILD_TYPE       "RelWithDebInfo"
+
+#define ECCODES_C_COMPILER_ID      "GNU"
+#define ECCODES_C_COMPILER_VERSION "14.2.1"
+
+#define ECCODES_CXX_COMPILER_ID      "GNU"
+#define ECCODES_CXX_COMPILER_VERSION "14.2.1"
+
+#define ECCODES_C_COMPILER       "/opt/rh/gcc-toolset-14/root/usr/bin/cc"
+#define ECCODES_C_FLAGS          " -pipe -O2 -g -DNDEBUG"
+
+#define ECCODES_CXX_COMPILER     "/opt/rh/gcc-toolset-14/root/usr/bin/c++"
+#define ECCODES_CXX_FLAGS        " -pipe -O2 -g -DNDEBUG"
+
+/* Needed for finding per package config files */
+
+#define ECCODES_INSTALL_DIR       "/tmp/eccodes/target/eccodes"
+#define ECCODES_INSTALL_BIN_DIR   "/tmp/eccodes/target/eccodes/bin"
+#define ECCODES_INSTALL_LIB_DIR   "/tmp/eccodes/target/eccodes/lib64"
+#define ECCODES_INSTALL_DATA_DIR  "/tmp/eccodes/target/eccodes/share/eccodes"
+
+#define ECCODES_DEVELOPER_SRC_DIR "/src/eccodes"
+#define ECCODES_DEVELOPER_BIN_DIR "/tmp/eccodes/build"
+
+/* Fortran support */
+
+#if 1
+
+#define ECCODES_Fortran_COMPILER_ID      "GNU"
+#define ECCODES_Fortran_COMPILER_VERSION "14.2.1"
+
+#define ECCODES_Fortran_COMPILER "/opt/rh/gcc-toolset-14/root/usr/bin/gfortran"
+#define ECCODES_Fortran_FLAGS    " -O2 -g -DNDEBUG"
+
+#endif
+
+#endif /* ECCODES_ecbuild_config_h */
