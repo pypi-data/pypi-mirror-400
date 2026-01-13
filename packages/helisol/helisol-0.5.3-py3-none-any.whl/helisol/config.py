49d8e411670874edb23d61f274e333a316bf53bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 09:08:24 +0000

    Add package helisol/0.5.3/helisol-0.5.3-py3-none-any.whl

---
+++ packages/helisol/helisol-0.5.3-py3-none-any.whl/helisol/config.py
@@ -0,0 +1,23 @@
+"""Config (options) for helisol package"""
+
+# ----------------------------- License information --------------------------
+
+# This file is part of the helisol python package.
+# Copyright (C) 2023 Olivier Vincent
+
+# The helisol package is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# The helisol package is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with the helisol python package.
+# If not, see <https://www.gnu.org/licenses/>
+
+CONFIG = {}
+CONFIG["default location"] = "Home"
