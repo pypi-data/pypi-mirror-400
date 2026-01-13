eb0e2f64c070537585c57ec833ed30580dbd5704
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 20:05:28 +0000

    Add package drizzle/2.2.0/drizzle-2.2.0-cp314-cp314t-macosx_10_15_x86_64.whl

---
+++ packages/drizzle/drizzle-2.2.0-cp314-cp314t-macosx_10_15_x86_64.whl/drizzle/__init__.py
@@ -0,0 +1,13 @@
+"""
+A package for combining dithered images into a single image
+"""
+
+from importlib.metadata import PackageNotFoundError, version
+
+from drizzle import cdrizzle, resample, utils  # noqa: F401
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:
+    # package is not installed
+    __version__ = ""
