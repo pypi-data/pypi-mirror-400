c1a9bfb98ba6038a4f4ec0b53eab35500768282b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 19:07:22 +0000

    Add package soxs/5.2.0/soxs-5.2.0-cp314-cp314-manylinux2014_aarch64.manylinux_2_17_aarch64.manylinux_2_28_aarch64.whl

---
+++ packages/soxs/soxs-5.2.0-cp314-cp314-manylinux2014_aarch64.manylinux_2_17_aarch64.manylinux_2_28_aarch64.whl/soxs/background/__init__.py
@@ -0,0 +1,8 @@
+from .diffuse import make_diffuse_background
+from .events import add_background_from_file
+from .point_sources import (
+    make_point_source_list,
+    make_point_sources_file,
+    make_ptsrc_background,
+)
+from .spectra import BackgroundSpectrum, ConvolvedBackgroundSpectrum
