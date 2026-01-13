d980177be44d0cbe0610530b9f76603aedd2b325
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 19:07:50 +0000

    Add package redaltus/0.0.1/redaltus-0.0.1.tar.gz

---
+++ packages/redaltus/redaltus-0.0.1.tar.gz/redaltus-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="redaltus",
+    version="0.0.1",
+    description="Official placeholder for the Red Altus SDK.",
+    author="Red Altus",
+    author_email="redaltus@gmail.com",
+    packages=[],  # Empty for now
+    install_requires=[],
+)
