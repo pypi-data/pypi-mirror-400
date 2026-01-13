167ca62208285fbcb3c4a15fef2a465f3a4077f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 21:06:18 +0000

    Add package huace-aigc-frame/0.1.0/huace_aigc_frame-0.1.0-py3-none-any.whl

---
+++ packages/huace-aigc-frame/huace_aigc_frame-0.1.0-py3-none-any.whl/huace_aigc_frame/logger.py
@@ -0,0 +1,14 @@
+"""
+日志模块
+"""
+import logging
+
+logger = logging.getLogger("huace_aigc_frame")
+logger.setLevel(logging.INFO)
+
+if not logger.handlers:
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
