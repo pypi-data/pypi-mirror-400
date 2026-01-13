878d9299619fc98914057bf99dfe5342d9dad822
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 22:06:12 +0000

    Add package LscEncryption/0.0.2/lscencryption-0.0.2-py3-none-any.whl

---
+++ packages/LscEncryption/lscencryption-0.0.2-py3-none-any.whl/LscEncryption/ShuffleEncryption.py
@@ -0,0 +1,13 @@
+import random as r
+
+
+def shuffle_encryption(s):
+    s = list(s)
+    r.shuffle(s)
+    return "".join(s)
+    
+
+se = SE = shuffle_encryption
+
+if __name__ == "__main__":
+    print(shuffle_encryption("sdlkfjl"))
