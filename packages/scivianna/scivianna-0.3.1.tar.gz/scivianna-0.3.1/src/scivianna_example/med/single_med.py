541a082638db1116e29daf3e788a82ab7857ec17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 21:05:45 +0000

    Add package scivianna/0.3.1/scivianna-0.3.1.tar.gz

---
+++ packages/scivianna/scivianna-0.3.1.tar.gz/scivianna-0.3.1/src/scivianna_example/med/single_med.py
@@ -0,0 +1,23 @@
+
+from scivianna.enums import UpdateEvent
+from scivianna.layout.split import SplitLayout
+from scivianna.panel.visualisation_panel import (
+    VisualizationPanel
+)
+from scivianna.notebook_tools import get_med_panel, _serve_panel
+
+def get_panel(*args, return_slaves = False, **kwargs) -> VisualizationPanel:
+
+    med_1 = get_med_panel(geo=None, title="MEDCoupling visualizer XY")
+
+    med_1.set_field("INTEGRATED_POWER")
+    med_1.update_event = UpdateEvent.CLIC
+
+    if return_slaves:
+        return SplitLayout(med_1), [med_1.get_slave()]
+    else:   
+        return SplitLayout(med_1)
+    
+if __name__ == "__main__":
+    # get_panel().show()
+    _serve_panel(get_panel_function=get_panel)
