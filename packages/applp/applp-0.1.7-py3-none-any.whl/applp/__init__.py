e2c14a4d3950d7f08e8e5b8169226b7ce64fea78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 03:32:12 +0000

    Add package applp/0.1.7/applp-0.1.7-py3-none-any.whl

---
+++ packages/applp/applp-0.1.7-py3-none-any.whl/applp/__init__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Date          : 2025-08-28
+# Author        : Lancelot PINCET
+# GitHub        : https://github.com/LancelotPincet
+# Library       : appLP
+
+"""
+A library for custom GUI interface creation.
+"""
+
+
+
+# %% Lazy imports
+from corelp import getmodule
+__getattr__, __all__ = getmodule(__file__)
+
+
+
+# %% Test function run
+if __name__ == "__main__":
+    from corelp import test
+    test(__file__)
