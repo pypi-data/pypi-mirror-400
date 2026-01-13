b4ded036e6439701a58e09985509b7dffd6430f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 18:10:40 +0000

    Add package bestdist/0.1.0/bestdist-0.1.0.tar.gz

---
+++ packages/bestdist/bestdist-0.1.0.tar.gz/bestdist-0.1.0/src/bestdist/distributions/continuous/__init__.py
@@ -0,0 +1,14 @@
+"""Continuous probability distributions."""
+
+from .normal import Normal
+from .gamma import Gamma
+from .beta import Beta
+from .weibull import Weibull
+
+__all__ = [
+    "Normal",
+    "Gamma",
+    "Beta",
+    "Weibull",
+]
+
