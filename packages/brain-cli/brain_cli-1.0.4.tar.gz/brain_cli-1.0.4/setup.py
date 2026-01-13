b1042bb940be0434f2b17fcbea4140cca4a92118
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 03:32:48 +0000

    Add package brain-cli/1.0.4/brain_cli-1.0.4.tar.gz

---
+++ packages/brain-cli/brain_cli-1.0.4.tar.gz/brain_cli-1.0.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name="brain-cli", 
+    version="1.0.4",            # Subi a versão
+    py_modules=["brain"],       
+    install_requires=[
+        "typer>=0.9.0",
+        "rich>=13.0.0",
+        "groq>=0.4.0",
+        "pyperclip>=1.8.0",
+        "typing-extensions>=4.0.0"
+    ],
+    entry_points={
+        "console_scripts": [
+            "brain=brain:app",
+        ],
+    },
+)
