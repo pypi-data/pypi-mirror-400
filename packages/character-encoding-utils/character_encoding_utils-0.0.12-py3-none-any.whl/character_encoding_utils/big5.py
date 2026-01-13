14f1dd6ae3a68c3624d294b6ed2f03e7d3b12792
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 03:32:17 +0000

    Add package character-encoding-utils/0.0.12/character_encoding_utils-0.0.12-py3-none-any.whl

---
+++ packages/character-encoding-utils/character_encoding_utils-0.0.12-py3-none-any.whl/character_encoding_utils/big5.py
@@ -0,0 +1,198 @@
+
+class Big5Exception(Exception):
+    pass
+
+
+class Big5EncodeError(Big5Exception):
+    obj: str
+    position: int
+    reason: str
+
+    def __init__(self, obj: str, position: int, reason: str):
+        self.obj = obj
+        self.position = position
+        self.reason = reason
+
+    def __str__(self) -> str:
+        return f"'big5' codec can't encode character '\\u{ord(self.obj):x}' in position {self.position}: {self.reason}"
+
+
+class Big5DecodeError(Big5Exception):
+    obj: bytes
+    position: int
+    reason: str
+
+    def __init__(self, obj: bytes, position: int, reason: str):
+        self.obj = obj
+        self.position = position
+        self.reason = reason
+
+    def __str__(self) -> str:
+        if len(self.obj) <= 1:
+            return f"'big5' codec can't decode byte 0x{self.obj[0]:x} in position {self.position}: {self.reason}"
+        else:
+            return f"'big5' codec can't decode bytes in position {self.position}-{self.position + len(self.obj) - 1}: {self.reason}"
+
+
+def encode(cs: str) -> bytes:
+    bs = bytearray()
+    for position, c in enumerate(cs):
+        match c:
+            case '〸':  # 0x3038
+                bs.extend(b'\xa2\xcc')
+            case '〹':  # 0x3039
+                bs.extend(b'\xa2\xcd')
+            case '〺':  # 0x303A
+                bs.extend(b'\xa2\xce')
+            case '十':  # 0x5341
+                bs.extend(b'\xa4\x51')
+            case '卄':  # 0x5344
+                # Big5 不包含汉字的 '卄'
+                raise Big5EncodeError(c, position, 'illegal multibyte sequence')
+            case '卅':  # 0x5345
+                bs.extend(b'\xa4\xca')
+            case '／':  # 0xFF0F
+                bs.extend(b'\xa1\xfe')
+            case '＼':  # 0xFF3C
+                bs.extend(b'\xa2\x40')
+            case '∕':  # 0x2215
+                bs.extend(b'\xa2\x41')
+            case '﹨':  # 0xFE68
+                bs.extend(b'\xa2\x42')
+            case _:
+                try:
+                    bs.extend(c.encode('big5'))
+                except UnicodeEncodeError as e:
+                    raise Big5EncodeError(c, position, e.reason) from e
+    return bytes(bs)
+
+
+def decode(bs: bytes | bytearray) -> str:
+    cs = []
+    cursor = 0
+    passed = 0
+    while True:
+        if cursor >= len(bs):
+            break
+        bc = bytearray([bs[cursor]])
+        cursor += 1
+
+        if bc[0] > 0x7F:
+            if cursor < len(bs):
+                bc.append(bs[cursor])
+                cursor += 1
+
+        match bc:
+            case b'\xa2\xcc':
+                cs.append('〸')  # 0x3038
+            case b'\xa2\xcd':
+                cs.append('〹')  # 0x3039
+            case b'\xa2\xce':
+                cs.append('〺')  # 0x303A
+            case b'\xa4\x51':
+                cs.append('十')  # 0x5341
+            case b'\xa4\xca':
+                cs.append('卅')  # 0x5345
+            case b'\xa1\xfe':
+                cs.append('／')  # 0xFF0F
+            case b'\xa2\x40':
+                cs.append('＼')  # 0xFF3C
+            case b'\xa2\x41':
+                cs.append('∕')  # 0x2215
+            case b'\xa2\x42':
+                cs.append('﹨')  # 0xFE68
+            case _:
+                try:
+                    cs.append(bc.decode('big5'))
+                except UnicodeDecodeError as e:
+                    raise Big5DecodeError(bc, passed, e.reason) from e
+        passed += len(bc)
+    return ''.join(cs)
+
+
+def query_code(c: str) -> int:
+    if len(c) != 1:
+        raise Big5Exception('Must be one character')
+    try:
+        bs = encode(c)
+    except Big5EncodeError as e:
+        raise Big5Exception(f"'\\u{ord(c):x}' is not a 'big5' character") from e
+    if len(bs) == 1:
+        raise Big5Exception(f"'\\u{ord(c):x}' is a ascii character")
+    code = int(f'{bs[0]:02x}{bs[1]:02x}', 16)
+    return code
+
+
+def query_chr(code: int) -> str:
+    try:
+        return decode(bytes.fromhex(f'{code:X}'))
+    except Big5DecodeError as e:
+        raise Big5Exception(f"'big5' code 0x{code:04X} is undefined") from e
+
+
+def get_categories() -> list[str]:
+    return ['other', 'level-1', 'level-2']
+
+
+def query_category(c: str) -> str | None:
+    try:
+        code = query_code(c)
+    except Big5Exception:
+        return None
+    if 0xA140 <= code <= 0xA3BF:
+        return 'other'
+    elif 0xA440 <= code <= 0xC67E:
+        return 'level-1'
+    elif 0xC940 <= code <= 0xF9D5:
+        return 'level-2'
+    else:
+        return None
+
+
+def _build_alphabet_by_codes_between(code_start: int, code_end: int) -> list[str]:
+    alphabet = []
+    for code in range(code_start, code_end + 1):
+        try:
+            c = query_chr(code)
+            alphabet.append(c)
+        except Big5Exception:
+            pass
+    return alphabet
+
+
+_alphabet_other = _build_alphabet_by_codes_between(0xA140, 0xA3BF)
+_alphabet_level_1 = _build_alphabet_by_codes_between(0xA440, 0xC67E)
+_alphabet_level_2 = _build_alphabet_by_codes_between(0xC940, 0xF9D5)
+_alphabet = _alphabet_other + _alphabet_level_1 + _alphabet_level_2
+
+
+def get_alphabet_other() -> list[str]:
+    return list(_alphabet_other)
+
+
+def get_alphabet_level_1() -> list[str]:
+    return list(_alphabet_level_1)
+
+
+def get_alphabet_level_2() -> list[str]:
+    return list(_alphabet_level_2)
+
+
+def get_alphabet() -> list[str]:
+    return list(_alphabet)
+
+
+def get_other_count() -> int:
+    return len(_alphabet_other)
+
+
+def get_level_1_count() -> int:
+    return len(_alphabet_level_1)
+
+
+def get_level_2_count() -> int:
+    return len(_alphabet_level_2)
+
+
+def get_count() -> int:
+    return len(_alphabet)
---
+++ packages/character-encoding-utils/character_encoding_utils-0.0.12-py3-none-any.whl/character_encoding_utils/ksx1001.py
@@ -0,0 +1,178 @@
+
+_EUC_OFFSET = 0xA0
+
+
+class KSX1001Exception(Exception):
+    pass
+
+
+class KSX1001EncodeError(KSX1001Exception):
+    obj: str
+    position: int
+    reason: str
+
+    def __init__(self, obj: str, position: int, reason: str):
+        self.obj = obj
+        self.position = position
+        self.reason = reason
+
+    def __str__(self) -> str:
+        return f"'ksx1001' codec can't encode character '\\u{ord(self.obj):x}' in position {self.position}: {self.reason}"
+
+
+class KSX1001DecodeError(KSX1001Exception):
+    obj: bytes
+    position: int
+    reason: str
+
+    def __init__(self, obj: bytes, position: int, reason: str):
+        self.obj = obj
+        self.position = position
+        self.reason = reason
+
+    def __str__(self) -> str:
+        if len(self.obj) <= 1:
+            return f"'ksx1001' codec can't decode byte 0x{self.obj[0]:x} in position {self.position}: {self.reason}"
+        else:
+            return f"'ksx1001' codec can't decode bytes in position {self.position}-{self.position + len(self.obj) - 1}: {self.reason}"
+
+
+def encode(cs: str) -> bytes:
+    bs = bytearray()
+    for position, c in enumerate(cs):
+        try:
+            bs.extend(c.encode('ksx1001'))
+        except UnicodeEncodeError as e:
+            raise KSX1001EncodeError(c, position, e.reason) from e
+    return bytes(bs)
+
+
+def decode(bs: bytes | bytearray) -> str:
+    cs = []
+    cursor = 0
+    passed = 0
+    while True:
+        if cursor >= len(bs):
+            break
+        bc = bytearray([bs[cursor]])
+        cursor += 1
+
+        if bc[0] > 0x7F:
+            if cursor < len(bs):
+                bc.append(bs[cursor])
+                cursor += 1
+
+        if bc == b'\xa4\xd4' and cursor + 5 < len(bs):
+            if bs[cursor] == 0xA4 and bs[cursor + 2] == 0xA4 and bs[cursor + 4] == 0xA4:
+                if 0xA1 <= bs[cursor + 1] <= 0xD4 and 0xA1 <= bs[cursor + 3] <= 0xD4 and 0xA1 <= bs[cursor + 5] <= 0xD4:
+                    bc.append(bs[cursor])
+                    bc.append(bs[cursor + 1])
+                    bc.append(bs[cursor + 2])
+                    bc.append(bs[cursor + 3])
+                    bc.append(bs[cursor + 4])
+                    bc.append(bs[cursor + 5])
+                    cursor += 6
+
+        if bc == b'\xa4\xd4':
+            cs.append(chr(0x3164))  # Hangul Filler
+        else:
+            try:
+                cs.append(bc.decode('ksx1001'))
+            except UnicodeDecodeError as e:
+                raise KSX1001DecodeError(bc, passed, e.reason) from e
+        passed += len(bc)
+    return ''.join(cs)
+
+
+def query_coord(c: str) -> tuple[int, int]:
+    if len(c) != 1:
+        raise KSX1001Exception('Must be one character')
+    try:
+        bs = encode(c)
+    except KSX1001EncodeError as e:
+        raise KSX1001Exception(f"'\\u{ord(c):x}' is not a 'ksx1001' character") from e
+    if len(bs) == 1:
+        raise KSX1001Exception(f"'\\u{ord(c):x}' is a ascii character")
+    elif bs.startswith(b'\xa4\xd4') and len(bs) > 2:
+        raise KSX1001Exception(f"'\\u{ord(c):x}' is not a 'ksx1001' character")
+    row = bs[0] - _EUC_OFFSET
+    col = bs[1] - _EUC_OFFSET
+    return row, col
+
+
+def query_chr(row: int, col: int):
+    if row < 1 or row > 94 or col < 1 or col > 94:
+        raise KSX1001Exception(f"'row' and 'col' must between 1 and 94")
+    try:
+        return decode(bytes([row + _EUC_OFFSET, col + _EUC_OFFSET]))
+    except KSX1001DecodeError as e:
+        raise KSX1001Exception(f"'ksx1001' coord at ({row}, {col}) is undefined'") from e
+
+
+def get_categories() -> list[str]:
+    return ['other', 'syllable', 'hanja']
+
+
+def query_category(c: str) -> str | None:
+    try:
+        row = query_coord(c)[0]
+    except KSX1001Exception:
+        return None
+    if 1 <= row <= 12:
+        return 'other'
+    elif 16 <= row <= 40:
+        return 'syllable'
+    elif 42 <= row <= 93:
+        return 'hanja'
+    else:
+        return None
+
+
+def _build_alphabet_by_rows_between(row_start: int, row_end: int) -> list[str]:
+    alphabet = []
+    for row in range(row_start, row_end + 1):
+        for col in range(1, 94 + 1):
+            try:
+                c = query_chr(row, col)
+                alphabet.append(c)
+            except KSX1001Exception:
+                pass
+    return alphabet
+
+
+_alphabet_other = _build_alphabet_by_rows_between(1, 12)
+_alphabet_syllable = _build_alphabet_by_rows_between(16, 40)
+_alphabet_hanja = _build_alphabet_by_rows_between(42, 93)
+_alphabet = _alphabet_other + _alphabet_syllable + _alphabet_hanja
+
+
+def get_alphabet_other() -> list[str]:
+    return list(_alphabet_other)
+
+
+def get_alphabet_syllable() -> list[str]:
+    return list(_alphabet_syllable)
+
+
+def get_alphabet_hanja() -> list[str]:
+    return list(_alphabet_hanja)
+
+
+def get_alphabet() -> list[str]:
+    return list(_alphabet)
+
+
+def get_other_count() -> int:
+    return len(_alphabet_other)
+
+
+def get_syllable_count() -> int:
+    return len(_alphabet_syllable)
+
+
+def get_hanja_count() -> int:
+    return len(_alphabet_hanja)
+
+
+def get_count() -> int:
+    return len(_alphabet)
