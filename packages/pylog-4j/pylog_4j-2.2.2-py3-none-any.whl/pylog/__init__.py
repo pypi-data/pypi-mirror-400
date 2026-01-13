e2e4d5c5c2785e8f839f4f28c2a273db13042f58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 21:05:45 +0000

    Add package pylog-4j/2.2.2/pylog_4j-2.2.2-py3-none-any.whl

---
+++ packages/pylog-4j/pylog_4j-2.2.2-py3-none-any.whl/pylog/__init__.py
@@ -0,0 +1,10 @@
+from .manager import LogManager
+from .core.context import ThreadContext
+from .core.marker import Marker
+from .core.logger import Logger
+
+def get_logger(name: str) -> Logger:
+    return LogManager.get_logger(name)
+
+__version__ = "2.2.2"
+__all__ = ["LogManager", "ThreadContext", "Marker", "get_logger", "Logger"]
