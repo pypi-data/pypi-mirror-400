7fa0e7f26e8529402e9b7686d6b6435dcc5c526e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 11:06:14 +0000

    Add package fairmat-readers-xrd/0.0.8/fairmat_readers_xrd-0.0.8.tar.gz

---
+++ packages/fairmat-readers-xrd/fairmat_readers_xrd-0.0.8.tar.gz/fairmat_readers_xrd-0.0.8/README.md
@@ -0,0 +1,100 @@
+# fairmat-readers-xrd
+A package developed by FAIRmat and collaborators that contains file readers for various
+X-ray diffraction (XRD) file formats.
+
+## Install and use
+To use the readers, install the package using `pip` and import the `read_file`
+function. It will check the file extension and, using an appropriate file
+reader, return a Python dictionary containing the data.
+
+```sh
+pip install fairmat-readers-xrd
+```
+```py
+from fairmat_readers_xrd import read_file
+
+file_path = "<filepath>.xrdml"
+data_dict =  read_file(file_path)
+```
+
+You can also import individual file readers.
+```py
+from fairmat_readers_xrd import read_panalytical_xrdml, read_bruker_raw
+
+# Read PANalytical XRDML file
+file_path = "<filepath>.xrdml"
+data_dict = read_panalytical_xrdml(file_path)
+
+# Read Bruker/Siemens RAW v4 file
+raw_file_path = "<filepath>.raw"
+raw_data_dict = read_bruker_raw(raw_file_path)
+```
+
+Currently, the following file extensions are supported:
+
+| File Extension    | Corresponding Reader Function     | Vendor/Format                            | Notes                                    |
+| ----------------- | --------------------------------- | ---------------------------------------- | ---------------------------------------- |
+| `.xrdml`          | `read_panalytical_xrdml`          | PANalytical                              |                                          |
+| `.rasx`           | `read_rigaku_rasx`                | Rigaku                                   |                                          |
+| `.brml`           | `read_bruker_brml`                | Bruker                                   |                                          |
+| `.raw`            | `read_bruker_raw`                 | Bruker/Siemens RAW v4                    | Single-axis scans only, extracts anode material and wavelengths |
+
+**Note on `.raw` files**: The parser supports **Bruker/Siemens RAW v4** format for single-axis theta-2theta powder diffraction scans. It extracts:
+- Scan parameters (angles, step size, scan axis)
+- X-ray tube anode material (from binary file offset 0x01A8)
+- Wavelengths (K-alpha1, K-alpha2, K-beta) via lookup table from International Tables for Crystallography
+
+Multi-axis scans (texture, pole figures) are not validated and may not parse correctly.
+
+## Development
+The package is still under development. To contribute, start with simply raising an
+[issue](https://github.com/FAIRmat-NFDI/readers-xrd/issues) on the repo.
+
+### Contributing code
+In case you want to contribute a piece of code, or a whole new reader, clone the repo in
+your local and start a virtual Python environment inside the directory.
+```sh
+git clone git@github.com:FAIRmat-NFDI/readers-xrd.git
+cd readers-xrd
+python3 -m venv .pyenv
+source .pyenv/bin/activate
+```
+
+Then install the package in editable mode (-e flag), with `dev` dependencies.
+You can also run the `pytest` to confirm all went well.
+As a recommended practice, you can also install the pre-commit hook for linting (more on
+this [here](#ruff-pre-commit-hook)).
+```sh
+python -m pip install --upgrade pip
+pip install -e .[dev]
+pytest
+pre-commit install
+```
+
+Now you can start tinkering around on a new branch (or a [fork](https://docs.github.com/en/pull-requests/collaborating-with-pull-requests/working-with-forks/fork-a-repo)).
+If you want to contribute the code back to the upstream, simply create a
+[Pull Request (PR)](https://docs.github.com/en/pull-requests/collaborating-with-pull-requests/proposing-changes-to-your-work-with-pull-requests/creating-a-pull-request),
+and we will get back to you. As a good practice, add tests for new readers and
+make sure existing tests (`pytest`) are passing before creating PR.
+
+### Ruff
+We have placed GitHub actions for checking the linting. This can lead to your PR failing
+the CI tests in case your code is not correctly linted or formatted.
+Best way to avoid this is to use [Ruff](https://docs.astral.sh/ruff/tutorial/) for
+automatic linting and formatting.
+This is already part of the `dev` environment and you can run Ruff before committing:
+```sh
+ruff check --fix
+ruff format
+```
+### Ruff pre-commit hook
+Additionally, we also provide [pre-commit](https://pre-commit.com) hook for Ruff. This
+will run Ruff every time you try to commit and raise errors (and provide fixes)
+in case your code needs linting or formatting. You simply have to add these fixes and
+commit again.
+
+To use this pre-commit hook, make sure to run the following in the terminal after you
+clone the repo:
+```sh
+pre-commit install
+```
---
+++ packages/fairmat-readers-xrd/fairmat_readers_xrd-0.0.8.tar.gz/fairmat_readers_xrd-0.0.8/src/fairmat_readers_xrd/readers.py
@@ -0,0 +1,419 @@
+#
+# Copyright The NOMAD Authors.
+#
+# This file is part of NOMAD. See https://nomad-lab.eu for further info.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import xml.etree.ElementTree as ET
+import collections
+from typing import Dict, Any, TYPE_CHECKING, Optional
+import numpy as np
+import pint
+import os
+import shutil
+import subprocess
+import tempfile
+import platform
+from pathlib import Path
+
+# from pynxtools.dataconverter.convert import transfer_data_into_template
+from fairmat_readers_xrd.utils import (
+    detect_scan_type,
+    modify_scan_data,
+)
+from fairmat_readers_xrd.ikz import RASXfile, BRMLfile
+from fairmat_readers_xrd.bruker_raw_parser import BrukerRAW4Parser
+
+if TYPE_CHECKING:
+    from structlog.stdlib import (
+        BoundLogger,
+    )
+
+ureg = pint.get_application_registry()
+
+
+def transfer_data_into_template(**kwargs):
+    raise NotImplementedError
+
+
+def read_file(file_path: str) -> dict:
+    """
+    Main function to figure out which specific file format to read.
+
+    Args:
+        file_path (str): The path to the file to be read.
+
+    Returns:
+        Dict[str, Any]: The X-ray diffraction data in a Python dictionary.
+    """
+    if file_path.endswith('.rasx'):
+        return read_rigaku_rasx(file_path)
+    if file_path.endswith('.xrdml'):
+        return read_panalytical_xrdml(file_path)
+    if file_path.endswith('.brml'):
+        return read_bruker_brml(file_path)
+    if file_path.endswith('.raw'):
+        return read_bruker_raw(file_path)
+    raise NotImplementedError('Unknown file type.')
+
+
+def read_panalytical_xrdml(
+    file_path: str, logger: 'BoundLogger' = None
+) -> Dict[str, Any]:
+    """
+    Function for reading the X-ray diffraction data in a Panalytical `.xrdml` file.
+
+    Args:
+        file_path (str): The path to the `.xrdml` file.
+        logger (BoundLogger): A structlog logger.
+
+    Returns:
+        Dict[str, Any]: The X-ray diffraction data in a Python dictionary.
+    """
+    with open(file_path, 'r', encoding='utf-8') as file:
+        element_tree = ET.parse(file)
+    root = element_tree.getroot()
+    ns_version = root.tag.split('}')[0].strip('{')
+    ns = {'xrdml': ns_version}
+
+    def find_float(path):
+        result = root.find(path, ns)
+        if result is not None:
+            unit = result.get('unit', '')
+            if unit == 'Angstrom':
+                unit = 'angstrom'
+            return float(result.text) * ureg(unit)
+        return None
+
+    def find_string(path):
+        result = root.find(path, ns)
+        if result is not None:
+            return result.text
+        return None
+
+    xrd_measurement = root.find('xrdml:xrdMeasurement', ns)
+    scans = xrd_measurement.findall('xrdml:scan', ns)
+
+    axes = collections.defaultdict(list)
+    counting_time_value = []
+    attenuation_values = []
+    intensities_array = []
+    counts_array = []
+
+    for scan in scans:
+        data_points = scan.find('xrdml:dataPoints', ns)
+        intensities = data_points.find('xrdml:intensities', ns)
+        counting_time = data_points.find('xrdml:commonCountingTime', ns)
+        attenuation = data_points.find('xrdml:beamAttenuationFactors', ns)
+
+        counting_time_value.append(
+            np.fromstring(counting_time.text, sep=' ') * ureg(counting_time.get('unit'))
+        )
+        if attenuation is not None:
+            attenuation_values.append(
+                np.fromstring(attenuation.text, sep=' ') * ureg.dimensionless
+            )
+            scaling_factor = attenuation_values[-1]
+        else:
+            attenuation_values = None
+            scaling_factor = 1
+        if intensities is not None:
+            intensities_array.append(
+                np.fromstring(intensities.text, sep=' ') * ureg.dimensionless
+            )
+            counts_array = None
+        else:
+            counts = data_points.find('xrdml:counts', ns)
+            counts_array.append(
+                np.fromstring(counts.text, sep=' ') * ureg.dimensionless
+            )
+            intensities_array.append(
+                counts_array[-1] * scaling_factor * ureg.dimensionless
+            )
+        n_points = len(intensities_array[-1])
+
+        for axis in data_points.findall('xrdml:positions', ns):
+            name = axis.get('axis')
+            unit = axis.get('unit')
+            listed = axis.find('xrdml:listPositions', ns)
+            start = axis.find('xrdml:startPosition', ns)
+            end = axis.find('xrdml:endPosition', ns)
+            common = axis.find('xrdml:commonPosition', ns)
+            if listed is not None:
+                axes[name].append(np.fromstring(listed.text, sep=' ') * ureg(unit))
+            elif start is not None and end is not None:
+                axes[name].append(
+                    np.linspace(float(start.text), float(end.text), n_points)
+                    * ureg(unit)
+                )
+            elif common is not None:
+                axes[name].append(np.array([float(common.text)]) * ureg(unit))
+            else:
+                if logger is not None:
+                    logger.warning('Unknown data for {name} axis.')
+                axes[name].append(None)
+
+    scan_data = collections.defaultdict(list)
+    scan_data.update(axes)
+    scan_data['intensity'] = intensities_array
+    scan_data['counts'] = counts_array
+    scan_data['countTime'] = counting_time_value
+    scan_data['beamAttenuationFactors'] = attenuation_values
+
+    scan_type = detect_scan_type(scan_data)
+    modified_scan_data = modify_scan_data(scan_data, scan_type)
+
+    return {
+        **modified_scan_data,
+        'scanmotname': scan.get('scanAxis', None),
+        'metadata': {
+            'sample_id': find_string('xrdml:sample/xrdml:id'),
+            'measurement_type': xrd_measurement.get('measurementType', None),
+            'sample_mode': xrd_measurement.get('sampleMode', None),
+            'source': {
+                'current': find_float(
+                    'xrdml:xrdMeasurement/xrdml:incidentBeamPath/xrdml:xRayTube'
+                    '/xrdml:current'
+                ),
+                'voltage': find_float(
+                    'xrdml:xrdMeasurement/xrdml:incidentBeamPath/xrdml:xRayTube'
+                    '/xrdml:tension'
+                ),
+                'kAlpha1': find_float(
+                    'xrdml:xrdMeasurement/xrdml:usedWavelength/xrdml:kAlpha1'
+                ),
+                'kAlpha2': find_float(
+                    'xrdml:xrdMeasurement/xrdml:usedWavelength/xrdml:kAlpha2'
+                ),
+                'kBeta': find_float(
+                    'xrdml:xrdMeasurement/xrdml:usedWavelength/xrdml:kBeta'
+                ),
+                'ratioKAlpha2KAlpha1': find_float(
+                    'xrdml:xrdMeasurement/xrdml:usedWavelength/xrdml:ratioKAlpha2KAlpha1'
+                ),
+                'anode_material': find_string(
+                    'xrdml:xrdMeasurement/xrdml:incidentBeamPath/xrdml:xRayTube'
+                    '/xrdml:anodeMaterial'
+                ),
+            },
+            'scan_mode': scan.get('mode', None),
+            'scan_axis': scan.get('scanAxis', None),
+            'scan_type': scan_type,
+        },
+    }
+
+
+def read_rigaku_rasx(file_path: str, logger: 'BoundLogger' = None) -> Dict[str, Any]:
+    """
+    Reads .rasx files from Rigaku instruments
+        - reader is based on IKZ module
+
+    Args:
+        file_path (string): absolute path of the file.
+        logger (BoundLogger): A structlog logger for propagating errors and warnings.
+
+    Returns:
+        Dict[str, Any]: The X-ray diffraction data in a Python dictionary.
+    """
+    reader = RASXfile(file_path, verbose=False)
+    scan_info = reader.get_scan_info()
+    scan_data = reader.get_scan_data(logger)
+    source = reader.get_source_info()
+
+    scan_type = detect_scan_type(scan_data)
+    modified_scan_data = modify_scan_data(scan_data, scan_type)
+
+    count_time = None
+    scan_axis = None
+
+    if scan_info:
+        required_keys = ['Mode', 'SpeedUnit', 'PositionUnit', 'Step', 'Speed']
+        if all(key in scan_info and scan_info[key] for key in required_keys):
+            if scan_info['Mode'].lower() == 'continuous':
+                speed_unit = ureg(scan_info['SpeedUnit'].replace('min', 'minute'))
+                count_time_unit = ureg(scan_info['PositionUnit']) / speed_unit
+                count_time = (
+                    np.array([scan_info['Step'] / scan_info['Speed']]) * count_time_unit
+                )
+        scan_axis = scan_info.get('AxisName', None)
+
+    return {
+        **modified_scan_data,
+        'countTime': count_time,
+        'metadata': {
+            'sample_id': None,
+            'scan_axis': scan_axis,
+            'scan_type': scan_type,
+            'source': {
+                'anode_material': source['TargetName'],
+                'kAlpha1': source['WavelengthKalpha1'],
+                'kAlpha2': source['WavelengthKalpha2'],
+                'kBeta': source['WavelengthKbeta'],
+                'voltage': source['Voltage'],
+                'current': source['Current'],
+                'ratioKAlpha2KAlpha1': None,
+            },
+        },
+    }
+
+
+def read_bruker_brml(file_path: str, logger: 'BoundLogger' = None) -> Dict[str, Any]:
+    """
+    Reads .brml files from Bruker instruments
+        - reader is based on IKZ module
+
+    Args:
+        file_path (string): absolute path of the file.
+        logger (BoundLogger): A structlog logger for propagating errors and warnings.
+
+    Returns:
+        Dict[str, Any]: The X-ray diffraction data in a Python dictionary.
+    """
+    reader = BRMLfile(file_path, verbose=False)
+    scan_info = reader.get_scan_info()
+    source = reader.get_source_info()
+    scan_data = reader.get_scan_data(logger)
+    scan_type = detect_scan_type(scan_data)
+    modified_scan_data = modify_scan_data(scan_data, scan_type)
+    modified_scan_data['Omega'] = modified_scan_data.pop(
+        'Theta', None
+    )  # theta and omega are synonymous in .brml
+
+    return {
+        **modified_scan_data,
+        'countTime': None,
+        'metadata': {
+            'sample_id': None,
+            'scan_axis': scan_info.get('ScanName', None),
+            'scan_type': scan_type,
+            'source': {
+                'anode_material': source['TubeMaterial'],
+                'kAlpha1': source['WaveLengthAlpha1'],
+                'kAlpha2': source['WaveLengthAlpha2'],
+                'kBeta': source['WaveLengthBeta'],
+                'ratioKAlpha2KAlpha1': source['WaveLengthRatio'],
+                'voltage': source['Voltage'],
+                'current': source['Current'],
+            },
+        },
+    }
+
+
+def read_bruker_raw(
+    file_path: str, logger: 'BoundLogger' = None
+) -> Optional[Dict[str, Any]]:
+    """
+    Read Bruker/Siemens RAW v4 binary files using native Python parser.
+
+    This function parses Bruker RAW v4 (magic header "RAW4.00") binary files directly
+    without requiring external conversion tools. All scan parameters (start_angle,
+    step_size, num_points, scan_axis) are extracted directly from the binary file.
+
+    Args:
+        file_path (str): Path to the Bruker .raw file.
+        logger (BoundLogger, optional): Logger instance for warnings/errors.
+
+    Returns:
+        Dict[str, Any] | None: XRD data dictionary compatible with nomad-measurements
+                               schema, or None if parsing fails.
+
+    Note:
+        - Only RAW v4 format is currently supported
+        - Designed for single-axis theta-2theta powder diffraction scans
+        - Multi-axis scans (texture, pole figures) may not parse correctly
+        - All scan parameters are extracted from the binary file
+        - No external files (.xrdml) are required
+
+    Citation for RAW format understanding:
+        Siemens/Bruker RAW format is proprietary but reverse-engineered
+        for open science data management. Based on DIFFRAC.EVA output files.
+    """
+    try:
+        # Parse the RAW file - all parameters are extracted from the file
+        parser = BrukerRAW4Parser(file_path)
+        raw_data = parser.parse()
+
+        # Prepare scan data in the format expected by detect_scan_type/modify_scan_data
+        # (list of arrays, like XRDML parser does)
+        scan_data = {
+            '2Theta': [np.array(parser.angles) * ureg.degree],
+            'intensity': [np.array(parser.intensities) * ureg.dimensionless],
+            'counts': None,
+            'countTime': None,
+            'beamAttenuationFactors': None,
+        }
+
+        # Detect scan type and modify data accordingly (like other readers)
+        scan_type = detect_scan_type(scan_data)
+        modified_scan_data = modify_scan_data(scan_data, scan_type)
+
+        # Build metadata dictionary
+        # Extract scan axis from raw data (e.g., "Theta")
+        scan_axis = raw_data['scan_params'].get('scan_axis', '')
+
+        # Build source metadata if anode material was found
+        source_metadata = {}
+        if 'anode_material' in raw_data['metadata']:
+            source_metadata = {
+                'anode_material': raw_data['metadata']['anode_material'],
+                'kAlpha1': raw_data['metadata'].get('wavelength_kalpha1'),
+                'kAlpha2': raw_data['metadata'].get('wavelength_kalpha2'),
+                'kBeta': raw_data['metadata'].get('wavelength_kbeta'),
+                'ratioKAlpha2KAlpha1': raw_data['metadata'].get(
+                    'kalpha2_kalpha1_ratio'
+                ),
+            }
+
+        metadata = {
+            'sample_id': raw_data['metadata'].get('sample_id'),
+            'scan_type': scan_type,
+            'scan_axis': scan_axis,  # Extracted from RAW file at offset 0x04D0
+            'source': source_metadata,  # Anode material and wavelengths from lookup table
+        }
+
+        # Return data in the same format as read_panalytical_xrdml
+        # Use scan_axis as scanmotname (e.g., "Theta")
+        return {
+            **modified_scan_data,
+            'scanmotname': scan_axis,  # Extracted from RAW file at offset 0x04D0
+            'metadata': metadata,
+        }
+
+    except Exception as e:
+        if logger:
+            logger.error(
+                f'Failed to parse Bruker/Siemens .raw file {file_path}: {str(e)}. '
+                f'The file may be corrupted or in an unsupported format.'
+            )
+        return None
+
+
+def read_nexus_xrd(file_path: str, logger: 'BoundLogger' = None) -> Dict[str, Any]:
+    """
+    Function for reading the X-ray diffraction data in a Nexus file.
+
+    Args:
+        file_path (str): The path to the X-ray diffraction data file.
+        logger (BoundLogger, optional): A structlog logger. Defaults to None.
+
+    Returns:
+        Dict[str, Any]: The X-ray diffraction data in a Python dictionary.
+    """
+    nxdl_name = 'NXxrd_pan'
+    xrd_template = transfer_data_into_template(
+        nxdl_name=nxdl_name,
+        input_file=file_path,
+        reader='xrd',
+    )
+    return xrd_template
