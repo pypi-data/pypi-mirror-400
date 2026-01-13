7d046845508329c1be19ef230278a3743ad6477e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 03:32:15 +0000

    Add package imglp/0.1.9/imglp-0.1.9.tar.gz

---
+++ packages/imglp/imglp-0.1.9.tar.gz/imglp-0.1.9/src/imglp/__init__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Date          : 2025-08-28
+# Author        : Lancelot PINCET
+# GitHub        : https://github.com/LancelotPincet
+# Library       : imgLP
+
+"""
+A library that provides tools for image processing.
+"""
+
+
+
+# %% Lazy imports
+from corelp import getmodule
+__getattr__, __all__ = getmodule(__file__)
+
+
+
+# %% Test function run
+if __name__ == "__main__":
+    from corelp import test
+    test(__file__)
