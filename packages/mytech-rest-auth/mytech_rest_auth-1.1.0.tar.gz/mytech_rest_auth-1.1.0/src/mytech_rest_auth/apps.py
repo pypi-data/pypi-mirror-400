ca48d238dcd29a1e14d179d297c36285399fedcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 12:06:22 +0000

    Add package mytech-rest-auth/1.1.0/mytech_rest_auth-1.1.0.tar.gz

---
+++ packages/mytech-rest-auth/mytech_rest_auth-1.1.0.tar.gz/mytech_rest_auth-1.1.0/src/mytech_rest_auth/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+class MytechRestAuthConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = "mytech_rest_auth"
+    verbose_name = "Mytech Rest Auth"
