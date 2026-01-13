2a9d9d6753de11f608443ffa7ba2267078c719ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 22:06:12 +0000

    Add package gicisky/0.2.0/gicisky-0.2.0.tar.gz

---
+++ packages/gicisky/gicisky-0.2.0.tar.gz/gicisky-0.2.0/gicisky/logger/__init__.py
@@ -0,0 +1,6 @@
+from .logger import GiciskyLogger, LogCategory
+
+__all__ = [
+    "GiciskyLogger",
+    "LogCategory"
+]
---
+++ packages/gicisky/gicisky-0.2.0.tar.gz/gicisky-0.2.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r", encoding="utf-8") as fh:
+    requirements = [line.strip() for line in fh if line.strip() and not line.startswith("#")]
+
+setup(
+    name="gicisky",
+    version="0.2.0",
+    author="MassiveBox",
+    author_email="box@boxo.cc",
+    description="A Python library for interacting with Gicisky E-Ink display tags via Bluetooth Low Energy",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://git.boxo.cc/massivebox/gicisky",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    python_requires=">=3.7",
+    install_requires=requirements,
+)
