b6778dda90c89ca7cd5175197591632dffbca090
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 07:07:46 +0000

    Add package videostream/2.1.1/videostream-2.1.1.tar.gz

---
+++ packages/videostream/videostream-2.1.1.tar.gz/videostream-2.1.1/videostream/__init__.py
@@ -0,0 +1,8 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright Ⓒ 2025 Au-Zone Technologies. All Rights Reserved.
+
+from .library import lib, version
+from .frame import Frame
+from .client import Client
+from .host import Host
+from .decoder import Decoder, DecoderCodec, CodecBackend, DecodeReturnCode
