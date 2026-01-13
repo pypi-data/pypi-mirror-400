59943f989194c079655b52d6616599aa5db60926
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 06:04:34 +0000

    Add package Sales-Manager-GVP/0.1.0/sales_manager_gvp-0.1.0.tar.gz

---
+++ packages/Sales-Manager-GVP/sales_manager_gvp-0.1.0.tar.gz/sales_manager_gvp-0.1.0/README.md
@@ -0,0 +1,11 @@
+# Sales App
+
+This package provides functionalities for managing sales, including price calculations, taxes, and discounts.
+
+## Installation
+
+You can install the package using:
+
+```bash
+pip install .
+"
---
+++ packages/Sales-Manager-GVP/sales_manager_gvp-0.1.0.tar.gz/sales_manager_gvp-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "Sales_Manager_GVP",
+    version = "0.1.0",
+    author = "Martín Guevara Ulloa",
+    author_email = "msgu0603@gmail.com",
+    description = "This package provides functionalities for managing sales, including price calculations, taxes, and discounts.",
+    long_description = open("README.md").read(),
+    long_description_content_type = "text/markdown",
+    url = ("https://github.com/msgu0603-code/Sales-Manager"),
+    packages = find_packages(),
+    install_requires = [],
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    python_requires = ">= 3.7"
+)
