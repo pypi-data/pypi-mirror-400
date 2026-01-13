0cfa51d946ec42ee19d9816f8255141231bccca2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 19:07:25 +0000

    Add package mofstructure/0.1.8.4/mofstructure-0.1.8.4-py3-none-any.whl

---
+++ packages/mofstructure/mofstructure-0.1.8.4-py3-none-any.whl/mofstructure/filetyper.py
@@ -0,0 +1,249 @@
+#!/usr/bin/python
+from __future__ import print_function
+__author__ = "Dr. Dinga Wonanke"
+__status__ = "production"
+import os
+import pickle
+import csv
+import json
+import codecs
+from zipfile import ZipFile
+import numpy as np
+from ase import Atoms
+import ase
+import pandas as pd
+import msgpack
+from importlib.resources import files
+
+
+class AtomsEncoder(json.JSONEncoder):
+    '''
+    ASE atom type encorder for json to enable serialising
+    ase atom object.
+    '''
+
+    def default(self, encorder_obj):
+        '''
+        define different encoder to serialise ase atom objects
+        '''
+        if isinstance(encorder_obj, Atoms):
+            coded = dict(positions=[list(pos) for pos in encorder_obj.get_positions()], lattice_vectors=[
+                         list(c) for c in encorder_obj.get_cell()], labels=list(encorder_obj.get_chemical_symbols()))
+            if len(encorder_obj.get_cell()) == 3:
+                coded['periodic'] = ['True', 'True', 'True']
+            coded['n_atoms'] = len(list(encorder_obj.get_chemical_symbols()))
+            coded['atomic_numbers'] = encorder_obj.get_atomic_numbers().tolist()
+            keys = list(encorder_obj.info.keys())
+            if 'atom_indices_mapping' in keys:
+                info = encorder_obj.info
+                coded.update(info)
+            return coded
+        if isinstance(encorder_obj, ase.spacegroup.Spacegroup):
+            return encorder_obj.todict()
+        return json.JSONEncoder.default(self, encorder_obj)
+
+
+def numpy_to_json(ndarray, file_name):
+    '''
+    Serialise a numpy object
+    '''
+    json.dump(ndarray.tolist(), codecs.open(file_name, 'w',
+              encoding='utf-8'), separators=(',', ':'), sort_keys=True)
+    return
+
+
+def write_json(json_obj, file_name):
+    '''
+    write a python dictionary object to json
+    '''
+    # Serializing json
+    json_object = json.dumps(json_obj, indent=4, sort_keys=True)
+    with open(file_name, "w", encoding='utf-8') as outfile:
+        outfile.write(json_object)
+
+
+def json_to_numpy(json_file):
+    '''
+    serialised a numpy array to json
+    '''
+    json_reader = codecs.open(json_file, 'r', encoding='utf-8').read()
+    json_reader = np.array(json.loads(json_reader))
+    return read_json
+
+
+def json_to_ase_atom(data,  encoder, filename):
+    '''
+    serialise an ase atom type and write as json
+    '''
+    with open(filename, 'w', encoding='utf-8') as f_obj:
+        json.dump(data, f_obj, indent=4, sort_keys=False, cls=encoder)
+    return
+
+
+def append_json_atom(data,  encoder, filename):
+    '''
+    append a data containing an ase atom object
+    '''
+    if not os.path.exists(filename):
+        with open(filename, 'w', encoding='utf-8') as f_obj:
+            f_obj.write('{}')
+    elif os.path.getsize(filename) == 0:
+        with open(filename, 'w', encoding='utf-8') as f_obj:
+            f_obj.write('{}')
+    with open(filename, 'r+', encoding='utf-8') as f_obj:
+        # First we load existing data into a dict.
+        file_data = json.load(f_obj)
+        # Join new_data with file_data inside emp_details
+        file_data.update(data)
+        # Sets file's current position at offset.
+        f_obj.seek(0)
+        # convert back to json.
+
+        json.dump(data, f_obj, indent=4, sort_keys=False, cls=encoder)
+
+
+def append_json(new_data, filename):
+    '''
+    append a new data in an existing json file
+    '''
+    if not os.path.exists(filename):
+        with open(filename, 'w', encoding='utf-8') as file:
+            file.write('{}')
+    elif os.path.getsize(filename) == 0:
+        with open(filename, 'w', encoding='utf-8') as file:
+            file.write('{}')
+    with open(filename, 'r+', encoding='utf-8') as file:
+        # First we load existing data into a dict.
+        file_data = json.load(file)
+        # Overwrite existing keys with new_data
+        file_data.update(new_data)
+        # Sets file's current position at offset.
+        file.seek(0)
+        # convert back to json.
+        json.dump(file_data, file, indent=4, sort_keys=True)
+
+
+def read_json(file_name):
+    '''
+    load a json file
+    '''
+    with open(file_name, 'r', encoding='utf-8') as f_obj:
+        data = json.load(f_obj)
+
+    return data
+
+
+def csv_read(csv_file):
+    '''
+    Read a csv file
+    '''
+    f_obj = open(csv_file, 'r', encoding='utf-8')
+    data = csv.reader(f_obj)
+    return data
+
+
+def get_contents(filename):
+    '''
+    Read a file and return a list content
+    '''
+    with open(filename, 'r', encoding='utf-8') as f_obj:
+        contents = f_obj.readlines()
+    return contents
+
+
+def put_contents(filename, output):
+    '''
+    write a list object into a file
+    '''
+    with open(filename, 'w', encoding='utf-8') as f_obj:
+        f_obj.writelines(output)
+    return
+
+
+def append_contents(filename, output):
+    '''
+    append contents into a file
+    '''
+    with open(filename, 'a', encoding='utf-8') as f_obj:
+        f_obj.writelines(output)
+    return
+
+
+def pickle_load(filename):
+    '''
+    load a pickle file
+    '''
+    data = open(filename, 'rb')
+    data = pickle.load(data)
+    return data
+
+
+def read_zip(zip_file):
+    '''
+    read a zip file
+    '''
+    content = ZipFile(zip_file, 'r')
+    content.extractall(zip_file)
+    content.close()
+    return content
+
+
+def save_dict_msgpack(data: dict, filename: str) -> None:
+    """Save a dictionary to a file using MessagePack."""
+    with open(filename, "wb") as f:
+        msgpack.pack(data, f, use_bin_type=True)
+
+
+def load_dict_msgpack(filename: str) -> dict:
+    """Load a dictionary from a MessagePack file."""
+    with open(filename, "rb") as f:
+        return msgpack.unpack(f, raw=False, strict_map_key=False)
+
+
+def convert_numpy_types(data):
+    '''
+    A function that jsonifies a dictionary by removing numpy data types.
+
+    **parameter:**
+        data (dict): data to jsonify
+
+    **return:**
+        data (dic): jsonified data
+    '''
+    if isinstance(data, dict):
+        return {key: convert_numpy_types(value) for key, value in data.items()}
+    elif isinstance(data, list):
+        return [convert_numpy_types(element) for element in data]
+    elif isinstance(data, np.integer):
+        return int(data)
+    elif isinstance(data, np.floating):
+        return float(data)
+    else:
+        return data
+
+def load_data(filename):
+    '''
+    function that recognises file extenion and chooses the correction
+    function to load the data.
+    '''
+    # file_ext = filename[filename.rindex('.')+1:]
+    basename = os.path.basename(filename)
+    file_ext = basename.split('.')[-1]
+
+    if file_ext == 'json':
+        data = read_json(filename)
+    elif file_ext == 'csv':
+        data = pd.read_csv(filename)
+    elif file_ext == 'p':
+        data = pickle_load(filename)
+    elif file_ext == 'xlsx':
+        data = pd.read_excel(filename)
+    elif file_ext == 'msgpack':
+        data = load_dict_msgpack(filename)
+    else:
+        data = get_contents(filename)
+    return data
+
+def load_iupac_names():
+    msgpack_path = files("mofstructure").joinpath("db/iupacname.msgpack")
+    return load_data(msgpack_path)
---
+++ packages/mofstructure/mofstructure-0.1.8.4-py3-none-any.whl/mofstructure/obsolate/buildingunits.py
@@ -0,0 +1,118 @@
+#!/usr/bin/python
+from __future__ import print_function
+__author__ = "Dr. Dinga Wonanke"
+__status__ = "production"
+import argparse
+import os
+from ase.io import read
+import pandas as pd
+import mofstructure.mofdeconstructor as MOF_deconstructor
+from mofstructure.porosity import zeo_calculation
+
+
+def sbu_data(ase_atom, path_to_file):
+    '''
+    Function to compile secondary building units and region of MOFs
+
+    Parameters
+    ----------
+    ase_atom : ASE atoms object
+    path_to_file : result directory or folder
+    '''
+    connected_components, atoms_indices_at_breaking_point, porpyrin_checker, all_regions = MOF_deconstructor.\
+        secondary_building_units(ase_atom)
+    metal_sbus, organic_sbus, building_unit_regions = MOF_deconstructor.\
+        find_unique_building_units(
+            connected_components,
+            atoms_indices_at_breaking_point,
+            ase_atom, porpyrin_checker,
+            all_regions,
+            cheminfo=True
+        )
+    for i, sbu_metal in enumerate(metal_sbus):
+        # print('sbu', sbu_metal.info['sbu_type'])
+        # print('point_of_extension', len(sbu_metal.info['point_of_extension']))
+        sbu_metal.write(path_to_file + '_metal_sbu_'+str(i+1)+'.xyz')
+    for j, sbu_linker in enumerate(organic_sbus):
+        sbu_linker.write(path_to_file + '_organic_sbu_'+str(j+1)+'.xyz')
+    return building_unit_regions
+
+
+def ligand_data(ase_atom, path_to_file):
+    '''
+    Function to compile organic ligands, metal clusters and region of MOFs
+
+    Parameters
+    ----------
+    ase_atom : ASE atoms object
+    path_to_file : result directory or folder
+    '''
+    connected_components, atoms_indices_at_breaking_point, porpyrin_checker, all_regions = MOF_deconstructor.\
+        ligands_and_metal_clusters(ase_atom)
+    metal_clusters, organic_ligands, building_unit_regions = MOF_deconstructor.\
+        find_unique_building_units(
+            connected_components,
+            atoms_indices_at_breaking_point,
+            ase_atom, porpyrin_checker,
+            all_regions,
+            cheminfo=True
+        )
+    for i, mof_metal in enumerate(metal_clusters):
+        mof_metal.write(path_to_file+'_metal_cluster_'+str(i+1)+'.xyz')
+    for j,  mof_ligand in enumerate(organic_ligands):
+        mof_ligand.write(path_to_file + '_organic_ligand_'+str(j+1)+'.xyz')
+    return building_unit_regions
+
+
+def remove_guest(ase_atom):
+    '''
+    Simple function to remove guest molecules in porous system
+    '''
+    index_non_guest = MOF_deconstructor.remove_unbound_guest(ase_atom)
+    return ase_atom[index_non_guest]
+
+
+def work_flow(cif_file, save_dir, verbose=False):
+    '''
+    A workflow to remove guest, compute porosity and deconstructure
+    mofs. The function starts with checking and removing any unbound
+    guest molecule present in the MOF. After that it computed the porosity
+    of the MOF and load this data in the output folder. Finally the deconstructs
+    the MOFs into the various building units and safe these building units
+    in '.xyz' formats in the output folder.
+    Parameters
+    ----------
+    cif_file : a cif file or any ase readable file containing a MOF.
+    save_dir : path to output folder
+    '''
+    try:
+        ase_atom = read(cif_file)
+        ase_atom = remove_guest(ase_atom)
+        base_name = cif_file[:cif_file.rindex('.')].split('/')[-1]
+        if not os.path.exists(save_dir):
+            os.makedirs(save_dir)
+        path_to_file = save_dir+'/'+base_name
+
+        ase_atom = remove_guest(ase_atom)
+        pores = zeo_calculation(ase_atom)
+        data_f = pd.DataFrame(pores, index=[0])
+        data_f.to_csv(path_to_file+'_porosity_data.csv')
+        sbu_data(ase_atom, path_to_file)
+        ligand_data(ase_atom, path_to_file)
+    except Exception:
+        pass
+    if verbose:
+        print(f"Saved results to {path_to_file}")
+    return
+
+
+def main():
+    '''
+    Routine runner for CLI
+    '''
+    parser = argparse.ArgumentParser(description='Run work_flow function with optional verbose output')
+    parser.add_argument('cif_file', type=str, help='path to CIF file')
+    parser.add_argument('-s', '--save_dir', type=str, default='MOF_building_units', help='directory to save output files')
+    parser.add_argument('-v', '--verbose', action='store_true', help='print verbose output')
+    args = parser.parse_args()
+    work_flow(args.cif_file, args.save_dir, args.verbose)
