22589bb6886144cf333cb12eae30d10a68024008
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 18:10:44 +0000

    Add package scikit-plots/0.4.0.post9/scikit_plots-0.4.0.post9-cp313-cp313t-musllinux_1_2_aarch64.whl

---
+++ packages/scikit-plots/scikit_plots-0.4.0.post9-cp313-cp313t-musllinux_1_2_aarch64.whl/scikitplot/externals/_sphinxext/__init__.py
@@ -0,0 +1,18 @@
+# fmt: off
+# ruff: noqa
+# ruff: noqa: PGH004
+# flake8: noqa
+# pylint: skip-file
+# mypy: ignore-errors
+# type: ignore
+
+# This module was copied from the matplotlib project.
+# https://github.com/matplotlib/matplotlib/tree/main/lib/matplotlib/sphinxext
+
+"""
+sphinxext [1]_.
+
+References
+----------
+.. [1] https://github.com/matplotlib/matplotlib/tree/main/lib/matplotlib/sphinxext
+"""
