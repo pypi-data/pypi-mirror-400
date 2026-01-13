5b6f973320b143f19d5fe2267e1415593482f8f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 09:10:58 +0000

    Add package roboto/0.35.0.post1/roboto-0.35.0.post1-py3-none-any.whl

---
+++ packages/roboto/roboto-0.35.0.post1-py3-none-any.whl/roboto/domain/files/__init__.py
@@ -0,0 +1,63 @@
+# Copyright (c) 2024 Roboto Technologies, Inc.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+from .file import File
+from .file_creds import (
+    CredentialProvider,
+    DatasetCredentials,
+    S3Credentials,
+)
+from .file_downloader import FileDownloader
+from .lazy_lookup_file import LazyLookupFile
+from .operations import (
+    AbortTransactionsRequest,
+    DeleteFileRequest,
+    DirectoryContentsPage,
+    FileRecordRequest,
+    ImportFileRequest,
+    QueryFilesRequest,
+    RenameFileRequest,
+    SignedUrlResponse,
+    UpdateFileRecordRequest,
+)
+from .record import (
+    DirectoryRecord,
+    FileRecord,
+    FileStatus,
+    FileStorageType,
+    FileTag,
+    FSType,
+    IngestionStatus,
+    is_directory,
+    is_file,
+)
+
+__all__ = (
+    "AbortTransactionsRequest",
+    "CredentialProvider",
+    "DatasetCredentials",
+    "DeleteFileRequest",
+    "DirectoryContentsPage",
+    "DirectoryRecord",
+    "FSType",
+    "File",
+    "FileDownloader",
+    "FileRecord",
+    "FileRecordRequest",
+    "FileStatus",
+    "FileStorageType",
+    "FileTag",
+    "ImportFileRequest",
+    "IngestionStatus",
+    "LazyLookupFile",
+    "QueryFilesRequest",
+    "RenameFileRequest",
+    "S3Credentials",
+    "SignedUrlResponse",
+    "UpdateFileRecordRequest",
+    "is_directory",
+    "is_file",
+)
