7df944480d15dde81c8349ae464f45bcf0b4890e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 23:05:39 +0000

    Add package singlestoredb/1.16.7/singlestoredb-1.16.7-cp38-abi3-win_amd64.whl

---
+++ packages/singlestoredb/singlestoredb-1.16.7-cp38-abi3-win_amd64.whl/singlestoredb/tests/utils.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python
+# type: ignore
+"""Utilities for testing."""
+import logging
+import os
+import uuid
+from typing import Any
+from typing import Dict
+from urllib.parse import urlparse
+
+import singlestoredb as s2
+from singlestoredb.connection import build_params
+
+
+logger = logging.getLogger(__name__)
+
+
+def apply_template(content: str, vars: Dict[str, Any]) -> str:
+    for k, v in vars.items():
+        key = '{{%s}}' % k
+        if key in content:
+            content = content.replace(key, v)
+    return content
+
+
+def load_sql(sql_file: str) -> str:
+    """
+    Load a file containing SQL code.
+
+    Parameters
+    ----------
+    sql_file : str
+        Name of the SQL file to load.
+
+    Returns
+    -------
+    (str, bool)
+        Name of database created for SQL file and a boolean indicating
+        whether the database already existed (meaning that it should not
+        be deleted when tests are finished).
+
+    """
+    dbname = None
+
+    # Use an existing database name if given.
+    if 'SINGLESTOREDB_URL' in os.environ:
+        dbname = build_params(host=os.environ['SINGLESTOREDB_URL']).get('database')
+    elif 'SINGLESTOREDB_HOST' in os.environ:
+        dbname = build_params(host=os.environ['SINGLESTOREDB_HOST']).get('database')
+    elif 'SINGLESTOREDB_DATABASE' in os.environ:
+        dbname = os.environ['SINGLESTOREDB_DATBASE']
+
+    # Use initializer URL if given for setup operations.
+    # HTTP can't change databases or execute certain commands like SET GLOBAL,
+    # so we always use the MySQL protocol URL for initialization.
+    args = {'local_infile': True}
+    if 'SINGLESTOREDB_INIT_DB_URL' in os.environ:
+        args['host'] = os.environ['SINGLESTOREDB_INIT_DB_URL']
+        logger.info(
+            f'load_sql: Using SINGLESTOREDB_INIT_DB_URL for setup: '
+            f'{os.environ["SINGLESTOREDB_INIT_DB_URL"]}',
+        )
+
+    http_port = 0
+    if 'SINGLESTOREDB_URL' in os.environ:
+        url = os.environ['SINGLESTOREDB_URL']
+        if url.startswith('http:') or url.startswith('https:'):
+            urlp = urlparse(url)
+            if urlp.port:
+                http_port = urlp.port
+
+    if 'SINGLESTOREDB_HTTP_PORT' in os.environ:
+        http_port = int(os.environ['SINGLESTOREDB_HTTP_PORT'])
+
+    dbexisted = bool(dbname)
+
+    template_vars = dict(DATABASE_NAME=dbname, TEST_PATH=os.path.dirname(sql_file))
+
+    # Always use the default driver since not all operations are
+    # permitted in the HTTP API.
+    with open(sql_file, 'r') as infile:
+        with s2.connect(**args) as conn:
+            with conn.cursor() as cur:
+                try:
+                    cur.execute('SET GLOBAL default_partitions_per_leaf=2')
+                    cur.execute('SET GLOBAL log_file_size_partitions=1048576')
+                    cur.execute('SET GLOBAL log_file_size_ref_dbs=1048576')
+                except s2.OperationalError:
+                    pass
+
+                if not dbname:
+                    dbname = 'TEST_{}'.format(uuid.uuid4()).replace('-', '_')
+                    cur.execute(f'CREATE DATABASE {dbname};')
+                    cur.execute(f'USE {dbname};')
+
+                    template_vars['DATABASE_NAME'] = dbname
+
+                    # Execute lines in SQL.
+                    for cmd in infile.read().split(';\n'):
+                        cmd = apply_template(cmd.strip(), template_vars)
+                        if cmd:
+                            cmd += ';'
+                            cur.execute(cmd)
+
+                elif not conn.driver.startswith('http'):
+                    cur.execute(f'USE {dbname};')
+
+                # Start HTTP server as needed.
+                if http_port and not conn.driver.startswith('http'):
+                    cur.execute(f'SET GLOBAL HTTP_PROXY_PORT={http_port};')
+                    cur.execute('SET GLOBAL HTTP_API=ON;')
+                    cur.execute('RESTART PROXY;')
+
+    return dbname, dbexisted
+
+
+def drop_database(name: str) -> None:
+    """Drop a database with the given name."""
+    if name:
+        args = {}
+        if 'SINGLESTOREDB_INIT_DB_URL' in os.environ:
+            args['host'] = os.environ['SINGLESTOREDB_INIT_DB_URL']
+        with s2.connect(**args) as conn:
+            with conn.cursor() as cur:
+                cur.execute(f'DROP DATABASE {name};')
+
+
+def create_user(name: str, password: str, dbname: str) -> None:
+    """Create a user for the test database."""
+    if name:
+        args = {}
+        if 'SINGLESTOREDB_INIT_DB_URL' in os.environ:
+            args['host'] = os.environ['SINGLESTOREDB_INIT_DB_URL']
+        with s2.connect(**args) as conn:
+            with conn.cursor() as cur:
+                cur.execute(f'DROP USER IF EXISTS {name};')
+                cur.execute(f'CREATE USER "{name}"@"%" IDENTIFIED BY "{password}"')
+                cur.execute(f'GRANT ALL ON {dbname}.* to "{name}"@"%"')
+
+
+def drop_user(name: str) -> None:
+    """Drop a database with the given name."""
+    if name:
+        args = {}
+        if 'SINGLESTOREDB_INIT_DB_URL' in os.environ:
+            args['host'] = os.environ['SINGLESTOREDB_INIT_DB_URL']
+        with s2.connect(**args) as conn:
+            with conn.cursor() as cur:
+                cur.execute(f'DROP USER IF EXISTS {name};')
