4363a929988bf3aa1a26025eb5d30e84aace2960
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 21:05:38 +0000

    Add package baihu-logging/0.1.0/baihu_logging-0.1.0.tar.gz

---
+++ packages/baihu-logging/baihu_logging-0.1.0.tar.gz/baihu_logging-0.1.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="baihu-logging",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+    ],
+    author="Baihu",
+    description="A logging bot which can log messages to Discord",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/baihufox3210/Baihu-Logging",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
