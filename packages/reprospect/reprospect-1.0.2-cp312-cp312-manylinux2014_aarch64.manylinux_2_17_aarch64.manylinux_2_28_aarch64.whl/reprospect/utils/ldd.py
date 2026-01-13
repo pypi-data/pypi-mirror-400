27e784a516595fe51da60ef70496c8500315d8bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 22:06:46 +0000

    Add package reprospect/1.0.2/reprospect-1.0.2-cp312-cp312-manylinux2014_aarch64.manylinux_2_17_aarch64.manylinux_2_28_aarch64.whl

---
+++ packages/reprospect/reprospect-1.0.2-cp312-cp312-manylinux2014_aarch64.manylinux_2_17_aarch64.manylinux_2_28_aarch64.whl/reprospect/utils/ldd.py
@@ -0,0 +1,20 @@
+import pathlib
+import re
+import typing
+
+from reprospect.utils.subprocess_helpers import popen_stream
+
+
+def get_shared_dependencies(*, file: str | pathlib.Path) -> typing.Generator[pathlib.Path, None, None]:
+    """
+    Get the list of shared object dependencies.
+
+    It assumes that ``ldd`` output follows::
+
+        libname => /path/to/lib.so (address)
+    """
+    return (
+        pathlib.Path(match.group(1))
+        for lib in popen_stream(args=('ldd', file))
+        if (match := re.search(r'[A-Za-z0-9.\+_\-]+ => ([A-Za-z0-9.\+_\-/]+) \(0x[0-9a-f]+\)', lib)) is not None
+    )
