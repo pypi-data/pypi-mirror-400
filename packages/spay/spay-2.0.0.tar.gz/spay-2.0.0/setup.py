97dfcdc1399bd884074c3000d032a523e602a05f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 09:10:19 +0000

    Add package spay/2.0.0/spay-2.0.0.tar.gz

---
+++ packages/spay/spay-2.0.0.tar.gz/spay-2.0.0/setup.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+from setuptools import setup, find_packages
+
+# Read the README file for long_description
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text(encoding="utf-8")
+
+setup(
+    name="spay",
+    version="2.0.0",
+    author="Saba Pardazesh",
+    author_email="info@spay.ir",
+    description="Python SDK for Spay internet gateway",
+    packages=["spay"],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    python_requires=">=3.8",
+    install_requires=["requests>=2.31.0"],
+)
---
+++ packages/spay/spay-2.0.0.tar.gz/spay-2.0.0/spay/configs.py
@@ -0,0 +1,3 @@
+TOKEN_URL = "https://my.spay.ir/api/ipg/get-ipg-token/"
+REQUEST_PAYMENT_URL = "https://my.spay.ir/api/ipg/reqeust-payment/"
+VERIFY_PAYMENT_URL = "https://my.spay.ir/api/ipg/verify-payment/"
