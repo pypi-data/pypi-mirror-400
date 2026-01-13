12aa7b0232d77fde74002db3361ddd1f1d0ab5ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 09:09:59 +0000

    Add package uploadoss/0.4.3/uploadoss-0.4.3.tar.gz

---
+++ packages/uploadoss/uploadoss-0.4.3.tar.gz/uploadoss-0.4.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import codecs
+
+# with open('README.md') as f:
+#     long_description = f.read()
+
+setup(
+    name='uploadoss',
+    version='0.4.3',
+    author='Binghe',
+    description='Uploadoss is the general module of uploading database data and local files to the Alibaba Cloud OSS',
+    packages=find_packages(),
+    install_requires=[
+        'oss2',
+        'pandas',
+        'pymysql',
+        'configparser',
+        'oracledb',
+        'gzip'
+    ],
+    project_urls={  
+        'Source': 'https://github.com/binghexmo/uploadoss/',   
+    },
+    long_description=(codecs.open("README.md", encoding='utf-8').read()),
+    long_description_content_type="text/markdown"
+)
+
