67e3e95efffabc8accf3e555c4103d4676ad3ec1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 21:05:48 +0000

    Add package migratore/0.5.9/migratore-0.5.9.tar.gz

---
+++ packages/migratore/migratore-0.5.9.tar.gz/migratore-0.5.9/src/migratore/info.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+NAME = "migratore"
+VERSION = "0.5.9"
+AUTHOR = "Hive Solutions Lda."
+EMAIL = "development@hive.pt"
+DESCRIPTION = "Migratore Infra-structure"
+LICENSE = "Apache Software License"
+KEYWORDS = "migratore migration sql"
+URL = "http://migratore.hive.pt"
+COPYRIGHT = "2008-2023 Hive Solutions Lda."
