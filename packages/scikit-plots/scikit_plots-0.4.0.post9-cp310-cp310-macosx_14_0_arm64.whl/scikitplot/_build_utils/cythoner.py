428fdad04f3b2e8c1080b214d559621f15c3633b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 18:10:23 +0000

    Add package scikit-plots/0.4.0.post9/scikit_plots-0.4.0.post9-cp310-cp310-macosx_14_0_arm64.whl

---
+++ packages/scikit-plots/scikit_plots-0.4.0.post9-cp310-cp310-macosx_14_0_arm64.whl/scikitplot/_build_utils/cythoner.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+# Authors: The scikit-plots developers
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""
+Scipy variant of Cython command
+
+Cython, as applied to single pyx file.
+
+Expects two arguments, infile and outfile.
+
+Other options passed through to cython command line parser.
+"""
+
+import os
+import subprocess
+import sys
+
+
+def main():
+    f_in, f_out = (os.path.abspath(p) for p in sys.argv[1:3])
+    subprocess.run(
+        [
+            "cython",
+            "-3",
+            "--fast-fail",
+            "--output-file",
+            f_out,
+            "--include-dir",
+            os.getcwd(),
+        ]
+        + sys.argv[3:]
+        + [f_in],
+        check=True,
+    )
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/scikit-plots/scikit_plots-0.4.0.post9-cp310-cp310-macosx_14_0_arm64.whl/scikitplot/_compat/numpycompat.py
@@ -0,0 +1,3 @@
+from __future__ import annotations
+
+from ..cexternals._astropy.utils.compat.numpycompat import *  # noqa: F403
