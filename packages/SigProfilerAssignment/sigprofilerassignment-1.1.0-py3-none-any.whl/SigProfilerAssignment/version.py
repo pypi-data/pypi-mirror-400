ce22482c1192ea5798e96b811046f3478a440103
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 18:11:17 +0000

    Add package SigProfilerAssignment/1.1.0/sigprofilerassignment-1.1.0-py3-none-any.whl

---
+++ packages/SigProfilerAssignment/sigprofilerassignment-1.1.0-py3-none-any.whl/SigProfilerAssignment/version.py
@@ -0,0 +1,7 @@
+
+# THIS FILE IS GENERATED FROM SigProfilerAssignment SETUP.PY
+short_version = '1.1.0'
+version = '1.1.0'
+Update = 'v1.1.0: Support for COSMIC v3.5 signatures and updated Tobacco_signatures subgroup (SBS100, SBS109)'
+
+    
