a868a8b8db7cf2cc481e30e15351f5d54876dd88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 18:11:23 +0000

    Add package fossyl/0.6.2/fossyl-0.6.2-py3-none-any.whl

---
+++ packages/fossyl/fossyl-0.6.2-py3-none-any.whl/fossyl/__init__.py
@@ -0,0 +1,7 @@
+from .mcspec import mcspec, obspec, gridinter, statmc, prior, log_posterior, specmc, mcbest, mcplot
+from .h5grid import h5grid
+from .gridfit import gridfit, obspec, chisquare
+
+__version__ = '0.6.2'
+__author__ = 'Hunter Brooks'
+__email__ = 'hcb98@nau.edu'
