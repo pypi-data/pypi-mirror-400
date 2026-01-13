bafb7687a291a8cd43006a3a0b8a2ec0f14be675
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 16:07:13 +0000

    Add package lookit/0.1.1/lookit-0.1.1.tar.gz

---
+++ packages/lookit/lookit-0.1.1.tar.gz/lookit-0.1.1/src/lookit/__init__.py
@@ -0,0 +1,19 @@
+"""Lookit - Vision-based GUI automation tool."""
+
+from lookit.tools.computer_use import (
+    ComputerUseInput,
+    ComputerUseTool,
+)
+from lookit.tools.mobile_use import (
+    MobileUseInput,
+    MobileUseTool,
+)
+
+__version__ = "0.1.0"
+
+__all__ = [
+    "ComputerUseTool",
+    "ComputerUseInput",
+    "MobileUseTool",
+    "MobileUseInput",
+]
