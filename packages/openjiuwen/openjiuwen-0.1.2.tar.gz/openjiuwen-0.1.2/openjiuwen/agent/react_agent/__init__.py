b160331dbde9349652ef19c5d755cde3d1d4bb4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 11:05:38 +0000

    Add package openjiuwen/0.1.2/openjiuwen-0.1.2.tar.gz

---
+++ packages/openjiuwen/openjiuwen-0.1.2.tar.gz/openjiuwen-0.1.2/openjiuwen/agent/react_agent/__init__.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+# Copyright (c) Huawei Technologies Co., Ltd. 2025. All rights reserved.
+"""
+ReAct Agent - Minimal version (no interruption, no Controller)
+"""
+
+from openjiuwen.agent.react_agent.react_agent import (
+    ReActAgent,
+    create_react_agent_config
+)
+
+__all__ = [
+    "ReActAgent",
+    "create_react_agent_config"
+]
---
+++ packages/openjiuwen/openjiuwen-0.1.2.tar.gz/openjiuwen-0.1.2/openjiuwen/agent_group/__init__.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+# Copyright (c) Huawei Technologies Co., Ltd. 2025. All rights reserved.
+"""AgentGroup 实现模块"""
+
+from openjiuwen.agent_group.hierarchical_group import (
+    HierarchicalGroup,
+    HierarchicalGroupConfig,
+    HierarchicalGroupController,
+)
+
+__all__ = [
+    'HierarchicalGroup',
+    'HierarchicalGroupConfig',
+    'HierarchicalGroupController',
+]
