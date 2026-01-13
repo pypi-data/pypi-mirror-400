e171323085deb85b66173106c2afdb33f8ea6e50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 22:06:47 +0000

    Add package reprospect/1.0.2/reprospect-1.0.2-cp313-cp313-win_amd64.whl

---
+++ packages/reprospect/reprospect-1.0.2-cp313-cp313-win_amd64.whl/reprospect/installers/nsight_systems.py
@@ -0,0 +1,74 @@
+import argparse
+import logging
+import os
+import re
+import shutil
+import subprocess
+import typing
+
+import semantic_version
+import system_helpers.apt.install
+
+from reprospect.utils import nvcc
+
+PACKAGE: typing.Final[str] = 'cuda-nsight-systems'
+
+def parse_args() -> argparse.Namespace:
+    """
+    Parse CLI arguments.
+    """
+    parser = argparse.ArgumentParser(
+        description="Install Nsight Systems with apt.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+
+    parser.add_argument('--version', type=str, required=False, help=f'Version of the package ({PACKAGE}) to install.', default=detect_cuda_version())
+
+    return parser.parse_args()
+
+def install(*, args: argparse.Namespace) -> None:
+    """
+    Install `Nsight Systems` through `apt`.
+    """
+    package = PACKAGE + '-' + args.version
+    logging.info(f'Installing nsight-systems with {package}.')
+    system_helpers.apt.install.install_packages(
+        packages=[package],
+        update=True, clean=True,
+    )
+
+def detect_cuda_version() -> str:
+    """
+    Detect CUDA version using the following strategies:
+
+    #. ``CUDA_VERSION`` environment variable
+    #. ``nvidia-smi --query``
+    #. ``nvcc --version``
+    """
+    if 'CUDA_VERSION' in os.environ:
+        version = semantic_version.Version(os.environ['CUDA_VERSION'])
+        return f'{version.major}-{version.minor}'
+
+    if shutil.which('nvidia-smi') is not None:
+        for line in subprocess.check_output(('nvidia-smi', '--version')).decode().splitlines():
+            if "CUDA Version" in line and (matched := re.search(pattern=r'([0-9]+).([0-9]+)', string=line)) is not None:
+                return f'{matched.group(1)}-{matched.group(2)}'
+
+    if shutil.which('nvcc') is not None:
+        version = nvcc.get_version()
+        return f'{version.major}-{version.minor}'
+
+    raise RuntimeError("Could not deduce CUDA version.")
+
+def main() -> None:
+
+    logging.basicConfig(level=logging.INFO)
+
+    args = parse_args()
+    logging.info(f"Received arguments: {args}")
+
+    install(args=args)
+
+if __name__ == "__main__":
+
+    main()
---
+++ packages/reprospect/reprospect-1.0.2-cp313-cp313-win_amd64.whl/reprospect/utils/rich_helpers.py
@@ -0,0 +1,90 @@
+import abc
+import typing
+
+import pandas
+import rich.console
+import rich.table
+import rich.tree
+import rich_tools
+
+
+def to_string(
+    ro: rich.table.Table | rich.tree.Tree,
+    *,
+    width: int = 200,
+    no_wrap: bool = True,
+    **kwargs,
+) -> str:
+    """
+    Use :py:class:`rich.console.Console` in capture mode to render a :py:mod:`rich`
+    object to a string.
+    """
+    with rich.console.Console(width=width, **kwargs) as console, console.capture() as capture:
+        console.print(ro, no_wrap=no_wrap)
+    return capture.get()
+
+def ds_to_table(ds: pandas.Series) -> rich.table.Table:
+    """
+    Convert a :py:class:`pandas.Series` to a :py:class:`rich.table.Table`.
+    """
+    rt = rich.table.Table()
+    for k in ds.index:
+        rt.add_column(str(k))
+    rt.add_row(*(str(v) for v in ds.values))
+    return rt
+
+def df_to_table(
+    df: pandas.DataFrame,
+    *,
+    rich_table: rich.table.Table | None = None,
+    show_index: bool = False,
+    **kwargs,
+) -> rich.table.Table:
+    """
+    Convert a :py:class:`pandas.DataFrame` to a :py:class:`rich.table.Table`.
+
+    .. note:
+
+        This wrapper around the equivalent function from the `rich-tools` package
+        can be avoided once an issue with their function is resolved:
+
+        * https://github.com/avi-perl/rich_tools/issues/10
+    """
+    if rich_table is None:
+        rich_table = rich.table.Table()
+
+    return rich_tools.df_to_table(df, rich_table=rich_table, show_index=show_index, **kwargs)
+
+class TableMixin(metaclass=abc.ABCMeta):
+    """
+    Define :py:meth:`__str__` based on the :py:class:`rich.table.Table` representation from :py:meth:`to_table`.
+    """
+    @abc.abstractmethod
+    def to_table(self) -> rich.table.Table:
+        """
+        Convert to a :py:class:`rich.table.Table`.
+        """
+
+    @typing.final
+    def __str__(self) -> str:
+        """
+        Use :py:class:`rich.console.Console` in capture mode.
+        """
+        return to_string(self.to_table())
+
+class TreeMixin(metaclass=abc.ABCMeta):
+    """
+    Define :py:meth:`__str__` based on the :py:class:`rich.tree.Tree` representation from :py:meth:`to_tree`.
+    """
+    @abc.abstractmethod
+    def to_tree(self) -> rich.tree.Tree:
+        """
+        Convert to a :py:class:`rich.tree.Tree`.
+        """
+
+    @typing.final
+    def __str__(self) -> str:
+        """
+        Use :py:class:`rich.console.Console` in capture mode.
+        """
+        return to_string(self.to_tree())
