a92b7f2aae10cb33b0d45bb232ec60beb380e652
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 04:19:51 +0000

    Add package fiuai-sdk-python/0.6.4/fiuai_sdk_python-0.6.4.tar.gz

---
+++ packages/fiuai-sdk-python/fiuai_sdk_python-0.6.4.tar.gz/fiuai_sdk_python-0.6.4/src/fiuai_sdk_python/__init__.py
@@ -0,0 +1,24 @@
+from .client import FiuaiSDK, get_client
+from .util import init_fiuai
+from .profile import UserProfileInfo
+from .type import UserProfile
+from .context import (
+    RequestContext, 
+    get_current_headers,
+    validate_current_context,
+    get_current_missing_fields,
+    is_current_context_valid
+)
+
+__all__ = [
+    'FiuaiSDK',
+    'init_fiuai',
+    'get_client',
+    'UserProfileInfo',
+    'UserProfile',
+    'RequestContext',
+    'get_current_headers',
+    'validate_current_context',
+    'get_current_missing_fields',
+    'is_current_context_valid'
+    ]
---
+++ packages/fiuai-sdk-python/fiuai_sdk_python-0.6.4.tar.gz/fiuai_sdk_python-0.6.4/src/fiuai_sdk_python/const.py
@@ -0,0 +1,17 @@
+# -- coding: utf-8 --
+# Project: fiuaiclient
+# Created Date: 2025 07 We
+# Author: liming
+# Email: lmlala@aliyun.com
+# Copyright (c) 2025 FiuAI
+
+
+V2_GET_LIST_URI = "/document"
+
+
+ASSISTANT_USER = "assistant@fiuai.com"
+
+DEFAULT_TENANT_ID = "8888888888888888888"
+PASSIVE_TENANT_ID = "7777777777777777777"
+
+
