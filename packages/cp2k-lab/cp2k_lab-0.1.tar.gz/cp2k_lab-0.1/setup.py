8b2efc7844ff6ddb6c74338b13c76230b624d30d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 05:07:38 +0000

    Add package cp2k-lab/0.1/cp2k_lab-0.1.tar.gz

---
+++ packages/cp2k-lab/cp2k_lab-0.1.tar.gz/cp2k_lab-0.1/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+  name = 'cp2k-lab',
+  packages = ['cp2klab'],
+  version = '0.1',
+  author = 'Ole Schuett',
+  author_email = 'ole.schuett@cp2k.org',
+  url = 'https://lab.cp2k.com',
+)
