cf04068f88a951afe4483911b04fc617f1e4f35d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 21:06:56 +0000

    Add package canviewer/1.5.0/canviewer-1.5.0.tar.gz

---
+++ packages/canviewer/canviewer-1.5.0.tar.gz/canviewer-1.5.0/README.md
@@ -0,0 +1,151 @@
+# Canviewer
+
+-----
+
+**Table of Contents**
+
+- [Installation](#installation)
+- [Usage](#usage)
+  - [Filtering](#filtering)
+  - [Pagination](#pagination)
+  - [Plotting signals](#plotting-signals)
+  - [Live commands](#live-commands)
+    - [Taking snapshots](#taking-snapshots)
+    - [Navigating](#navigating)
+    - [Zooming in and out](#zooming-in-and-out)
+- [Other utilities](#other-utilities)
+  - [canviewer-jsonify](#canviewer-jsonify)
+- [License](#license)
+
+# Installation
+
+- **From PyPi**
+This package is available on PyPi and can be directly installed with:
+`pip install canviewer`
+**Requires python3.12+.**
+
+- **From source**:<br>
+Clone this repository and simply run `pip install .` at the root (preferrably in a virtual environment). This tool uses hatchling backend for building, you can consider using hatch as a frontend (`hatch shell`).<br>
+
+# Usage
+
+After installing, you can summon this utility with `canviewer`:
+
+```shell
+Usage: canviewer [OPTIONS]
+
+  For every CAN ID found on the CAN bus, displays the data for the last
+  message received. If the message is declared in one of the passed databases,
+  shows the decoded data.
+
+Options:
+  -c, --channel TEXT              Name of the CAN channel to monitor
+  -d, --driver TEXT               Specifies which CAN driver to use if
+                                  multiple available
+  -db, --databases TEXT           Path to .kcd files or to a folder containing
+                                  kcd files
+  -f, --filters TEXT              Either a name or a numeric ID, only passed
+                                  messages will be displayed
+  -s, --single-message TEXT       Tracks a single message, shows a custom
+                                  table with one column per signal
+  -i, --ignore-unknown-messages   Hides messages that are not declared in one
+                                  of your databases
+  -r, --record-signals TEXT       Records the values for a given signal,
+                                  exports them to CSV on exiting. You shall
+                                  pass your target signal as
+                                  message_name.signal_name
+  -n, --inline                    Disables full-screen
+  -sf, --snapshot-format [json|csv]
+                                  Format to use for snapshots
+  --help                          Show this message and exit.
+
+
+```
+
+`canviewer` will show the latest data for every message received on your CAN bus.<br>
+You can pass `dbc` or `kcd` databases to the tool if you want it to decode your data as follows:
+
+```
+canviewer -db path/to/afolder path/to/a_database.kcd
+```
+
+When passing a folder, `dbc` or `kcd` files will be automatically discovered in the folder. You can pass many items after the `-db` flag.<br>
+
+If omitting the CAN channel the tool will use the default on your system: `can0` on Linux and `PCAN_USBBUS1` on Windows. You can specify the channel with `-c` flag. Same applies to the CAN driver, which can be passed with `-d`.
+
+## Filtering
+
+You can filter out unknown messages by passsing `-i`.<br>
+You can only display some selected messages by passing their name or ID to `-f` flag (flag can be passed multiple times):
+
+```
+canviewer -db my_db.kcd -f My_Message_Name
+```
+
+## Plotting signals
+
+You can ask `canviewer` to plot one or multiple signals in real-time by using the `pl` (`--plot`) flag. It should be apssed as`-pl message_name.signal_name` and you can pass this flag as many times as you want (one plot will be spawned per signal). The signal will also be recorded to csv file. Note that if you'd like to only record without plotting, use `-r` instead.
+
+## Pagination
+
+If the tables cannot fit on your screen, it will be splitted in multiple tables. You can press enter to navigate (enter goes forwards and `b` + enter goes backward, see live commands below).
+
+## Live commands
+
+There are a few interactive commands you can use whil canviewer is running. Not that all commands require to press enter (*to flush stdin*) to be applied, as `canviewer` is a simple textual program and not a full-fledged user interface.
+
+### Taking snapshots
+
+While running, press `s` + ENTER to take a snapshot. The snapshot will contain a dump of the current value for every known signal of the loaded databases. Your filters will be applied. You may select the snapshot format when starting the utility through the `-sf/--snapshot-format` flag (`csv` or `json` supported).<br>
+The snapshot is created in your current working directory and will be named `snapshot_canviewer_{datetime}.{format}`.
+
+### Navigating
+
+To navigate through pages, you can:
+- Use an empty string (`[ENTER]`) to go one page forward
+- Use `b` (`b + [ENTER]`) to go one page backward
+- type any page number to go to that page directly
+
+### Zooming in and out
+
+Use `+` and `-` to decrease or increase the number of lines per page.
+
+# Other utilities
+
+## canviewer-jsonify
+
+*Linux-only*
+
+This is an alternate entrypoint you can call with `canviewer-jsonify`. This command expects to receive the path to a database file, and will spawn one JSON file per message in a temporary folder (note: everything will be wiped on exiting the command).
+This command monitors the can bus actively and does the following:
+- **RX**: everytime a message is received, the corresponding JSON file will be updated with the new values. You can watch them in real time.with `tail -f` or similar commands.
+- **TX**: Whenever a JSON file for TX message is edited manually, the new values will be sent automatically to the CAN bus.
+
+> [!WARNING]
+> Modifications on JSON files are monitored using inotify. Any message received on the bus that's not sent by this command itself will be considered RX and filtered out from inotify monitoring, so modifying them manually will not trigger a message send.
+
+```shell
+Usage: canviewer-jsonify [OPTIONS] DATABASE
+
+  database: Path to the database to JSONify
+
+Options:
+  -c, --channel TEXT              Name of the CAN channel to monitor
+  -l, --log-level [critical|fatal|error|warn|warning|info|debug|notset]
+                                  Log level to apply
+  --help                          Show this message and exit.
+```
+
+ (e.g.) If calling `canviewer-jsonify my_database.kcd` you should get something like this:
+
+ ```shell
+Path to model:
+/tmp/tmp0zkw00sm
+Use Ctrl + C to leave
+ ```
+
+Simply `cd` to the displayed path and you should find all the JSON files for the messages in the passed database there. Files should be named following the schema `{message_name}.json`.
+
+# License
+
+`canviewer` is distributed under the terms of the [MIT](https://spdx.org/licenses/MIT.html) license.
---
+++ packages/canviewer/canviewer-1.5.0.tar.gz/canviewer-1.5.0/src/canviewer/_entrypoints.py
@@ -0,0 +1,569 @@
+"""
+Application entrypoint(s) for this package.
+CLI tools are based on click and will be installed
+automatically as package scripts.
+
+@date: 04.10.2024
+@author: Baptiste Pestourie
+"""
+
+from __future__ import annotations
+
+import asyncio
+import json
+import logging
+import os
+import sys
+import time
+from dataclasses import dataclass, field
+from datetime import datetime
+from enum import Enum, auto
+
+# built-in
+from typing import IO, Callable, Iterable, Iterator, Literal
+
+import asciichartpy as acp
+import can
+import cantools
+
+# 3rd-party
+import click
+import pyperclip
+import rich
+from cantools.database.can import Database
+from exhausterr import Err, Ok
+from rich.console import Console, Group
+from rich.live import Live
+
+from ._console import MessageTable
+from ._jsonify import JsonModel, ModelConfig
+
+# Local
+from ._monitor import (
+    CanMonitor,
+    get_platform_default_channel,
+    get_platform_default_driver,
+)
+from ._utils import CanIdPattern, InvalidPattern, convert_pattern_to_mask
+
+_logger = logging.getLogger(__name__)
+
+# Number of lines for the actual table
+HEIGHT_MARGIN: int = 10
+WIDTH_MARGIN: int = 10
+DEFAULT_HEIGHT = 30
+ZOOM_FACTOR = 1.1
+PLOT_MAX_SIZE = 50
+
+
+class UserCommands(Enum):
+    """
+    All the commands which can be received from the user
+    """
+
+    TAKE_SNAPSHOT = auto()
+
+
+@dataclass
+class UserInterface:
+    """
+    Manages the interaction with the user
+    """
+
+    page_index: int = 0
+    total_pages: int = 1
+    dispatcher: dict[UserCommands, Callable[[], None]] = field(default_factory=dict)
+    log: str = ""
+    height: int = DEFAULT_HEIGHT
+
+    def on_input(self, stream: IO[str]) -> None:
+        """
+        Process user input from the given stream
+        """
+        # clearing previous log
+        self.log = ""
+        command = stream.readline().strip()
+        match command:
+            case "":
+                self.page_index = (self.page_index + 1) % self.total_pages
+
+            case "b":
+                self.page_index = (self.page_index - 1) % self.total_pages
+
+            case "s":
+                if (cmd := self.dispatcher.get(UserCommands.TAKE_SNAPSHOT)) is not None:
+                    match cmd():
+                        case Ok(_):
+                            self.log = "[green]Took snapshot successfully !"
+                        case Err(err):
+                            self.log = str(err)
+
+            # zoom in / zoom out commands
+            case "+" | "++" | "+++":
+                zoom_factor = ZOOM_FACTOR ** len(command)
+                self.height = round(self.height / zoom_factor)
+
+            case "-" | "--" | "---":
+                zoom_factor = ZOOM_FACTOR ** len(command)
+                self.height = round(self.height * zoom_factor)
+
+            case _:
+                if command.isnumeric():
+                    try:
+                        idx = int(command) - 1
+                    except ValueError:
+                        # ignoring
+                        return
+
+                    if 0 <= idx < self.total_pages:
+                        self.page_index = idx
+                else:
+                    self.log = f"[red]Unknown command: {command}"
+
+    def page_indication(self) -> str:
+        return (
+            f"Page {self.page_index + 1}/{self.total_pages}"
+            " (Press enter go to next page)"
+        )
+
+
+async def _canviewer(
+    channel: str,
+    driver: str,
+    databases: Iterable[Database],
+    ignore_unknown_messages: bool,
+    message_filters: Iterable[int | str],
+    single_message: str | None = None,
+    record_signals: list[str] = [],
+    plot_signals: list[str] = [],
+    inline: bool = False,
+    snapshot_type: Literal["csv", "json"] = "csv",
+    mask: int = 0xFFFF_FFFF,
+    id_pattern: CanIdPattern | int | None = None,
+    always_show_value: bool = False,
+) -> None:
+    """
+    Main asynchronous runner for the console application.
+
+    Parameters
+    ----------
+    channel: str
+        The name of the CAN channel to monitor
+
+    driver: str
+        The name of the CAN driver (interface)
+
+    databases: Iterable[Database]
+        The paths to .kcd files or to a folder containing kcd files
+    """
+    message_table = MessageTable(
+        ignore_unknown_messages=ignore_unknown_messages, filters=message_filters
+    )
+    console = Console()
+    loop = asyncio.get_event_loop()
+    started = datetime.now()
+
+    def on_snapshot() -> Ok[None]:
+        dict_data = message_table.take_snapshot()
+        fname = (
+            "snapshot_canviewer_"
+            + started.strftime("%Y_%m_%d_%H_%M_%S")
+            + "."
+            + snapshot_type
+        )
+        now = time.time()
+        if not os.path.exists(fname) and snapshot_type == "csv":
+            with open(fname, "w+") as f:
+                header = ",".join(("timestamp", *dict_data.keys()))
+                f.write(header + "\n")
+        # converting to CSV
+        with open(fname, "a+") as f:
+            match snapshot_type:
+                case "json":
+                    dict_data["timestamp"] = now
+                    f.write(json.dumps(dict_data, default=str, indent=4))
+
+                case "csv":
+                    values = ",".join((str(now), *[str(v) for v in dict_data.values()]))
+                    f.write(values + "\n")
+        return Ok()
+
+    for message_signal in (*record_signals, *plot_signals):
+        if not message_table.start_plot(message_signal):
+            click.echo(f"Invalid message signal: {message_signal}")
+            return
+
+    with can.Bus(interface=driver, channel=channel) as bus:
+        with Live(console=console, screen=not inline) as live:
+            interface = UserInterface(height=console.size.height)
+
+            # registering commands
+            interface.dispatcher[UserCommands.TAKE_SNAPSHOT] = on_snapshot
+            loop.add_reader(sys.stdin, interface.on_input, sys.stdin)
+            backend = CanMonitor(
+                bus,
+                *databases,
+                mask=mask,
+                id_pattern=id_pattern,
+                always_show_value=always_show_value,
+            )
+            try:
+                while True:  # Ctrl + C to leave
+                    message = await backend.queue.get()
+                    message_table.update(message)
+
+                    page_height = interface.height - HEIGHT_MARGIN
+                    page_width = console.size.width - WIDTH_MARGIN
+                    interface.total_pages = message_table.set_page_dimensions(
+                        page_width, page_height
+                    )
+                    if single_message is not None:
+                        renderable_table = message_table.export_single_message(
+                            single_message
+                        )
+                        if renderable_table is None:
+                            continue
+                    else:
+                        renderable_table = message_table.export_paginated(
+                            interface.page_index
+                        )
+
+                    plots = []
+                    for message_signal in plot_signals:
+                        match message_table.get_plot_by_name(message_signal):
+                            case Ok(data):
+                                _, y = data
+                                plots.append(
+                                    Group(
+                                        message_signal,
+                                        acp.plot(y[:PLOT_MAX_SIZE]),
+                                    )
+                                )
+
+                            case Err(error):
+                                interface.log = f"[red]{error}"
+
+                    if interface.total_pages > 1 and single_message is None:
+                        renderable = Group(
+                            interface.page_indication(),
+                            interface.log,
+                            *plots,
+                            renderable_table,
+                        )
+                    else:
+                        renderable = Group(*plots, renderable_table)
+
+                    live.update(renderable)
+
+            finally:
+                csv_paths = message_table.export_plots_to_csv()
+                if csv_paths:
+                    click.echo(f"CSV files created: {csv_paths}")
+
+
+def collect_databases(*paths: str) -> Iterator[str]:
+    """
+    Based on a selection or folder and/or direct paths to
+    CAN databases, discovers the databases within the folders
+    and yields a flattened list of all discovered databases.
+
+    Yields
+    ------
+    str
+        Path to a KCD or DBC database to loadd
+    """
+    for path in paths:
+        if os.path.isdir(path):
+            yield from (
+                os.path.join(path, f)
+                for f in os.listdir(path)
+                if f.endswith(".kcd") or f.endswith(".dbc")
+            )
+        else:
+            yield path
+
+
+@click.command()
+@click.option(
+    "-c", "--channel", default=None, type=str, help="Name of the CAN channel to monitor"
+)
+@click.option(
+    "-d",
+    "--driver",
+    default=None,
+    type=str,
+    help="Specifies which CAN driver to use if multiple available",
+)
+@click.option(
+    "-db",
+    "--databases",
+    default=(),
+    type=str,
+    multiple=True,
+    help="Path to .kcd files or to a folder containing kcd files",
+)
+@click.option(
+    "-f",
+    "--filters",
+    default=(),
+    type=str,
+    multiple=True,
+    help="Either a name or a numeric ID, only passed messages will be displayed",
+)
+@click.option(
+    "-s",
+    "--single-message",
+    type=str,
+    default=None,
+    help="Tracks a single message, shows a custom table with one column per signal",
+)
+@click.option(
+    "-i",
+    "--ignore-unknown-messages",
+    is_flag=True,
+    help="Hides messages that are not declared in one of your databases",
+)
+@click.option(
+    "-r",
+    "--record-signals",
+    multiple=True,
+    type=str,
+    help=(
+        "Records the values for a given signal, exports them to CSV on exiting.\n"
+        "If you'd like to also plot the signal in realtime, use -pl/--plot instead\n"
+        "You shall pass your target signal as message_name.signal_name"
+    ),
+)
+@click.option(
+    "-pl",
+    "--plot-signals",
+    multiple=True,
+    type=str,
+    help=(
+        "Plots the values for a given signal, exports them to CSV on exiting.\n"
+        "You shall pass your target signal as message_name.signal_name"
+    ),
+)
+@click.option(
+    "-n",
+    "--inline",
+    is_flag=True,
+    help="Disables full-screen",
+)
+@click.option(
+    "-sf",
+    "--snapshot-format",
+    type=click.Choice(["json", "csv"]),
+    default="csv",
+    help="Format to use for snapshots",
+)
+@click.option(
+    "-mk",
+    "--mask",
+    type=str,
+    default="FFFFFFFF",
+    help="Applies this mask on CAN IDs before feeding to decoder",
+)
+@click.option(
+    "-p",
+    "--pattern",
+    type=str,
+    default=None,
+    help="Filter in all the messages following that pattern",
+)
+@click.option(
+    "-asv",
+    "--always-show-value",
+    is_flag=True,
+    help="Shows the value instead of the name for signals using named values",
+)
+def canviewer(
+    channel: str | None,
+    driver: str | None,
+    databases: Iterable[str],
+    filters: Iterable[str],
+    single_message: str | None,
+    ignore_unknown_messages: bool,
+    record_signals: list[str],
+    plot_signals: list[str],
+    inline: bool,
+    snapshot_format: Literal["json", "csv"],
+    mask: str,
+    pattern: str | None,
+    always_show_value: bool,
+) -> None:
+    """
+    For every CAN ID found on the CAN bus,
+    displays the data for the last message received.
+    If the message is declared in one of the passed databases,
+    shows the decoded data.
+    """
+
+    try:
+        id_pattern = convert_pattern_to_mask(pattern) if pattern else None
+    except InvalidPattern as exc:
+        raise click.BadParameter(str(exc), param_hint="pattern") from exc
+
+    if channel is None:
+        match get_platform_default_channel():
+            case Ok(channel_name):
+                channel = channel_name
+            case Err(error):
+                click.echo(str(error))
+                return
+
+    if driver is None:
+        match get_platform_default_driver():
+            case Ok(driver_name):
+                driver = driver_name
+            case Err(error):
+                click.echo(str(error))
+                return
+    converted_filters: list[int | str] = [
+        int(f) if f.isnumeric() else f for f in filters
+    ]
+    loaded_dbs: Iterable[Database] = map(
+        cantools.database.load_file,  # type: ignore[arg-type]
+        collect_databases(*databases),
+    )
+    asyncio.run(
+        _canviewer(
+            channel,
+            driver,
+            loaded_dbs,
+            ignore_unknown_messages,
+            converted_filters,
+            single_message=single_message,
+            record_signals=record_signals,
+            plot_signals=plot_signals,
+            inline=inline,
+            snapshot_type=snapshot_format,
+            id_pattern=id_pattern,
+            mask=int(mask, 16),
+            always_show_value=always_show_value,
+        )
+    )
+
+
+@click.command()
+@click.argument("database", type=str)
+@click.option(
+    "-c",
+    "--channel",
+    default="can0",
+    type=str,
+    help="Name of the CAN channel to monitor",
+)
+@click.option(
+    "-l",
+    "--log-level",
+    default="ERROR",
+    type=click.Choice(list(logging._nameToLevel), case_sensitive=False),
+    help="Log level to apply",
+)
+# JSON model config options
+@click.option(
+    "-a",
+    "--accumulate",
+    is_flag=True,
+    help=(
+        "When passed, stores all passed values in the message JSON file "
+        "instead of only the last one"
+    ),
+)
+@click.option(
+    "-o",
+    "--output-folder",
+    type=str,
+    help=(
+        "If passed, the temp folder for JSON files will be created in this location.\n"
+        "If not, it will be created somewhere in /tmp"
+    ),
+)
+@click.option(
+    "-p",
+    "--preserve-files",
+    is_flag=True,
+    help=(
+        "Whether the temp folder and its JSON files should be deleted on exit.\n"
+        "Disabled by default."
+    ),
+)
+@click.option(
+    "-t",
+    "--timestamps",
+    is_flag=True,
+    help=(
+        """
+        Writes last reception timestamp in the JSON file for message,
+        in a LAST_RECEIVED key.
+        """
+    ),
+)
+@click.option(
+    "-no-rx",
+    "--disable-rx",
+    is_flag=True,
+    help=(
+        "Does not monitor RX messages and uses the model only to send message. "
+        "Can be used when trying to override RX messages, "
+        "as they are by default not overwritable when being monitored"
+    ),
+)
+def canviewer_jsonify(
+    database: str,
+    channel: str,
+    log_level: str,
+    accumulate: bool,
+    output_folder: str,
+    preserve_files: bool,
+    timestamps: bool,
+    disable_rx: bool,
+) -> None:
+    """
+    database: Path to the database to JSONify
+    """
+    logging.basicConfig(
+        level=logging._nameToLevel[log_level],
+        format="{asctime}: {levelname:<7}: {threadName:<20}: {message}",
+        style="{",
+    )
+    logging.getLogger("inotify").setLevel(logging.ERROR)
+
+    config = ModelConfig(
+        accumulate=accumulate,
+        target_folder=output_folder,
+        preserve_files=preserve_files,
+        enable_timestamping=timestamps,
+    )
+    try:
+        can_db = cantools.database.load_file(database)
+    except FileNotFoundError:
+        rich.print(f"[red]: File does not exist: {database}")
+        return
+
+    def report_error(message_name: str, exc: Exception) -> None:
+        rich.print(f"[red] Values for message {message_name} are incorrect: {exc}")
+
+    assert isinstance(can_db, Database)
+    model = JsonModel(can_db, config=config)
+    with can.interface.Bus(interface="socketcan", channel=channel) as bus:
+        with model.open() as tmp:
+            rich.print(f"Path to model:\n[green]{tmp}")
+            try:
+                pyperclip.copy(tmp)
+                rich.print("[green]> Path has been copied to your clipboard ! ")
+            except Exception as exc:
+                _logger.error(
+                    "Failed to copy tmp path to clipboard: %s", exc, exc_info=True
+                )
+            model.start_inotify_watcher(bus, on_error=report_error)
+            if disable_rx:
+                input("Press any key to cleanup temp files and exit...")
+                return
+
+            rich.print("Use Ctrl + C to leave")
+            while True:
+                next_message = bus.recv()
+                assert next_message is not None  # value can only be None on timeout
+                model.update_model(next_message)
