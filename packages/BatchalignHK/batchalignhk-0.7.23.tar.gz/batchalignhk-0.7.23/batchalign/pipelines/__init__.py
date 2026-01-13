9f142acff648695e6f8e78f1be49f430eae89449
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 07:08:08 +0000

    Add package BatchalignHK/0.7.23/batchalignhk-0.7.23.tar.gz

---
+++ packages/BatchalignHK/batchalignhk-0.7.23.tar.gz/batchalignhk-0.7.23/batchalign/pipelines/__init__.py
@@ -0,0 +1,20 @@
+from .pipeline import BatchalignPipeline
+from .base import BatchalignEngine
+from .asr import (WhisperEngine, RevEngine, WhisperXEngine,
+                  TencentEngine, OAIWhisperEngine, AliyunEngine, FunAudioEngine)
+
+from .morphosyntax import StanzaEngine, CorefEngine
+from .cleanup import NgramRetraceEngine, DisfluencyReplacementEngine
+from .speaker import NemoSpeakerEngine
+
+from .fa import WhisperFAEngine, Wave2VecFAEngine, IICFAEngine, Wave2VecFAEngineCantonese
+from .utr import WhisperUTREngine, RevUTREngine, TencentUTREngine, FunAudioUTREngine
+
+from .analysis import EvaluationEngine
+from .utterance import StanzaUtteranceEngine
+
+from .translate import SeamlessTranslationModel, GoogleTranslateEngine
+from .avqi import AVQIEngine
+
+from .diarization import PyannoteEngine
+from .opensmile import OpenSMILEEngine
