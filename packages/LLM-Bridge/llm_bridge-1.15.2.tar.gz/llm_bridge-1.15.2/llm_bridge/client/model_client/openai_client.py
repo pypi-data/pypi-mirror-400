a49d2684f1115ca9c0904472c141d86ac2c0f85f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 06:05:01 +0000

    Add package LLM-Bridge/1.15.2/llm_bridge-1.15.2.tar.gz

---
+++ packages/LLM-Bridge/llm_bridge-1.15.2.tar.gz/llm_bridge-1.15.2/llm_bridge/client/model_client/openai_client.py
@@ -0,0 +1,39 @@
+from typing import AsyncGenerator, Iterable, Type, Any
+from pydantic import BaseModel
+
+import openai.lib.azure
+from openai.types import Reasoning
+from openai.types.responses import ToolParam
+
+from llm_bridge.client.chat_client import ChatClient
+from llm_bridge.type.chat_response import ChatResponse
+from llm_bridge.type.model_message.openai_message import OpenAIMessage
+from llm_bridge.type.model_message.openai_responses_message import OpenAIResponsesMessage
+
+
+class OpenAIClient(ChatClient):
+    def __init__(
+            self,
+            model: str,
+            messages: list[OpenAIMessage | OpenAIResponsesMessage],
+            temperature: float,
+            api_type: str,
+            client: openai.AsyncOpenAI | openai.lib.azure.AsyncAzureOpenAI,
+            tools: Iterable[ToolParam],
+            reasoning: Reasoning,
+            structured_output_base_model: Type[BaseModel] | None = None,
+    ):
+        self.model = model
+        self.messages = messages
+        self.temperature = temperature
+        self.api_type = api_type
+        self.client = client
+        self.tools = tools
+        self.reasoning = reasoning
+        self.structured_output_base_model = structured_output_base_model
+
+    async def generate_non_stream_response(self) -> ChatResponse:
+        raise NotImplementedError
+
+    async def generate_stream_response(self) -> AsyncGenerator[ChatResponse, None]:
+        raise NotImplementedError
