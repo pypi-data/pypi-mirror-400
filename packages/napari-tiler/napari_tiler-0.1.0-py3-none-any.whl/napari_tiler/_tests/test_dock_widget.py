756188867b2b99558cab1ce926210e010fd91693
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 11:04:58 +0000

    Add package napari-tiler/0.1.0/napari_tiler-0.1.0-py3-none-any.whl

---
+++ packages/napari-tiler/napari_tiler-0.1.0-py3-none-any.whl/napari_tiler/_tests/test_dock_widget.py
@@ -0,0 +1,165 @@
+import numpy as np
+import pytest
+
+import napari_tiler
+from napari_tiler import TilerWidget, MergerWidget
+
+sample_image_data = [
+    (np.random.random((512, 512)), False),  # 2d
+    (np.random.random((5, 512, 512)), False),  # 3d
+    (np.random.random((512, 512, 3)), True),  # rgb
+    (np.random.random((512, 512, 4)), True),  # rgba
+]
+
+
+def test_load_widgets(make_napari_viewer):
+    """Test that napari loads the widget through the plugin manager."""
+    viewer = make_napari_viewer()
+    tiler = TilerWidget(viewer)
+    merger = MergerWidget(viewer)
+    num_dw = len(viewer.window.dock_widgets)
+    viewer.window.add_dock_widget(
+        tiler
+    )
+    assert len(viewer.window.dock_widgets) == num_dw + 1
+    num_dw = len(viewer.window.dock_widgets)
+    viewer.window.add_dock_widget(
+        merger
+    )
+    assert len(viewer.window.dock_widgets) == num_dw + 1
+
+
+
+def test_tiler_widget_no_image(make_napari_viewer):
+    """Test error raised when no image is loaded."""
+    viewer = make_napari_viewer()
+    widget = napari_tiler.tiler_widget.TilerWidget(viewer)
+    viewer.window.add_dock_widget(widget)
+    with pytest.raises(ValueError):
+        widget._run()
+
+
+@pytest.mark.parametrize("image_data,rgb", sample_image_data)
+def test_tiler_widget_default_parameters(make_napari_viewer, image_data, rgb):
+    """Test basic functionality of the tiler widget."""
+    viewer = make_napari_viewer()
+    widget = napari_tiler.tiler_widget.TilerWidget(viewer)
+    viewer.window.add_dock_widget(widget)
+
+    viewer.add_image(image_data, rgb=rgb)
+    num_layers = len(viewer.layers)
+    widget._run()
+    assert len(viewer.layers) == num_layers + 1
+
+
+@pytest.mark.parametrize("image_data,rgb", sample_image_data)
+def test_tiler_widget_generate_preview(make_napari_viewer, image_data, rgb):
+    """Test basic functionality of the tiler widget."""
+    viewer = make_napari_viewer()
+    widget = napari_tiler.tiler_widget.TilerWidget(viewer)
+    viewer.window.add_dock_widget(widget)
+
+    viewer.add_image(image_data, rgb=rgb)
+    num_layers = len(viewer.layers)
+
+    widget.preview_chkb.setChecked(True)
+    assert len(viewer.layers) == num_layers + 1
+    assert "tiler preview" in viewer.layers
+
+    # test changing parameters updates preview
+    old_preview_layer = viewer.layers["tiler preview"].data
+    widget.overlap_dsb.setValue(widget.overlap_dsb.value() + 1)
+    new_preview_layer = viewer.layers["tiler preview"].data
+    assert not np.array_equal(old_preview_layer, new_preview_layer)
+
+    widget.preview_chkb.setChecked(False)
+    assert len(viewer.layers) == num_layers
+    assert "tiler preview" not in viewer.layers
+
+
+def test_tiler_widget_show_hide_constant_input(make_napari_viewer):
+    """Test that constant QSpinBox hides when mode is not 'constant'."""
+    viewer = make_napari_viewer()
+    widget = napari_tiler.tiler_widget.TilerWidget(viewer)
+    viewer.window.add_dock_widget(widget)
+
+    # FIXME this test is broken, isVisible always returns False
+    widget.mode_select.setCurrentIndex(1)
+    assert not widget.constant_dsb.isVisible()
+    assert not widget.constant_lbl.isVisible()
+
+
+def test_tiler_widget_parameter_input_validation(make_napari_viewer):
+    """Test that user input types are corrected to fit Tiler class."""
+    viewer = make_napari_viewer()
+    widget = napari_tiler.tiler_widget.TilerWidget(viewer)
+    viewer.window.add_dock_widget(widget)
+    viewer.add_image(np.random.random((512, 512)))
+
+    widget.overlap_dsb.setValue(0.5)
+    widget._initialize_tiler()
+    assert widget.overlap_dsb.value() == 0.5
+
+    widget.overlap_dsb.setValue(2)
+    widget._initialize_tiler()
+    assert widget.overlap_dsb.value() == 2
+
+    widget.overlap_dsb.setValue(2.1)
+    widget._initialize_tiler()
+    assert widget.overlap_dsb.value() == 2
+
+
+def test_tiler_widget_add_remove_tile_dimensions(make_napari_viewer):
+    """Test that extra tile dimensions can be added and removed."""
+    viewer = make_napari_viewer()
+    widget = napari_tiler.tiler_widget.TilerWidget(viewer)
+    viewer.window.add_dock_widget(widget)
+
+    dims_layout = widget.tile_dims_container.layout()
+    cnt = dims_layout.count()
+    dims_layout.itemAt(0).widget()._add_below()
+    assert dims_layout.count() == cnt + 1
+    dims_layout.itemAt(1).widget()._remove()
+    assert dims_layout.count() == cnt
+
+
+@pytest.mark.parametrize("image_data,rgb", sample_image_data)
+def test_tiler_widget(image_data, rgb, make_napari_viewer):
+    """Test that tiler widget raises an error for too many tile dimensions."""
+    viewer = make_napari_viewer()
+    widget = napari_tiler.tiler_widget.TilerWidget(viewer)
+    viewer.window.add_dock_widget(widget)
+
+    viewer.add_image(image_data, rgb=rgb)
+    widget._run()
+
+    # test raises value error when tile dims > image dims
+    with pytest.raises(ValueError):
+        dims_layout = widget.tile_dims_container.layout()
+        dims_layout.itemAt(0).widget()._add_below()
+        dims_layout.itemAt(0).widget()._add_below()
+        widget._run()
+
+
+@pytest.mark.parametrize("image_data,rgb", sample_image_data)
+def test_merger_widget_default_parameters(image_data, rgb, make_napari_viewer):
+    """Test that merger widget output matches pre-tiled image."""
+    viewer = make_napari_viewer()
+    tiler_widget = napari_tiler.tiler_widget.TilerWidget(viewer)
+    merger_widget = napari_tiler.merger_widget.MergerWidget(viewer)
+    viewer.window.add_dock_widget(tiler_widget)
+    viewer.window.add_dock_widget(merger_widget)
+
+    viewer.add_image(image_data, rgb=rgb)
+    tiler_widget._run()
+    merger_widget.image_select.native.setCurrentIndex(1)
+
+    # merger creates a new layer
+    num_layers = len(viewer.layers)
+    merger_widget._run()
+    assert len(viewer.layers) == num_layers + 1
+
+    # merged layer is same as original
+    merged_image_data = viewer.layers[-1].data
+    np.testing.assert_almost_equal(image_data, merged_image_data)
+
---
+++ packages/napari-tiler/napari_tiler-0.1.0-py3-none-any.whl/napari_tiler/merger_widget.py
@@ -0,0 +1,104 @@
+"""This provides the widgets to make or merge tiles."""
+from typing import TYPE_CHECKING, Optional
+
+from magicgui.widgets import create_widget
+from qtpy.QtCore import QEvent
+from qtpy.QtWidgets import (
+    QComboBox,
+    QFormLayout,
+    QLabel,
+    QPushButton,
+    QVBoxLayout,
+    QWidget,
+)
+from tiler import Merger, Tiler
+
+if TYPE_CHECKING:
+    import napari  # pragma: no cover
+
+
+class MergerWidget(QWidget):
+    """A class for the Merger widget."""
+
+    def __init__(self, viewer: "napari.viewer.Viewer") -> None:
+        """Init the merger class."""
+        super().__init__()
+        # create main layout
+        self.viewer = viewer
+        self.setLayout(QVBoxLayout())
+        # add title
+        title = QLabel("<b>Merge Tiles</b>")
+        self.layout().addWidget(title)
+        # image selection
+        self.image_select = create_widget(
+            annotation="napari.layers.Image", label="image_layer"
+        )
+        # mode selection
+        self.mode_select = QComboBox()
+        self.mode_select.addItems(Merger.SUPPORTED_WINDOWS)
+        # add form to main layout
+        form_layout = QFormLayout()
+        form_layout.setFieldGrowthPolicy(QFormLayout.AllNonFixedFieldsGrow)
+        form_layout.addRow("Image", self.image_select.native)
+        form_layout.addRow("Mode", self.mode_select)
+        self.layout().addLayout(form_layout)
+        # `run` button
+        self.run_btn = QPushButton("Run")
+        self.run_btn.clicked.connect(self._run)
+        self.layout().addWidget(self.run_btn)
+
+    def _initialize_merger(self) -> None:
+        image = self.image_select.value
+        metadata = image.metadata
+        print(metadata)
+        # is_rgb = image.rgb
+        tiler = Tiler(
+            data_shape=metadata["data_shape"],
+            tile_shape=metadata["tile_shape"],
+            overlap=metadata["overlap"],
+            channel_dimension=metadata["channel_dimension"],
+            mode=metadata["mode"],
+            constant_value=metadata["constant_value"],
+        )
+        self._merger = Merger(
+            tiler=tiler, window=self.mode_select.currentText()
+        )
+
+    def _run(self) -> None:
+        # TODO copy over other image data like transform, colormap, ...
+        self._initialize_merger()
+        merger = self._merger
+        image = self.image_select.value
+        metadata = image.metadata
+        is_rgb = image.rgb
+        num_tiles = image.data.shape[0]
+        for i in range(num_tiles):
+            merger.add(i, image.data[i, ...])
+        merged = merger.merge(dtype=image.dtype)
+        self.viewer.add_image(
+            merged,
+            name=f"{image.name} merged",
+            rgb=is_rgb,
+            metadata=metadata,
+            colormap=image.colormap,
+        )
+
+    # thanks to https://github.com/BiAPoL/napari-clusters-plotter/blob/main/napari_clusters_plotter/_measure.py  # noqa
+    def showEvent(self, event: QEvent) -> None:  # noqa: D102
+        super().showEvent(event)
+        self.reset_choices()
+
+    def reset_choices(self, event: Optional[QEvent] = None) -> None:
+        """Repopulate image list."""
+        self.image_select.reset_choices(event)
+
+
+# if __name__ == "__main__":
+#     from napari import Viewer
+
+#     from napari_tiler.tiler_widget import TilerWidget
+
+#     viewer = Viewer()
+#     viewer.open_sample("scikit-image", "cell")
+#     viewer.window.add_dock_widget(TilerWidget(viewer))
+#     viewer.window.add_dock_widget(MergerWidget(viewer))
