9dceb67e83103dcb707e2e9830506341bfcda464
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 05:07:47 +0000

    Add package airbrowser-client/1.13.0/airbrowser_client-1.13.0-py3-none-any.whl

---
+++ packages/airbrowser-client/airbrowser_client-1.13.0-py3-none-any.whl/airbrowser_client/api/__init__.py
@@ -0,0 +1,8 @@
+# flake8: noqa
+
+# import apis into api package
+from airbrowser_client.api.browser_api import BrowserApi
+from airbrowser_client.api.health_api import HealthApi
+from airbrowser_client.api.pool_api import PoolApi
+from airbrowser_client.api.profiles_api import ProfilesApi
+
