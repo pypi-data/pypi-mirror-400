6140bf84837715018f3cf2e8f512372634802bcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 09:09:26 +0000

    Add package phonopy/2.47.1/phonopy-2.47.1-cp314-cp314-win_amd64.whl

---
+++ packages/phonopy/phonopy-2.47.1-cp314-cp314-win_amd64.whl/phonopy/cui/create_force_sets.py
@@ -0,0 +1,288 @@
+"""Utilities to create force sets for main CUI script."""
+
+# Copyright (C) 2020 Atsushi Togo
+# All rights reserved.
+#
+# This file is part of phonopy.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+# * Redistributions of source code must retain the above copyright
+#   notice, this list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright
+#   notice, this list of conditions and the following disclaimer in
+#   the documentation and/or other materials provided with the
+#   distribution.
+#
+# * Neither the name of the phonopy project nor the names of its
+#   contributors may be used to endorse or promote products derived
+#   from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+from __future__ import annotations
+
+import copy
+import os
+
+import numpy as np
+from numpy.typing import NDArray
+
+from phonopy.cui.load_helper import get_nac_params
+from phonopy.file_IO import parse_disp_yaml, write_FORCE_SETS
+from phonopy.interface.calculator import get_calc_dataset, get_calc_dataset_wien2k
+from phonopy.interface.lammps import rotate_lammps_forces
+from phonopy.interface.phonopy_yaml import PhonopyYaml
+from phonopy.structure.atoms import PhonopyAtoms
+from phonopy.structure.dataset import get_displacements_and_forces
+
+
+def create_FORCE_SETS(
+    interface_mode: str | None,
+    force_filenames: list[str | os.PathLike],
+    phpy_yaml: PhonopyYaml | None = None,
+    symmetry_tolerance: float | None = None,
+    wien2k_P1_mode: bool = False,
+    force_sets_zero_mode: bool = False,
+    disp_filename: str | os.PathLike = "phonopy_disp.yaml",
+    force_sets_filename: str | os.PathLike = "FORCE_SETS",
+    save_params: bool = False,
+    log_level: int = 0,
+):
+    """Create FORCE_SETS from phonopy_disp.yaml and calculator output files.
+
+    Reading disp.yaml instead of phonopy_disp.yaml is deprecated.
+
+    """
+    if log_level > 0:
+        if interface_mode:
+            print(f"Calculator interface: {interface_mode}")
+        print(f'Displacements were read from "{disp_filename}".')
+        if disp_filename == "disp.yaml":
+            print("")
+            print("NOTE:")
+            print(
+                "  From phonopy v2.0, displacements are written into "
+                '"phonopy_disp.yaml".'
+            )
+            print(
+                '  "disp.yaml" is still supported for reading except for '
+                "Wien2k interface, "
+            )
+            print("  and this supported will be removed at version 3.")
+            print("")
+        if force_sets_zero_mode:
+            print(
+                f'Forces in "{force_filenames[0]}" are subtracted from forces in all '
+                "other files."
+            )
+
+    if disp_filename == "disp.yaml":
+        disp_dataset, supercell = parse_disp_yaml(
+            filename=disp_filename, return_cell=True
+        )
+        if save_params:
+            raise RuntimeError('"disp.yaml" cannot be used with --save-params option.')
+    elif phpy_yaml is not None:
+        supercell = phpy_yaml.supercell
+        disp_dataset = phpy_yaml.dataset
+    else:
+        raise RuntimeError("Could not read displacement dataset.")
+
+    assert disp_dataset is not None
+
+    if "natom" in disp_dataset:  # type-1 dataset
+        num_atoms = disp_dataset["natom"]
+        num_displacements = len(disp_dataset["first_atoms"])
+        dataset_type = 1
+    elif "displacements" in disp_dataset:  # type-2 dataset
+        num_atoms = disp_dataset["displacements"].shape[1]
+        num_displacements = disp_dataset["displacements"].shape[0]
+        dataset_type = 2
+    else:
+        raise RuntimeError(
+            "Number of atoms could not be retrieved from %s" % disp_filename
+        )
+
+    if log_level > 0:
+        print(f"  Number of supercell files: {len(force_filenames)}")
+        print(f'  Number of displacements in "{disp_filename}": {num_displacements}')
+
+    if "first_atoms" in disp_dataset and not check_number_of_force_files(
+        num_displacements,
+        force_filenames,
+        disp_filename,
+        force_sets_zero_mode=force_sets_zero_mode,
+    ):
+        force_sets = []
+    else:
+        if interface_mode == "wien2k":
+            calc_dataset = get_calc_dataset_wien2k(
+                force_filenames,
+                supercell,
+                disp_dataset,
+                wien2k_P1_mode=wien2k_P1_mode,
+                symmetry_tolerance=symmetry_tolerance,
+                verbose=(log_level > 0),
+            )
+            force_sets = calc_dataset["forces"]
+        else:
+            calc_dataset = get_calc_dataset(
+                interface_mode,
+                num_atoms,
+                force_filenames,
+                verbose=(log_level > 0),
+            )
+            force_sets = calc_dataset["forces"]
+            if "points" in calc_dataset:
+                if force_sets_zero_mode:
+                    range_start = 1
+                else:
+                    range_start = 0
+                assert supercell is not None
+                if filename := check_agreements_of_displacements(
+                    supercell,
+                    disp_dataset,
+                    calc_dataset["points"][range_start:],
+                    force_filenames[range_start:],
+                ):
+                    raise RuntimeError(
+                        "Displacements don't match with atomic positions in "
+                        f'"{filename}".'
+                    )
+                if force_sets_zero_mode:
+                    if check_agreement_of_supercell_positions(
+                        supercell, calc_dataset["points"][0]
+                    ):
+                        raise RuntimeError(
+                            "Supercell doesn't match with atomic positions in "
+                            f'"{force_filenames[0]}".'
+                        )
+
+    if log_level > 0 and "first_atoms" not in disp_dataset:
+        if len(force_filenames) - force_sets_zero_mode * 1 < num_displacements:
+            print("** Number of supercell files is less than displacements. **")
+
+    if interface_mode == "lammps":
+        assert supercell is not None
+        rotate_lammps_forces(force_sets, supercell.cell, verbose=(log_level > 0))
+
+    if force_sets:
+        energies = calc_dataset.get("supercell_energies")
+        if force_sets_zero_mode:
+            force_sets = _subtract_residual_forces(force_sets)
+            if energies is not None:
+                energies = energies[1:]
+
+        if dataset_type == 1:
+            dataset = copy.deepcopy(disp_dataset)
+            for forces, disp in zip(force_sets, dataset["first_atoms"], strict=True):
+                disp["forces"] = forces
+            if energies is not None:
+                for energy, disp in zip(energies, dataset["first_atoms"], strict=True):
+                    disp["supercell_energy"] = energy
+        elif dataset_type == 2:
+            dataset = {
+                "displacements": disp_dataset["displacements"][: len(force_sets)],
+                "forces": force_sets,
+            }
+            if energies is not None:
+                dataset["supercell_energies"] = energies
+
+        if save_params:
+            assert phpy_yaml is not None
+            phpy_yaml.dataset = dataset
+            nac_params = get_nac_params(primitive=phpy_yaml.primitive)
+            if nac_params:
+                phpy_yaml.nac_params = nac_params
+            yaml_filename = "phonopy_params.yaml"
+            with open(yaml_filename, "w") as w:
+                w.write(str(phpy_yaml))
+            if log_level > 0:
+                print(f'"{yaml_filename}" has been created.')
+        else:
+            write_FORCE_SETS(dataset, filename=force_sets_filename)
+            if log_level > 0:
+                print(f'"{force_sets_filename}" has been created.')
+
+    else:
+        if log_level > 0:
+            print("%s could not be created." % force_sets_filename)
+
+
+def check_number_of_force_files(
+    num_displacements: int,
+    force_filenames: list[str | os.PathLike],
+    disp_filename: str | os.PathLike,
+    force_sets_zero_mode: bool = False,
+):
+    """Verify number of supercell force files.
+
+    This function is public because being used from phono3py.
+
+    """
+    if force_sets_zero_mode:
+        num_force_filenames = len(force_filenames) - 1
+    else:
+        num_force_filenames = len(force_filenames)
+
+    if num_displacements != num_force_filenames:
+        print(f"Number of files to be read ({num_force_filenames}) don't match to")
+        print(
+            f'the number of displacements ({num_displacements}) in "{disp_filename}".'
+        )
+        return False
+    else:
+        return True
+
+
+def check_agreements_of_displacements(
+    supercell: PhonopyAtoms,
+    dataset: dict,
+    all_points: list[NDArray],
+    force_filenames: list[str | os.PathLike],
+) -> str | os.PathLike | None:
+    """Check agreements of displacements.
+
+    Length of force_filenames can be less than that of displacements in dataset.
+
+    """
+    displacements = get_displacements_and_forces(dataset)[0][
+        : len(force_filenames)
+    ] @ np.linalg.inv(supercell.cell)
+    for disp, points, filename in zip(
+        displacements, all_points, force_filenames, strict=True
+    ):
+        diff = supercell.scaled_positions + disp - points
+        diff -= np.rint(diff)
+        if (np.linalg.norm(diff @ supercell.cell, axis=1) > 1e-5).any():
+            return filename
+
+
+def check_agreement_of_supercell_positions(
+    supercell: PhonopyAtoms, points: NDArray
+) -> bool:
+    """Check agreement of supercell positions."""
+    diff = supercell.scaled_positions - points
+    diff -= np.rint(diff)
+    return (np.linalg.norm(diff @ supercell.cell, axis=1) > 1e-5).any()
+
+
+def _subtract_residual_forces(force_sets: list[NDArray]) -> list[NDArray]:
+    for i in range(1, len(force_sets)):
+        force_sets[i] -= force_sets[0]
+    return force_sets[1:]
---
+++ packages/phonopy/phonopy-2.47.1-cp314-cp314-win_amd64.whl/phonopy/gruneisen/band_structure.py
@@ -0,0 +1,314 @@
+"""Mode Grueneisen parameter band structure calculation."""
+
+# Copyright (C) 2012 Atsushi Togo
+# All rights reserved.
+#
+# This file is part of phonopy.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+# * Redistributions of source code must retain the above copyright
+#   notice, this list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright
+#   notice, this list of conditions and the following disclaimer in
+#   the documentation and/or other materials provided with the
+#   distribution.
+#
+# * Neither the name of the phonopy project nor the names of its
+#   contributors may be used to endorse or promote products derived
+#   from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+from __future__ import annotations
+
+import gzip
+import lzma
+import sys
+
+import numpy as np
+import yaml
+
+from phonopy.gruneisen.core import GruneisenBase
+from phonopy.physical_units import get_physical_units
+
+
+class GruneisenBandStructure(GruneisenBase):
+    """Class to calculate mode Grueneisen parameter along band structure paths."""
+
+    def __init__(
+        self,
+        paths,
+        dynmat,
+        dynmat_plus,
+        dynmat_minus,
+        delta_strain=None,
+        path_connections=None,
+        labels=None,
+        factor=None,
+    ):
+        """Init method."""
+        super().__init__(
+            dynmat,
+            dynmat_plus,
+            dynmat_minus,
+            delta_strain=delta_strain,
+            is_band_connection=True,
+        )
+        self._cell = dynmat.primitive
+        rec_lattice = np.linalg.inv(self._cell.cell)
+        distance_shift = 0.0
+
+        if factor is None:
+            _factor = get_physical_units().DefaultToTHz
+        else:
+            _factor = factor
+
+        self._paths = []
+        for qpoints_ in paths:
+            qpoints = np.array(qpoints_)
+            distances = np.zeros(len(qpoints))
+            delta_qpoints = qpoints[1:] - qpoints[:-1]
+            delta_distances = np.sqrt(
+                (np.dot(delta_qpoints, rec_lattice) ** 2).sum(axis=1)
+            )
+            for i, dd in enumerate(delta_distances):
+                distances[i + 1] = distances[i] + dd
+
+            self.set_qpoints(qpoints)
+            eigenvalues = self._eigenvalues
+            frequencies = np.sqrt(abs(eigenvalues)) * np.sign(eigenvalues) * _factor
+            distances_with_shift = distances + distance_shift
+
+            self._paths.append(
+                [
+                    qpoints,
+                    distances,
+                    self._gruneisen,
+                    eigenvalues,
+                    self._eigenvectors,
+                    frequencies,
+                    distances_with_shift,
+                ]
+            )
+
+            distance_shift = distances_with_shift[-1]
+        self._labels = None
+        self._path_connections = None
+        if path_connections is None:
+            self._path_connections = [
+                True,
+            ] * len(self._paths)
+            self._path_connections[-1] = False
+        else:
+            self._path_connections = path_connections
+        if (
+            labels is not None
+            and len(labels) == (2 - np.array(self._path_connections)).sum()
+        ):
+            self._labels = labels
+
+    def get_qpoints(self):
+        """Return q-points."""
+        return [path[0] for path in self._paths]
+
+    def get_distances(self):
+        """Return distances."""
+        return [path[6] for path in self._paths]
+
+    def get_gruneisen(self):
+        """Return mode Gruneisen parameters."""
+        return [path[2] for path in self._paths]
+
+    def get_eigenvalues(self):
+        """Return eigenvalues."""
+        return [path[3] for path in self._paths]
+
+    def get_eigenvectors(self):
+        """Return eigenvectors."""
+        return [path[4] for path in self._paths]
+
+    def get_frequencies(self):
+        """Return frequencies."""
+        return [path[5] for path in self._paths]
+
+    def write_yaml(self, comment=None, filename=None, compression=None):
+        """Write results to file in yaml."""
+        if filename is not None:
+            _filename = filename
+
+        if compression is None:
+            if filename is None:
+                _filename = "gruneisen.yaml"
+            with open(_filename, "w") as w:
+                self._write_yaml(w, comment)
+        elif compression == "gzip":
+            if filename is None:
+                _filename = "gruneisen.yaml.gz"
+            with gzip.open(_filename, "wb") as w:
+                self._write_yaml(w, comment, is_binary=True)
+        elif compression == "lzma":
+            if filename is None:
+                _filename = "gruneisen.yaml.xz"
+            with lzma.open(_filename, "w") as w:
+                self._write_yaml(w, comment, is_binary=True)
+
+    def _write_yaml(self, w, comment, is_binary=False):
+        natom = len(self._cell)
+        rec_lattice = np.linalg.inv(self._cell.cell)  # column vecs
+        nq_paths = []
+        for qpoints in self._paths:
+            nq_paths.append(len(qpoints))
+        text = []
+        if comment is not None:
+            text.append(yaml.dump(comment, default_flow_style=False).rstrip())
+        text.append("nqpoint: %-7d" % np.sum(nq_paths))
+        text.append("npath: %-7d" % len(self._paths))
+        text.append("segment_nqpoint:")
+        text += ["- %d" % nq for nq in nq_paths]
+        if self._labels:
+            text.append("labels:")
+            if self._is_legacy_plot:
+                for i in range(len(self._paths)):
+                    text.append(
+                        "- [ '%s', '%s' ]" % (self._labels[i], self._labels[i + 1])
+                    )
+            else:
+                i = 0
+                for c in self._path_connections:
+                    text.append(
+                        "- [ '%s', '%s' ]" % (self._labels[i], self._labels[i + 1])
+                    )
+                    if c:
+                        i += 1
+                    else:
+                        i += 2
+        text.append("reciprocal_lattice:")
+        for vec, axis in zip(rec_lattice.T, ("a*", "b*", "c*"), strict=True):
+            text.append("- [ %12.8f, %12.8f, %12.8f ] # %2s" % (tuple(vec) + (axis,)))
+        text.append("natom: %-7d" % (natom))
+        text.append(str(self._cell))
+        text.append("")
+        text.append("path:")
+        text.append("")
+
+        for band_structure in self._paths:
+            (
+                qpoints,
+                distances,
+                gamma,
+                eigenvalues,
+                _,
+                frequencies,
+                distances_with_shift,
+            ) = band_structure
+
+            text.append("- nqpoint: %d" % len(qpoints))
+            text.append("  phonon:")
+            for q, d, gs, freqs in zip(
+                qpoints, distances, gamma, frequencies, strict=True
+            ):
+                text.append("  - q-position: [ %10.7f, %10.7f, %10.7f ]" % tuple(q))
+                text.append("    distance: %10.7f" % d)
+                text.append("    band:")
+                for i, (g, freq) in enumerate(zip(gs, freqs, strict=True)):
+                    text.append("    - # %d" % (i + 1))
+                    text.append("      gruneisen: %15.10f" % g)
+                    text.append("      frequency: %15.10f" % freq)
+                text.append("")
+
+        self._write_lines(w, text, is_binary)
+
+    def _write_lines(self, w, lines, is_binary):
+        text = "\n".join(lines)
+        if is_binary:
+            if sys.version_info < (3, 0):
+                w.write(bytes(text))
+            else:
+                w.write(bytes(text, "utf8"))
+        else:
+            w.write(text)
+
+    def plot(self, axarr, epsilon: float | None = None, color_scheme=None):
+        """Return pyplot of band structure calculation results."""
+        for band_structure in self._paths:
+            self._plot(axarr, band_structure, epsilon, color_scheme)
+
+    def _plot(self, axarr, band_structure, epsilon, color_scheme):
+        (
+            qpoints,
+            distances,
+            gamma,
+            eigenvalues,
+            _,
+            frequencies,
+            distances_with_shift,
+        ) = band_structure
+
+        n = len(gamma.T) - 1
+        ax1, ax2 = axarr
+
+        for i, (curve, freqs) in enumerate(
+            zip(gamma.T.copy(), frequencies.T, strict=True)
+        ):
+            if epsilon is not None:
+                if np.linalg.norm(qpoints[0]) < epsilon:
+                    cutoff_index = 0
+                    for j, q in enumerate(qpoints):
+                        if not np.linalg.norm(q) < epsilon:
+                            cutoff_index = j
+                            break
+                    for j in range(cutoff_index):
+                        if abs(freqs[j]) < abs(max(freqs)) / 10:
+                            curve[j] = curve[cutoff_index]
+
+                if np.linalg.norm(qpoints[-1]) < epsilon:
+                    cutoff_index = len(qpoints) - 1
+                    for j in reversed(range(len(qpoints))):
+                        q = qpoints[j]
+                        if not np.linalg.norm(q) < epsilon:
+                            cutoff_index = j
+                            break
+                    for j in reversed(range(len(qpoints))):
+                        if j == cutoff_index:
+                            break
+                        if abs(freqs[j]) < abs(max(freqs)) / 10:
+                            curve[j] = curve[cutoff_index]
+
+            self._plot_a_band(ax1, curve, distances_with_shift, i, n, color_scheme)
+        ax1.set_xlim(0, distances_with_shift[-1])
+
+        for i, freqs in enumerate(frequencies.T):
+            self._plot_a_band(ax2, freqs, distances_with_shift, i, n, color_scheme)
+        ax2.set_xlim(0, distances_with_shift[-1])
+
+    def _plot_a_band(self, ax, curve, distances_with_shift, i, n, color_scheme):
+        color = None
+        if color_scheme == "RB":
+            color = (1.0 / n * i, 0, 1.0 / n * (n - i))
+        elif color_scheme == "RG":
+            color = (1.0 / n * i, 1.0 / n * (n - i), 0)
+        elif color_scheme == "RGB":
+            color = (
+                max(2.0 / n * (i - n / 2.0), 0),
+                min(2.0 / n * i, 2.0 / n * (n - i)),
+                max(2.0 / n * (n / 2.0 - i), 0),
+            )
+        if color:
+            ax.plot(distances_with_shift, curve, color=color)
+        else:
+            ax.plot(distances_with_shift, curve)
