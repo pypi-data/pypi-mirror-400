5d86b061d841733af634b0673cf4d952bf2aa7a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 14:06:31 +0000

    Add package django-lti/0.9.2/django_lti-0.9.2-py3-none-any.whl

---
+++ packages/django-lti/django_lti-0.9.2-py3-none-any.whl/lti_tool/utils.py
@@ -0,0 +1,277 @@
+from typing import Optional
+
+from django.http.request import HttpRequest
+
+from pylti1p3.contrib.django.launch_data_storage.cache import DjangoCacheDataStorage
+from pylti1p3.contrib.django.message_launch import DjangoMessageLaunch
+from pylti1p3.deployment import Deployment
+from pylti1p3.tool_config.abstract import ToolConfAbstract
+
+from .lti_core.constants import AgsScope, ContextRole, ContextType
+from .lti_core.utils import normalize_role
+from .models import (
+    LtiContext,
+    LtiDeployment,
+    LtiLaunch,
+    LtiMembership,
+    LtiPlatformInstance,
+    LtiRegistration,
+    LtiResourceLink,
+    LtiUser,
+)
+
+
+def _prepare_deployment(lti_deployment):
+    return Deployment().set_deployment_id(lti_deployment.deployment_id)
+
+
+class DjangoToolConfig(ToolConfAbstract):
+    """LTI tool configuration class.
+
+    A registration UUID may be specified on init to address situations
+    where client_id isn't included in OIDC initiation params.
+    """
+
+    registration_uuid = None
+    registration = None
+    deployment = None
+
+    def __init__(self, registration_uuid=None):
+        super().__init__()
+        self.registration_uuid = registration_uuid
+
+    def check_iss_has_one_client(self, iss):
+        return False
+
+    def check_iss_has_many_clients(self, iss):
+        return True
+
+    def find_registration_by_issuer(self, iss, *args, **kwargs):
+        try:
+            self.registration = LtiRegistration.objects.active().get(
+                uuid=self.registration_uuid, issuer=iss
+            )
+            return self.registration.to_registration()
+        except LtiRegistration.DoesNotExist:
+            return None
+
+    def find_registration_by_params(self, iss, client_id, *args, **kwargs):
+        lookups = {"issuer": iss, "client_id": client_id}
+        if self.registration_uuid is not None:
+            lookups.update(uuid=self.registration_uuid)
+        try:
+            self.registration = LtiRegistration.objects.active().get(**lookups)
+            return self.registration.to_registration()
+        except LtiRegistration.DoesNotExist:
+            return None
+
+    def find_deployment(self, iss, deployment_id):
+        try:
+            self.deployment = LtiDeployment.objects.get(
+                registration__uuid=self.registration_uuid,
+                registration__issuer=iss,
+                registration__is_active=True,
+                deployment_id=deployment_id,
+            )
+        except LtiDeployment.DoesNotExist:
+            self.deployment = LtiDeployment.objects.create(
+                registration=self.registration, deployment_id=deployment_id
+            )
+        return _prepare_deployment(self.deployment)
+
+    def find_deployment_by_params(self, iss, deployment_id, client_id, *args, **kwargs):
+        lookups = {
+            "registration__issuer": iss,
+            "registration__client_id": client_id,
+            "registration__is_active": True,
+            "deployment_id": deployment_id,
+        }
+        if self.registration_uuid is not None:
+            lookups.update(registration__uuid=self.registration_uuid)
+        try:
+            self.deployment = LtiDeployment.objects.get(**lookups)
+        except LtiDeployment.DoesNotExist:
+            self.deployment = LtiDeployment.objects.create(
+                registration=self.registration, deployment_id=deployment_id
+            )
+        return _prepare_deployment(self.deployment)
+
+
+def get_launch_from_request(
+    request: HttpRequest, launch_id: Optional[str] = None
+) -> LtiLaunch:
+    """Returns the DjangoMessageLaunch associated with a request.
+
+    Optionally, a launch_id may be specified to retrieve the launch from the cache.
+    """
+    tool_conf = DjangoToolConfig()
+    launch_data_storage = DjangoCacheDataStorage()
+    if launch_id is not None:
+        message_launch = DjangoMessageLaunch.from_cache(
+            launch_id, request, tool_conf, launch_data_storage=launch_data_storage
+        )
+    else:
+        message_launch = DjangoMessageLaunch(
+            request, tool_conf, launch_data_storage=launch_data_storage
+        )
+        message_launch.validate()
+    return LtiLaunch(message_launch)
+
+
+def sync_user_from_launch(
+    lti_launch: LtiLaunch, lti1p1_secret: Optional[str] = None
+) -> LtiUser:
+    sub = lti_launch.get_claim("sub")
+    user_claims = {
+        "given_name": lti_launch.get_claim("given_name"),
+        "family_name": lti_launch.get_claim("family_name"),
+        "name": lti_launch.get_claim("name"),
+        "email": lti_launch.get_claim("email"),
+        "picture_url": lti_launch.get_claim("picture"),
+    }
+    if lti1p1_secret is not None and lti_launch.has_valid_migration_claim(
+        lti1p1_secret
+    ):
+        user_claims["lti1p1_id_on_platform"] = lti_launch.migration_claim.get(
+            "user_id", sub
+        )
+    lti_user, _created = LtiUser.objects.update_or_create(
+        registration=lti_launch.registration,
+        sub=sub,
+        defaults={k: v for k, v in user_claims.items() if v is not None},
+    )
+    return lti_user
+
+
+def _get_ags_props(lti_launch: LtiLaunch) -> dict:
+    ags_claim = lti_launch.ags_claim
+    if ags_claim is None:
+        return {}
+    ags_scope = ags_claim.get("scope", [])
+    return {
+        "lineitems_url": ags_claim.get("lineitems", ""),
+        "can_query_lineitems": AgsScope.QUERY_LINEITEMS in ags_scope,
+        "can_manage_lineitems": AgsScope.MANAGE_LINEITEMS in ags_scope,
+        "can_publish_scores": AgsScope.PUBLISH_SCORES in ags_scope,
+        "can_access_results": AgsScope.ACCESS_RESULTS in ags_scope,
+    }
+
+
+def sync_context_from_launch(
+    lti_launch: LtiLaunch, lti1p1_secret: Optional[str] = None
+) -> LtiContext:
+    context_claim = {} if lti_launch.context_claim is None else lti_launch.context_claim
+    nrps_claim = lti_launch.nrps_claim
+    nrps_endpoint = "" if nrps_claim is None else nrps_claim["context_memberships_url"]
+    context_types = context_claim.get("type", [])
+    defaults = {
+        "title": context_claim.get("title", ""),
+        "label": context_claim.get("label", ""),
+        "is_course_template": (ContextType.COURSE_TEMPLATE in context_types),
+        "is_course_offering": (ContextType.COURSE_OFFERING in context_types),
+        "is_course_section": (ContextType.COURSE_SECTION in context_types),
+        "is_group": (ContextType.GROUP in context_types),
+    }
+    if nrps_endpoint:
+        defaults["memberships_url"] = nrps_endpoint
+    defaults.update(_get_ags_props(lti_launch))
+    if lti1p1_secret is not None and lti_launch.has_valid_migration_claim(
+        lti1p1_secret
+    ):
+        defaults["lti1p1_id_on_platform"] = lti_launch.migration_claim.get(
+            "context_id", context_claim.get("id", "")
+        )
+    context, _created = LtiContext.objects.update_or_create(
+        deployment=lti_launch.deployment,
+        id_on_platform=context_claim.get("id", ""),
+        defaults=defaults,
+    )
+    return context
+
+
+def sync_membership_from_launch(
+    lti_launch: LtiLaunch, user: LtiUser, context: LtiContext
+) -> LtiMembership:
+    roles = [normalize_role(role) for role in lti_launch.roles_claim]
+    defaults = {}
+    if ContextRole.ADMINISTRATOR in roles:
+        defaults["is_administrator"] = True
+    if ContextRole.CONTENT_DEVELOPER in roles:
+        defaults["is_content_developer"] = True
+    if ContextRole.INSTRUCTOR in roles:
+        defaults["is_instructor"] = True
+    if ContextRole.LEARNER in roles:
+        defaults["is_learner"] = True
+    if ContextRole.MENTOR in roles:
+        defaults["is_mentor"] = True
+    membership, _created = LtiMembership.objects.update_or_create(
+        user=user, context=context, defaults=defaults
+    )
+    return membership
+
+
+def sync_resource_link_from_launch(
+    lti_launch: LtiLaunch, context: LtiContext, lti1p1_secret: Optional[str] = None
+) -> LtiResourceLink:
+    resource_link_claim = {
+        k: v for k, v in lti_launch.resource_link_claim.items() if v is not None
+    }
+    defaults = {
+        "title": resource_link_claim.get("title", ""),
+        "description": resource_link_claim.get("description", ""),
+    }
+    if lti1p1_secret is not None and lti_launch.has_valid_migration_claim(
+        lti1p1_secret
+    ):
+        defaults["lti1p1_id_on_platform"] = lti_launch.migration_claim.get(
+            "resource_link_id", resource_link_claim["id"]
+        )
+    resource_link, _created = LtiResourceLink.objects.update_or_create(
+        context=context,
+        id_on_platform=resource_link_claim["id"],
+        defaults=defaults,
+    )
+    return resource_link
+
+
+def sync_platform_instance_from_launch(
+    lti_launch: LtiLaunch, lti1p1_secret: Optional[str] = None
+) -> Optional[LtiPlatformInstance]:
+    platform_instance_claim = lti_launch.platform_instance_claim
+    if platform_instance_claim is None:
+        return None
+    defaults = {
+        "contact_email": platform_instance_claim.get("contact_email", ""),
+        "description": platform_instance_claim.get("description", ""),
+        "name": platform_instance_claim.get("name", ""),
+        "url": platform_instance_claim.get("url", ""),
+        "product_family_code": platform_instance_claim.get("product_family_code", ""),
+        "version": platform_instance_claim.get("version", ""),
+    }
+    if lti1p1_secret is not None and lti_launch.has_valid_migration_claim(
+        lti1p1_secret
+    ):
+        defaults["lti1p1_id_on_platform"] = lti_launch.migration_claim.get(
+            "tool_consumer_instance_guid", platform_instance_claim["guid"]
+        )
+    platform_instance, _created = LtiPlatformInstance.objects.update_or_create(
+        issuer=lti_launch.get_claim("iss"),
+        guid=platform_instance_claim["guid"],
+        defaults=defaults,
+    )
+    deployment = lti_launch.deployment
+    deployment.platform_instance = platform_instance
+    deployment.save(update_fields=["platform_instance"])
+    return platform_instance
+
+
+def sync_data_from_launch(
+    lti_launch: LtiLaunch, lti1p1_secret: Optional[str] = None
+) -> None:
+    user = sync_user_from_launch(lti_launch, lti1p1_secret)
+    if not lti_launch.is_data_privacy_launch:
+        context = sync_context_from_launch(lti_launch, lti1p1_secret)
+        sync_membership_from_launch(lti_launch, user, context)
+        if not lti_launch.is_deep_link_launch:
+            sync_resource_link_from_launch(lti_launch, context, lti1p1_secret)
+    sync_platform_instance_from_launch(lti_launch, lti1p1_secret)
---
+++ packages/django-lti/django_lti-0.9.2-py3-none-any.whl/lti_tool/views.py
@@ -0,0 +1,153 @@
+from typing import Optional
+
+from django.http import (
+    HttpRequest,
+    HttpResponse,
+    HttpResponseBadRequest,
+    HttpResponseForbidden,
+    JsonResponse,
+)
+from django.http.response import HttpResponseRedirect
+from django.utils.decorators import method_decorator
+from django.utils.translation import gettext as _
+from django.views import View
+from django.views.decorators.clickjacking import xframe_options_exempt
+from django.views.decorators.csrf import csrf_exempt
+
+from pylti1p3.contrib.django import DjangoCacheDataStorage, DjangoOIDCLogin
+
+from lti_tool.utils import DjangoToolConfig, get_launch_from_request
+
+from .constants import SESSION_KEY
+from .models import Key, LtiLaunch
+from .types import LtiHttpRequest
+from .utils import sync_data_from_launch
+
+
+def jwks(request):
+    """Makes a JWKS available to LTI platforms."""
+    return JsonResponse(Key.objects.as_jwks())
+
+
+@method_decorator((csrf_exempt, xframe_options_exempt), name="dispatch")
+class OIDCLoginInitView(View):
+    """Handles OIDC 3rd-party login initiation for an LTI launch."""
+
+    main_msg = (
+        "Your browser prevents embedded content from using cookies.  To work "
+        "around this, the content must be opened in a new tab or window.  "
+    )
+    click_msg = "Open a new tab or window now."
+    loading_msg = "Loading..."
+
+    def get(self, request, *args, **kwargs):
+        registration_uuid = kwargs.get("registration_uuid")
+        return self.get_oidc_response(request, registration_uuid, request.GET)
+
+    def post(self, request, *args, **kwargs):
+        registration_uuid = kwargs.get("registration_uuid")
+        return self.get_oidc_response(request, registration_uuid, request.POST)
+
+    def get_redirect_url(self, target_link_uri: str) -> str:
+        """Returns the redirect_uri to use for the OIDC initiation response."""
+        return target_link_uri
+
+    def get_oidc_response(self, request, registration_uuid, params):
+        tool_conf = DjangoToolConfig(registration_uuid)
+        launch_data_storage = DjangoCacheDataStorage()
+        oidc_login = DjangoOIDCLogin(
+            request, tool_conf, launch_data_storage=launch_data_storage
+        )
+        target_link_uri = params.get("target_link_uri")
+        if target_link_uri is None:
+            return HttpResponseBadRequest("Missing target_link_uri parameter.")
+        redirect_url = self.get_redirect_url(target_link_uri)
+        return oidc_login.enable_check_cookies(
+            self.main_msg, self.click_msg, self.loading_msg
+        ).redirect(redirect_url)
+
+
+@method_decorator(csrf_exempt, name="dispatch")
+class LtiLaunchBaseView(View):
+    """Base view for handling LTI 1.3 launches.
+
+    Inherit from this view and implement the `handle_resource_launch` method. Other
+    message types can be handled as well by overriding the appropriate method.
+    """
+
+    def post(self, request: LtiHttpRequest, *args, **kwargs):
+        request.session.clear()
+        lti_launch = get_launch_from_request(request)
+        lti1p1_secret = self.get_lti1p1_secret(lti_launch.lti1p1_consumer_key)
+        sync_data_from_launch(lti_launch, lti1p1_secret)
+        self.launch_setup(request, lti_launch)
+        if not lti_launch.deployment.is_active:
+            return self.handle_inactive_deployment(request, lti_launch)
+        request.session[SESSION_KEY] = lti_launch.get_launch_id()
+        request.lti_launch = lti_launch
+        if request.lti_launch.is_resource_launch:
+            return self.handle_resource_launch(request, lti_launch)
+        if request.lti_launch.is_deep_link_launch:
+            return self.handle_deep_linking_launch(request, lti_launch)
+        if request.lti_launch.is_submission_review_launch:
+            return self.handle_submission_review_launch(request, lti_launch)
+        if request.lti_launch.is_data_privacy_launch:
+            return self.handle_data_privacy_launch(request, lti_launch)
+
+    def get_lti1p1_secret(self, oauth_consumer_key: Optional[str]):
+        """Returns the LTI 1.1 secret for the provided consumer key.
+
+        Override this method to validate and sync data from the LTI 1.1 migration claim.
+        """
+        return None
+
+    def launch_setup(self, request: HttpRequest, lti_launch: LtiLaunch) -> None:
+        pass
+
+    def handle_inactive_deployment(
+        self, request: HttpRequest, lti_launch: LtiLaunch
+    ) -> HttpResponse:
+        """Handles LTI launches with inactive deployments."""
+        error_msg = _("This deployment is not active.")
+        return_url = lti_launch.get_return_url(lti_errormsg=error_msg)
+        if return_url is None:
+            return HttpResponseForbidden(error_msg)
+        return HttpResponseRedirect(return_url)
+
+    def handle_resource_launch(
+        self, request: HttpRequest, lti_launch: LtiLaunch
+    ) -> HttpResponse:
+        """Handles an LTI resource launch.
+
+        Implement this method to handle LTI resource link launch requests,
+        as described in the LTI 1.3 core spec at
+        https://www.imsglobal.org/spec/lti/v1p3/#resource-link-launch-request-message
+        """
+        raise NotImplementedError(
+            "Subclasses of LtiLaunchBaseView must implement handle_resource_launch"
+        )
+
+    def handle_deep_linking_launch(
+        self, request: HttpRequest, lti_launch: LtiLaunch
+    ) -> HttpResponse:
+        error_msg = _("Deep linking is not supported.")
+        # TODO: Update once pylti1p3 supports error_msg claim.
+        return HttpResponseForbidden(error_msg)
+
+    def handle_submission_review_launch(
+        self, request: HttpRequest, lti_launch: LtiLaunch
+    ) -> HttpResponse:
+        error_msg = _("Submission review launch is not supported.")
+        return_url = lti_launch.get_return_url(lti_errormsg=error_msg)
+        if return_url is None:
+            return HttpResponseForbidden(error_msg)
+        return HttpResponseRedirect(return_url)
+
+    def handle_data_privacy_launch(
+        self, request: HttpRequest, lti_launch: LtiLaunch
+    ) -> HttpResponse:
+        error_msg = _("Data privacy launch is not supported.")
+        return_url = lti_launch.get_return_url(lti_errormsg=error_msg)
+        if return_url is None:
+            return HttpResponseForbidden(error_msg)
+        return HttpResponseRedirect(return_url)
