ca6a91d8a79eeea00f57c4531d985b72fad503a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 05:08:17 +0000

    Add package mlrl-testbed-arff/0.15.1/mlrl_testbed_arff-0.15.1-py3-none-any.whl

---
+++ packages/mlrl-testbed-arff/mlrl_testbed_arff-0.15.1-py3-none-any.whl/mlrl/testbed_arff/experiments/input/sources/source_arff.py
@@ -0,0 +1,277 @@
+"""
+Author Michael Rapp (michael.rapp.ml@gmail.com)
+
+Provides classes that allow reading datasets from ARFF files.
+"""
+import logging as log
+
+from dataclasses import replace
+from functools import cached_property
+from pathlib import Path
+from typing import Any, List, Optional, Set, override
+from xml.dom import minidom
+
+import arff
+import numpy as np
+
+from scipy.sparse import coo_array, csc_array, sparray
+
+from mlrl.testbed_sklearn.experiments.dataset import Attribute, AttributeType, TabularDataset
+
+from mlrl.testbed.experiments.context import Context
+from mlrl.testbed.experiments.data import Properties
+from mlrl.testbed.experiments.dataset import Dataset
+from mlrl.testbed.experiments.file_path import FilePath
+from mlrl.testbed.experiments.input.data import DatasetInputData
+from mlrl.testbed.experiments.input.sources.source import DatasetFileSource
+from mlrl.testbed.experiments.state import ExperimentState
+from mlrl.testbed.util.io import open_readable_file
+
+
+def normalize_attribute_name(name: str) -> str:
+    """
+    Normalizes the name of an attribute by removing forbidden characters.
+
+    :param name:    The name to be normalized
+    :return:        The normalized name
+    """
+    name = name.strip()
+    if name.startswith('\'') or name.startswith('"'):
+        name = name[1:]
+    if name.endswith('\'') or name.endswith('"'):
+        name = name[:(len(name) - 1)]
+    return name.replace('\\\'', '\'').replace('\\"', '"')
+
+
+class ArffFileSource(DatasetFileSource):
+    """
+    Allows to read a dataset from an ARFF file.
+    """
+
+    SUFFIX_ARFF = 'arff'
+
+    SUFFIX_XML = 'xml'
+
+    class ArffFile:
+        """
+        Provides access to the content of an ARFF file.
+        """
+
+        def __init__(self, matrix: sparray, arff_attributes: List[Any], relation: str):
+            """
+            :param matrix:           The data matrix that is stored in the file
+            :param arff_attributes:  The attributes defined in the file
+            :param relation:         The @relation declaration contained in the file
+            """
+            self.matrix = matrix
+            self.arff_attributes = arff_attributes
+            self.relation = relation
+
+        @staticmethod
+        def from_file(file_path: Path, sparse: bool, dtype: np.dtype) -> 'ArffFileSource.ArffFile':
+            """
+            Loads the content of an ARFF file.
+
+            :param file_path:   The path to the ARFF file
+            :param sparse:      True, if the ARFF file is given in sparse format, False otherwise. If the given format
+                                is incorrect, an `arff.BadLayout` is raised
+            :param dtype:       The type of the data matrix to be read from the file
+            :return:            An `ArffFileSource.ArffFile` that has been loaded
+            """
+            with open_readable_file(file_path) as arff_file:
+                arff_dict = arff.load(arff_file, encode_nominal=True, return_type=arff.COO if sparse else arff.DENSE)
+
+                if sparse:
+                    data = arff_dict['data']
+                    values = data[0]
+                    row_indices = data[1]
+                    col_indices = data[2]
+                    shape = (max(row_indices) + 1, max(col_indices) + 1)
+                    matrix = coo_array((values, (row_indices, col_indices)), shape=shape, dtype=dtype)
+                    matrix = matrix.tocsc()
+                else:
+                    data = arff_dict['data']
+                    matrix = csc_array(data, dtype=dtype)
+
+            attributes = arff_dict['attributes']
+            relation = arff_dict['relation']
+            return ArffFileSource.ArffFile(matrix, arff_attributes=attributes, relation=relation)
+
+        @cached_property
+        def attributes(self) -> List[Attribute]:
+            """
+            A list that contains all attributes defined in the ARFF file.
+            """
+            attributes = []
+
+            for attribute in self.arff_attributes:
+                attribute_name = normalize_attribute_name(attribute[0])
+                type_definition = attribute[1]
+
+                if isinstance(type_definition, list):
+                    attribute_type = AttributeType.NOMINAL
+                    nominal_values = type_definition
+                else:
+                    type_definition = str(type_definition).lower()
+                    nominal_values = None
+
+                    if type_definition == 'integer':
+                        attribute_type = AttributeType.ORDINAL
+                    elif type_definition in ('real', 'numeric'):
+                        attribute_type = AttributeType.NUMERICAL
+                    else:
+                        raise ValueError('Encountered unsupported attribute type: ' + type_definition)
+
+                attribute = Attribute(name=attribute_name, attribute_type=attribute_type, nominal_values=nominal_values)
+                attributes.append(attribute)
+
+            return attributes
+
+    class ArffDataset:
+        """
+        Provides access to the content of an ARFF file and the corresponding Mulan XML file, if available.
+        """
+
+        def __parse_output_names_from_relation(self) -> Set[str]:
+            parameter_name = '-C '
+            arff_file = self.arff_file
+            relation = arff_file.relation
+            index = relation.find(parameter_name)
+
+            if index >= 0:
+                parameter_value = relation[index + len(parameter_name):]
+                index = parameter_value.find(' ')
+
+                if index >= 0:
+                    parameter_value = parameter_value[:index]
+
+                integer_value = int(parameter_value)
+                num_outputs = abs(integer_value)
+                attributes = arff_file.attributes
+
+                if integer_value < 0:
+                    outputs = attributes[(len(attributes) - num_outputs):]
+                else:
+                    outputs = attributes[:num_outputs]
+
+                return {normalize_attribute_name(output.name) for output in outputs}
+
+            return set()
+
+        def __init__(self, arff_file: 'ArffFileSource.ArffFile', output_names: Optional[Set[str]]):
+            """
+            :param arff_file:       The content of the ARFF file
+            :param output_names:    The names of all outputs contained in the dataset
+            """
+            self.arff_file = arff_file
+            self.output_names = output_names if output_names else self.__parse_output_names_from_relation()
+
+        @staticmethod
+        def from_file(arff_file: 'ArffFileSource.ArffFile', file_path: Path) -> 'ArffFileSource.ArffDataset':
+            """
+            Creates and returns an ARFF dataset from given ARFF file and a corresponding Mulan XML file, if available.
+
+
+            :param arff_file:       The content of the ARFF file
+            :param file_path:       The path to the XML file
+            :return:                The ARFF dataset that has been created
+            """
+            if file_path.is_file():
+                log.debug('Parsing meta-data from file \"%s\"...', file_path)
+                xml_doc = minidom.parse(str(file_path))
+                tags = xml_doc.getElementsByTagName('label')
+                output_names = {normalize_attribute_name(tag.getAttribute('name')) for tag in tags}
+            else:
+                output_names = None
+                log.debug(
+                    'Mulan XML file \"%s\" does not exist. If possible, information about the dataset\'s outputs is '
+                    + 'parsed from the ARFF file\'s @relation declaration as intended by the MEKA dataset format...',
+                    file_path)
+
+            return ArffFileSource.ArffDataset(arff_file=arff_file, output_names=output_names)
+
+        @cached_property
+        def features(self) -> List[Attribute]:
+            """
+            A list that stores all features contained in the dataset.
+            """
+            return [attribute for attribute in self.arff_file.attributes if attribute.name not in self.output_names]
+
+        @cached_property
+        def outputs(self) -> List[Attribute]:
+            """
+            A list that stores all outputs contained in the dataset.
+            """
+            return [attribute for attribute in self.arff_file.attributes if attribute.name in self.output_names]
+
+        @property
+        def outputs_at_start(self) -> bool:
+            """
+            True, if the outputs are defined before the features, False otherwise.
+            """
+            attributes = self.arff_file.attributes
+            return bool(attributes) and attributes[0].name in self.output_names
+
+        @property
+        def feature_matrix(self) -> sparray:
+            """
+            The feature matrix contained in the dataset.
+            """
+            num_outputs = len(self.outputs)
+            matrix = self.arff_file.matrix
+            return matrix[:, num_outputs:] if self.outputs_at_start else matrix[:, :-num_outputs]
+
+        @property
+        def output_matrix(self) -> sparray:
+            """
+            The output matrix contained in the dataset.
+            """
+            num_outputs = len(self.outputs)
+            matrix = self.arff_file.matrix
+            return matrix[:, :num_outputs] if self.outputs_at_start else matrix[:, -num_outputs:]
+
+    @staticmethod
+    def __read_arff_file(file_path: Path, dtype: np.dtype) -> ArffFile:
+        try:
+            return ArffFileSource.ArffFile.from_file(file_path, sparse=True, dtype=dtype)
+        except arff.BadLayout:
+            return ArffFileSource.ArffFile.from_file(file_path, sparse=False, dtype=dtype)
+
+    def __find_xml_file(self, state: ExperimentState, directory: Path, properties: Properties,
+                        context: Context) -> Path:
+        file_path = FilePath(directory=directory,
+                             file_name=properties.file_name,
+                             suffix=self.SUFFIX_XML,
+                             context=context)
+        resolved_file_path = file_path.resolve(state)
+
+        if resolved_file_path.is_file():
+            return resolved_file_path
+
+        least_specific_context = Context(include_dataset_type=False, include_prediction_scope=False, include_fold=False)
+
+        if least_specific_context != context:
+            resolved_file_path = replace(file_path, context=least_specific_context).resolve(state)
+
+        return resolved_file_path
+
+    def __init__(self, directory: Path):
+        """
+        :param directory: The path to the directory of the file
+        """
+        super().__init__(directory=directory, suffix=self.SUFFIX_ARFF)
+
+    @override
+    def _read_dataset_from_file(self, state: ExperimentState, file_path: Path,
+                                input_data: DatasetInputData) -> Optional[Dataset]:
+        problem_domain = state.problem_domain
+        arff_file = self.__read_arff_file(file_path=file_path, dtype=problem_domain.feature_dtype)
+        xml_file_path = self.__find_xml_file(state=state,
+                                             directory=file_path.parent,
+                                             properties=input_data.properties,
+                                             context=input_data.context)
+        arff_dataset = ArffFileSource.ArffDataset.from_file(arff_file=arff_file, file_path=xml_file_path)
+        return TabularDataset(x=arff_dataset.feature_matrix.tolil(),
+                              y=arff_dataset.output_matrix.astype(problem_domain.output_dtype).tolil(),
+                              features=arff_dataset.features,
+                              outputs=arff_dataset.outputs)
---
+++ packages/mlrl-testbed-arff/mlrl_testbed_arff-0.15.1-py3-none-any.whl/mlrl/testbed_arff/experiments/output/sinks/sink_arff.py
@@ -0,0 +1,127 @@
+"""
+Author Michael Rapp (michael.rapp.ml@gmail.com)
+
+Provides classes that allow writing datasets to ARFF files.
+"""
+import xml.etree.ElementTree as XmlTree
+
+from pathlib import Path
+from typing import Any, List, Optional, override
+from xml.dom import minidom
+
+import arff
+
+from scipy.sparse import dok_array
+
+from mlrl.testbed_arff.experiments.input.sources import ArffFileSource
+
+from mlrl.testbed_sklearn.experiments.dataset import Attribute, AttributeType, TabularDataset
+
+from mlrl.testbed.experiments.dataset import Dataset
+from mlrl.testbed.experiments.input.sources import Source
+from mlrl.testbed.experiments.output.sinks.sink import DatasetFileSink
+from mlrl.testbed.experiments.state import ExperimentState
+from mlrl.testbed.util.format import to_int_or_float
+from mlrl.testbed.util.io import ENCODING_UTF8, open_writable_file
+
+from mlrl.util.options import Options
+
+
+class ArffFileSink(DatasetFileSink):
+    """
+    Allows to write a dataset to an ARFF file.
+    """
+
+    @staticmethod
+    def __create_arff_data(dataset: TabularDataset) -> List[Any]:
+        num_examples = dataset.num_examples
+
+        if dataset.has_sparse_features and dataset.has_sparse_outputs:
+            return [{} for _ in range(num_examples)]
+
+        num_attributes = dataset.num_features + dataset.num_outputs
+        attributes = dataset.features + dataset.outputs
+        return [[ArffFileSink.__format_value(attributes[i], 0) for i in range(num_attributes)]
+                for _ in range(num_examples)]
+
+    @staticmethod
+    def __format_value(attribute: Attribute, value: Any) -> Any:
+        nominal = attribute.attribute_type != AttributeType.NUMERICAL and attribute.nominal_values
+        nominal_values = attribute.nominal_values if nominal else None
+        return nominal_values[int(value)] if nominal_values else to_int_or_float(value)
+
+    @staticmethod
+    def __fill_arff_data(dataset: TabularDataset, data: List[Any]):
+        features = dataset.features
+
+        for (example_index, feature_index), value in dok_array(dataset.x).items():
+            feature = features[feature_index]
+            data[example_index][feature_index] = ArffFileSink.__format_value(feature, value)
+
+        num_features = dataset.num_features
+        outputs = dataset.outputs
+
+        for (example_index, output_index), value in dok_array(dataset.y).items():
+            output = outputs[output_index]
+            data[example_index][num_features + output_index] = ArffFileSink.__format_value(output, value)
+
+    @staticmethod
+    def __write_arff_file(file_path: Path, dataset: TabularDataset):
+        features = dataset.features
+        x_features = [(features[i].name, 'NUMERIC' if features[i].attribute_type == AttributeType.NUMERICAL
+                       or features[i].nominal_values is None else features[i].nominal_values)
+                      for i in range(dataset.num_features)]
+
+        outputs = dataset.outputs
+        y_features = [(outputs[i].name, 'NUMERIC' if outputs[i].attribute_type == AttributeType.NUMERICAL
+                       or outputs[i].nominal_values is None else outputs[i].nominal_values)
+                      for i in range(dataset.num_outputs)]
+
+        data = ArffFileSink.__create_arff_data(dataset)
+        ArffFileSink.__fill_arff_data(dataset, data)
+
+        with open_writable_file(file_path) as arff_file:
+            arff_file.write(
+                arff.dumps({
+                    'description': 'traindata',
+                    'relation': 'traindata: -C ' + str(-dataset.num_outputs),
+                    'attributes': x_features + y_features,
+                    'data': data
+                }))
+
+    @staticmethod
+    def __write_xml_file(file_path: Path, dataset: TabularDataset):
+        root_element = XmlTree.Element('labels')
+        root_element.set('xmlns', 'http://mulan.sourceforge.net/labels')
+
+        for output in dataset.outputs:
+            label_element = XmlTree.SubElement(root_element, 'label')
+            label_element.set('name', output.name)
+
+        with open_writable_file(file_path) as xml_file:
+            xml_string = minidom.parseString(XmlTree.tostring(root_element)).toprettyxml(encoding=ENCODING_UTF8)
+            xml_file.write(xml_string.decode(ENCODING_UTF8))
+
+    def __init__(self, directory: Path, options: Options = Options(), create_directory: bool = False):
+        """
+        :param directory:           The path to the directory, where the ARFF file should be located
+        :param options:             Options to be taken into account
+        :param create_directory:    True, if the given directory should be created, if it does not exist, False
+                                    otherwise
+        """
+        super().__init__(directory=directory,
+                         suffix=ArffFileSource.SUFFIX_ARFF,
+                         options=options,
+                         create_directory=create_directory)
+
+    # pylint: disable=unused-argument
+    def _write_dataset_to_file(self, file_path: Path, state: ExperimentState, dataset: Dataset, **_):
+        self.__write_arff_file(file_path=file_path, dataset=dataset)
+        self.__write_xml_file(file_path=file_path.with_suffix('.' + ArffFileSource.SUFFIX_XML), dataset=dataset)
+
+    @override
+    def create_source(self, input_directory: Path) -> Optional[Source]:
+        """
+        See :func:`mlrl.testbed.experiments.output.sinks.sink.Sink.create_source`
+        """
+        return ArffFileSource(input_directory)
