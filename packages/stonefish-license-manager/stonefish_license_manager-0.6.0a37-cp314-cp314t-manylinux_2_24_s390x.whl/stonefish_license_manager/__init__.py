ab7c126527fd89ebe67788d763e8e40f940c2ef9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 17:09:43 +0000

    Add package stonefish-license-manager/0.6.0a37/stonefish_license_manager-0.6.0a37-cp314-cp314t-manylinux_2_24_s390x.whl

---
+++ packages/stonefish-license-manager/stonefish_license_manager-0.6.0a37-cp314-cp314t-manylinux_2_24_s390x.whl/stonefish_license_manager/__init__.py
@@ -0,0 +1,5 @@
+from .stonefish_license_manager import *
+
+__doc__ = stonefish_license_manager.__doc__
+if hasattr(stonefish_license_manager, "__all__"):
+    __all__ = stonefish_license_manager.__all__
