14f7fb22a2356258416e7dad4cb0727e4e050457
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 16:07:27 +0000

    Add package pybigbuy/3.25.0/pybigbuy-3.25.0.tar.gz

---
+++ packages/pybigbuy/pybigbuy-3.25.0.tar.gz/pybigbuy-3.25.0/README.md
@@ -0,0 +1,37 @@
+# PyBigBuy
+
+**PyBigBuy** is a Python client for BigBuy’s REST API.
+
+Note: PyBigBuy is not affiliated to nor endorsed by BigBuy.
+
+## Coverage
+
+Starting with 3.17.0 PyBigBuy implements all API endpoints.
+We release new versions of the API client each time there are new or modified endpoints.
+
+## Install
+
+Requirements:
+* From PyBigBuy 3.21.0: Python 3.9+
+* Up to 3.20.1: Python 3.8+
+
+### Pip
+
+    python -m pip install pybigbuy
+
+### Poetry
+
+    poetry add pybigbuy
+
+## Usage
+
+```python3
+from bigbuy import BigBuy
+
+
+client = BigBuy("your-API-token")
+```
+
+## License
+
+Copyright 2020-2025 [Bixoto](https://bixoto.com/).
---
+++ packages/pybigbuy/pybigbuy-3.25.0.tar.gz/pybigbuy-3.25.0/bigbuy/rate_limit.py
@@ -0,0 +1,49 @@
+import time
+from datetime import datetime, timedelta
+from typing import Optional, Callable
+
+from requests import Response
+from typing_extensions import Self
+
+RATE_LIMIT_RESPONSE_TEXT = "You exceeded the rate limit"
+
+
+class RateLimit:
+    """
+    Class that represents a rate-limit.
+    """
+
+    def __init__(self, reset_time: datetime):
+        self.reset_time = reset_time
+
+    @classmethod
+    def from_response(cls, response: Response) -> Optional[Self]:
+        if response.ok or response.text != RATE_LIMIT_RESPONSE_TEXT:
+            return None
+
+        reset_timestamp: str = response.headers.get("X-Ratelimit-Reset", "")
+        if reset_timestamp.isdigit():
+            return cls(reset_time=datetime.fromtimestamp(int(reset_timestamp)))
+
+        return None
+
+    def reset_timedelta(self, utcnow: Optional[datetime] = None) -> timedelta:
+        """
+        Return a timedelta object representing the delta between the current UTC time and the reset time.
+        Note the delta may be negative.
+
+        :param utcnow: if passed, this is used instead of datetime.utcnow()
+        """
+        if utcnow is None:
+            utcnow = datetime.utcnow()
+
+        return self.reset_time - utcnow
+
+    def wait_until_expiration(self, *, wait_function: Callable[[float], None] = time.sleep) -> None:
+        """
+        Wait until the rate limit expires.
+        """
+        delta = self.reset_timedelta()
+        wait_seconds = delta.total_seconds()
+        if wait_seconds >= 0:
+            wait_function(wait_seconds)
