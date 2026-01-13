9e299c2e57e9d358e3813a9acb1901461c37d4d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 21:06:52 +0000

    Add package pingen2sdk/2.1.0/pingen2sdk-2.1.0-py3-none-any.whl

---
+++ packages/pingen2sdk/pingen2sdk-2.1.0-py3-none-any.whl/pingen2sdk/api_resources/__init__.py
@@ -0,0 +1,14 @@
+# flake8: noqa
+from pingen2sdk.api_resources.letters import Letters as Letters
+from pingen2sdk.api_resources.file_upload import FileUpload as FileUpload
+from pingen2sdk.api_resources.letter_events import LetterEvents as LetterEvents
+from pingen2sdk.api_resources.users import Users as Users
+from pingen2sdk.api_resources.user_associations import (
+    UserAssociations as UserAssociations,
+)
+from pingen2sdk.api_resources.organisations import Organisations as Organisations
+from pingen2sdk.api_resources.batches import Batches as Batches
+from pingen2sdk.api_resources.batch_events import BatchEvents as BatchEvents
+from pingen2sdk.api_resources.webhooks import Webhooks as Webhooks
+from pingen2sdk.api_resources.ebills import Ebills as Ebills
+from pingen2sdk.api_resources.emails import Emails as Emails
