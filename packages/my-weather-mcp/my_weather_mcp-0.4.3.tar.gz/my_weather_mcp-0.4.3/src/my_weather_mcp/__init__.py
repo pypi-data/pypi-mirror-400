4fd863042d195c37ab6386c0a566c492c5e6ba88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 05:07:46 +0000

    Add package my-weather-mcp/0.4.3/my_weather_mcp-0.4.3.tar.gz

---
+++ packages/my-weather-mcp/my_weather_mcp-0.4.3.tar.gz/my_weather_mcp-0.4.3/src/my_weather_mcp/__init__.py
@@ -0,0 +1,6 @@
+from my_weather_mcp.main import mcp
+def main() -> None:
+    mcp.settings.host = "0.0.0.0"
+    mcp.settings.port = 8000
+    mcp.run(transport='stdio')
+    print("MCP服务已启动，等待工具调用...")
