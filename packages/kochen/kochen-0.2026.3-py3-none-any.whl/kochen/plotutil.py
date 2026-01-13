e8282beeaecf8fab07b83de36066f49a686eb134
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 05:07:57 +0000

    Add package kochen/0.2026.3/kochen-0.2026.3-py3-none-any.whl

---
+++ packages/kochen/kochen-0.2026.3-py3-none-any.whl/kochen/plotutil.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Quick commands for matplotlib figures"""
+
+import matplotlib.axes as maxes
+import matplotlib.dates as mdates
+
+
+def set_angled_dtimes(ax: maxes.Axes, angle=30, format="%-d %b, %-I%P"):
+    dformat = mdates.DateFormatter(format)  # e.g. "7 Jun, 4am"
+    ax.xaxis.set_major_formatter(dformat)
+    ticks = ax.get_xticks()  # pyright: ignore[reportCallIssue]
+    ax.set_xticks(ticks)  # pyright: ignore[reportCallIssue]
+    labels = ax.get_xticklabels()  # pyright: ignore[reportCallIssue]
+    ax.set_xticklabels(labels, rotation=angle, ha="right", rotation_mode="anchor")  # pyright: ignore[reportCallIssue]
