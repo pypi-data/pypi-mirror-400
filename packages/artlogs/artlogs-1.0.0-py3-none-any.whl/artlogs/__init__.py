2ec9d20cd606a2e40cc66091b6dd27676a9061a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 18:11:05 +0000

    Add package artlogs/1.0.0/artlogs-1.0.0-py3-none-any.whl

---
+++ packages/artlogs/artlogs-1.0.0-py3-none-any.whl/artlogs/__init__.py
@@ -0,0 +1,9 @@
+"""
+ArtLogs - Библиотека для красивого и структурированного логирования
+"""
+
+from .core import ArtLogProfile, ArtLogs, HelpFunction
+
+__version__ = "1.0.0"
+__author__ = "Na9ash1"
+__all__ = ['ArtLogProfile', 'ArtLogs', 'HelpFunction']
