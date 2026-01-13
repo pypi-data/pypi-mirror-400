cc1c1bbe347bc05a296e5326fc580d85dd9c64c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 04:19:56 +0000

    Add package argx/0.4.1/argx-0.4.1.tar.gz

---
+++ packages/argx/argx-0.4.1.tar.gz/argx-0.4.1/argx/__init__.py
@@ -0,0 +1,15 @@
+"""Supercharged argparse for Python"""
+# Shortcuts
+from argparse import (
+    Namespace,
+    ONE_OR_MORE,
+    OPTIONAL,
+    PARSER,
+    REMAINDER,
+    SUPPRESS,
+    ZERO_OR_MORE,
+)
+
+from .parser import ArgumentParser
+
+__version__ = "0.4.1"
---
+++ packages/argx/argx-0.4.1.tar.gz/argx-0.4.1/argx/type_.py
@@ -0,0 +1,44 @@
+"""Additional types for argx"""
+from typing import Any
+
+
+def py(s: str) -> Any:
+    from ast import literal_eval
+    return literal_eval(s)
+
+
+def json(s: str) -> Any:
+    import json
+    return json.loads(s)
+
+
+def anypath(s: str) -> Any:
+    from panpath import PanPath
+    return PanPath(s)
+
+
+def auto(s: str) -> Any:
+    if s in ("True", "TRUE", "true"):
+        return True
+    if s in ("False", "FALSE", "false"):
+        return False
+    if s in ("None", "NONE", "none"):
+        return None
+
+    try:
+        return int(s)
+    except (TypeError, ValueError):
+        pass
+
+    try:
+        return float(s)
+    except (TypeError, ValueError):
+        pass
+
+    import json
+    try:
+        return json.loads(s)
+    except (TypeError, ValueError, json.JSONDecodeError):
+        pass
+
+    return s
