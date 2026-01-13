72fac7b3ecc4268f3e0ab65bc65d3f49f6a15bc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 15:08:41 +0000

    Add package bokeh/3.9.0.dev7/bokeh-3.9.0.dev7-py3-none-any.whl

---
+++ packages/bokeh/bokeh-3.9.0.dev7-py3-none-any.whl/bokeh/server/static/js/lib/styles/tabs.css.d.ts
@@ -0,0 +1,12 @@
+export const above: string
+export const below: string
+export const left: string
+export const right: string
+export const header: string
+export const tab: string
+export const active: string
+export const close: string
+export const disabled: string
+export const icon: string
+const __css__: string
+export default __css__
