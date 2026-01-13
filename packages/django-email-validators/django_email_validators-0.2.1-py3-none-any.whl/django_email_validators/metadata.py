5f002a0d029ebba2b622b941c66232702a6eb26e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 03:32:18 +0000

    Add package django-email-validators/0.2.1/django_email_validators-0.2.1-py3-none-any.whl

---
+++ packages/django-email-validators/django_email_validators-0.2.1-py3-none-any.whl/django_email_validators/metadata.py
@@ -0,0 +1,7 @@
+__author__ = "Fabio Caccamo"
+__copyright__ = "Copyright (c) 2025-present Fabio Caccamo"
+__description__ = "no more invalid or disposable emails in your database."
+__email__ = "fabio.caccamo@gmail.com"
+__license__ = "MIT"
+__title__ = "django-email-validators"
+__version__ = "0.2.1"
