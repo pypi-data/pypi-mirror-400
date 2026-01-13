c31760cc0790b2d9f974d0207d75620aebc5442b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 16:07:14 +0000

    Add package yuantao-fmk/0.2.0a5/yuantao_fmk-0.2.0a5.tar.gz

---
+++ packages/yuantao-fmk/yuantao_fmk-0.2.0a5.tar.gz/yuantao_fmk-0.2.0a5/tests/test_cann_community_installer.py
@@ -0,0 +1,25 @@
+from yuantao_fmk.impl.run_package_installer.cann_community_installer import CANNCommunityInstaller
+from yuantao_fmk import Config
+import unittest
+
+class TestCANNCommunityInstaller(unittest.TestCase):
+    def test_list_version(self):
+        installer = CANNCommunityInstaller(None, "910b")
+        Config.FORCE_STABLE = False
+        print(installer.get_version_list(reverse=True))
+
+
+    def test_get_latest_version(self):
+        installer = CANNCommunityInstaller(None, "910b")
+        print(installer.get_latest_version())
+
+    def test_get_default_resource_tags(self):
+        installer = CANNCommunityInstaller(None, "910b")
+        print(installer.get_default_resource_tag_list())
+    
+    def test_install_resource(self):
+        installer = CANNCommunityInstaller("8.5.0.alpha001", "910b")
+        installer.download_and_install_resource("/home/yuantao/Ascend/test", ["toolkit"])
+
+if __name__ == "__main__":
+    unittest.main()
