6b8ef1ca1c707f1366f6a945f5440d60f8cf6982
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 16:07:04 +0000

    Add package yamlns/0.12.4/yamlns-0.12.4-py3-none-any.whl

---
+++ packages/yamlns/yamlns-0.12.4-py3-none-any.whl/yamlns/cli/json2yaml.py
@@ -0,0 +1,14 @@
+import sys
+from ..core import ns
+
+
+def main():  # pragma: no cover
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        description="Converts JSON into pretty printed YAML.",
+    )
+    args = parser.parse_args()
+
+    data = ns.load(sys.stdin)
+    data.dump(sys.stdout)
