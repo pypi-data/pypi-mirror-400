e5c3e4b38f181038276e8c7fd51ded8b94a575a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 03:32:51 +0000

    Add package lyriks/0.5.6/lyriks-0.5.6-py3-none-any.whl

---
+++ packages/lyriks/lyriks-0.5.6-py3-none-any.whl/lyriks/providers/qqm.py
@@ -0,0 +1,41 @@
+from lyriks.lyrics import Lyrics
+from lyriks.mb_client import Mbid, Release
+from .api import qqm_api
+from .api.qqm_api import QQMSong
+from .provider import Provider
+from .registry import register_provider
+
+
+@register_provider('qq', 'qqm', 'qqmusic')
+class QQMusic(Provider):
+    """
+    Provider for QQ Music.
+    """
+
+    async def fetch_recording_lyrics(self, track_release: Release, recording_mbid: Mbid) -> Lyrics | None:
+        # Resolve album
+        qqm_songs = await self.get_mapped_provider_songs(
+            track_release,
+            lambda r: r.extract_url_str(r'https://y.qq.com/n/ryqq(?:_v2)?/albumDetail/(\w+)'),
+            lambda album_id: qqm_api.get_album_songs(self.http_client, album_id),
+        )
+        if not qqm_songs:
+            return None
+
+        # Get song for recording
+        qqm_song = qqm_songs.get(recording_mbid)
+        if not qqm_song:
+            return None
+
+        # Fetch lyrics
+        return await self.fetch_provider_song_lyrics(qqm_song)
+
+    async def fetch_song_by_id(self, song_id: int) -> QQMSong | None:
+        return await qqm_api.get_song_info(self.http_client, song_id)
+
+    async def fetch_provider_song_lyrics(self, song: QQMSong) -> Lyrics | None:
+        return await qqm_api.get_song_lyrics(self.http_client, song)
+
+    @property
+    def provider_domain(self) -> str:
+        return 'y.qq.com'
