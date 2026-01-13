9ced5140a208120bee7838a47c99d328f21a7bb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 19:07:08 +0000

    Add package oedisi/3.0.0/oedisi-3.0.0-py3-none-any.whl

---
+++ packages/oedisi/oedisi-3.0.0-py3-none-any.whl/oedisi/types/common.py
@@ -0,0 +1,30 @@
+from typing import Dict, Optional
+from pydantic import BaseModel
+from enum import Enum
+
+BASE_DOCKER_IMAGE = "python:3.10.6-slim-bullseye"
+BROKER_SERVICE = "broker"
+APP_NAME = "oedisi"
+DOCKER_HUB_USER = "aadillatif"
+KUBERNETES_SERVICE_NAME = "oedisi-service"
+
+class DefaultFileNames(str, Enum):
+    INPUT_MAPPING = "input_mapping.json"
+    STATIC_INPUTS = "static_inputs.json"
+
+class BrokerConfig(BaseModel):
+    broker_port: int = 23404
+    broker_ip: str = "10.5.0.2"
+    api_port: int = 12345
+    feeder_host: str | None = None
+    feeder_port: int | None = None
+
+
+class HeathCheck(BaseModel):
+    hostname: str
+    host_ip: str
+
+
+class ServerReply(BaseModel):
+    detail: str
+    action: Optional[str] = None
