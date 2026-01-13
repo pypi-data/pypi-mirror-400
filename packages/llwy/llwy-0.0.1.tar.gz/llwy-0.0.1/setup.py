df5d6ce677476aa76304fd203b8a285048e16db3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 04:20:05 +0000

    Add package llwy/0.0.1/llwy-0.0.1.tar.gz

---
+++ packages/llwy/llwy-0.0.1.tar.gz/llwy-0.0.1/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+
+setuptools.setup(
+	name="llwy",
+	version="0.0.1",
+	author="lhqvq",
+	author_email="lhqvq@mail.ustc.edu.cn",
+	description="init",
+	long_description=long_description,
+	url="https://github.com/",
+	packages=setuptools.find_packages(),
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+)
