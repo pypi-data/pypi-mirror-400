a5979c17bc891504061ad2780b41eeef20853b5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 15:07:47 +0000

    Add package calfire-wildfires/1.1.3/calfire_wildfires-1.1.3-py3-none-any.whl

---
+++ packages/calfire-wildfires/calfire_wildfires-1.1.3-py3-none-any.whl/calfire_wildfires/cli.py
@@ -0,0 +1,26 @@
+import click
+
+from calfire_wildfires import get_active_fires, get_all_fires
+
+
+@click.group()
+def cmd():
+    """A command-line interface for downloading wildfires data from CalFire.
+
+    Returns GeoJSON.
+    """
+    pass
+
+
+@cmd.command(help="The latest active fires JSON")
+def active_fires():
+    click.echo(get_active_fires())
+
+
+@cmd.command(help="The latest active fires JSON")
+def all_fires():
+    click.echo(get_all_fires())
+
+
+if __name__ == "__main__":
+    cmd()
