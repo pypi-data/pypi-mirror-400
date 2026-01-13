9356f0dbedd2b00d5c4e737d87e06158df1e456e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 16:07:58 +0000

    Add package gengar/0.1.2/gengar-0.1.2.tar.gz

---
+++ packages/gengar/gengar-0.1.2.tar.gz/gengar-0.1.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="gengar",  # CHANGE 'yourname' to make it unique
+    version="0.1.2",
+    author="Your Name",
+    author_email="your.email@example.com",
+    description="A simple library for AI/ML algorithm code snippets",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
