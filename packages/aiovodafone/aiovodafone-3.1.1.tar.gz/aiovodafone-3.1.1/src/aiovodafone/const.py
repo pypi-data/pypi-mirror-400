5388dce9d7b2153c5e9362b07d715328caf50e91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 22:06:15 +0000

    Add package aiovodafone/3.1.1/aiovodafone-3.1.1.tar.gz

---
+++ packages/aiovodafone/aiovodafone-3.1.1.tar.gz/aiovodafone-3.1.1/src/aiovodafone/const.py
@@ -0,0 +1,63 @@
+"""Constants for Vodafone Station."""
+
+import logging
+from enum import StrEnum
+from typing import Any
+
+from aiohttp import ClientTimeout
+
+_LOGGER = logging.getLogger(__package__)
+
+HEADERS = {
+    "User-Agent": (
+        "Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0"
+    ),
+    "Accept-Language": "en-GB,en;q=0.5",
+    "X-Requested-With": "XMLHttpRequest",
+    "Priority": "u=1",
+}
+DEFAULT_TIMEOUT = ClientTimeout(10)
+POST_RESTART_TIMEOUT = ClientTimeout(2)
+WIFI_DATA = "wifi_data"
+
+
+class WifiType(StrEnum):
+    """Wi-Fi types."""
+
+    MAIN = "main"
+    GUEST = "guest"
+
+
+class WifiBand(StrEnum):
+    """Wi-Fi bands."""
+
+    BAND_2_4_GHZ = "2.4ghz"
+    BAND_5_GHZ = "5ghz"
+
+
+DEVICES_SETTINGS: dict[str, Any] = {
+    "Sercomm": {
+        "login_url": "login.html",
+        "total_fields_num": 8,
+    },
+    "Technicolor": {
+        "login_url": "api/v1/login_conf",
+        "user_already_logged_in": "MSG_LOGIN_150",
+    },
+    "UltraHub": {
+        "login_url": "api/config/details.jst",
+        "params": {"X_INTERNAL_FIELDS": "X_VODAFONE_ServiceStatus_1"},
+        "default_id": 3,
+    },
+}
+
+DEVICE_SERCOMM_LOGIN_STATUS = [
+    "not logged",
+    "logged",
+    "already logged",
+    "credential error",
+    "credential error",
+    "password mismatch",
+    "incorrect challenge",
+    "password mismatch",
+]
---
+++ packages/aiovodafone/aiovodafone-3.1.1.tar.gz/aiovodafone-3.1.1/src/aiovodafone/exceptions.py
@@ -0,0 +1,43 @@
+"""Vodafone Station library exceptions."""
+
+from __future__ import annotations
+
+
+class VodafoneError(Exception):
+    """Base class for aiovodafone errors."""
+
+
+class GenericLoginError(VodafoneError):
+    """Exception raised when login fails."""
+
+
+class GenericResponseError(VodafoneError):
+    """Exception raised when GET/POST fails."""
+
+
+class CannotConnect(VodafoneError):
+    """Exception raised when connection fails."""
+
+
+class CannotAuthenticate(VodafoneError):
+    """Exception raised when credentials are incorrect."""
+
+
+class AlreadyLogged(VodafoneError):
+    """Exception raised if a user is already logged."""
+
+
+class ModelNotSupported(VodafoneError):
+    """Exception raised when using a model not yet supported."""
+
+
+class ResultTimeoutError(VodafoneError):
+    """Exception raised when a debug request times out."""
+
+
+class CsrfError(VodafoneError):
+    """Exception raised when a CSRF Error occurs."""
+
+
+class SJCLError(VodafoneError):
+    """Exception raised when an SJCL decryption/encryption error occurs."""
