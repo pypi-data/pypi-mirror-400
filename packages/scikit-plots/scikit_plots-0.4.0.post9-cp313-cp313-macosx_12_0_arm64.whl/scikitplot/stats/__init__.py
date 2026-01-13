d6b12dd2d0a255dc7a5b41414b50fdcca912b519
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 18:10:37 +0000

    Add package scikit-plots/0.4.0.post9/scikit_plots-0.4.0.post9-cp313-cp313-macosx_12_0_arm64.whl

---
+++ packages/scikit-plots/scikit_plots-0.4.0.post9-cp313-cp313-macosx_12_0_arm64.whl/scikitplot/stats/__init__.py
@@ -0,0 +1,31 @@
+# Authors: The scikit-plots developers
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""
+Elegant statistical tools for intuitive and insightful data visualization and interpretation.
+
+The :py:mod:`~scikitplot.stats` module offers a wide range of probability distributions, summary
+and frequency statistics, correlation functions, statistical tests,
+masked statistics, and additional tools.
+"""
+
+from __future__ import annotations
+
+from contextlib import suppress as _suppress
+
+from .. import logger as _logger
+from ..exceptions import ScikitplotException as _ScikitplotException
+
+try:
+    with _suppress(ImportError):
+        from ..cexternals._astropy.stats import *  # noqa: F401,F403
+except _ScikitplotException:
+    _logger.warning(
+        "Failed to import astropy.stats. Some features may not work as expected."
+    )
+
+try:
+    with _suppress(ImportError):
+        from ..externals._tweedie import *  # noqa: F401,F403
+except _ScikitplotException:
+    _logger.warning("Failed to import tweedie. Some features may not work as expected.")
