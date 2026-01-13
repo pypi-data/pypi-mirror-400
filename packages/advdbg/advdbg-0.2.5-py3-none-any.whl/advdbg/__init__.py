12e82631aed304e7a1c99242ee918f7e0c683ce7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 20:06:35 +0000

    Add package advdbg/0.2.5/advdbg-0.2.5-py3-none-any.whl

---
+++ packages/advdbg/advdbg-0.2.5-py3-none-any.whl/advdbg/__init__.py
@@ -0,0 +1,18 @@
+'''
+Welcome to Advanced Debug.
+Licensed with MIT
+
+Change Logs:
+	- Hotfix 0.2.3
+'''
+
+__version__ = '0.2.4'
+
+from .core import AdvDBG
+
+from .analyze import Breakpoint
+
+__all__ = ['AdvDBG', 'Breakpoint']
+
+# For easy access, you can create a default instance
+DebuGGer = AdvDBG()
