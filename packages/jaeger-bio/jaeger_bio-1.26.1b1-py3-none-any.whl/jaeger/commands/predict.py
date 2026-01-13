c1eba8804fc6515b261eeb124008fca249508597
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 17:09:27 +0000

    Add package jaeger-bio/1.26.1b1/jaeger_bio-1.26.1b1-py3-none-any.whl

---
+++ packages/jaeger-bio/jaeger_bio-1.26.1b1-py3-none-any.whl/jaeger/commands/predict.py
@@ -0,0 +1,246 @@
+import os
+
+os.environ["GRPC_VERBOSITY"] = "ERROR"
+os.environ["GLOG_minloglevel"] = "2"
+import traceback
+import sys
+import psutil
+import shutil
+import time
+from importlib.resources import files
+from importlib.metadata import version
+from pathlib import Path
+import tensorflow as tf
+
+from jaeger.nnlib.inference import InferModel
+from jaeger.preprocess.fasta import fragment_generator
+from jaeger.utils.gpu import get_device_name
+from jaeger.utils.termini import scan_for_terminal_repeats
+from jaeger.utils.fs import validate_fasta_entries
+from jaeger.utils.misc import json_to_dict, AvailableModels, get_model_id
+from jaeger.utils.logging import description, get_logger
+# from jaeger.utils.tandem import split_fasta_with_pyfastx, run_batch, merge_masked_files
+GB_BYTES = 1024**3
+
+
+def run_core(**kwargs):
+    current_process = psutil.Process()
+    
+    USER_MODEL_PATH = kwargs.get("model_path")
+    CONFIG_PATH = kwargs.get("config") or files("jaeger.data") / "config.json"
+
+    if not USER_MODEL_PATH:
+        # Use default model from config
+        model_name = kwargs.get("model")
+        model_id = get_model_id(model_name)
+
+        model_paths = json_to_dict(CONFIG_PATH).get("model_paths")
+        info = AvailableModels(path=model_paths).info
+
+        model_info = info[model_name]
+    else:
+        # Use provided model path
+        info = AvailableModels(path=USER_MODEL_PATH).info
+        model_paths = USER_MODEL_PATH
+        model_name = next(iter(info))
+        model_id = get_model_id(model_name)
+        model_info = info[model_name]
+
+    MEMORY_LIMIT = 1024 * kwargs.get("mem", 4)
+    THREADS = kwargs.get("workers")
+    input_file_path = Path(kwargs.get("input"))
+    input_file = input_file_path.name
+    file_base = input_file_path.stem
+    # INPUT_FILE_MASKED = input_file_path.with_name(file_base + "_masked" + input_file_path.suffix)
+
+    OUTPUT_DIR = Path(kwargs.get("output")) / model_id
+    OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
+    # CHUNKS_DIR = OUTPUT_DIR / "chunks"
+    # MAKSED_DIR = OUTPUT_DIR / "masked"
+
+    log_file = Path(f"{file_base}_jaeger.log")
+    logger = get_logger(OUTPUT_DIR, log_file, level=kwargs.get("verbose"))
+    logger.info(
+        description(version("jaeger-bio")) + "\n{:-^80}".format("validating parameters")
+    )
+    logger.debug(info)
+    logger.debug(model_info)
+    try:
+        num = validate_fasta_entries(str(input_file_path), min_len=kwargs.get("fsize"))
+    except Exception as e:
+        logger.error(e)
+        logger.debug(traceback.format_exc())
+        sys.exit(1)
+
+    output_table_path = OUTPUT_DIR / f"{file_base}.tsv"
+    output_phage_table_path = OUTPUT_DIR / f"{file_base}_phages.tsv"
+
+    if output_table_path.exists() and not kwargs.get("overwrite"):
+        logger.error(
+            "output file exists. enable --overwrite option to overwrite the output file."
+        )
+        sys.exit(1)
+
+    if not model_info["graph"].exists():
+        logger.error(f"could not find model graph. please check {model_paths}")
+        sys.exit(1)
+    tf.config.threading.set_inter_op_parallelism_threads(THREADS)
+    tf.config.threading.set_intra_op_parallelism_threads(THREADS)
+    tf.config.set_soft_device_placement(True)
+    gpus = tf.config.list_physical_devices("GPU")
+    mode = None
+
+    if kwargs.get("cpu"):
+        mode = "CPU"
+        tf.config.set_visible_devices([], "GPU")
+        logger.info("CPU only mode selected")
+    elif gpus:
+        mode = "GPU"
+        tf.config.set_visible_devices([gpus[kwargs.get("physicalid")]], "GPU")
+        try:
+            tf.config.set_logical_device_configuration(
+                gpus[kwargs.get("physicalid")],
+                [
+                    tf.config.LogicalDeviceConfiguration(
+                        memory_limit=MEMORY_LIMIT, experimental_device_ordinal=10
+                    )
+                ],
+            )
+        except Exception as e:
+            logger.error(f"an error {e} occurred during virtual device initialization ")
+            logger.debug(traceback.format_exc())
+    else:
+        mode = "CPU"
+        logger.warning(
+            "could not find a GPU on the system. For optimal performance run Jaeger on a GPU."
+        )
+
+    logger.info(f"tensorflow: {version('tensorflow')}")
+    logger.info(f"input file: {input_file}")
+    logger.info(f"log file: {log_file.name}")
+    logger.info(f"outpath: {OUTPUT_DIR.resolve()}")
+    logger.info(f"fragment size: {kwargs.get('fsize')}")
+    logger.info(f"stride: {kwargs.get('stride')}")
+    logger.info(f"batch size: {kwargs.get('batch')}")
+    logger.info(f"mode: {mode}")
+    logger.info(f"model: {model_id}")
+    logger.info(f"avail mem: {psutil.virtual_memory().available / (GB_BYTES):.2f}GB")
+    logger.info(f"intra threads: {tf.config.threading.get_intra_op_parallelism_threads()}")
+    logger.info(f"inter threads: {tf.config.threading.get_inter_op_parallelism_threads()}")
+    logger.info(f"CPU time(s) : {current_process.cpu_times().user:.2f}")
+    logger.info(f"wall time(s) : {time.time() - current_process.create_time():.2f}")
+    logger.info(
+        f"memory usage : {current_process.memory_full_info().rss / GB_BYTES:.2f}GB ({current_process.memory_percent():.2f}%)"
+    )
+
+    device = tf.config.list_logical_devices(mode)
+    device_names = [get_device_name(i) for i in device]
+    logger.debug(f"{device}, {device_names}")
+    if len(device) > 1:
+        logger.info(f"Using MirroredStrategy {device_names}")
+        strategy = tf.distribute.MirroredStrategy(device_names)
+    else:
+        logger.info(f"Using OneDeviceStrategy {device_names}")
+        strategy = tf.distribute.OneDeviceStrategy(device_names[0])
+    # # 1. Split the input FASTA
+    # chunk_files = split_fasta_with_pyfastx(input_file_path, CHUNKS_DIR, chunks=16)
+    # # Or: chunk_files = split_fasta_with_pyfastx(input_fasta, chunks_dir, chunks=10)
+
+    # # 2. Run TRF on chunks in parallel
+    # masked_files = run_batch(chunk_files, out_dir=MAKSED_DIR, n_threads=16)
+
+    # # 3. Merge all masked files into one
+    # merge_masked_files(masked_files, INPUT_FILE_MASKED)
+
+    # # 4. Cleanup temporary directories
+    # shutil.rmtree(CHUNKS_DIR)
+    # shutil.rmtree(MAKSED_DIR)
+
+    term_repeats = scan_for_terminal_repeats(
+        # file_path=str(INPUT_FILE_MASKED),
+        file_path=str(input_file_path),
+        num=num,
+        workers=THREADS,
+        fsize=kwargs.get("fsize"),
+    )
+
+    model = InferModel(model_info)
+    string_processor_config = model.string_processor_config
+    input_dataset = tf.data.Dataset.from_generator(
+        fragment_generator(
+            #str(INPUT_FILE_MASKED),
+            file_path=str(input_file_path),
+            no_progress=False,
+            fragsize=kwargs.get("fsize"),
+            stride=kwargs.get("stride"),
+            num=num,
+        ),
+        output_signature=(tf.TensorSpec(shape=(), dtype=tf.string)),
+    )
+
+    from jaeger.preprocess.latest.convert import process_string_inference
+    #from icecream import ic
+    #ic(string_processor_config)
+    idataset = (
+        input_dataset.map(
+            process_string_inference(
+                    codons=string_processor_config.get("codon"),
+                    codon_num=string_processor_config.get("codon_id"),
+                    codon_depth=string_processor_config.get("codon_depth"),
+                    ngram_width=string_processor_config.get("ngram_width"),
+                    seq_onehot=string_processor_config.get("seq_onehot"),
+                    crop_size=string_processor_config.get("crop_size"),
+                    input_type=string_processor_config.get("input_type"),
+                    masking=string_processor_config.get("masking"),
+                    mutate=string_processor_config.get("mutate"),
+                    mutation_rate=string_processor_config.get("mutation_rate"),
+                    shuffle=string_processor_config.get("shuffle"),
+            ),
+            num_parallel_calls=tf.data.AUTOTUNE,
+        )
+        .batch(kwargs.get("batch"), num_parallel_calls=tf.data.AUTOTUNE)
+        .prefetch(25)
+    )
+
+    with strategy.scope():
+        try:
+            logger.info("starting model inference")
+            y_pred = model.predict(idataset, no_progress=True)
+        except Exception as e:
+            logger.debug(traceback.format_exc())
+            logger.error(
+                f"an error {e} occured during inference on {'|'.join(device_names)}! check {log_file} for traceback."
+            )
+            sys.exit(1)
+
+        from jaeger.postprocess.collect import pred_to_dict, write_output
+
+        if kwargs.get("getalllabels"):
+            pass
+        data, _ = pred_to_dict(
+            y_pred,
+            class_map=model.class_map,
+            fsize=kwargs.get("fsize"),
+            term_repeats=term_repeats,
+        )
+        #print(len(data['headers']))
+        num_written = write_output(
+            data,
+            labels=model.class_map.get("class"),
+            indices=model.class_map.get("index"),
+            output_table_path=output_table_path,
+            output_phage_table_path=output_phage_table_path,
+        )
+
+        logger.info(f"processed {num_written}/{num} sequences")
+        logger.info(f"CPU time(s) : {current_process.cpu_times().user:.2f}")
+        logger.info(f"wall time(s) : {time.time() - current_process.create_time():.2f}")
+        logger.info(
+            f"memory usage : {current_process.memory_full_info().rss / GB_BYTES:.2f}GB ({current_process.memory_percent():.2f}%)"
+        )
+        import numpy as np
+        if 'embedding' in y_pred:
+            np.savez(OUTPUT_DIR / f"{file_base}_embedding.npz", embedding=y_pred['embedding'], headers=y_pred["meta_0"])
+        if 'nmd' in y_pred:
+            np.savez(OUTPUT_DIR / f"{file_base}_nmd.npz", embedding=y_pred['nmd'], headers=y_pred["meta_0"])
+        # INPUT_FILE_MASKED.unlink() 
