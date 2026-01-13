182e568cfb580b65c8c6373e4eed4a50c30356b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 21:05:24 +0000

    Add package business-card-qr/0.1.0/business_card_qr-0.1.0-py3-none-any.whl

---
+++ packages/business-card-qr/business_card_qr-0.1.0-py3-none-any.whl/business_card_qr/__init__.py
@@ -0,0 +1,9 @@
+"""Business Card QR Code Generator"""
+
+__version__ = "0.1.0"
+__author__ = "Habib Ben Kehdher"
+__email__ = "habibbenkheder5@gmail.com"
+
+from .qr_generator import BusinessCardQRGenerator
+
+__all__ = ['BusinessCardQRGenerator']
