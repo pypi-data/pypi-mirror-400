e9d0f437275727ce6662e24fded163282b582bdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 21:07:07 +0000

    Add package advdbg/0.2.6/advdbg-0.2.6.tar.gz

---
+++ packages/advdbg/advdbg-0.2.6.tar.gz/advdbg-0.2.6/advdbg/colors.py
@@ -0,0 +1,14 @@
+class Colors:
+	
+	ERROR = '\033[38;2;255;200;200m'
+	SUCCESS = '\033[38;2;200;255;200m'
+	WARN = '\033[38;2;255;255;200m'  
+	INFO = '\033[38;2;200;255;255m'
+	
+	# Legacy
+	
+	VIOLETTE = '\033[90m'
+	
+	# Another
+
+	RESET = '\033[0m'
