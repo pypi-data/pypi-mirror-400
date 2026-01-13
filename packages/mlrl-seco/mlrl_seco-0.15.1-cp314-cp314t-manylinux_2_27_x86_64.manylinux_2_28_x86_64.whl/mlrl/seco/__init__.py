244abcdee2c0383f596b581b5d4625bef8bfb5bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 05:08:16 +0000

    Add package mlrl-seco/0.15.1/mlrl_seco-0.15.1-cp314-cp314t-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/mlrl-seco/mlrl_seco-0.15.1-cp314-cp314t-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl/mlrl/seco/__init__.py
@@ -0,0 +1,12 @@
+"""
+This module provides rule learners that make use of the Separate-and-Conquer (SeCo) paradigm.
+"""
+from mlrl.seco.learners import SeCoClassifier
+
+try:
+    from mlrl.seco.testbed.runnables import SeCoRunnable as Runnable
+except ImportError:
+    # Dependency 'mlrl-testbed' is not available, but that's okay since it's optional.
+    pass
+
+SeCo = SeCoClassifier
