fb59fee8dbf5f91135e8f3b5ea8857b97a96a9d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 03:32:06 +0000

    Add package pyfasthttp/0.1.8/pyfasthttp-0.1.8-py3-none-any.whl

---
+++ packages/pyfasthttp/pyfasthttp-0.1.8-py3-none-any.whl/fasthttp/logging.py
@@ -0,0 +1,13 @@
+import logging
+
+
+def get_logger(name: str = "fasthttp", level: int = logging.INFO) -> logging.Logger:
+    logger = logging.getLogger(name)
+    if not logger.handlers:
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter("[%(levelname)s] %(message)s")
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+    logger.setLevel(level)
+    logger.propagate = False
+    return logger
