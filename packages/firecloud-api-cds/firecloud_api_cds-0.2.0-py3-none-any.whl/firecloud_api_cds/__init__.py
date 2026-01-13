ce7c8a14828d419bec658e80ac5d38c8149e837c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 19:07:17 +0000

    Add package firecloud-api-cds/0.2.0/firecloud_api_cds-0.2.0-py3-none-any.whl

---
+++ packages/firecloud-api-cds/firecloud_api_cds-0.2.0-py3-none-any.whl/firecloud_api_cds/__init__.py
@@ -0,0 +1,11 @@
+from importlib import metadata as importlib_metadata
+
+
+def get_version() -> str:
+    try:
+        return importlib_metadata.version(__name__)
+    except importlib_metadata.PackageNotFoundError:
+        return "unknown"
+
+
+version: str = get_version()
