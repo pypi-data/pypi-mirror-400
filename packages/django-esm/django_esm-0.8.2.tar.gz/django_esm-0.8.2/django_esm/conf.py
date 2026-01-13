87ae5b5961f5c835f20da7c324ab7b191ae81e3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 22:07:02 +0000

    Add package django-esm/0.8.2/django_esm-0.8.2.tar.gz

---
+++ packages/django-esm/django_esm-0.8.2.tar.gz/django_esm-0.8.2/django_esm/conf.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+
+from django.conf import settings
+
+__all__ = ["get_settings"]
+
+
+def get_settings():
+    return type(
+        "Settings",
+        (),
+        {
+            "PACKAGE_DIR": Path(getattr(settings, "BASE_DIR", "")),
+            "STATIC_DIR": Path(settings.STATIC_ROOT) / "esm",
+            "STATIC_PREFIX": "esm",
+            **getattr(settings, "ESM", {}),
+        },
+    )
