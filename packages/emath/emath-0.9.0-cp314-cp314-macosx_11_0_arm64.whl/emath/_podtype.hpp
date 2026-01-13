0b37e85d4bedf93d72b772a2222d0f1b1972d9d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 06:04:17 +0000

    Add package emath/0.9.0/emath-0.9.0-cp314-cp314-macosx_11_0_arm64.whl

---
+++ packages/emath/emath-0.9.0-cp314-cp314-macosx_11_0_arm64.whl/emath/_podtype.hpp
@@ -0,0 +1,143 @@
+
+// generated from codegen/templates/_podtype.hpp
+
+#ifndef E_MATH_PODTYPE_HPP
+#define E_MATH_PODTYPE_HPP
+
+// python
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
+
+
+
+struct BArray
+{
+    PyObject_HEAD
+    PyObject *weakreflist;
+    size_t length;
+    bool *pod;
+};
+
+
+
+struct DArray
+{
+    PyObject_HEAD
+    PyObject *weakreflist;
+    size_t length;
+    double *pod;
+};
+
+
+
+struct FArray
+{
+    PyObject_HEAD
+    PyObject *weakreflist;
+    size_t length;
+    float *pod;
+};
+
+
+
+struct I8Array
+{
+    PyObject_HEAD
+    PyObject *weakreflist;
+    size_t length;
+    int8_t *pod;
+};
+
+
+
+struct U8Array
+{
+    PyObject_HEAD
+    PyObject *weakreflist;
+    size_t length;
+    uint8_t *pod;
+};
+
+
+
+struct I16Array
+{
+    PyObject_HEAD
+    PyObject *weakreflist;
+    size_t length;
+    int16_t *pod;
+};
+
+
+
+struct U16Array
+{
+    PyObject_HEAD
+    PyObject *weakreflist;
+    size_t length;
+    uint16_t *pod;
+};
+
+
+
+struct I32Array
+{
+    PyObject_HEAD
+    PyObject *weakreflist;
+    size_t length;
+    int32_t *pod;
+};
+
+
+
+struct U32Array
+{
+    PyObject_HEAD
+    PyObject *weakreflist;
+    size_t length;
+    uint32_t *pod;
+};
+
+
+
+struct IArray
+{
+    PyObject_HEAD
+    PyObject *weakreflist;
+    size_t length;
+    int *pod;
+};
+
+
+
+struct UArray
+{
+    PyObject_HEAD
+    PyObject *weakreflist;
+    size_t length;
+    unsigned int *pod;
+};
+
+
+
+struct I64Array
+{
+    PyObject_HEAD
+    PyObject *weakreflist;
+    size_t length;
+    int64_t *pod;
+};
+
+
+
+struct U64Array
+{
+    PyObject_HEAD
+    PyObject *weakreflist;
+    size_t length;
+    uint64_t *pod;
+};
+
+
+
+#endif
