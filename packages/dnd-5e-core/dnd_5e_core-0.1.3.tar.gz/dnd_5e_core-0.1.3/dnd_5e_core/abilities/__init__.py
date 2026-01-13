e3f87b3ee7f4785e4f99f1c665915d97b0ed91da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 09:09:53 +0000

    Add package dnd-5e-core/0.1.3/dnd_5e_core-0.1.3.tar.gz

---
+++ packages/dnd-5e-core/dnd_5e_core-0.1.3.tar.gz/dnd_5e_core-0.1.3/dnd_5e_core/abilities/__init__.py
@@ -0,0 +1,9 @@
+"""
+D&D 5e Core - Abilities Module
+Contains the six core abilities and related systems
+"""
+
+from .abilities import AbilityType, Abilities
+
+__all__ = ['AbilityType', 'Abilities']
+
---
+++ packages/dnd-5e-core/dnd_5e_core-0.1.3.tar.gz/dnd_5e_core-0.1.3/dnd_5e_core/spells/__init__.py
@@ -0,0 +1,10 @@
+"""
+D&D 5e Core - Spells Module
+Contains all spellcasting-related classes
+"""
+
+from .spell import Spell
+from .spellcaster import SpellCaster
+
+__all__ = ['Spell', 'SpellCaster']
+
