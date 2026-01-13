e7878001603a9f024a537ea737361aebd4949e81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 06:04:01 +0000

    Add package django-asgi-lifespan/0.5.1/django_asgi_lifespan-0.5.1-py3-none-any.whl

---
+++ packages/django-asgi-lifespan/django_asgi_lifespan-0.5.1-py3-none-any.whl/django_asgi_lifespan/__init__.py
@@ -0,0 +1,7 @@
+"""Django ASGI handler with Lifespan Protocol support."""
+
+from __future__ import annotations
+
+__author__ = """Václav Dohnal"""
+__email__ = "vaclav.dohnal@gmail.com"
+__version__ = "0.5.1"
---
+++ packages/django-asgi-lifespan/django_asgi_lifespan-0.5.1-py3-none-any.whl/django_asgi_lifespan/middleware.py
@@ -0,0 +1,35 @@
+import logging
+from collections.abc import Awaitable, Callable
+from typing import Final
+
+from asgiref.sync import iscoroutinefunction, markcoroutinefunction
+from django.core.handlers.asgi import ASGIRequest
+from django.http import HttpRequest, HttpResponseBase
+
+logger: Final = logging.getLogger(__name__)
+__all__ = ["AsyncMiddleware", "LifespanStateMiddleware"]
+
+
+class LifespanStateMiddleware:
+    async_capable = True
+    sync_capable = False
+
+    def __init__(
+        self, get_response: Callable[[HttpRequest], Awaitable[HttpResponseBase]]
+    ) -> None:
+        self.get_response = get_response
+
+        if iscoroutinefunction(self.get_response):
+            markcoroutinefunction(self)
+
+    async def __call__(self, request: HttpRequest) -> HttpResponseBase:
+        if isinstance(request, ASGIRequest):
+            request.state = request.scope["state"]
+        else:
+            request.state = {}
+
+        return await self.get_response(request)
+
+
+# Leave this alias here for backwards compatibility
+AsyncMiddleware: Final = LifespanStateMiddleware
