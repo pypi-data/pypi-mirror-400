96c95905124acdd2dd70ec0756044b2d5248b5f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 12:06:32 +0000

    Add package adrs/1.0.3/adrs-1.0.3-cp312-cp312-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/adrs/adrs-1.0.3-cp312-cp312-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/adrs/search/grid.py
@@ -0,0 +1,17 @@
+from itertools import product
+from typing import Sequence, override
+
+from adrs.search import Search, ParameterGrid, Permutation
+
+
+class GridSearch(Search):
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def id() -> str:
+        return "gridsearch"
+
+    @override
+    def search(self, grid: ParameterGrid) -> Sequence[Permutation]:
+        return [dict(zip(grid, combo)) for combo in product(*grid.values())]  # type: ignore
