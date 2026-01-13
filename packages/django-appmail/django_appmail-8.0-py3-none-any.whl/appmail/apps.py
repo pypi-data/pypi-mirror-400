61d00e5fec1412aa1f0953f1d02732c7cf0f7a34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 18:09:49 +0000

    Add package django-appmail/8.0/django_appmail-8.0-py3-none-any.whl

---
+++ packages/django-appmail/django_appmail-8.0-py3-none-any.whl/appmail/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class AppmailConfig(AppConfig):
+    name = "appmail"
+    verbose_name = "Application Mail"
+    default_auto_field = "django.db.models.AutoField"
