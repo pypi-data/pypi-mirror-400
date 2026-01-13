428810f2ac30e45bac63eb514b737bb422ab36e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 18:11:11 +0000

    Add package django-request-token/2.4.0/django_request_token-2.4.0-py3-none-any.whl

---
+++ packages/django-request-token/django_request_token-2.4.0-py3-none-any.whl/request_token/apps.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+from django.apps import AppConfig
+
+
+class RequestTokenAppConfig(AppConfig):
+    """AppConfig for request_token app."""
+
+    name = "request_token"
+    verbose_name = "JWT Request Tokens"
+    default_auto_field = "django.db.models.AutoField"
