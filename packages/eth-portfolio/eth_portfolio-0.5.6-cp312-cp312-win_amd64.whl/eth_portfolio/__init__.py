5aae71dd7a7613a530ef6343e934caecd777c4f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 15:07:47 +0000

    Add package eth-portfolio/0.5.6/eth_portfolio-0.5.6-cp312-cp312-win_amd64.whl

---
+++ packages/eth-portfolio/eth_portfolio-0.5.6-cp312-cp312-win_amd64.whl/eth_portfolio/__init__.py
@@ -0,0 +1,25 @@
+import warnings
+
+warnings.filterwarnings(
+    "ignore",
+    message="Changing decimals precision could have unintended side effects!",
+    category=UserWarning,
+)
+
+
+import a_sync._smart
+
+a_sync._smart.set_smart_task_factory()
+
+
+from eth_portfolio.portfolio import Portfolio, portfolio
+
+# make sure we init the extended db before we init ypm somewhere
+from eth_portfolio._db import utils
+from eth_portfolio._shitcoins import SHITCOINS
+
+__all__ = [
+    "Portfolio",
+    "portfolio",
+    "SHITCOINS",
+]
