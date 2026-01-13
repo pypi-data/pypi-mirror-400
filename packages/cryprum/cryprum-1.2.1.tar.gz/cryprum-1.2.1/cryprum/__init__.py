c9749309a241cdfd3f6ed447099973d6a6f482b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 12:06:21 +0000

    Add package cryprum/1.2.1/cryprum-1.2.1.tar.gz

---
+++ packages/cryprum/cryprum-1.2.1.tar.gz/cryprum-1.2.1/cryprum/__init__.py
@@ -0,0 +1,25 @@
+import logging
+
+logger = logging.getLogger("cryprum")
+
+
+try:
+    from .api import Client
+    from .asyncapi import AsyncClient
+    from .exceptions import CryprumError
+except ImportError as e:
+    logger.exception(e)
+
+    def Client(*args, **kwargs):
+        import sys
+
+        print(
+            "The cryprum client could not init because the required "
+            "dependencies were not installed.\nMake sure you've installed "
+            "everything with: pip install cryprum"
+        )
+        sys.exit(1)
+
+    AsyncClient = Client
+
+__all__ = ["Client", "AsyncClient", "CryprumError"]
