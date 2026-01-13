5e984f65cffbb8aabb27a844389be8d29b80473f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 06:04:07 +0000

    Add package picows/1.10.2/picows-1.10.2-cp314-cp314-manylinux2014_aarch64.manylinux_2_17_aarch64.manylinux_2_28_aarch64.whl

---
+++ packages/picows/picows-1.10.2-cp314-cp314-manylinux2014_aarch64.manylinux_2_17_aarch64.manylinux_2_28_aarch64.whl/picows/__init__.py
@@ -0,0 +1,35 @@
+from .picows import (
+    WSError,
+    WSMsgType,
+    WSCloseCode,
+    WSAutoPingStrategy,
+    WSFrame,
+    WSTransport,
+    WSListener,
+    WSUpgradeRequest,
+    WSUpgradeResponse,
+    WSUpgradeResponseWithListener,
+    ws_connect,
+    ws_create_server,
+    PICOWS_DEBUG_LL
+)
+
+
+__all__ = [
+    'WSError',
+    'WSMsgType',
+    'WSCloseCode',
+    'WSAutoPingStrategy',
+    'WSFrame',
+    'WSTransport',
+    'WSListener',
+    'WSUpgradeRequest',
+    'WSUpgradeResponse',
+    'WSUpgradeResponseWithListener',
+    'ws_connect',
+    'ws_create_server',
+    'PICOWS_DEBUG_LL'
+]
+
+__version__ = "1.10.2"
+__author__ = "Taras Kozlov"
