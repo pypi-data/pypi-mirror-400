f1501b499fe33f5168c909f96ce6e8cb06d95c2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 05:08:04 +0000

    Add package mlrl-common/0.15.1/mlrl_common-0.15.1-cp312-cp312-musllinux_1_2_aarch64.whl

---
+++ packages/mlrl-common/mlrl_common-0.15.1-cp312-cp312-musllinux_1_2_aarch64.whl/mlrl/common/testbed/experiments/output/model_text/writer.py
@@ -0,0 +1,61 @@
+"""
+Author: Michael Rapp (michael.rapp.ml@gmail.com)
+
+Provides classes that allow writing textual representations of models to one or several sinks.
+"""
+import logging as log
+
+from typing import List, Tuple, override
+
+from mlrl.common.cython.rule_model import RuleModel
+from mlrl.common.mixins import ClassifierMixin, RegressorMixin
+from mlrl.common.testbed.experiments.output.model_text.model_text import RuleModelAsText
+
+from mlrl.testbed_sklearn.experiments.dataset import TabularDataset
+
+from mlrl.testbed.experiments.input.data import TextualInputData
+from mlrl.testbed.experiments.output.data import OutputData
+from mlrl.testbed.experiments.output.sinks import Sink
+from mlrl.testbed.experiments.output.writer import DataExtractor, ResultWriter, TextualDataExtractor
+from mlrl.testbed.experiments.state import ExperimentState
+
+
+class RuleModelAsTextWriter(ResultWriter):
+    """
+    Allows to write textual representations of models to one or several sinks.
+    """
+
+    class DefaultExtractor(DataExtractor):
+        """
+        Allows to extract textual representation of rules from a `RuleModel`.
+        """
+
+        @override
+        def extract_data(self, state: ExperimentState, _: List[Sink]) -> List[Tuple[ExperimentState, OutputData]]:
+            """
+            See :func:`mlrl.testbed.experiments.output.writer.DataExtractor.extract_data`
+            """
+            dataset = state.dataset_as(TabularDataset)
+            learner = state.learner_as(ClassifierMixin, RegressorMixin)
+
+            if dataset and learner:
+                model = learner.model_
+
+                if isinstance(model, RuleModel):
+                    return [(state, RuleModelAsText(model, dataset))]
+
+                log.error('%s expected type of model to be %s, but model has type %s',
+                          type(self).__name__, RuleModel.__name__,
+                          type(model).__name__)
+
+            return []
+
+    def __init__(self, *extractors: DataExtractor):
+        """
+        :param extractors: Extractors that should be used for extracting the output data to be written to the sinks
+        """
+        super().__init__(TextualDataExtractor(properties=RuleModelAsText.PROPERTIES, context=RuleModelAsText.CONTEXT),
+                         *extractors,
+                         RuleModelAsTextWriter.DefaultExtractor(),
+                         input_data=TextualInputData(properties=RuleModelAsText.PROPERTIES,
+                                                     context=RuleModelAsText.CONTEXT))
