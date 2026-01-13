639aa33660db328d675ca25800a109814c75bfff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 20:05:45 +0000

    Add package le-map-tools/0.36.5/le_map_tools-0.36.5.tar.gz

---
+++ packages/le-map-tools/le_map_tools-0.36.5.tar.gz/le_map_tools-0.36.5/le_map_tools/__init__.py
@@ -0,0 +1,62 @@
+"""Top-level package for le_map_tools."""
+
+__author__ = """Qiusheng Wu"""
+__email__ = "giswqs@gmail.com"
+__version__ = "0.35.3"
+
+import os
+
+
+def in_colab_shell():
+    """Tests if the code is being executed within Google Colab."""
+    import sys
+
+    if "google.colab" in sys.modules:
+        return True
+    else:
+        return False
+
+
+def use_folium():
+    """Whether to use the folium or ipyleaflet plotting backend."""
+    if os.environ.get("USE_FOLIUM") is not None:
+        return True
+    else:
+        return False
+
+
+def _use_eerepr(token="USE_EEREPR"):
+    """Whether to use lerepr for printing Earth Engine objects.
+
+    Returns:
+        bool: True if lerepr is used for printing Earth Engine objects.
+    """
+
+    if os.environ.get(token) is None:
+        return True
+    else:
+        return False
+
+
+if use_folium():
+    from .foliumap import *
+else:
+    try:
+        from .le_map_tools import *
+    except Exception as e:
+        if in_colab_shell():
+            print(
+                "Please restart Colab runtime after installation if you encounter any errors when importing le_map_tools."
+            )
+        else:
+            print(
+                "Please restart Jupyter kernel after installation if you encounter any errors when importing le_map_tools."
+            )
+        raise e
+
+if _use_eerepr():
+    import lerepr
+
+    lerepr.initialize()
+
+from .report import Report
