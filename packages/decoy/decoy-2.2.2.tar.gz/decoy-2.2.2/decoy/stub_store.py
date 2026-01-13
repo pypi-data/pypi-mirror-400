03b25e826e4c1534a16a1e930ebbeb830d7c7a65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 07:08:08 +0000

    Add package decoy/2.2.2/decoy-2.2.2.tar.gz

---
+++ packages/decoy/decoy-2.2.2.tar.gz/decoy-2.2.2/decoy/stub_store.py
@@ -0,0 +1,69 @@
+"""Stub creation and storage."""
+
+from typing import Any, Callable, List, NamedTuple, Optional, Union
+
+from .spy_events import SpyEvent, WhenRehearsal, match_event
+
+
+class _MISSING:
+    pass
+
+
+MISSING = _MISSING()
+"""Value not specified sentinel.
+
+Used when `None` could be a valid value,
+so `Optional` would be inappropriate.
+"""
+
+
+class StubBehavior(NamedTuple):
+    """A recorded stub behavior."""
+
+    return_value: Optional[Any] = None
+    context_value: Union[_MISSING, Any] = MISSING
+    error: Optional[Exception] = None
+    action: Optional[Callable[..., Any]] = None
+    once: bool = False
+
+
+class StubEntry(NamedTuple):
+    """An entry in the StubStore for later behavior lookup."""
+
+    rehearsal: WhenRehearsal
+    behavior: StubBehavior
+
+
+class StubStore:
+    """Stored stub behaviors."""
+
+    def __init__(self) -> None:
+        """Initialize a StubStore with an empty stubbings list."""
+        self._stubs: List[StubEntry] = []
+
+    def add(
+        self,
+        rehearsal: WhenRehearsal,
+        behavior: StubBehavior,
+    ) -> None:
+        """Create and add a new StubBehavior to the store."""
+        self._stubs.append(StubEntry(rehearsal=rehearsal, behavior=behavior))
+
+    def get_by_call(self, call: SpyEvent) -> Optional[StubBehavior]:
+        """Get the latest StubBehavior matching this call."""
+        reversed_indices = range(len(self._stubs) - 1, -1, -1)
+
+        for i in reversed_indices:
+            stub = self._stubs[i]
+
+            if match_event(call, stub.rehearsal):
+                if stub.behavior.once:
+                    self._stubs.pop(i)
+
+                return stub.behavior
+
+        return None
+
+    def clear(self) -> None:
+        """Remove all stored Stubs."""
+        self._stubs.clear()
---
+++ packages/decoy/decoy-2.2.2.tar.gz/decoy-2.2.2/decoy/verifier.py
@@ -0,0 +1,49 @@
+"""Spy call verification."""
+
+from typing import Optional, Sequence
+
+from .errors import VerifyError
+from .spy_events import SpyEvent, VerifyRehearsal, match_event
+
+# ensure decoy.verifier does not pollute Pytest tracebacks
+__tracebackhide__ = True
+
+
+class Verifier:
+    """An interface to verify that spies were called as expected."""
+
+    @staticmethod
+    def verify(
+        rehearsals: Sequence[VerifyRehearsal],
+        calls: Sequence[SpyEvent],
+        times: Optional[int] = None,
+    ) -> None:
+        """Verify that a list of calls satisfies a given list of rehearsals.
+
+        Arguments:
+            rehearsals: Rehearsal calls to verify.
+            calls: All calls made to the spies in the rehearsals list.
+            times: Number of times the rehearsal sequence should appear in calls.
+                If omitted, will look for "at least once."
+
+        Raises:
+            VerifyError: the actual calls to the spy did not match the given
+                rehearsals.
+        """
+        match_count = 0
+
+        for i in range(len(calls)):
+            calls_subset = calls[i : i + len(rehearsals)]
+            matches = [match_event(c, r) for c, r in zip(calls_subset, rehearsals)]
+
+            if all(matches) and len(calls_subset) == len(rehearsals):
+                match_count = match_count + 1
+
+        calls_verified = match_count != 0 if times is None else match_count == times
+
+        if not calls_verified:
+            raise VerifyError.create(
+                rehearsals=rehearsals,
+                calls=calls,
+                times=times,
+            )
