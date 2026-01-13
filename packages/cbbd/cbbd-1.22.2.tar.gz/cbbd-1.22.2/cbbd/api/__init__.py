2599a53b331606c2816554fbeeafa2c6e2559b49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 07:07:53 +0000

    Add package cbbd/1.22.2/cbbd-1.22.2.tar.gz

---
+++ packages/cbbd/cbbd-1.22.2.tar.gz/cbbd-1.22.2/cbbd/api/__init__.py
@@ -0,0 +1,16 @@
+# flake8: noqa
+
+# import apis into api package
+from cbbd.api.conferences_api import ConferencesApi
+from cbbd.api.draft_api import DraftApi
+from cbbd.api.games_api import GamesApi
+from cbbd.api.lines_api import LinesApi
+from cbbd.api.lineups_api import LineupsApi
+from cbbd.api.plays_api import PlaysApi
+from cbbd.api.rankings_api import RankingsApi
+from cbbd.api.ratings_api import RatingsApi
+from cbbd.api.recruiting_api import RecruitingApi
+from cbbd.api.stats_api import StatsApi
+from cbbd.api.teams_api import TeamsApi
+from cbbd.api.venues_api import VenuesApi
+
