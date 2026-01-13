8f624c4f284d3b55c40bbb5f1e37474eb6cc86e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 21:07:08 +0000

    Add package dlubal.api/2.12.13.post1/dlubal_api-2.12.13.post1-py3-none-any.whl

---
+++ packages/dlubal.api/dlubal_api-2.12.13.post1-py3-none-any.whl/dlubal/api/rfem/rsection_stresses/__init__.py
@@ -0,0 +1,1 @@
+from .member_configuration_pb2 import *
---
+++ packages/dlubal.api/dlubal_api-2.12.13.post1-py3-none-any.whl/dlubal/api/rsection/rsection_stresses/member_configuration_pb2.pyi
@@ -0,0 +1,54 @@
+from dlubal.api.common import common_pb2 as _common_pb2
+from dlubal.api.rsection import referenced_object_pb2 as _referenced_object_pb2
+from google.protobuf.internal import containers as _containers
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar, Iterable as _Iterable, Mapping as _Mapping, Optional as _Optional, Union as _Union
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class MemberConfiguration(_message.Message):
+    __slots__ = ("no", "user_defined_name_enabled", "name", "assigned_to_members", "assigned_to_member_sets", "special_settings", "comment", "assigned_to_deep_beams", "assigned_to_shear_walls", "id_for_export_import", "metadata_for_export_import")
+    class SpecialSettingsTreeTable(_message.Message):
+        __slots__ = ("rows",)
+        ROWS_FIELD_NUMBER: _ClassVar[int]
+        rows: _containers.RepeatedCompositeFieldContainer[MemberConfiguration.SpecialSettingsTreeTableRow]
+        def __init__(self, rows: _Optional[_Iterable[_Union[MemberConfiguration.SpecialSettingsTreeTableRow, _Mapping]]] = ...) -> None: ...
+    class SpecialSettingsTreeTableRow(_message.Message):
+        __slots__ = ("key", "caption", "symbol", "value", "unit", "rows")
+        KEY_FIELD_NUMBER: _ClassVar[int]
+        CAPTION_FIELD_NUMBER: _ClassVar[int]
+        SYMBOL_FIELD_NUMBER: _ClassVar[int]
+        VALUE_FIELD_NUMBER: _ClassVar[int]
+        UNIT_FIELD_NUMBER: _ClassVar[int]
+        ROWS_FIELD_NUMBER: _ClassVar[int]
+        key: str
+        caption: str
+        symbol: str
+        value: _common_pb2.Value
+        unit: str
+        rows: _containers.RepeatedCompositeFieldContainer[MemberConfiguration.SpecialSettingsTreeTableRow]
+        def __init__(self, key: _Optional[str] = ..., caption: _Optional[str] = ..., symbol: _Optional[str] = ..., value: _Optional[_Union[_common_pb2.Value, _Mapping]] = ..., unit: _Optional[str] = ..., rows: _Optional[_Iterable[_Union[MemberConfiguration.SpecialSettingsTreeTableRow, _Mapping]]] = ...) -> None: ...
+    NO_FIELD_NUMBER: _ClassVar[int]
+    USER_DEFINED_NAME_ENABLED_FIELD_NUMBER: _ClassVar[int]
+    NAME_FIELD_NUMBER: _ClassVar[int]
+    ASSIGNED_TO_MEMBERS_FIELD_NUMBER: _ClassVar[int]
+    ASSIGNED_TO_MEMBER_SETS_FIELD_NUMBER: _ClassVar[int]
+    SPECIAL_SETTINGS_FIELD_NUMBER: _ClassVar[int]
+    COMMENT_FIELD_NUMBER: _ClassVar[int]
+    ASSIGNED_TO_DEEP_BEAMS_FIELD_NUMBER: _ClassVar[int]
+    ASSIGNED_TO_SHEAR_WALLS_FIELD_NUMBER: _ClassVar[int]
+    ID_FOR_EXPORT_IMPORT_FIELD_NUMBER: _ClassVar[int]
+    METADATA_FOR_EXPORT_IMPORT_FIELD_NUMBER: _ClassVar[int]
+    no: int
+    user_defined_name_enabled: bool
+    name: str
+    assigned_to_members: _containers.RepeatedScalarFieldContainer[int]
+    assigned_to_member_sets: _containers.RepeatedScalarFieldContainer[int]
+    special_settings: MemberConfiguration.SpecialSettingsTreeTable
+    comment: str
+    assigned_to_deep_beams: _containers.RepeatedScalarFieldContainer[int]
+    assigned_to_shear_walls: _containers.RepeatedScalarFieldContainer[int]
+    id_for_export_import: str
+    metadata_for_export_import: str
+    def __init__(self, no: _Optional[int] = ..., user_defined_name_enabled: bool = ..., name: _Optional[str] = ..., assigned_to_members: _Optional[_Iterable[int]] = ..., assigned_to_member_sets: _Optional[_Iterable[int]] = ..., special_settings: _Optional[_Union[MemberConfiguration.SpecialSettingsTreeTable, _Mapping]] = ..., comment: _Optional[str] = ..., assigned_to_deep_beams: _Optional[_Iterable[int]] = ..., assigned_to_shear_walls: _Optional[_Iterable[int]] = ..., id_for_export_import: _Optional[str] = ..., metadata_for_export_import: _Optional[str] = ...) -> None: ...
