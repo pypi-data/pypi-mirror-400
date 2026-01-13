f09fa054040732880df0d21af676d13292cf03c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 22:06:51 +0000

    Add package baihu-logging/1.0.0/baihu_logging-1.0.0-py3-none-any.whl

---
+++ packages/baihu-logging/baihu_logging-1.0.0-py3-none-any.whl/baihu_logging/constants.py
@@ -0,0 +1,11 @@
+LOG_COLORS = {
+    "DEBUG": 0x95A5A6,
+    "INFO": 0x5865F2,
+    "WARNING": 0xF1C40F,
+    "ERROR": 0xE74C3C,
+    "CRITICAL": 0x9B59B6
+}
+
+LOG_LEVELS = list(LOG_COLORS.keys())
+
+BASE_URL = "https://discord.com/api/v10"
