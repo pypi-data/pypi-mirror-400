747c35b724d08cde6f58e43ad2bb7e1143d2bbca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 21:05:51 +0000

    Add package django-xprez/0.2.36/django_xprez-0.2.36-py3-none-any.whl

---
+++ packages/django-xprez/django_xprez-0.2.36-py3-none-any.whl/xprez/ck_editor/parse_text.py
@@ -0,0 +1,104 @@
+import io
+import urllib
+
+from bs4 import BeautifulSoup
+from django.template import Context, Template
+from django.utils.safestring import mark_safe
+from PIL import Image
+
+
+def _replace_wrapper_with_templatetag(wrapper, img, request):
+    # wrapper and img may be the same tag - that is the case of inline images
+    src = img.get("src")
+
+    if not src:
+        wrapper.extract()
+        return
+
+    if not src.lower().startswith("http") and request:
+        src = request.build_absolute_uri(src)
+
+    request = urllib.request.Request(
+        src, headers={"User-agent": ""}  # needed for cloudflare
+    )
+
+    try:
+        file = io.BytesIO(urllib.request.urlopen(request).read())
+    except Exception as e:
+        return
+
+    im = Image.open(file)
+    width, height = im.size
+    classes = list(img.get("class", [])) + list(wrapper.get("class", []))
+    if "image-style-align-right" in classes:
+        align = "right"
+    elif "image-style-align-left" in classes:
+        align = "left"
+    else:
+        align = "center"
+
+    caption_tag = wrapper.find("figcaption")
+    if caption_tag:
+        caption = caption_tag.text
+    else:
+        caption = ""
+
+    alt_text = img.get("alt", "")
+
+    tag_link = wrapper.find("a")
+    if tag_link:
+        link_url = tag_link.get("href")
+        link_new_window = tag_link.get("target") == "_blank"
+    else:
+        link_url = ""
+        link_new_window = False
+
+    wrapper.replaceWith(
+        '{%% ckeditor_content_image "%s" "%s" %s %s caption="%s" alt_text="%s" link_url="%s" link_new_window=%s %%}'
+        % (
+            src,
+            align,
+            width,
+            height,
+            caption,
+            alt_text,
+            link_url,
+            "True" if link_new_window else "False",
+        )
+    )
+
+
+def parse_text(text_source, request):
+    if not text_source:
+        return ""
+    soup = BeautifulSoup(text_source, "html5lib")
+
+    # extract empty p elements from end of text
+    for last_p in reversed(soup.find_all("p")):
+        if last_p.text:
+            break
+        else:
+            last_p.extract()
+
+    for wrapper in soup.find_all("figure", attrs={"class": "image"}):
+        img = wrapper.find("img")
+        if img:
+            _replace_wrapper_with_templatetag(wrapper, img, request)
+
+    inline_images = []
+    inline_images += soup.find_all("img", attrs={"class": "image-style-align-left"})
+    inline_images += soup.find_all("img", attrs={"class": "image-style-align-right"})
+    for img in inline_images:
+        _replace_wrapper_with_templatetag(img, img, request)
+
+    for tag in soup.find_all(attrs={"contenteditable": True}):
+        del tag["contenteditable"]
+
+    return str(soup)
+
+
+def render_text_parsed(text_parsed, extra_context={}):
+    t = Template("{% load xprez %}" + text_parsed)
+    c = Context({})
+    c.update(extra_context)
+    return mark_safe(t.render(c))
---
+++ packages/django-xprez/django_xprez-0.2.36-py3-none-any.whl/xprez/ck_editor/widgets.py
@@ -0,0 +1,178 @@
+import json
+
+from django import forms
+from django.urls import reverse
+
+from xprez.conf import settings
+
+
+class CkEditorWidgetBase(forms.widgets.Textarea):
+    template_name = "xprez/widgets/ck_editor.html"
+
+    def get_config(self, file_upload_dir=None):
+        raise NotImplementedError
+
+    class Media:
+        css = {"all": ()}
+        js = tuple(settings.XPREZ_JQUERY_INIT_MEDIA_JS) + (
+            "ck_editor/libs/ck_editor/ckeditor.js",
+            "ck_editor/js/ck_editor_widget.js",
+        )
+
+    def __init__(self, file_upload_dir=None, attrs=None):
+        config = self.get_config(file_upload_dir=None)
+
+        default_attrs = {
+            "class": "js-ck-editor-source",
+            "data-ck-editor-config": json.dumps(config),
+        }
+        if attrs:
+            default_attrs.update(attrs)
+
+        super().__init__(default_attrs)
+
+
+class CkEditorWidgetSimple(CkEditorWidgetBase):
+    def get_config(self, *args, **kwargs):
+        return {
+            "toolbar": ("bold", "italic", "link"),
+            "blockToolbar": (),
+        }
+
+
+class CkEditorWidgetFullBase(CkEditorWidgetBase):
+    def get_config(self, *args, **kwargs):
+        return {
+            "blockToolbar": (
+                "heading",
+                "|",
+                "blockQuote",
+                "bulletedList",
+                "numberedList",
+            ),
+            "toolbar": (
+                "bold",
+                "italic",
+                "link",
+                "|",
+                "heading",
+                "|",
+                "blockQuote",
+                "bulletedList",
+                "numberedList",
+            ),
+            "placeholder": "Type your text",
+            "link": {
+                "decorators": {
+                    "toggleButtonPrimary": {
+                        "mode": "manual",
+                        "label": "Primary button",
+                        "classes": ["btn", "btn-primary"],
+                    },
+                    "toggleButtonSecondary": {
+                        "mode": "manual",
+                        "label": "Secondary button",
+                        "classes": ["btn", "btn-secondary"],
+                    },
+                    "openInNewTab": {
+                        "mode": "manual",
+                        "label": "Open in a new tab",
+                        "defaultValue": False,
+                        "attributes": {
+                            "target": "_blank",
+                        },
+                    },
+                }
+            },
+            "heading": {
+                "options": (
+                    {
+                        "model": "paragraph",
+                        "title": "Paragraph",
+                        "class": "ck-heading_paragraph",
+                    },
+                    {
+                        "model": "heading2",
+                        "view": "h2",
+                        "title": "Heading 2",
+                        "class": "ck-heading_heading2",
+                    },
+                    {
+                        "model": "heading3",
+                        "view": "h3",
+                        "title": "Heading 3",
+                        "class": "ck-heading_heading3",
+                    },
+                ),
+            },
+            "fontSize": {
+                "options": (
+                    "tiny",
+                    "default",
+                    "big",
+                )
+            },
+        }
+
+
+class CkEditorWidgetFullNoInsertPlugin(CkEditorWidgetFullBase):
+    def get_config(self, *args, **kwargs):
+        config = super().get_config(*args, **kwargs)
+        config["image"] = {"toolbar": ("|",)}
+        return config
+
+
+class CkEditorWidgetFull(CkEditorWidgetFullBase):
+    def get_config(self, file_upload_dir, *args, **kwargs):
+        config = super().get_config(*args, **kwargs)
+        config["blockToolbar"] += (
+            "|",
+            "imageUpload",
+            "MediaEmbed",
+        )
+        config["toolbar"] += (
+            "|",
+            "imageUpload",
+            "MediaEmbed",
+        )
+        config["simpleUpload"] = {
+            "uploadUrl": reverse("xprez:ckeditor_file_upload", args=[file_upload_dir])
+        }
+        config["mediaEmbed "] = {"previewsInData": True}
+        config["image"] = {
+            "toolbar": (
+                "imageTextAlternative",
+                "toggleImageCaption",
+                "|",
+                "imageStyle:alignLeft",
+                "imageStyle:block",
+                "imageStyle:alignRight",
+                "|",
+                "linkImage",
+            ),
+            "styles": (
+                "block",
+                "alignLeft",
+                "alignRight",
+            ),
+        }
+        return config
+
+
+class CkEditorWidgetFullWithTable(CkEditorWidgetFull):
+    def get_config(self, file_upload_dir, *args, **kwargs):
+        config = super().get_config(file_upload_dir, *args, **kwargs)
+
+        config["toolbar"] += ("|", "insertTable")
+        config["blockToolbar"] += ("|", "insertTable")
+        config["table"] = {
+            "contentToolbar": (
+                "tableColumn",
+                "tableRow",
+                "mergeTableCells",
+            ),
+        }
+        return config
+
+
+CkEditorWidget = CkEditorWidgetFull
