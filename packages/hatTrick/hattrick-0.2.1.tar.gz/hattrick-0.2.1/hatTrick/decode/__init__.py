6cee1631552b331e6b7c37e4e91e87ba3f766983
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 17:09:38 +0000

    Add package hatTrick/0.2.1/hattrick-0.2.1.tar.gz

---
+++ packages/hatTrick/hattrick-0.2.1.tar.gz/hattrick-0.2.1/hatTrick/decode/__init__.py
@@ -0,0 +1,4 @@
+from ._helpers import compute_hadamard_inverse, generate_s_matrix
+from .HATRX import decode_hadamard_files
+
+__all__ = ["generate_s_matrix", "decode_hadamard_files", "compute_hadamard_inverse"]
