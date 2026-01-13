2b91885cf9cf67a37c86f0dec3e02657f7a568c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 03:31:55 +0000

    Add package sentinel1decoder/1.1.1/sentinel1decoder-1.1.1-cp313-cp313-manylinux_2_34_s390x.whl

---
+++ packages/sentinel1decoder/sentinel1decoder-1.1.1-cp313-cp313-manylinux_2_34_s390x.whl/sentinel1decoder/__init__.py
@@ -0,0 +1,14 @@
+"""Sentinel-1 decoder package."""
+
+from . import constants, utilities
+from .l0decoder import Level0Decoder
+from .l0file import Level0File
+
+__version__ = "1.1.1"
+
+__all__ = [
+    "Level0Decoder",
+    "Level0File",
+    "utilities",
+    "constants",
+]
