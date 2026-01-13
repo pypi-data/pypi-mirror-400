58dc5c52e64c29a2a99359e20604997b83b98a85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 12:04:51 +0000

    Add package ni.measurementlink.sessionmanagement.v1.proto/1.1.0/ni_measurementlink_sessionmanagement_v1_proto-1.1.0-py3-none-any.whl

---
+++ packages/ni.measurementlink.sessionmanagement.v1.proto/ni_measurementlink_sessionmanagement_v1_proto-1.1.0-py3-none-any.whl/ni/measurementlink/sessionmanagement/v1/annotations/__init__.py
@@ -0,0 +1,19 @@
+"""Constants for session client details annotations."""
+
+RESERVED_HOSTNAME = "ni/reserved.hostname"
+"""Key for an annotation indicating the hostname that reserved the session."""
+
+RESERVED_USERNAME = "ni/reserved.username"
+"""Key for an annotation indicating the user name that reserved the session."""
+
+RESERVED_IPADDRESS = "ni/reserved.ipaddress"
+"""Key for an annotation indicating the IP address that reserved the session."""
+
+REGISTERED_HOSTNAME = "ni/registered.hostname"
+"""Key for an annotation indicating the hostname that registered the session."""
+
+REGISTERED_USERNAME = "ni/registered.username"
+"""Key for an annotation indicating the user name that registered the session."""
+
+REGISTERED_IPADDRESS = "ni/registered.ipaddress"
+"""Key for an annotation indicating the IP address that registered the session."""
