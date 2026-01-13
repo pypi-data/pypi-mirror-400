3f3ecc5bf3ab7ffeabdb96c8b84ba30d46894cb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 16:07:14 +0000

    Add package jcloudai/1.0.15/jcloudai-1.0.15.tar.gz

---
+++ packages/jcloudai/jcloudai-1.0.15.tar.gz/jcloudai-1.0.15/jcloudai/core/response/error.py
@@ -0,0 +1,16 @@
+from .base import BaseResponse
+
+
+class ErrorResponse(BaseResponse):
+    """
+    Class for handling error responses.
+    """
+
+    def __init__(
+        self,
+        value="Unfortunately, I was not able to get your answer. Please try again.",
+        last_code_executed: str = None,
+        last_sql_executed: str = None,
+        error: str = None,
+    ):
+        super().__init__(value, "error", last_code_executed, last_sql_executed, error)
