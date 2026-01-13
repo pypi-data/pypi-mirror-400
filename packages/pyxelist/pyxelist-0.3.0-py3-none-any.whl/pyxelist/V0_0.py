78ab30211de9b11f358cb7bd25dc239c07bb8cad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 06:03:49 +0000

    Add package pyxelist/0.3.0/pyxelist-0.3.0-py3-none-any.whl

---
+++ packages/pyxelist/pyxelist-0.3.0-py3-none-any.whl/pyxelist/V0_0.py
@@ -0,0 +1,86 @@
+""" v info v """"""
+Name: pyxelist
+version: Vo.0
+"""""" ^ info ^ """
+
+""" v imports v """
+import turtle
+""" ^ imports ^ """
+
+""" v CODE v """
+class pyxelist:
+    def __init__(self,xpos:int,ypos:int,data:list|dict,size:int,obj_type:str='solid',width:str|int='auto',height:str|int='auto'):
+        self.data=data
+        self.size=size
+        self.type=obj_type
+        self.pos=(xpos,ypos)
+        if height=='auto':height=len(data)
+        if width=='auto':width=len(data[0])
+        self.dym=(width,height)
+        self.t=turtle
+        self.t.hideturtle()
+        self.t.penup()
+        return
+    def draw(self,frame=0):
+        sx=round(self.pos[0]-self.dym[0]*self.size/2)
+        sy=round(self.pos[1]+self.dym[1]*self.size/2)
+        self.t.goto(sx,sy)
+        self.t.setheading(0)
+        if self.type=='solid':
+            nl_loop=0
+            for list in self.data:
+                self.t.goto(sx,sy-nl_loop*self.size)
+                for color in list:
+                    if color=='#======':pass
+                    else:self.t.color(color),self.t.begin_fill()
+                    for side in range(4):
+                        self.t.forward(self.size)
+                        self.t.right(90)
+                    if color!='#======':self.t.end_fill()
+                    self.t.forward(self.size)
+                nl_loop+=1
+            pass
+        elif self.type=='animated':
+            nl_loop=0
+            for list in self.data[frame]:
+                self.t.goto(sx,sy-nl_loop*self.size)
+                for color in list:
+                    if color=='#======':pass
+                    else:self.t.color(color),self.t.begin_fill()
+                    for side in range(4):
+                        self.t.forward(self.size)
+                        self.t.right(90)
+                    if color!='#======':self.t.end_fill()
+                    self.t.forward(self.size)
+                nl_loop+=1
+            pass
+        return
+    def set(self,pos:str,by:int|tuple):
+        if pos.lower()=='x':
+            self.pos[0]=by
+            pass
+        elif pos.lower()=='y':
+            self.pos[1]=by
+            pass
+        elif pos.lower()=='both':
+            self.pos=by
+            pass
+        pass
+    def move(self,pos:str,by:int|tuple):
+        if pos.lower()=='x':
+            self.pos[0]+=by
+            pass
+        elif pos.lower()=='y':
+            self.pos[1]+=by
+            pass
+        elif pos.lower()=='both':
+            self.pos+=by
+            pass
+        pass
+    pass
+""" ^ CODE ^ """
+
+""" v CREDITS v """"""
+Developer:
+    Github user@yanlin522: created it
+"""""" ^ CREDITS ^ """
