370b9390e277be6400d6a91187499b4866f7a938
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 15:08:01 +0000

    Add package ygglib/0.1.2/ygglib-0.1.2.tar.gz

---
+++ packages/ygglib/ygglib-0.1.2.tar.gz/ygglib-0.1.2/setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="ygglib",
+    version="0.1.2",
+    author="Klug Labs",
+    author_email="techpport@kluglabs.net",
+    description="High-performance cache client with automatic authentication and multi-tenancy",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Klug-Labs/python-ygglib",
+    package_dir={"ygglib": "ygg"},
+    packages=["ygglib"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+    ],
+    python_requires=">=3.7",
+    install_requires=[
+        "requests>=2.25.0",
+    ],
+    extras_require={
+        "dev": [
+            "pytest>=6.0",
+            "pytest-cov>=2.0",
+            "black>=21.0",
+            "flake8>=3.8",
+            "mypy>=0.800",
+        ],
+    },
+)
+
