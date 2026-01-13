ca3a564bebc8e475cc7c2eb9a56dc436f879ee82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 23:05:47 +0000

    Add package vortex-data/0.58.0/vortex_data-0.58.0-cp311-abi3-macosx_10_12_x86_64.whl

---
+++ packages/vortex-data/vortex_data-0.58.0-cp311-abi3-macosx_10_12_x86_64.whl/vortex/_lib/io.pyi
@@ -0,0 +1,23 @@
+#  SPDX-License-Identifier: Apache-2.0
+#  SPDX-FileCopyrightText: Copyright the Vortex contributors
+
+from ..type_aliases import IntoArrayIterator
+from .arrays import Array
+from .expr import Expr
+
+def read_url(
+    url: str,
+    *,
+    projection: list[str] | list[int] | None = None,
+    row_filter: Expr | None = None,
+    indices: Array | None = None,
+) -> Array: ...
+def write(iter: IntoArrayIterator, path: str) -> None: ...
+
+class VortexWriteOptions:
+    @staticmethod
+    def default() -> VortexWriteOptions: ...
+    @staticmethod
+    def compact() -> VortexWriteOptions: ...
+    @staticmethod
+    def write_path(iter: IntoArrayIterator, path: str) -> VortexWriteOptions: ...
