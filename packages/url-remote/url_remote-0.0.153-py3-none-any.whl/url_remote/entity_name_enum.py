55b09e73f6eb6ab4fb7526fcef74f835a1732dfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 14:07:15 +0000

    Add package url-remote/0.0.153/url_remote-0.0.153-py3-none-any.whl

---
+++ packages/url-remote/url_remote-0.0.153-py3-none-any.whl/url_remote/entity_name_enum.py
@@ -0,0 +1,18 @@
+from enum import Enum
+
+
+# Sorted by A..Z of Entity Type
+class EntityName(Enum):
+    AUTH_LOGIN = "auth"
+    EVENT = "event"
+    GENDER_DETECTION = "gender-detection"
+    GROUP = "group"
+    GROUP_PROFILE = "group-profile"
+    LOGGER = "logger"
+    MARKETPLACE_GOODS = "marketplace-goods"
+    STORAGE = "storage"
+    STORAGE_GRAPHQL = "storage-graphql"
+    TIMELINE = "timeline"
+    USER_REGISTRATION = "user-registration"
+    SMARTLINK = "smartlink"
+    DIALOG_WORKFLOW = "dialogWorkflow"
