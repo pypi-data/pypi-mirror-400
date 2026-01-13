092a6eeadd107d1dd1608bf3f9ed826ab129567b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 18:10:40 +0000

    Add package scikit-plots/0.4.0.post9/scikit_plots-0.4.0.post9-cp313-cp313-musllinux_1_2_x86_64.whl

---
+++ packages/scikit-plots/scikit_plots-0.4.0.post9-cp313-cp313-musllinux_1_2_x86_64.whl/scikitplot/preprocessing/__init__.py
@@ -0,0 +1,11 @@
+# Authors: The scikit-plots developers
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""Methods for scaling, centering, normalization, binarization, and more."""
+
+from ._encoders import DummyCodeEncoder, GetDummies
+
+__all__ = [
+    "DummyCodeEncoder",
+    "GetDummies",
+]
