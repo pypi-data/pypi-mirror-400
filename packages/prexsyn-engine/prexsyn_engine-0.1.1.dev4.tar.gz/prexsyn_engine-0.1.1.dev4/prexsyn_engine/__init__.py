10e3b2d537513103b1d22058b4144a309abe4336
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 19:07:19 +0000

    Add package prexsyn-engine/0.1.1.dev4/prexsyn_engine-0.1.1.dev4.tar.gz

---
+++ packages/prexsyn-engine/prexsyn_engine-0.1.1.dev4.tar.gz/prexsyn_engine-0.1.1.dev4/prexsyn_engine/__init__.py
@@ -0,0 +1,7 @@
+__version__ = "0.1.1.dev4"
+
+raise NotImplementedError(
+    "Installing prexsyn-engine from PyPI is not supported due to linking restrictions. "
+    "Please refer to the GitHub repository for conda-based installation instructions: "
+    "https://github.com/luost26/prexsyn-engine"
+)
