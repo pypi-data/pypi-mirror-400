8b93f130a05359c90ccd7f2e3160b36234a71201
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 22:06:22 +0000

    Add package covjsonkit/0.2.10/covjsonkit-0.2.10-py3-none-any.whl

---
+++ packages/covjsonkit/covjsonkit-0.2.10-py3-none-any.whl/covjsonkit/decoder/Path.py
@@ -0,0 +1,155 @@
+import xarray as xr
+
+from .decoder import Decoder
+
+
+class Path(Decoder):
+    def __init__(self, covjson):
+        super().__init__(covjson)
+        self.domains = self.get_domains()
+        self.ranges = self.get_ranges()
+
+    def get_domains(self):
+        domains = []
+        for coverage in self.coverage.coverages:
+            domains.append(coverage["domain"])
+        return domains
+
+    def get_ranges(self):
+        ranges = []
+        for coverage in self.coverage.coverages:
+            ranges.append(coverage["ranges"])
+        return ranges
+
+    def get_values(self):
+        values = {}
+        for parameter in self.parameters:
+            values[parameter] = []
+            for range in self.ranges:
+                values[parameter].append(range[parameter]["values"])
+        return values
+
+    def get_coordinates(self):
+        return self.domains[0]["axes"]
+
+    def to_geopandas(self):
+        pass
+
+    def to_geotiff(self):
+        raise TypeError("Path domain cannot be converted to GeoTIFF.")
+
+    def to_geojson(self):
+        features = []
+        for coverage in self.coverages:
+            coords = coverage["domain"]["axes"]["composite"]["values"]
+            values = {}
+            for key in coverage["ranges"]:
+                values[key] = coverage["ranges"][key]["values"]
+            if "mars:metadata" in coverage:
+                mars_metadata = coverage["mars:metadata"]
+
+            for idx, coord in enumerate(coords):
+                param_vals = {}
+                for key in values.keys():
+                    param_vals[key] = values[key][idx]
+                if "mars:metadata" in coverage:
+                    param_vals["mars:metadata"] = mars_metadata
+                param_vals["datetime"] = coord[0]
+                features.append(
+                    {
+                        "type": "Feature",
+                        "geometry": {
+                            "type": "Point",
+                            "coordinates": [[coord[1], coord[2], coord[3]]],  # lon, lat, z
+                        },
+                        "properties": param_vals,
+                    }
+                )
+
+        geojson = {"type": "FeatureCollection", "features": features}
+
+        return geojson
+
+    def to_xarray(self):
+        dims = ["datetimes", "number", "steps", "points"]
+        dataarraydict = {}
+
+        # Get coordinates
+        x = []
+        y = []
+        levelist = []
+        time = []
+        for coord in self.get_coordinates()["composite"]["values"]:
+            x.append(float(coord[1]))
+            y.append(float(coord[2]))
+            levelist.append(float(coord[3]))
+            time.append(coord[0])
+
+        values = {}
+        for parameter in self.parameters:
+            values[parameter] = {}
+
+        datetimes = []
+        numbers = []
+        steps = []
+        for coverage in self.coverages:
+            if "number" not in coverage["mars:metadata"]:
+                coverage["mars:metadata"]["number"] = 0
+            numbers.append(coverage["mars:metadata"]["number"])
+            if "step" not in coverage["mars:metadata"]:
+                coverage["mars:metadata"]["step"] = 0
+            steps.append(coverage["mars:metadata"]["step"])
+            datetimes.append(coverage["mars:metadata"]["Forecast date"])
+            for parameter in self.parameters:
+                # values[parameter].append(coverage["ranges"][parameter]["values"])
+                if coverage["mars:metadata"]["Forecast date"] not in values[parameter]:
+                    values[parameter][coverage["mars:metadata"]["Forecast date"]] = {}
+                if (
+                    coverage["mars:metadata"]["number"]
+                    not in values[parameter][coverage["mars:metadata"]["Forecast date"]]
+                ):
+                    values[parameter][coverage["mars:metadata"]["Forecast date"]][
+                        coverage["mars:metadata"]["number"]
+                    ] = {}
+                values[parameter][coverage["mars:metadata"]["Forecast date"]][coverage["mars:metadata"]["number"]][
+                    coverage["mars:metadata"]["step"]
+                ] = coverage["ranges"][parameter]["values"]
+
+        datetimes = list(set(datetimes))
+        numbers = list(set(numbers))
+        steps = list(set(steps))
+
+        new_values = {}
+        for parameter in values.keys():
+            new_values[parameter] = []
+            for i, datetime in enumerate(datetimes):
+                new_values[parameter].append([])
+                for j, number in enumerate(numbers):
+                    new_values[parameter][i].append([])
+                    for k, step in enumerate(steps):
+                        new_values[parameter][i][j].append(values[parameter][datetime][number][step])
+
+        for parameter in self.parameters:
+            dataarray = xr.DataArray(new_values[parameter], dims=dims)
+            dataarray.attrs["type"] = self.get_parameter_metadata(parameter)["type"]
+            dataarray.attrs["units"] = self.get_parameter_metadata(parameter)["unit"]["symbol"]
+            dataarray.attrs["long_name"] = self.get_parameter_metadata(parameter)["observedProperty"]["id"]
+            dataarraydict[dataarray.attrs["long_name"]] = dataarray
+
+        ds = xr.Dataset(
+            dataarraydict,
+            coords=dict(
+                datetimes=(["datetimes"], datetimes),
+                number=(["number"], numbers),
+                steps=(["steps"], steps),
+                points=(["points"], list(range(0, len(x)))),
+                latitude=(["points"], x),
+                longitude=(["points"], y),
+                levelist=(["points"], levelist),
+                time=(["points"], time),
+            ),
+        )
+        for mars_metadata in self.mars_metadata[0]:
+            ds.attrs[mars_metadata] = self.mars_metadata[0][mars_metadata]
+
+        return ds
---
+++ packages/covjsonkit/covjsonkit-0.2.10-py3-none-any.whl/covjsonkit/decoder/decoder.py
@@ -0,0 +1,124 @@
+import json
+from abc import ABC, abstractmethod
+
+from covjsonkit.Coverage import Coverage
+from covjsonkit.CoverageCollection import CoverageCollection
+from covjsonkit.utils import coverage_to_coveragecollection
+
+
+class Decoder(ABC):
+    def __init__(self, covjson):
+        """
+        Base class for decoding CovJSON data.
+
+        The Decoder class provides functionality to parse and extract information from CovJSON
+        data, supporting both Coverage and CoverageCollection types. It handles parameters,
+        referencing systems, Mars metadata, and coverages, and provides abstract methods for
+        further processing and conversion of the decoded data.
+
+        Attributes:
+            covjson (dict): The CovJSON data being decoded, either as a dictionary or loaded from a file.
+            type (str): The type of CovJSON data, either "Coverage" or "CoverageCollection".
+            coverage (Coverage or CoverageCollection): An instance representing the decoded coverage data.
+            parameters (list): A list of parameter names included in the CovJSON data.
+            coordinates (list): A list of referencing coordinates extracted from the CovJSON data.
+            mars_metadata (list): A list of Mars metadata extracted from the coverages.
+            coverages (list): A list of individual coverages within the CovJSON data.
+
+        Methods:
+            get_type(): Returns the type of the CovJSON data ("Coverage" or "CoverageCollection").
+            get_parameters(): Returns a list of parameter names included in the CovJSON data.
+            get_parameter_metadata(parameter): Returns metadata for a specific parameter.
+            get_referencing(): Extracts and returns referencing coordinates from the CovJSON data.
+            get_mars_metadata(): Extracts and returns Mars metadata from the coverages.
+
+        Abstract Methods:
+            get_ranges(): Abstract method for extracting range data from the CovJSON.
+            get_domains(): Abstract method for extracting domain information from the CovJSON.
+            get_coordinates(): Abstract method for extracting coordinate data from the CovJSON.
+            get_values(): Abstract method for extracting values from the CovJSON.
+            to_geopandas(): Abstract method for converting the CovJSON data to a GeoPandas DataFrame.
+            to_xarray(): Abstract method for converting the CovJSON data to an xarray Dataset.
+        """
+        # if python dictionary no need for loading, otherwise load json file
+        if isinstance(covjson, dict):
+            self.covjson = covjson
+        elif isinstance(covjson, str):
+            with open(covjson) as json_file:
+                self.covjson = json.load(json_file)
+        else:
+            raise TypeError("Covjson must be dictionary or covjson file")
+
+        self.type = self.get_type()
+        if self.type == "Coverage":
+            self.covjson = coverage_to_coveragecollection(self.covjson)
+            self.type = self.get_type()
+            self.coverage = CoverageCollection(self.covjson)
+
+        if self.type == "Coverage":
+            self.coverage = Coverage(self.covjson)
+        elif self.type == "CoverageCollection":
+            self.coverage = CoverageCollection(self.covjson)
+        else:
+            raise TypeError("Type must be Coverage or CoverageCollection")
+
+        self.parameters = self.get_parameters()
+        self.coordinates = self.get_referencing()
+        self.mars_metadata = self.get_mars_metadata()
+        self.coverages = self.coverage.coverages
+
+    def get_type(self):
+        return self.covjson["type"]
+
+    def get_parameters(self):
+        return list(self.covjson["parameters"].keys())
+
+    def get_parameter_metadata(self, parameter):
+        return self.covjson["parameters"][parameter]
+
+    def get_referencing(self):
+        coordinates = []
+        for coords in self.covjson["referencing"]:
+            coordinates.append(coords["coordinates"])
+        return [coord for sublist in coordinates for coord in sublist]
+
+    def get_mars_metadata(self):
+        mars_metadata = []
+        if "mars:metadata" not in self.coverage.coverages[0]:
+            return mars_metadata
+        else:
+            for coverage in self.coverage.coverages:
+                mars_metadata.append(coverage["mars:metadata"])
+        return mars_metadata
+
+    @abstractmethod
+    def get_ranges(self):
+        pass
+
+    @abstractmethod
+    def get_domains(self):
+        pass
+
+    @abstractmethod
+    def get_coordinates(self):
+        pass
+
+    @abstractmethod
+    def get_values(self):
+        pass
+
+    @abstractmethod
+    def to_geopandas(self):
+        pass
+
+    @abstractmethod
+    def to_xarray(self):
+        pass
+
+    @abstractmethod
+    def to_geojson(self):
+        pass
+
+    @abstractmethod
+    def to_geotiff(self):
+        pass
