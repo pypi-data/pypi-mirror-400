064eb70c5fc12f0c2f74b916dafa023ac6de0867
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 23:05:17 +0000

    Add package vita_module/1.0.0/vita_module-1.0.0-py3-none-any.whl

---
+++ packages/vita_module/vita_module-1.0.0-py3-none-any.whl/vita_module/__init__.py
@@ -0,0 +1,3 @@
+from .vita_module import (ModuleExportLibrary, ModuleImportLibrary,
+                          SceModuleImports, SceModuleImports2, SceModuleInfo,
+                          SceModuleLibaryExports, VitaModule)
