6f330ed68fe8eea99e8f5bb6c78fb2d3221664f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 18:10:09 +0000

    Add package django-nps/1.3/django_nps-1.3-py3-none-any.whl

---
+++ packages/django-nps/django_nps-1.3-py3-none-any.whl/net_promoter_score/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class NpsConfig(AppConfig):
+    name = "net_promoter_score"
+    verbose_name = "NPS (Net Promoter Score)"
+    default_auto_field = "django.db.models.AutoField"
