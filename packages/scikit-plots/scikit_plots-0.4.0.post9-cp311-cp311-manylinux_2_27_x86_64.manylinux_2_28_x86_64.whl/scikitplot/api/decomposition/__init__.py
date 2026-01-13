489d4c513545528f048fc616b9fdadec6bbfd526
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 18:10:29 +0000

    Add package scikit-plots/0.4.0.post9/scikit_plots-0.4.0.post9-cp311-cp311-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/scikit-plots/scikit_plots-0.4.0.post9-cp311-cp311-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl/scikitplot/api/decomposition/__init__.py
@@ -0,0 +1,20 @@
+"""
+Visualizations for matrix decomposition algorithms.
+
+The :py:mod:`~scikitplot.api.decomposition` module includes plots built specifically for
+scikit-learn estimators that are used for dimensionality reduction e.g. PCA.
+
+You can use your own estimators, but these plots assume specific properties shared by
+scikit-learn estimators. The specific requirements are documented per function.
+"""
+
+# Authors: The scikit-plots developers
+# SPDX-License-Identifier: BSD-3-Clause
+
+# scikitplot/api/decomposition/__init__.py
+
+# Your package/module initialization code goes here
+from ._components import *
+from ._projection import *
+
+# TODO:from .tsne_plot
