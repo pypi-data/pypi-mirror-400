b1d6654d177fceb92c0f3df95ff48794f759bb29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 03:32:01 +0000

    Add package abstract-solcatcher-database/0.0.0.60/abstract_solcatcher_database-0.0.0.60.tar.gz

---
+++ packages/abstract-solcatcher-database/abstract_solcatcher_database-0.0.0.60.tar.gz/abstract_solcatcher_database-0.0.0.60/src/abstract_solcatcher_database/connect/envs/solana/env.py
@@ -0,0 +1,7 @@
+from ..imports import *
+SOLANA_MINT = get_env_val("SOLANA_MINT") or '';
+SOLANA_WS_ENDPOINT = get_env_val("SOLANA_WS_ENDPOINT") or '';
+SOLANA_RPC_URL = get_env_val("SOLANA_RPC_URL") or '';
+ANKR_RPC_URL = get_env_val("ANKR_RPC_URL") or '';
+ANKR_WS_ENDPOINT = get_env_val("ANKR_WS_ENDPOINT") or '';
+PUMP_FUN_PROGRAMID = get_env_val("PUMP_FUN_PROGRAMID") or '6EF8rrecthR5Dkzon8Nwu78hRvfCKubJ14M5uBEwF6P';
