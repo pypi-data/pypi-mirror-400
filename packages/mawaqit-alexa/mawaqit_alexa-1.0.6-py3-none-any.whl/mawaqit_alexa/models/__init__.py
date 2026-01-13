3c2153e9e2b20fdc072686f74a67b0ade75f8114
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 14:07:07 +0000

    Add package mawaqit-alexa/1.0.6/mawaqit_alexa-1.0.6-py3-none-any.whl

---
+++ packages/mawaqit-alexa/mawaqit_alexa-1.0.6-py3-none-any.whl/mawaqit_alexa/models/__init__.py
@@ -0,0 +1,4 @@
+from mawaqit_alexa.models.types import MawaqitYearCalendar
+
+__all__ = ['MawaqitYearCalendar']
+
---
+++ packages/mawaqit-alexa/mawaqit_alexa-1.0.6-py3-none-any.whl/mawaqit_alexa/util/__init__.py
@@ -0,0 +1,5 @@
+from mawaqit_alexa.util.ttl_cache import persistent_ttl_cache
+from mawaqit_alexa.util.util import Util
+
+__all__ = ['persistent_ttl_cache', 'Util']
+
