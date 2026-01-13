5cc216174ec0be3742a23385961205619c7daee1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 05:07:37 +0000

    Add package myatoms/0.1/myatoms-0.1.tar.gz

---
+++ packages/myatoms/myatoms-0.1.tar.gz/myatoms-0.1/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+  name = 'myatoms',
+  packages = ['myatoms'],
+  version = '0.1',
+  author = 'Ole Schuett',
+  author_email = 'ole.schuett@cp2k.org',
+  url = 'https://www.myatoms.com',
+)
