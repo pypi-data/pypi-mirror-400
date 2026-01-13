4b31b5519f11e23702633b7b22c77fa594eac87c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 23:05:22 +0000

    Add package llama_cloud/1.0.0b2/llama_cloud-1.0.0b2-py3-none-any.whl

---
+++ packages/llama_cloud/llama_cloud-1.0.0b2-py3-none-any.whl/llama_cloud/types/parsing_get_params.py
@@ -0,0 +1,33 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing import Optional
+from typing_extensions import TypedDict
+
+from .._types import SequenceNotStr
+
+__all__ = ["ParsingGetParams"]
+
+
+class ParsingGetParams(TypedDict, total=False):
+    expand: SequenceNotStr[str]
+    """
+    Fields to include: text, markdown, items, text_content_metadata,
+    markdown_content_metadata, items_content_metadata, xlsx_content_metadata,
+    output_pdf_content_metadata, images_content_metadata. Metadata fields include
+    presigned URLs.
+    """
+
+    image_filenames: Optional[str]
+    """Comma-delimited list of image filenames to fetch.
+
+    Supersedes return_all_images. Example: image_0.png,image_1.jpg
+    """
+
+    organization_id: Optional[str]
+
+    project_id: Optional[str]
+
+    return_all_images: bool
+    """Return all available images when true. Ignored if image_filenames is provided."""
