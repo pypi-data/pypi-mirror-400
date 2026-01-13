c20f6e5b07b8041b440715aa7e4f4f572f101d0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 15:08:20 +0000

    Add package LinuxRecycle/2.0.3/linuxrecycle-2.0.3.tar.gz

---
+++ packages/LinuxRecycle/linuxrecycle-2.0.3.tar.gz/linuxrecycle-2.0.3/linuxrecycle/argv.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# encoding: utf-8
+# ====================================================
+#   Copyright (C)2019 All rights reserved.
+#
+#   Author        : Xin-Xin MA
+#   Email         : xxmawhu@163.com
+#   File Name     : argv.py
+#   Created Time  : 2019-09-19 19:35
+#   Last Modified : 2019-09-19 19:47
+#   Describe      :
+#
+# ====================================================
+"""
+core: set the numbers of processes
+"""
+import sys
+
+opt = []
+argv = []
+core = 200
+tmpFile = ["tmp"]
