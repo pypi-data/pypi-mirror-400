135c1198309cc8540fa68ea7e559ce9826139d90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 12:04:47 +0000

    Add package fusion-bench/0.2.31/fusion_bench-0.2.31-py3-none-any.whl

---
+++ packages/fusion-bench/fusion_bench-0.2.31-py3-none-any.whl/fusion_bench/method/simple_average.py
@@ -0,0 +1,137 @@
+import logging
+from copy import deepcopy
+from typing import Dict, List, Mapping, Optional, Union
+
+import torch
+from torch import nn
+
+from fusion_bench.method.base_algorithm import BaseAlgorithm
+from fusion_bench.mixins import SimpleProfilerMixin, auto_register_config
+from fusion_bench.modelpool import BaseModelPool
+from fusion_bench.utils import LazyStateDict
+from fusion_bench.utils.state_dict_arithmetic import (
+    state_dict_add,
+    state_dict_avg,
+    state_dict_div,
+    state_dict_mul,
+)
+from fusion_bench.utils.type import StateDictType
+
+log = logging.getLogger(__name__)
+
+
+def simple_average(
+    modules: List[Union[nn.Module, StateDictType]],
+    base_module: Optional[nn.Module] = None,
+):
+    R"""
+    Averages the parameters of a list of PyTorch modules or state dictionaries.
+
+    This function takes a list of PyTorch modules or state dictionaries and returns a new module with the averaged parameters, or a new state dictionary with the averaged parameters.
+
+    Args:
+        modules (List[Union[nn.Module, StateDictType]]): A list of PyTorch modules or state dictionaries.
+        base_module (Optional[nn.Module]): A base module to use for the new module. If provided, the averaged parameters will be loaded into this module. If not provided, a new module will be created by copying the first module in the list.
+
+    Returns:
+        module_or_state_dict (Union[nn.Module, StateDictType]): A new PyTorch module with the averaged parameters, or a new state dictionary with the averaged parameters.
+
+    Examples:
+        >>> import torch.nn as nn
+        >>> model1 = nn.Linear(10, 10)
+        >>> model2 = nn.Linear(10, 10)
+        >>> averaged_model = simple_average([model1, model2])
+
+        >>> state_dict1 = model1.state_dict()
+        >>> state_dict2 = model2.state_dict()
+        >>> averaged_state_dict = simple_average([state_dict1, state_dict2])
+    """
+    assert len(modules) > 0, "modules must be a non-empty list"
+    if isinstance(modules[0], nn.Module):
+        if base_module is None:
+            new_module = deepcopy(modules[0])
+        else:
+            new_module = base_module
+        state_dict = state_dict_avg([module.state_dict() for module in modules])
+        new_module.load_state_dict(state_dict)
+        return new_module
+    elif isinstance(modules[0], Mapping):
+        return state_dict_avg(modules)
+
+
+@auto_register_config
+class SimpleAverageAlgorithm(
+    SimpleProfilerMixin,
+    BaseAlgorithm,
+):
+    def __init__(self, show_pbar: bool = False, inplace: bool = True, **kwargs):
+        """
+        Args:
+            show_pbar (bool): If True, shows a progress bar during model loading and merging. Default is False.
+            inplace (bool): If True, overwrites the weights of the first model in the model pool.
+                If False, creates a new model for the merged weights. Default is True.
+        """
+        super().__init__(**kwargs)
+
+    @torch.no_grad()
+    def run(self, modelpool: Union[BaseModelPool, Dict[str, nn.Module]]) -> nn.Module:
+        """
+        Fuse the models in the given model pool using simple averaging.
+
+        This method iterates over the names of the models in the model pool, loads each model, and appends it to a list.
+        It then returns the simple average of the models in the list.
+
+        Args:
+            modelpool: The pool of models to fuse.
+
+        Returns:
+            The fused model obtained by simple averaging.
+        """
+        if isinstance(modelpool, dict):
+            modelpool = BaseModelPool(modelpool)
+
+        log.info(
+            f"Fusing models using simple average on {len(modelpool.model_names)} models. "
+            f"models: {modelpool.model_names}"
+        )
+        sd: Optional[StateDictType] = None
+        forward_model = None
+        merged_model_names = []
+
+        for model_name in modelpool.model_names:
+            with self.profile("load model"):
+                model = modelpool.load_model(model_name)
+                merged_model_names.append(model_name)
+                print(f"load model of type: {type(model).__name__}")
+            with self.profile("merge weights"):
+                if sd is None:
+                    # Initialize the state dictionary with the first model's state dictionary
+                    sd = model.state_dict()
+                    forward_model = model if self.inplace else deepcopy(model)
+                else:
+                    # Add the current model's state dictionary to the accumulated state dictionary
+                    sd = state_dict_add(
+                        sd, model.state_dict(), show_pbar=self.show_pbar
+                    )
+        with self.profile("merge weights"):
+            # Divide the accumulated state dictionary by the number of models to get the average
+            sd = state_dict_div(
+                sd, len(modelpool.model_names), show_pbar=self.show_pbar
+            )
+
+        if isinstance(forward_model, LazyStateDict):
+            # if the model is a LazyStateDict, convert it to an empty module
+            forward_model = deepcopy(forward_model.meta_module).to_empty(
+                device=forward_model._device
+            )
+        result = forward_model.load_state_dict(sd, strict=False)
+        if result.unexpected_keys:
+            raise ValueError(f"Unexpected keys in state dict: {result.unexpected_keys}")
+        if result.missing_keys:
+            log.warning(f"Missing keys in state dict: {result.missing_keys}")
+        # print profile report and log the merged models
+        self.print_profile_summary()
+        log.info(f"merged {len(merged_model_names)} models:")
+        for model_name in merged_model_names:
+            log.info(f"  - {model_name}")
+        return forward_model
---
+++ packages/fusion-bench/fusion_bench-0.2.31-py3-none-any.whl/fusion_bench/models/parameter_dict.py
@@ -0,0 +1,121 @@
+from typing import List, Mapping, Optional, Tuple
+
+import torch
+from torch import nn
+
+__all__ = "ParamterDictModel"
+
+
+def _set_attr(
+    obj,
+    names: List[str],
+    val,
+    check_parent: bool = False,
+    parent_builder=nn.Module,
+):
+    """
+    Sets an attribute of an object recursively.
+
+    Args:
+        obj (object): Object to set attribute of.
+        names (list): List of attribute names to set recursively.
+        val (object): Value to set the attribute to.
+        check_parent (bool): If True, checks if the parent attribute exists; otherwise, creates it if it does not exist.
+    """
+    if len(names) == 1:
+        setattr(obj, names[0], val)
+    else:
+        if check_parent and not hasattr(obj, names[0]):
+            setattr(obj, names[0], parent_builder())
+        _set_attr(
+            getattr(obj, names[0]),
+            names[1:],
+            val,
+            check_parent=check_parent,
+            parent_builder=parent_builder,
+        )
+
+
+def has_attr(obj, names: List[str]):
+    """
+    Checks if an attribute exists in an object recursively.
+
+    Args:
+        obj (object): Object to check attribute of.
+        names (list): List of attribute names to check recursively.
+
+    Returns:
+        bool: True if the attribute exists; otherwise, False.
+    """
+    if len(names) == 1:
+        return hasattr(obj, names[0])
+    else:
+        return has_attr(getattr(obj, names[0]), names[1:])
+
+
+class ParameterDictModel(nn.Module):
+    """
+    This model is used to create a model with parameters from a dictionary.
+    It behaves like a normal `nn.ParameterDict`, but support keys with dots.
+    """
+
+    def __init__(
+        self,
+        parameters: Optional[Mapping[str, nn.Parameter]] = None,
+    ):
+        super().__init__()
+        if parameters is not None:
+            for name, param in parameters.items():
+                assert isinstance(
+                    param, (nn.Parameter, nn.Buffer)
+                ), f"{name} is not a nn.Parameter or nn.Buffer"
+                _set_attr(
+                    self,
+                    name.split("."),
+                    param,
+                    check_parent=True,
+                    parent_builder=__class__,
+                )
+
+    def __repr__(self):
+        """
+        Generate a string representation of the model's parameters.
+
+        Returns:
+            str: A string representation of the model's parameters.
+        """
+        param_reprs = []
+        for name, param in self.named_parameters():
+            param_repr = f"{name}: {param.size()}"
+            param_reprs.append(param_repr)
+        return f"{self.__class__.__name__}({', '.join(param_reprs)})"
+
+    def __getitem__(self, key: str):
+        if not has_attr(self, key.split(".")):
+            raise KeyError(f"Key {key} not found in {self}")
+        key = key.split(".")
+        obj = self
+        for k in key:
+            obj = getattr(obj, k)
+        return obj
+
+    def __setitem__(self, key: str, value: nn.Parameter):
+        if not has_attr(self, key.split(".")):
+            _set_attr(self, key.split("."), value, check_parent=True)
+        else:
+            _set_attr(self, key.split("."), value, check_parent=False)
+
+    def __contains__(self, key: str):
+        return has_attr(self, key.split("."))
+
+    def keys(self):
+        return [name for name, _ in self.named_parameters()]
+
+    def items(self) -> List[Tuple[str, nn.Parameter]]:
+        return [(name, self[name]) for name in self.keys()]
+
+    def values(self) -> List[nn.Parameter]:
+        return [self[name] for name in self.keys()]
+
+    def __len__(self):
+        return len(self.keys())
