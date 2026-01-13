08fcc96a530e81507a40a15c3fcfc9f1421cb0c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 12:05:43 +0000

    Add package oneiric/0.3.11/oneiric-0.3.11-py3-none-any.whl

---
+++ packages/oneiric/oneiric-0.3.11-py3-none-any.whl/oneiric/adapters/vector/__init__.py
@@ -0,0 +1,26 @@
+"""Vector database adapters for Oneiric."""
+
+from oneiric.adapters.vector.common import (
+    VectorBase,
+    VectorBaseSettings,
+    VectorCollection,
+    VectorDocument,
+    VectorSearchResult,
+)
+from oneiric.adapters.vector.pgvector import PgvectorAdapter, PgvectorSettings
+from oneiric.adapters.vector.pinecone import PineconeAdapter, PineconeSettings
+from oneiric.adapters.vector.qdrant import QdrantAdapter, QdrantSettings
+
+__all__ = [
+    "VectorBase",
+    "VectorBaseSettings",
+    "VectorCollection",
+    "VectorDocument",
+    "VectorSearchResult",
+    "PgvectorAdapter",
+    "PgvectorSettings",
+    "PineconeAdapter",
+    "PineconeSettings",
+    "QdrantAdapter",
+    "QdrantSettings",
+]
