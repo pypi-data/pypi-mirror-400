f4e069cc8ef184561da9ce8d16f2e47461635526
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 21:05:21 +0000

    Add package red-tidegear/1.27.0/red_tidegear-1.27.0-py3-none-any.whl

---
+++ packages/red-tidegear/red_tidegear-1.27.0-py3-none-any.whl/tidegear/types.py
@@ -0,0 +1,31 @@
+# SPDX-FileCopyrightText: 2025 cswimr <copyright@csw.im>
+# SPDX-License-Identifier: MPL-2.0
+
+"""This module contains type aliases related to the Discord API or general cog development."""
+
+import discord
+from redbot.core.commands.context import GuildContext
+
+__all__ = ["GuildChannel", "GuildMessageableChannel", "GuildMessageable"]
+
+GuildChannel = discord.abc.GuildChannel | discord.Thread
+"""A superset of [`discord.abc.GuildChannel`][] that adds [`discord.Thread`][]."""
+
+GuildMessageableChannel = discord.TextChannel | discord.VoiceChannel | discord.StageChannel | discord.Thread
+"""A subset of Discord guild channels that support sending messages through [`.send`][discord.abc.Messageable.send].
+
+Specifically, this type covers:
+
+- [`discord.TextChannel`][]
+- [`discord.VoiceChannel`][]
+- [`discord.StageChannel`][]
+- [`discord.Thread`][]
+"""
+GuildMesseagableChannel = GuildMessageableChannel  # kept due to incorrect spelling in a previous Tidegear version
+
+GuildMessageable = GuildMessageableChannel | GuildContext
+"""Any messeagable target that is scoped to a guild and implements [`.send`][discord.abc.Messageable.send].
+This includes [`GuildMesseagableChannel`][tidegear.types.GuildMesseagableChannel]
+and [`GuildContext`][redbot.core.commands.context.GuildContext].
+"""
+GuildMesseagable = GuildMessageable  # kept due to incorrect spelling in a previous Tidegear version
