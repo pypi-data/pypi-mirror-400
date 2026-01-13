4c18b5332cd5a423a8b18d2dd42e7662f0730f46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 05:07:27 +0000

    Add package wath/1.2.2/wath-1.2.2.tar.gz

---
+++ packages/wath/wath-1.2.2.tar.gz/wath-1.2.2/setup.py
@@ -0,0 +1,31 @@
+import os
+
+from Cython.Build import cythonize
+from setuptools import Extension, find_namespace_packages, setup
+
+
+def module_name(dirpath, filename):
+    l = [os.path.splitext(filename)[0]]
+    head = dirpath
+    while True:
+        head, tail = os.path.split(head)
+        l.append(tail)
+        if not head:
+            break
+    return '.'.join(reversed(l))
+
+
+def get_extensions():
+    extensions = []
+    for dirpath, dirnames, filenames in os.walk('wath'):
+        for filename in filenames:
+            if filename.endswith('.pyx'):
+                extensions.append(
+                    Extension(module_name(dirpath, filename),
+                              [os.path.join(dirpath, filename)]))
+
+    return extensions
+
+
+setup(packages=find_namespace_packages(),
+      ext_modules=cythonize(get_extensions(), build_dir=f"build"))
