7607f94fac079b7595a77e78d671f8c320328079
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 11:06:16 +0000

    Add package aiohttp-things/1.2.0/aiohttp_things-1.2.0.tar.gz

---
+++ packages/aiohttp-things/aiohttp_things-1.2.0.tar.gz/aiohttp_things-1.2.0/src/aiohttp_things/views.py
@@ -0,0 +1,27 @@
+"""
+Reserved for simple views for traversal routing.
+
+More complex classes must naming web handlers.
+"""
+
+from aiohttp_things.web_handlers import (
+    ContextMixin,
+    ItemMixin,
+    Jinja2Mixin,
+    JSONMixin,
+    ListMixin,
+    PaginationMixin,
+    PrimaryKeyMixin,
+    ResponseFormatMixin,
+)
+
+__all__ = [
+    "ContextMixin",
+    "ItemMixin",
+    "JSONMixin",
+    "Jinja2Mixin",
+    "ListMixin",
+    "PaginationMixin",
+    "PrimaryKeyMixin",
+    "ResponseFormatMixin",
+]
