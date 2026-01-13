d3a37c60cd8d4e92ee297b469ec67c98c3142d0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 07:07:30 +0000

    Add package pyreactlab-core/0.1.1/pyreactlab_core-0.1.1-py3-none-any.whl

---
+++ packages/pyreactlab-core/pyreactlab_core-0.1.1-py3-none-any.whl/pyreactlab_core/configs/info.py
@@ -0,0 +1,12 @@
+# version
+__version__ = "0.1.1"
+# author
+__author__ = "Sina Gilassi"
+# email
+__email__ = "sina.gilassi@gmail.com"
+# license
+__license__ = "Apache-2.0"
+# package name
+__package_name__ = "PyReactLab-Core"
+# package description
+__description__ = "PyReactLab-Core is the core foundation of the PyReactLab ecosystem, offering shared data structures and algorithms for chemical reaction representation, stoichiometry, and reaction analysis."
