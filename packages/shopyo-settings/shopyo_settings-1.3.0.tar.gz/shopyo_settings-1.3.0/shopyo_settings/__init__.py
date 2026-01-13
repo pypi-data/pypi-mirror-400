42c2361b338231bb2c67cdecae47556196ee315b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 19:07:18 +0000

    Add package shopyo-settings/1.3.0/shopyo_settings-1.3.0.tar.gz

---
+++ packages/shopyo-settings/shopyo_settings-1.3.0.tar.gz/shopyo_settings-1.3.0/shopyo_settings/__init__.py
@@ -0,0 +1,30 @@
+from typing import Any
+import os
+import json
+from flask import Flask
+from .upload import upload
+from .helpers import get_setting
+
+__version__ = "1.3.0"
+
+info = {}
+with open(os.path.dirname(os.path.abspath(__file__)) + os.sep + "info.json") as f:
+    info = json.load(f)
+
+
+class ShopyoSettings:
+    def __init__(self, app: Any = None) -> None:
+        if app is not None:
+            self.init_app(app)
+        self.upload = upload
+        self.get_setting = get_setting
+
+    def init_app(self, app: Flask) -> None:
+        if not hasattr(app, "extensions"):
+            app.extensions = {}
+
+        app.extensions["shopyo_settings"] = self
+        app.jinja_env.globals["shopyo_settings"] = self
+
+    def get_info(self):
+        return info
