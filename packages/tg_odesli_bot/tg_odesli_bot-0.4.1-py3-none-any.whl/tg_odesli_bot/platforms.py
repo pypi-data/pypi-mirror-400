49c7b6592b8ced0011e688de2add75b59a0b4c65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 18:11:29 +0000

    Add package tg_odesli_bot/0.4.1/tg_odesli_bot-0.4.1-py3-none-any.whl

---
+++ packages/tg_odesli_bot/tg_odesli_bot-0.4.1-py3-none-any.whl/tg_odesli_bot/platforms.py
@@ -0,0 +1,144 @@
+"""Supported platforms."""
+
+import re
+from abc import ABC
+from re import Pattern
+
+#: Supported platforms registry
+PLATFORMS = {}
+
+
+class PlatformABC(ABC):
+    """Platform data ABC."""
+
+    # Platform's Odesli name
+    key: str
+    # RegEx to find platform's URL in a message text
+    url_re: str | Pattern
+    # Human-readable name which will appear in a bot message
+    name: str
+    # Order of platform's link in a bot's message
+    order: int
+
+    def __init_subclass__(cls, **kwargs):
+        """Compile regex and add platform to `PLATFORM` registry."""
+        super().__init_subclass__(**kwargs)
+        cls.url_re = re.compile(cls.url_re)
+        PLATFORMS[cls.key] = cls()
+
+    def postprocess_url(self, url: str) -> str:
+        """Postprocess the platform URL."""
+        return url
+
+
+class DeezerPlatform(PlatformABC):
+    """Deezer platform."""
+
+    key = 'deezer'
+    url_re = (
+        r'(https?://([a-zA-Z\d-]+\.)*deezer\.com(/\w\w)?/'
+        r'(album|track)/[^\s.,]*)'
+        r'|(https?://deezer\.page\.link/[^\s.,]*)'
+        r'|(https?://link.deezer.com/[^\s.,]*)'
+    )
+    name = 'Deezer'
+    order = 0
+
+
+class SoundCloudPlatform(PlatformABC):
+    """SoundCloud platform."""
+
+    key = 'soundcloud'
+    url_re = (
+        r'https?://([a-zA-Z\d-]+\.)*soundcloud\.(com|app\.goo\.gl)/[^\s.,]*'
+    )
+    name = 'SoundCloud'
+    order = 1
+
+
+class YandexMusicPlatform(PlatformABC):
+    """Yandex Music platform."""
+
+    key = 'yandex'
+    url_re = (
+        r'https?://([a-zA-Z\d-]+\.)*music\.yandex\.(com|ru|by|kz)/(album|track)/'
+        r'[^\s.,]*'
+    )
+    name = 'Yandex Music'
+    order = 2
+
+
+class SpotifyPlatform(PlatformABC):
+    """Spotify platform."""
+
+    key = 'spotify'
+    url_re = (
+        r'https?://([a-zA-Z\d-]+\.)*'
+        r'((spotify\.com/(?:intl-\w+/)?(album|track)/[^\s.,]*)'
+        r'|(tospotify\.com/[^\s.,]*)'
+        r'|(spotify\.link/[^\s]*))'
+    )
+    name = 'Spotify'
+    order = 3
+
+
+class YouTubeMusicPlatform(PlatformABC):
+    """YouTube Music platform."""
+
+    key = 'youtubeMusic'
+    url_re = (
+        r'(https?://([a-zA-Z\d-]+\.)*music\.youtube\.com/(watch|playlist)\?'
+        r'(v|list)=[^\s.,]*)'
+    )
+    name = 'YouTube Music'
+    order = 4
+
+
+class YouTubePlatform(PlatformABC):
+    """YouTube platform."""
+
+    key = 'youtube'
+    url_re = (
+        r'https?://(((www\.)?youtube\.com/(watch|playlist)\?(v|list)=[^\s,]*)'
+        r'|(youtu\.be/[^\s.,]*))'
+    )
+    name = 'YouTube'
+    order = 5
+
+
+class AppleMusicPlatform(PlatformABC):
+    """Apple Music platform."""
+
+    key = 'appleMusic'
+    url_re = r'https?://([a-zA-Z\d-]+\.)*music\.apple\.com/.*?/album/[^\s,.]*'
+    name = 'Apple Music'
+    order = 6
+
+    def postprocess_url(self, url: str) -> str:
+        """Postprocess the platform URL.
+
+        Remove ".geo" prefix
+        (see https://github.com/9dogs/tg-odesli-bot/issues/39).
+        """
+        return url.replace('://geo.', '://')
+
+
+class TidalPlatform(PlatformABC):
+    """Tidal platform."""
+
+    key = 'tidal'
+    url_re = (
+        r'https?://(www\.|listen\.)?tidal\.com(/browse)?/(track|album)/\d+'
+        r'(/track/\d+)?'
+    )
+    name = 'Tidal'
+    order = 7
+
+
+class BandcampPlatform(PlatformABC):
+    """Bandcamp platform."""
+
+    key = 'bandcamp'
+    url_re = r'https?://[^\s.,]*\.bandcamp\.com/(album|track)/[^\s.,]*'
+    name = 'Bandcamp'
+    order = 8
---
+++ packages/tg_odesli_bot/tg_odesli_bot-0.4.1-py3-none-any.whl/tg_odesli_bot/schemas.py
@@ -0,0 +1,40 @@
+"""Odesli API schemas."""
+
+from pydantic import BaseModel, Field
+
+
+class SongSchema(BaseModel):
+    """Platform song schema."""
+
+    #: Identifier
+    id: str | int
+    #: Platform
+    platform: str = Field(..., validation_alias='apiProvider')
+    #: Artist (can be missing)
+    artist: str = Field('<Unknown>', validation_alias='artistName')
+    #: Title
+    title: str | None = None
+    #: Thumbnail URL
+    thumbnail_url: str | None = Field(None, validation_alias='thumbnailUrl')
+
+
+class PlatformLink(BaseModel):
+    """Platform link schema."""
+
+    #: Identifier
+    id: str | int = Field(..., validation_alias='entityUniqueId')
+    #: URL
+    url: str
+
+
+class ApiResponseSchema(BaseModel):
+    """Odesli API response schema."""
+
+    #: Dictionary of entity_id -> SongSchema
+    songs: dict[str, SongSchema] = Field(
+        ..., validation_alias='entitiesByUniqueId'
+    )
+    #: Dictionary of platform -> LinkSchema
+    links: dict[str, PlatformLink] = Field(
+        ..., validation_alias='linksByPlatform'
+    )
