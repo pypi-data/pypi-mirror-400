960f4b81e1cf711886869c2e78e1b2e1a8bae8c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 22:06:15 +0000

    Add package mpt-extension-sdk/5.17.2/mpt_extension_sdk-5.17.2-py3-none-any.whl

---
+++ packages/mpt-extension-sdk/mpt_extension_sdk-5.17.2-py3-none-any.whl/mpt_extension_sdk/mpt_http/base.py
@@ -0,0 +1,55 @@
+from urllib.parse import urljoin
+
+from requests import Session
+from requests.adapters import HTTPAdapter, Retry
+
+from mpt_extension_sdk.constants import POOL_MAX_SIZE, USER_AGENT
+
+
+class MPTClient(Session):
+    """Client for interacting with the MPT API."""
+
+    def __init__(self, base_url, api_token):
+        super().__init__()
+        retries = Retry(
+            total=5,
+            backoff_factor=0.1,
+            status_forcelist=[500, 502, 503, 504],
+        )
+        self.mount(
+            "http://",
+            HTTPAdapter(
+                max_retries=retries,
+                pool_maxsize=POOL_MAX_SIZE,
+            ),
+        )
+        self.mount(
+            "https://",
+            HTTPAdapter(
+                max_retries=retries,
+                pool_maxsize=POOL_MAX_SIZE,
+            ),
+        )
+        self.headers.update(
+            {
+                "User-Agent": USER_AGENT,
+                "Authorization": f"Bearer {api_token}",
+            },
+        )
+        self.base_url = base_url if base_url[-1] == "/" else f"{base_url}/"
+        self.api_token = api_token
+
+    def request(self, method, url, *args, **kwargs):
+        """Send a request to the API."""
+        url = self.join_url(url)
+        return super().request(method, url, *args, **kwargs)
+
+    def prepare_request(self, request, *args, **kwargs):
+        """Prepare the request by joining the base URL."""
+        request.url = self.join_url(request.url)
+        return super().prepare_request(request, *args, **kwargs)
+
+    def join_url(self, url):
+        """Join the base URL with the given URL."""
+        url = url[1:] if url[0] == "/" else url
+        return urljoin(self.base_url, url)
---
+++ packages/mpt-extension-sdk/mpt_extension_sdk-5.17.2-py3-none-any.whl/mpt_extension_sdk/runtime/logging.py
@@ -0,0 +1,45 @@
+from typing import ClassVar
+
+from rich.highlighter import ReprHighlighter as _ReprHighlighter
+from rich.logging import RichHandler as _RichHandler
+
+
+class ReprHighlighter(_ReprHighlighter):
+    """Highlighter for MPT IDs."""
+
+    accounts_prefixes = ("ACC", "BUY", "LCE", "MOD", "SEL", "USR", "AUSR", "UGR")
+    catalog_prefixes = (
+        "PRD",
+        "ITM",
+        "IGR",
+        "PGR",
+        "MED",
+        "DOC",
+        "TCS",
+        "TPL",
+        "WHO",
+        "PRC",
+        "LST",
+        "AUT",
+        "UNT",
+    )
+    commerce_prefixes = ("AGR", "ORD", "SUB", "REQ")
+    aux_prefixes = ("FIL", "MSG")
+    all_prefixes = (
+        *accounts_prefixes,
+        *catalog_prefixes,
+        *commerce_prefixes,
+        *aux_prefixes,
+    )
+    prefixes_pattern = "|".join(all_prefixes)
+    pattern = rf"(?P<mpt_id>(?:{prefixes_pattern})(?:-\d{{4}})*)"
+    highlights: ClassVar[list[str]] = [
+        *_ReprHighlighter.highlights,
+        pattern,
+    ]
+
+
+class RichHandler(_RichHandler):
+    """Rich handler for logging with color support."""
+
+    HIGHLIGHTER_CLASS = ReprHighlighter
