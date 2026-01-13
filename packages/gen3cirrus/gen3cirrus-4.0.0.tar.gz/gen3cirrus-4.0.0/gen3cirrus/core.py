d72d297504c2f89498ce01b23f0dd08960d11715
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 23:05:17 +0000

    Add package gen3cirrus/4.0.0/gen3cirrus-4.0.0.tar.gz

---
+++ packages/gen3cirrus/gen3cirrus-4.0.0.tar.gz/gen3cirrus-4.0.0/gen3cirrus/core.py
@@ -0,0 +1,23 @@
+"""
+gen3cirrus - Cloud API Wrapper Layer exposing easier Cloud Management
+
+Current Capabilities:
+- Manage Google resources, policies, and access (specific Google APIs
+  are abstracted through a Management class that exposes needed behavior)
+- Manage AWS resources amd access S3 APIs
+"""
+
+
+class CloudManager(object):
+    """
+    Generic Class for Cloud Management inherited by difference services
+    """
+
+    def __init__(self):
+        pass
+
+    def init_users(self, users):
+        raise NotImplementedError()
+
+    def get_access_key(self, account):
+        raise NotImplementedError()
