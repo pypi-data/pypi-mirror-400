1637537324e07ef180a64edc432935d9f4f0490a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 09:10:44 +0000

    Add package oarepo-theme/1.0.0.dev5/oarepo_theme-1.0.0.dev5-py3-none-any.whl

---
+++ packages/oarepo-theme/oarepo_theme-1.0.0.dev5-py3-none-any.whl/oarepo_theme/__init__.py
@@ -0,0 +1,17 @@
+#
+# Copyright (c) 2025 CESNET z.s.p.o.
+#
+# This file is a part of oarepo-ui (see https://github.com/oarepo/pytest-oarepo).
+#
+# pytest-oarepo is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+#
+"""oarepo-theme module initialization."""
+
+from __future__ import annotations
+
+__version__ = "1.0.0dev5"
+
+__all__ = [
+    "__version__",
+]
