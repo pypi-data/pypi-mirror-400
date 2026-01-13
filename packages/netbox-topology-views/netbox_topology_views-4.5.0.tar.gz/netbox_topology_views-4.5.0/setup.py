64b30dc808feda6337c4f0f7deada3d853a47b15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 21:07:04 +0000

    Add package netbox-topology-views/4.5.0/netbox_topology_views-4.5.0.tar.gz

---
+++ packages/netbox-topology-views/netbox_topology_views-4.5.0.tar.gz/netbox_topology_views-4.5.0/setup.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+readme = Path(__file__).parent / "README.md"
+long_description = readme.read_text()
+
+setup(
+    name="netbox-topology-views",
+    version="4.5.0",
+    description="An NetBox plugin to create Topology maps",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mattieserver/netbox-topology-views",
+    author="Mattijs Vanhaverbeke",
+    license="Apache 2.0",
+    install_requires=[],
+    packages=find_packages(),
+    include_package_data=True,
+    keywords=["netbox-plugin"],
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
+    project_urls={
+        "Source": "https://github.com/mattieserver/netbox-topology-views",
+    },
+)
