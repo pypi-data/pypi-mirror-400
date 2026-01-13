85c753fb9624bb32b79907f4d66cb679fd8efcc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 19:07:09 +0000

    Add package copick-utils/1.0.2/copick_utils-1.0.2.tar.gz

---
+++ packages/copick-utils/copick_utils-1.0.2.tar.gz/copick_utils-1.0.2/src/copick_utils/process/__init__.py
@@ -0,0 +1,47 @@
+"""Segmentation processing utilities for copick."""
+
+from .connected_components import (
+    extract_individual_components,
+    print_component_stats,
+    separate_components_batch,
+    separate_connected_components_3d,
+    separate_segmentation_components,
+)
+from .skeletonize import (
+    TubeSkeletonizer3D,
+    find_matching_segmentations,
+    skeletonize_batch,
+    skeletonize_segmentation,
+)
+from .spline_fitting import (
+    SkeletonSplineFitter,
+    find_matching_segmentations_for_spline,
+    fit_spline_batch,
+    fit_spline_to_segmentation,
+    fit_spline_to_skeleton,
+)
+from .validbox import (
+    create_validbox_mesh,
+    generate_validbox,
+    validbox_batch,
+)
+
+__all__ = [
+    "separate_connected_components_3d",
+    "extract_individual_components",
+    "print_component_stats",
+    "separate_segmentation_components",
+    "separate_components_batch",
+    "TubeSkeletonizer3D",
+    "skeletonize_segmentation",
+    "find_matching_segmentations",
+    "skeletonize_batch",
+    "SkeletonSplineFitter",
+    "fit_spline_to_skeleton",
+    "fit_spline_to_segmentation",
+    "find_matching_segmentations_for_spline",
+    "fit_spline_batch",
+    "create_validbox_mesh",
+    "generate_validbox",
+    "validbox_batch",
+]
