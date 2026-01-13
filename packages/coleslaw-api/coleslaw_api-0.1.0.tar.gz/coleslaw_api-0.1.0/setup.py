66817bf1f9fd314059f2041581d6a3a4091b5fac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 21:07:10 +0000

    Add package coleslaw-api/0.1.0/coleslaw_api-0.1.0.tar.gz

---
+++ packages/coleslaw-api/coleslaw_api-0.1.0.tar.gz/coleslaw_api-0.1.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="coleslaw_api",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "aiohttp>=3.8.5",
+        "aiomysql>=0.1.1",
+    ],
+    python_requires=">=3.10",
+    url="https://github.com/DiscoKnax6808/Coleslaw",
+    author="Exception2018",
+    author_email="knax6808real22@gmail.com",
+    description="Fast Python web server with async MySQL and routing",
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Framework :: AsyncIO",
+    ],
+)
