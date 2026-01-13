a9daff21a26e876cae4370dbf0e4d682b8a2525c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 23:05:38 +0000

    Add package xlavm-lib/1.0.1/xlavm_lib-1.0.1.tar.gz

---
+++ packages/xlavm-lib/xlavm_lib-1.0.1.tar.gz/xlavm_lib-1.0.1/src/xlavm_lib/__init__.py
@@ -0,0 +1,11 @@
+from xlavm_lib.visual_test.visual_test import VisualTest
+from xlavm_lib.visual_test.visual_test_report import VisualTestReport
+from xlavm_lib.get_ids.get_ids import GetIds
+
+__all__ = [
+    "VisualTest",
+    "VisualTestReport",
+    "GetIds"
+]
+
+# Asi el usuario usa: from xlavm_lib import VisualTest, VisualTestReport
