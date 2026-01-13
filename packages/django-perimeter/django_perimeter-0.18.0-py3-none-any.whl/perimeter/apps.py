8eb2a85d81a7eb24bc1820d46ba20b73c52498da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 09:08:22 +0000

    Add package django-perimeter/0.18.0/django_perimeter-0.18.0-py3-none-any.whl

---
+++ packages/django-perimeter/django_perimeter-0.18.0-py3-none-any.whl/perimeter/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class PerimeterAppConfig(AppConfig):
+    name = "perimeter"
+    verbose_name = "Perimeter"
+    default_auto_field = "django.db.models.AutoField"
