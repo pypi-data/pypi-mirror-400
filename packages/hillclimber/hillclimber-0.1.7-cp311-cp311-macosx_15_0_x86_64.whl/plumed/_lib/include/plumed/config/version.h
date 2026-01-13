2e3bbd3101d70c5da227d552248fef1d3922e638
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 09:08:40 +0000

    Add package hillclimber/0.1.7/hillclimber-0.1.7-cp311-cp311-macosx_15_0_x86_64.whl

---
+++ packages/hillclimber/hillclimber-0.1.7-cp311-cp311-macosx_15_0_x86_64.whl/plumed/_lib/include/plumed/config/version.h
@@ -0,0 +1,9 @@
+#ifndef __PLUMED_config_version_h
+#define __PLUMED_config_version_h
+#define PLUMED_VERSION_SHORT "2.11"
+#define PLUMED_VERSION_LONG "2.11.0-dev"
+#define PLUMED_VERSION_GIT "Unknown"
+#define PLUMED_VERSION_MAJOR 2
+#define PLUMED_VERSION_MINOR 11
+#define PLUMED_VERSION_PATCH 0
+#endif
