9d35e031c25e2a5cea649a7d7ca2cc49959aac19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 16:07:09 +0000

    Add package bsg-sdk/0.1.0/bsg_sdk-0.1.0.tar.gz

---
+++ packages/bsg-sdk/bsg_sdk-0.1.0.tar.gz/bsg_sdk-0.1.0/bsg_api/api/__init__.py
@@ -0,0 +1,28 @@
+# flake8: noqa
+
+# import apis into api package
+from bsg_api.api.account_settings_api import AccountSettingsApi
+from bsg_api.api.auth_api import AuthApi
+from bsg_api.api.balance_api import BalanceApi
+from bsg_api.api.campaign_api import CampaignApi
+from bsg_api.api.campaign_rcs_api import CampaignRCSApi
+from bsg_api.api.campaign_sms_api import CampaignSMSApi
+from bsg_api.api.campaign_whats_app_api import CampaignWhatsAppApi
+from bsg_api.api.contact_api import ContactApi
+from bsg_api.api.contact_field_api import ContactFieldApi
+from bsg_api.api.contact_list_api import ContactListApi
+from bsg_api.api.email_api import EmailApi
+from bsg_api.api.internal_core_price_api import InternalCorePriceApi
+from bsg_api.api.internal_country_api import InternalCountryApi
+from bsg_api.api.internal_currency_api import InternalCurrencyApi
+from bsg_api.api.internal_two_fa_api import InternalTwoFAApi
+from bsg_api.api.internal_wst_price_api import InternalWstPriceApi
+from bsg_api.api.messages_sms_api import MessagesSMSApi
+from bsg_api.api.messages_whats_app_api import MessagesWhatsAppApi
+from bsg_api.api.senders_api import SendersApi
+from bsg_api.api.short_domains_api import ShortDomainsApi
+from bsg_api.api.short_links_api import ShortLinksApi
+from bsg_api.api.stop_list_api import StopListApi
+from bsg_api.api.two_faotp_api import TwoFAOTPApi
+from bsg_api.api.two_fa_templates_api import TwoFATemplatesApi
+
