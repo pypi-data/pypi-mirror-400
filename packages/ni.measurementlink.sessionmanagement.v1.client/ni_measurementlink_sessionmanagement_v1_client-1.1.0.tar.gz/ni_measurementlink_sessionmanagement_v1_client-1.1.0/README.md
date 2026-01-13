d3b42318aa5f225d360f8130e62d215ab9a21add
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 15:08:27 +0000

    Add package ni.measurementlink.sessionmanagement.v1.client/1.1.0/ni_measurementlink_sessionmanagement_v1_client-1.1.0.tar.gz

---
+++ packages/ni.measurementlink.sessionmanagement.v1.client/ni_measurementlink_sessionmanagement_v1_client-1.1.0.tar.gz/ni_measurementlink_sessionmanagement_v1_client-1.1.0/README.md
@@ -0,0 +1,26 @@
+# Table of Contents
+
+- [Table of Contents](#table-of-contents)
+- [About](#about)
+  - [Operating System Support](#operating-system-support)
+  - [Python Version Support](#python-version-support)
+
+# About
+
+`ni.measurementlink.sessionmanagement.v1.client` is a Python package that provides a client API for
+the [ni.measurementlink.sessionmanagement.v1 package](https://github.com/ni/ni-apis/tree/main/ni/measurementlink/sessionmanagement/v1).
+
+NI created and supports this package.
+
+## Operating System Support
+
+`ni.measurementlink.sessionmanagement.v1.client` supports Windows and Linux operating systems.
+
+## Python Version Support
+
+`ni.measurementlink.sessionmanagement.v1.client` supports CPython 3.10+.
+
+## Installation
+
+You can directly install the `ni.measurementlink.sessionmanagement.v1.client` package using `pip` or by listing it as a
+dependency in your project's `pyproject.toml` file.
---
+++ packages/ni.measurementlink.sessionmanagement.v1.client/ni_measurementlink_sessionmanagement_v1_client-1.1.0.tar.gz/ni_measurementlink_sessionmanagement_v1_client-1.1.0/src/ni/measurementlink/sessionmanagement/v1/client/_drivers/_nidcpower.py
@@ -0,0 +1,63 @@
+from __future__ import annotations
+
+from typing import Any
+
+import nidcpower
+from ni.measurementlink.discovery.v1.client import DiscoveryClient
+from ni_grpc_extensions.channelpool import GrpcChannelPool
+
+from ni.measurementlink.sessionmanagement.v1.client._drivers._configuration import (
+    NIDCPOWER_OPTIONS,
+)
+from ni.measurementlink.sessionmanagement.v1.client._drivers._grpcdevice import (
+    get_insecure_grpc_device_server_channel,
+)
+from ni.measurementlink.sessionmanagement.v1.client._types import (
+    SessionInformation,
+    SessionInitializationBehavior,
+)
+
+_INITIALIZATION_BEHAVIOR = {
+    SessionInitializationBehavior.AUTO: nidcpower.SessionInitializationBehavior.AUTO,
+    SessionInitializationBehavior.INITIALIZE_SERVER_SESSION: nidcpower.SessionInitializationBehavior.INITIALIZE_SERVER_SESSION,
+    SessionInitializationBehavior.ATTACH_TO_SERVER_SESSION: nidcpower.SessionInitializationBehavior.ATTACH_TO_SERVER_SESSION,
+    SessionInitializationBehavior.INITIALIZE_SESSION_THEN_DETACH: nidcpower.SessionInitializationBehavior.INITIALIZE_SERVER_SESSION,
+    SessionInitializationBehavior.ATTACH_TO_SESSION_THEN_CLOSE: nidcpower.SessionInitializationBehavior.ATTACH_TO_SERVER_SESSION,
+}
+
+
+class SessionConstructor:
+    """Constructs sessions based on SessionInformation."""
+
+    def __init__(
+        self,
+        discovery_client: DiscoveryClient,
+        grpc_channel_pool: GrpcChannelPool,
+        reset: bool,
+        options: dict[str, Any] | None,
+        initialization_behavior: SessionInitializationBehavior,
+    ) -> None:
+        """Initialize the session constructor."""
+        self._grpc_channel = get_insecure_grpc_device_server_channel(
+            discovery_client, grpc_channel_pool, nidcpower.GRPC_SERVICE_INTERFACE_NAME
+        )
+        self._reset = reset
+        self._options = NIDCPOWER_OPTIONS.to_dict() if options is None else options
+        self._initialization_behavior = _INITIALIZATION_BEHAVIOR[initialization_behavior]
+
+    def __call__(self, session_info: SessionInformation) -> nidcpower.Session:
+        """Construct a session object."""
+        kwargs: dict[str, Any] = {}
+        if self._grpc_channel:
+            kwargs["grpc_options"] = nidcpower.GrpcSessionOptions(
+                grpc_channel=self._grpc_channel,
+                session_name=session_info.session_name,
+                initialization_behavior=self._initialization_behavior,
+            )
+
+        return nidcpower.Session(
+            resource_name=session_info.resource_name,
+            reset=self._reset,
+            options=self._options,
+            **kwargs,
+        )
