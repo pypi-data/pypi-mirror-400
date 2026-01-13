ed75966e555450230e01a118698d616629036267
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 05:08:17 +0000

    Add package pyaudiosynth/0.2.5/pyaudiosynth-0.2.5.tar.gz

---
+++ packages/pyaudiosynth/pyaudiosynth-0.2.5.tar.gz/pyaudiosynth-0.2.5/pyaudiosynth/wave.py
@@ -0,0 +1,10 @@
+from enum import Enum
+class Wave(Enum):
+    #: A sine wave
+    Sine = 1
+    #: A square wave
+    Square = 2
+    #: A sawtooth wave
+    Saw = 3
+    #: A triangle wave
+    Triangle = 4
