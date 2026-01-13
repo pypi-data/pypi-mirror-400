72690ba4cb5bcffeab7619f6cf9af29824a42a25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 14:07:18 +0000

    Add package SigProfilerAssignment/1.0.4/sigprofilerassignment-1.0.4-py3-none-any.whl

---
+++ packages/SigProfilerAssignment/sigprofilerassignment-1.0.4-py3-none-any.whl/SigProfilerAssignment/version.py
@@ -0,0 +1,7 @@
+
+# THIS FILE IS GENERATED FROM SigProfilerAssignment SETUP.PY
+short_version = '1.0.4'
+version = '1.0.4'
+Update = 'v1.0.4: Support decompose_fit in higher contexts (288/1536), custom signature names, and fix reference signature packaging'
+
+    
