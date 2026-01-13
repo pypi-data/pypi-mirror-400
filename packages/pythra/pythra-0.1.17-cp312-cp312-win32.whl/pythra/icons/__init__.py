49698fec3fcd4a892182785993f90d1fae6e58dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 19:07:46 +0000

    Add package pythra/0.1.17/pythra-0.1.17-cp312-cp312-win32.whl

---
+++ packages/pythra/pythra-0.1.17-cp312-cp312-win32.whl/pythra/icons/__init__.py
@@ -0,0 +1,11 @@
+from .icons_sharp import IconsSharp
+from .icons_rounded import IconsRounded
+from .icons_outlined import IconsOutlined
+from .icons_outlined_1 import IconsOutlined1
+from .icons_sharp_1 import IconsSharp1
+from .icons_rounded_1 import IconsRounded1
+from .base import *
+
+class Icons(IconsSharp, IconsRounded, IconsOutlined, IconsSharp1, IconsRounded1, IconsOutlined1):
+    """Unified access to all icons."""
+    pass
