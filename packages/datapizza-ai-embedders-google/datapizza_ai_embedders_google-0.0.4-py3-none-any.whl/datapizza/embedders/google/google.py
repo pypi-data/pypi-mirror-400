5b452f5613e2dd52be627d6343dde158dc3a1fc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 09:10:22 +0000

    Add package datapizza-ai-embedders-google/0.0.4/datapizza_ai_embedders_google-0.0.4-py3-none-any.whl

---
+++ packages/datapizza-ai-embedders-google/datapizza_ai_embedders_google-0.0.4-py3-none-any.whl/datapizza/embedders/google/google.py
@@ -0,0 +1,80 @@
+from datapizza.core.embedder import BaseEmbedder
+
+from google import genai
+from google.genai import types
+
+
+class GoogleEmbedder(BaseEmbedder):
+    def __init__(
+        self,
+        *,
+        api_key: str,
+        model_name: str | None = None,
+        task_type: str = "RETRIEVAL_DOCUMENT",
+        output_dimensionality: int = 3072,
+    ):
+        self.api_key = api_key
+        self.model_name = model_name
+        self.task_type = task_type
+        self.output_dimensionality = output_dimensionality
+
+        self.client = None
+        self.a_client = None
+
+    def _set_client(self):
+        if not self.client:
+            client = genai.Client(api_key=self.api_key)
+            self.client = client
+
+    def _set_a_client(self):
+        if not self.a_client:
+            client = genai.Client(api_key=self.api_key)
+            self.a_client = client
+
+    def embed(
+        self, text: str | list[str], model_name: str | None = None
+    ) -> list[float] | list[list[float]]:
+        model = model_name or self.model_name
+        if not model:
+            raise ValueError("Model name is required.")
+
+        texts = [text] if isinstance(text, str) else text
+
+        client = self._get_client()
+
+        result = client.models.embed_content(
+            model=model,
+            contents=texts,
+            config=types.EmbedContentConfig(
+                task_type=self.task_type,
+                output_dimensionality=self.output_dimensionality,
+            ),
+        )
+
+        res = [embedding.values for embedding in result.embeddings]
+
+        return res[0] if isinstance(text, str) else res
+
+    async def a_embed(
+        self, text: str | list[str], model_name: str | None = None
+    ) -> list[float] | list[list[float]]:
+        model = model_name or self.model_name
+        if not model:
+            raise ValueError("Model name is required.")
+
+        texts = [text] if isinstance(text, str) else text
+
+        client = self._get_a_client()
+
+        result = await client.models.embed_content_async(
+            model=model,
+            contents=texts,
+            config=types.EmbedContentConfig(
+                task_type=self.task_type,
+                output_dimensionality=self.output_dimensionality,
+            ),
+        )
+
+        res = [embedding.values for embedding in result.embeddings]
+
+        return res[0] if isinstance(text, str) else res
