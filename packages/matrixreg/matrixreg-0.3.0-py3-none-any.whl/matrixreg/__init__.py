afb6da7f7671df8f16e2743213f889fadec80942
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 19:08:11 +0000

    Add package matrixreg/0.3.0/matrixreg-0.3.0-py3-none-any.whl

---
+++ packages/matrixreg/matrixreg-0.3.0-py3-none-any.whl/matrixreg/__init__.py
@@ -0,0 +1,14 @@
+"""Matrix Regression
+
+This python module was originally created as part of
+the Text Mining and Sentiment Analysis exam project (2020).
+
+Author: Nicolò Verardo
+
+License: MIT License
+"""
+
+from .matrixregression import MatrixRegression
+
+__all__ = ["MatrixRegression"]
+__version__ = "0.3.0"
