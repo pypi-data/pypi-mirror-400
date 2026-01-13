f25cf090270c21dd8cb7f06e27bc23b796ce62b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 09:10:07 +0000

    Add package bizyengine/1.2.81/bizyengine-1.2.81-py3-none-any.whl

---
+++ packages/bizyengine/bizyengine-1.2.81-py3-none-any.whl/bizyengine/bizyair_extras/third_party_api/__init__.py
@@ -0,0 +1,16 @@
+modules = [
+    ".nodes_hailuo",
+    ".nodes_gemini",
+    ".nodes_gpt",
+    ".nodes_kling",
+    ".nodes_doubao",
+    ".nodes_flux",
+    ".nodes_sora",
+    ".nodes_veo3",
+    ".nodes_wan_api",
+    ".nodes_vidu",
+]
+from bizyengine.core.common.utils import safe_star_import
+
+for mod in modules:
+    safe_star_import(mod, package=__package__)
