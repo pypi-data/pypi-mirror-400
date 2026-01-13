271fdc3fdd645007b9d3fb6ab8cd44340ed2546a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 18:10:52 +0000

    Add package scikit-plots/0.4.0.post9/scikit_plots-0.4.0.post9-cp38-cp38-macosx_12_0_arm64.whl

---
+++ packages/scikit-plots/scikit_plots-0.4.0.post9-cp38-cp38-macosx_12_0_arm64.whl/scikitplot/cexternals/_numcpp/__init__.py
@@ -0,0 +1,20 @@
+# Authors: David Pilger
+# SPDX-License-Identifier: MIT
+
+"""
+NumCpp: A Templatized Header Only C++ Library with Implementation of the Python NumPy-Compatible API.
+
+* Author: `David Pilger <dpilger26@gmail.com>`_
+* License: MIT
+
+Compilers:
+
+* C++ Standards: C++17 C++20 C++23
+* MSVC Visual Studio: 2022
+* GCC GNU: 13.3, 14.2
+* Clang LLVM: 18, 19
+* Boost Versions: 1.73+
+
+.. seealso::
+   * https://github.com/dpilger26/NumCpp
+"""
