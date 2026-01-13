bddaf3f5ad37087ae3a92ae5f0a7d8d3d7a99221
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 14:06:17 +0000

    Add package pyasyncore/1.0.5/pyasyncore-1.0.5.tar.gz

---
+++ packages/pyasyncore/pyasyncore-1.0.5.tar.gz/pyasyncore-1.0.5/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+python_classifiers = ['Programming Language :: Python :: %s' % version for version in ['3.12', '3.13', '3.14']]
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+setup(
+    name='pyasyncore',
+    version='1.0.5',
+    description='Make asyncore available for Python 3.12 onwards',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    author='Simon Robinson',
+    author_email='simon@robinson.ac',
+    url='https://github.com/simonrob/pyasyncore',
+
+    platforms='any',
+    packages=['asyncore'],
+
+    license='Python Software Foundation License Version 2',
+    classifiers=['Topic :: Software Development :: Libraries',
+                 'Development Status :: 5 - Production/Stable',
+                 'License :: OSI Approved :: Python Software Foundation License'] + python_classifiers,
+)
