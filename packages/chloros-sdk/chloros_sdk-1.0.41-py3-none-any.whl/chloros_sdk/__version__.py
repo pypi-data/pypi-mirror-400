6269ace0242118e2cd68394cd52477edbf001f9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 07:08:21 +0000

    Add package chloros-sdk/1.0.41/chloros_sdk-1.0.41-py3-none-any.whl

---
+++ packages/chloros-sdk/chloros_sdk-1.0.41-py3-none-any.whl/chloros_sdk/__version__.py
@@ -0,0 +1,24 @@
+"""Version information for Chloros SDK"""
+
+__title__ = 'chloros-sdk'
+__description__ = 'Official Python SDK for MAPIR Chloros image processing'
+__version__ = '1.0.41'
+__author__ = 'MAPIR Inc.'
+__author_email__ = 'info@mapir.camera'
+__license__ = 'Proprietary'
+__copyright__ = 'Copyright 2025 MAPIR Inc.'
+__url__ = 'https://www.mapir.camera'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
