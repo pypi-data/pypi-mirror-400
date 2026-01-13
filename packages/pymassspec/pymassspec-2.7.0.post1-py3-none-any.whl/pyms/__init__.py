ba71428fbdac0b9d8db315e25b98b1bd634dd16e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 12:06:57 +0000

    Add package pymassspec/2.7.0.post1/pymassspec-2.7.0.post1-py3-none-any.whl

---
+++ packages/pymassspec/pymassspec-2.7.0.post1-py3-none-any.whl/pyms/__init__.py
@@ -0,0 +1,36 @@
+"""
+The root of the package pyms.
+"""
+
+################################################################################
+#                                                                              #
+#    PyMassSpec software for processing of mass-spectrometry data              #
+#    Copyright (C) 2005-2012 Vladimir Likic                                    #
+#    Copyright (C) 2019-2020 Dominic Davis-Foster                              #
+#                                                                              #
+#    This program is free software; you can redistribute it and/or modify      #
+#    it under the terms of the GNU General Public License version 2 as         #
+#    published by the Free Software Foundation.                                #
+#                                                                              #
+#    This program is distributed in the hope that it will be useful,           #
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of            #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             #
+#    GNU General Public License for more details.                              #
+#                                                                              #
+#    You should have received a copy of the GNU General Public License         #
+#    along with this program; if not, write to the Free Software               #
+#    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.                 #
+#                                                                              #
+################################################################################
+
+# from pyms import Baseline, Deconvolution, Display, Experiment, Gapfill, GCMS, Noise, Peak, Simulator, Utils
+
+name = "PyMassSpec"
+__author__: str = "See AUTHORS"
+__license__: str = "GPLv2"
+__maintainer_email__: str = "dominic@davis-foster.co.uk"
+__url__: str = "http://domdfcoding.github.io/PyMassSpec"
+__version__: str = "2.7.0.post1"
+
+__copyright__: str = "2005-2012 Vladimir Likic, 2019-2020 Dominic Davis-Foster"
+__email__: str = "dominic@davis-foster.co.uk"
