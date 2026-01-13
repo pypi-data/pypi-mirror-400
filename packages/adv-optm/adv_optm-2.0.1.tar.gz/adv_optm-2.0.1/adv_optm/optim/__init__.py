510ceca64fa01ec5cb000bc8cd0c3238811db554
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 14:06:15 +0000

    Add package adv-optm/2.0.1/adv_optm-2.0.1.tar.gz

---
+++ packages/adv-optm/adv_optm-2.0.1.tar.gz/adv_optm-2.0.1/adv_optm/optim/__init__.py
@@ -0,0 +1,19 @@
+from .AdamW_adv import AdamW_adv
+from .Prodigy_adv import Prodigy_adv
+from .Adopt_adv import Adopt_adv
+from .Simplified_AdEMAMix import Simplified_AdEMAMix
+from .Lion_adv import Lion_adv
+from .Lion_Prodigy_adv import Lion_Prodigy_adv
+from .Muon_adv import Muon_adv
+from .AdaMuon_adv import AdaMuon_adv
+
+__all__ = [
+    "AdamW_adv",
+    "Prodigy_adv",
+    "Adopt_adv",
+    "Simplified_AdEMAMix",
+    "Lion_adv",
+    "Lion_Prodigy_adv",
+    "Muon_adv",
+    "AdaMuon_adv",
+]
