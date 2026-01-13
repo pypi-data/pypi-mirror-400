ff2ecd57f176e6fc253cace94ff5944e79eed93e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 11:06:02 +0000

    Add package icat-plus-client/4.54.1/icat_plus_client-4.54.1.tar.gz

---
+++ packages/icat-plus-client/icat_plus_client-4.54.1.tar.gz/icat_plus_client-4.54.1/icat_plus_client/api/__init__.py
@@ -0,0 +1,24 @@
+# flake8: noqa
+
+# import apis into api package
+from icat_plus_client.api.actions_api import ActionsApi
+from icat_plus_client.api.cache_api import CacheApi
+from icat_plus_client.api.catalogue_api import CatalogueApi
+from icat_plus_client.api.dmp_api import DMPApi
+from icat_plus_client.api.doi_api import DOIApi
+from icat_plus_client.api.data_acquisition_api import DataAcquisitionApi
+from icat_plus_client.api.ewoks_api import EWOKSApi
+from icat_plus_client.api.elastic_search_api import ElasticSearchApi
+from icat_plus_client.api.h5_grove_api import H5GroveApi
+from icat_plus_client.api.ids_api import IDSApi
+from icat_plus_client.api.logbook_api import LogbookApi
+from icat_plus_client.api.messages_api import MESSAGESApi
+from icat_plus_client.api.oaipmh_api import OAIPMHApi
+from icat_plus_client.api.panosc_api import PanoscApi
+from icat_plus_client.api.resource_api import ResourceApi
+from icat_plus_client.api.sample_metadata_api import SampleMetadataApi
+from icat_plus_client.api.session_api import SessionApi
+from icat_plus_client.api.tracking_api import TrackingApi
+from icat_plus_client.api.users_api import USERSApi
+from icat_plus_client.api.user_management_api import UserManagementApi
+
