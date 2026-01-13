8cc604964a1d7e593b515321abbaa0d73ec8409c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 09:10:54 +0000

    Add package django-xladmin-enhanced/1.0.6/django_xladmin_enhanced-1.0.6-py3-none-any.whl

---
+++ packages/django-xladmin-enhanced/django_xladmin_enhanced-1.0.6-py3-none-any.whl/xladmin/plugins/__init__.py
@@ -0,0 +1,42 @@
+﻿
+PLUGINS = (
+    'actions',
+    'filters',
+    'bookmark',
+    'export',
+    'layout',
+    'refresh',
+    'details',
+    'editable',
+    'relate',
+    'chart',
+    'ajax',
+    'relfield',
+    'inline',
+    'topnav',
+    'portal',
+    'quickform',
+    'wizard',
+    'images',
+    'auth',
+    'multiselect',
+    'themes',
+    'aggregation',
+    # 'mobile',
+    'passwords',
+    'sitemenu',
+    'language',
+    'quickfilter',
+    'sortablelist',
+    'importexport'
+)
+
+
+def register_builtin_plugins(site):
+    from importlib import import_module
+    from django.conf import settings
+
+    exclude_plugins = getattr(settings, 'XLADMIN_EXCLUDE_PLUGINS', [])
+
+    [import_module('xladmin.plugins.%s' % plugin) for plugin in PLUGINS if plugin not in exclude_plugins]
+
