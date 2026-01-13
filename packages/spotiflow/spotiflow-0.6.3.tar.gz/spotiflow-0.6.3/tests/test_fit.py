385d7408631efb4390e06e02767a2bc1ec131200
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 21:07:17 +0000

    Add package spotiflow/0.6.3/spotiflow-0.6.3.tar.gz

---
+++ packages/spotiflow/spotiflow-0.6.3.tar.gz/spotiflow-0.6.3/tests/test_fit.py
@@ -0,0 +1,50 @@
+
+import numpy as np
+from spotiflow.utils import points_to_prob, estimate_params
+from spotiflow.model import Spotiflow
+
+def test_fit2d(return_results: bool=False):
+    np.random.seed(42)
+    
+    n_points=64
+    points = np.random.randint(20,245-20, (n_points,2))
+    sigmas = np.random.uniform(1, 5, n_points)
+    
+    x = points_to_prob(points, (256,256), sigma=sigmas, mode='sum')
+    
+    x += .2+0.05*np.random.normal(0, 1, x.shape)
+    
+    params = estimate_params(x, points)
+    assert params is not None
+    if return_results:
+        return x, sigmas, params
+
+def test_fit3d(return_results: bool=False):
+
+    np.random.seed(42)
+    ndim=3 
+    
+    n_points=64
+    points = np.random.randint(20,128-20, (n_points,ndim))
+    sigmas = np.random.uniform(1, 5, n_points)
+    
+    x = points_to_prob(points, (128,)*ndim, sigma=sigmas, mode='sum')
+    
+    x += .2+0.05*np.random.normal(0, 1, x.shape)
+    
+    params = estimate_params(x, points)
+    assert params is not None
+    if return_results:
+        return x, sigmas, params
+        
+if __name__ == "__main__":
+    
+    
+    x, sigmas, params = test_fit3d(return_results=True)
+
+    
+    model = Spotiflow.from_pretrained("synth_3d")
+    
+    img = np.clip(200*x, 0,255).astype(np.uint8)
+    
+    points, details = model.predict(img, fit_params=True)
