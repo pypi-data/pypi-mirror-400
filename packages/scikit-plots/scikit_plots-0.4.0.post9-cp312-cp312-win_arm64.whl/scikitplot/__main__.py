a612be445f81e0fa4073c17edc7627693cab8f76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 18:10:36 +0000

    Add package scikit-plots/0.4.0.post9/scikit_plots-0.4.0.post9-cp312-cp312-win_arm64.whl

---
+++ packages/scikit-plots/scikit_plots-0.4.0.post9-cp312-cp312-win_arm64.whl/scikitplot/__main__.py
@@ -0,0 +1,9 @@
+# Authors: The scikit-plots developers
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""CLI Interface."""
+
+from ._cli.cli import cli
+
+if __name__ == "__main__":
+    cli.main()
---
+++ packages/scikit-plots/scikit_plots-0.4.0.post9-cp312-cp312-win_arm64.whl/scikitplot/config/__init__.py
@@ -0,0 +1,34 @@
+# Flake8: noqa: F401,F403,F405
+# type: ignore[]
+# pylint: disable=import-error,import-self,undefined-all-variable
+
+# Authors: The scikit-plots developers
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""
+Configuration module for the package.
+
+This module consolidates configuration-related components.
+"""
+
+from .._testing._pytesttester import PytestTester  # Pytest testing
+from .__config__ import *
+from ._citation import *
+from ._config import *
+
+test = PytestTester(__name__)
+del PytestTester
+
+# __all__ = [s for s in dir() if not s.startswith("_")]  # Remove dunders.
+__all__ = [
+    # "_citation",
+    "__bibtex__",
+    "__citation__",
+    # "_config",
+    "config_context",
+    "get_config",
+    "set_config",
+    # "__config__",
+    "show_config",
+    "test",
+]
