a566547ad1d70abeb847da5c8181e885c94f15da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 23:06:11 +0000

    Add package typedb-driver/3.7.0a3/typedb_driver-3.7.0a3-py312-none-manylinux_2_17_x86_64.whl

---
+++ packages/typedb-driver/typedb_driver-3.7.0a3-py312-none-manylinux_2_17_x86_64.whl/typedb/api/concept/type/entity_type.py
@@ -0,0 +1,57 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from __future__ import annotations
+
+from abc import ABC
+from typing import TYPE_CHECKING
+
+from typedb.api.concept.type.type import Type
+
+if TYPE_CHECKING:
+    pass
+
+
+class EntityType(Type, ABC):
+    """
+    Entity types represent the classification of independent objects
+    in the data model of the business domain.
+    """
+
+    def is_entity_type(self) -> bool:
+        """
+        Checks if the concept is an ``EntityType``.
+
+        Examples
+        --------
+        ::
+
+            entity_type.is_entity_type()
+        """
+        return True
+
+    def as_entity_type(self) -> EntityType:
+        """
+        Casts the concept to ``EntityType``.
+
+        Examples
+        --------
+        ::
+
+            entity_type.as_entity_type()
+        """
+        return self
