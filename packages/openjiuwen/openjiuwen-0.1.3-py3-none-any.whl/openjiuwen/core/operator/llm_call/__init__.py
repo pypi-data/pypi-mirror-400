e51738fcf5086d049909918f5005c51b9fea2d2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 17:09:25 +0000

    Add package openjiuwen/0.1.3/openjiuwen-0.1.3-py3-none-any.whl

---
+++ packages/openjiuwen/openjiuwen-0.1.3-py3-none-any.whl/openjiuwen/core/operator/llm_call/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: UTF-8 -*-
+# Copyright (c) Huawei Technologies Co., Ltd. 2025. All rights reserved.
+
+from openjiuwen.core.operator.llm_call.base import LLMCall
+
+__all__ = [
+    "LLMCall",
+]
