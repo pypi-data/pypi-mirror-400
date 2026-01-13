4b28a1de2838c48f043eabba8323ccb7fd858c6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 11:05:58 +0000

    Add package zeushell/0.6.0/zeushell-0.6.0-py3-none-any.whl

---
+++ packages/zeushell/zeushell-0.6.0-py3-none-any.whl/zeushell/__init__.py
@@ -0,0 +1,56 @@
+def start():
+    qqqqqqq=open("w.txt","w")
+    qqqqqqq.write("[]")
+    qqqqqqq.close()
+def name(a):
+    global shellname
+    shellname=a
+def function():
+    import ast
+    def dd(func):
+        try:
+            qqqqqqq=open("w.txt","r")
+            funclist=ast.literal_eval(qqqqqqq.read())
+            qqqqqqq.close()
+        except:
+            funclist=[]
+        funclist.append(func.__name__)
+        qqqqqqq=open("w.txt","w")
+        qqqqqqq.write(str(funclist))
+        qqqqqqq.close()
+        return func
+    return dd
+def run():
+    import __main__
+    import ast
+    try:
+        shellname+=": "
+    except:
+        shellname="Zeushell: "
+    while True:
+        ino=input(shellname)
+        s=ino.split()
+        njdo=[]
+        for i in s:
+          try:
+            siu=int(i)
+            njdo.append(siu)
+          except:
+              njdo.append(i)
+        s=njdo      
+        if s==[]:
+            continue
+        aaaaa=s[0]
+        qqqqqqq=open("w.txt","r")
+        wigeiu=qqqqqqq.read()
+        qqqqqqq.close()
+        if aaaaa not in ast.literal_eval(wigeiu):
+            print('You didn\'t have '+aaaaa+' function') 
+            continue
+        try:
+          s=s[1:]
+        except:
+          s=[]
+          kwargs = {}
+        __main__.__dict__[aaaaa](*s, **kwargs)
+    return 1
