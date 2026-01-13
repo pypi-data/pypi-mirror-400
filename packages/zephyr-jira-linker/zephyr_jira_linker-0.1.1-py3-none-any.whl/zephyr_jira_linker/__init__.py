9dd2f3ecd98f461843578e09e455b912fade3a36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 17:10:13 +0000

    Add package zephyr-jira-linker/0.1.1/zephyr_jira_linker-0.1.1-py3-none-any.whl

---
+++ packages/zephyr-jira-linker/zephyr_jira_linker-0.1.1-py3-none-any.whl/zephyr_jira_linker/__init__.py
@@ -0,0 +1,8 @@
+"""Zephyr Test Linker - A tool for linking test cases in Zephyr Scale to Jira issues."""
+
+__version__ = "0.1.0"
+__author__ = "Pandiyaraj Karuppasamy"
+__email__ = "pandiyarajk@live.com"
+__description__ = "A command-line tool for linking test cases in Zephyr Scale to Jira issues"
+
+from .main import *
