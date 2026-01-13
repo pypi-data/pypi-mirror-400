ca16f75cc5cfb6616dc73f205709bd28fbcd0ba6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 20:05:33 +0000

    Add package SigProfilerAssignment/1.1.1/sigprofilerassignment-1.1.1-py3-none-any.whl

---
+++ packages/SigProfilerAssignment/sigprofilerassignment-1.1.1-py3-none-any.whl/SigProfilerAssignment/version.py
@@ -0,0 +1,7 @@
+
+# THIS FILE IS GENERATED FROM SigProfilerAssignment SETUP.PY
+short_version = '1.1.1'
+version = '1.1.1'
+Update = 'v1.1.1: Added missing COSMIC v3.5 signature files (exome versions, mm39, rn7)'
+
+    
