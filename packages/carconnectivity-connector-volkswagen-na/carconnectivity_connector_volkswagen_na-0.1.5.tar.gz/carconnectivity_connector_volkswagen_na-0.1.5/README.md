d97044d25a4f05f2e7ac7eb0ba8741f9f570d51b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 07:08:15 +0000

    Add package carconnectivity-connector-volkswagen-na/0.1.5/carconnectivity_connector_volkswagen_na-0.1.5.tar.gz

---
+++ packages/carconnectivity-connector-volkswagen-na/carconnectivity_connector_volkswagen_na-0.1.5.tar.gz/carconnectivity_connector_volkswagen_na-0.1.5/README.md
@@ -0,0 +1,87 @@
+
+
+# CarConnectivity Connector for Volkswagen North American Vehicles
+[![GitHub sourcecode](https://img.shields.io/badge/Source-GitHub-green)](https://github.com/zackcornelius/CarConnectivity-connector-volkswagen-na/)
+[![GitHub release (latest by date)](https://img.shields.io/github/v/release/zackcornelius/CarConnectivity-connector-volkswagen-na)](https://github.com/zackcornelius/CarConnectivity-connector-volkswagen-na/releases/latest)
+[![GitHub](https://img.shields.io/github/license/zackcornelius/CarConnectivity-connector-volkswagen-na)](https://github.com/zackcornelius/CarConnectivity-connector-volkswagen-na/blob/master/LICENSE)
+[![GitHub issues](https://img.shields.io/github/issues/zackcornelius/CarConnectivity-connector-volkswagen-na)](https://github.com/zackcornelius/CarConnectivity-connector-volkswagen-na/issues)
+[![PyPI - Downloads](https://img.shields.io/pypi/dm/carconnectivity-connector-volkswagen-na?label=PyPI%20Downloads)](https://pypi.org/project/carconnectivity-connector-volkswagen-na/)
+[![PyPI - Python Version](https://img.shields.io/pypi/pyversions/carconnectivity-connector-volkswagen-na)](https://pypi.org/project/carconnectivity-connector-volkswagen-na/)
+[![Donate at PayPal](https://img.shields.io/badge/Donate-PayPal-2997d8)](https://www.paypal.com/donate?hosted_button_id=2BVFF5GJ9SXAJ)
+[![Sponsor at Github](https://img.shields.io/badge/Sponsor-GitHub-28a745)](https://github.com/sponsors/tillsteinbach)
+
+
+## CarConnectivity will become the successor of [WeConnect-python](https://github.com/tillsteinbach/WeConnect-python) in 2025 with similar functionality but support for other brands beyond Volkswagen!
+
+[CarConnectivity](https://github.com/tillsteinbach/CarConnectivity) is a python API to connect to various car services. This connector enables the integration of volkswagen vehicles through the WeConnect API. Look at [CarConnectivity](https://github.com/tillsteinbach/CarConnectivity) for other supported brands.
+
+## Configuration
+In your carconnectivity.json configuration add a section for the volkswagen connector like this:
+```
+{
+    "carConnectivity": {
+        "connectors": [
+            {
+                "type": "volkswagen_na",
+                "config": {
+                    "username": "test@test.de",
+                    "password": "testpassword123",
+                    "country": "us"
+                }
+            }
+        ]
+    }
+}
+``
+
+Country is optional, and defaults to 'us'. 'ca' may work, but is currently untested.
+
+
+### Credentials
+If you do not want to provide your username or password inside the configuration you have to create a ".netrc" file at the appropriate location (usually this is your home folder):
+```
+# For MyVW
+machine volkswagen_na
+login test@test.de
+password testpassword123
+```
+In this case the configuration needs to look like this:
+```
+{
+    "carConnectivity": {
+        "connectors": [
+            {
+                "type": "volkswagen_na",
+                "config": {
+                }
+            }
+        ]
+    }
+}
+```
+
+You can also provide the location of the netrc file in the configuration.
+```
+{
+    "carConnectivity": {
+        "connectors": [
+            {
+                "type": "volkswagen_na",
+                "config": {
+                    "netrc": "/some/path/on/your/filesystem"
+                }
+            }
+        ]
+    }
+}
+```
+The optional S-PIN needed for some commands can be provided in the account section of the netrc:
+```
+# For WeConnect
+machine volkswagen_na
+login test@test.de
+password testpassword123
+account 1234
+```
+### Limitations
+The connector is currently only for US based vehicles, and is limited in support for non EV (id.4) vehicles. If someone can test with US based non-id.4 vehicles (or newer 2025 id.4's where lock/unlock works), I can work with them to get this working there too
---
+++ packages/carconnectivity-connector-volkswagen-na/carconnectivity_connector_volkswagen_na-0.1.5.tar.gz/carconnectivity_connector_volkswagen_na-0.1.5/src/carconnectivity_connectors/volkswagen_na/vehicle.py
@@ -0,0 +1,100 @@
+"""Module for vehicle classes."""
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+from carconnectivity.vehicle import GenericVehicle, ElectricVehicle, CombustionVehicle, HybridVehicle
+from carconnectivity.attributes import StringAttribute
+from carconnectivity.attributes import BooleanAttribute
+
+from carconnectivity_connectors.volkswagen_na.capability import Capabilities
+from carconnectivity_connectors.volkswagen_na.climatization import VolkswagenClimatization
+from carconnectivity_connectors.volkswagen_na.charging import VolkswagenNACharging
+
+SUPPORT_IMAGES = False
+try:
+    from PIL import Image
+    SUPPORT_IMAGES = True
+except ImportError:
+    pass
+
+if TYPE_CHECKING:
+    from typing import Optional, Dict
+    from carconnectivity.garage import Garage
+    from carconnectivity_connectors.base.connector import BaseConnector
+
+
+class VolkswagenNAVehicle(GenericVehicle):  # pylint: disable=too-many-instance-attributes
+    """
+    A class to represent a generic volkswagen vehicle.
+
+    Attributes:
+    -----------
+    vin : StringAttribute
+        The vehicle identification number (VIN) of the vehicle.
+    license_plate : StringAttribute
+        The license plate of the vehicle.
+    """
+    def __init__(self, vin: Optional[str] = None, garage: Optional[Garage] = None, managing_connector: Optional[BaseConnector] = None,
+                 origin: Optional[VolkswagenNAVehicle] = None, initialization: Optional[Dict] = None) -> None:
+        if origin is not None:
+            super().__init__(garage=garage, origin=origin, initialization=initialization)
+            self.capabilities: Capabilities = origin.capabilities
+            self.capabilities.parent = self
+            self.is_active: BooleanAttribute = origin.is_active
+            self.is_active.parent = self
+            self.uuid: StringAttribute = origin.uuid
+            self.uuid.parent = self
+            self.spin_token = origin.spin_token
+            self.spin_token.parent = self
+            if SUPPORT_IMAGES:
+                self._car_images = origin._car_images
+        else:
+            super().__init__(vin=vin, garage=garage, managing_connector=managing_connector, initialization=initialization)
+            self.capabilities: Capabilities = Capabilities(vehicle=self, initialization=self.get_initialization('capabilities'))
+            self.climatization = VolkswagenClimatization(vehicle=self, origin=self.climatization, initialization=self.get_initialization('climatization'))
+            self.is_active = BooleanAttribute(name='is_active', parent=self, tags={'connector_custom'}, initialization=self.get_initialization('is_active'))
+            self.uuid = StringAttribute('uuid', self, tags={'connector_custom'}, initialization=self.get_initialization('uuid'))
+            self.spin_token = StringAttribute('spin_token', self, tags={'connector_custom'}, initialization=self.get_initialization('spin_token'))
+            if SUPPORT_IMAGES:
+                self._car_images: Dict[str, Image.Image] = {}
+        self.manufacturer._set_value(value='Volkswagen')  # pylint: disable=protected-access
+
+
+class VolkswagenNAElectricVehicle(ElectricVehicle, VolkswagenNAVehicle):
+    """
+    Represents a Volkswagen electric vehicle.
+    """
+    def __init__(self, vin: Optional[str] = None, garage: Optional[Garage] = None, managing_connector: Optional[BaseConnector] = None,
+                 origin: Optional[VolkswagenNAVehicle] = None, initialization: Optional[Dict] = None) -> None:
+        if origin is not None:
+            super().__init__(garage=garage, origin=origin, initialization=initialization)
+            if isinstance(origin, ElectricVehicle):
+                self.charging = VolkswagenNACharging(vehicle=self, origin=origin.charging)
+            else:
+                self.charging = VolkswagenNACharging(vehicle=self, origin=self.charging)
+        else:
+            super().__init__(vin=vin, garage=garage, managing_connector=managing_connector, initialization=initialization)
+            self.charging = VolkswagenNACharging(vehicle=self, initialization=self.get_initialization('charging'))
+
+
+class VolkswagenNACombustionVehicle(CombustionVehicle, VolkswagenNAVehicle):
+    """
+    Represents a Volkswagen combustion vehicle.
+    """
+    def __init__(self, vin: Optional[str] = None, garage: Optional[Garage] = None, managing_connector: Optional[BaseConnector] = None,
+                 origin: Optional[VolkswagenNAVehicle] = None, initialization: Optional[Dict] = None) -> None:
+        if origin is not None:
+            super().__init__(garage=garage, origin=origin, initialization=initialization)
+        else:
+            super().__init__(vin=vin, garage=garage, managing_connector=managing_connector, initialization=initialization)
+
+class VolkswagenNAHybridVehicle(HybridVehicle, VolkswagenNAElectricVehicle, VolkswagenNACombustionVehicle):
+    """
+    Represents a Volkswagen hybrid vehicle.
+    """
+    def __init__(self, vin: Optional[str] = None, garage: Optional[Garage] = None, managing_connector: Optional[BaseConnector] = None,
+                 origin: Optional[VolkswagenNAVehicle] = None, initialization: Optional[Dict] = None) -> None:
+        if origin is not None:
+            super().__init__(garage=garage, origin=origin, initialization=initialization)
+        else:
+            super().__init__(vin=vin, garage=garage, managing_connector=managing_connector, initialization=initialization)
