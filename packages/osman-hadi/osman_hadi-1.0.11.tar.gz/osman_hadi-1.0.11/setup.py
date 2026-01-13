07198e6a436b2521c8ec27ac267be9e6c9a576fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 07:08:12 +0000

    Add package osman-hadi/1.0.11/osman_hadi-1.0.11.tar.gz

---
+++ packages/osman-hadi/osman_hadi-1.0.11.tar.gz/osman_hadi-1.0.11/setup.py
@@ -0,0 +1,21 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name="osman_hadi",
+    version="1.0.11",
+    author="Mahdi bin iqbal",
+    author_email="islammdmahadi943@gmail.com",
+    description="A bilingual Python library documenting the life and legacy of Shaheed Osman Bin Hadi.",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/mahadi99900/Osman_Hadi",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
