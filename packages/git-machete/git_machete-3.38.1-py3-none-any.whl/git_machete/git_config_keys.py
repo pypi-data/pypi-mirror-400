dda4c70c209ab34654fa7c52bc4b87810421b720
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 11:05:01 +0000

    Add package git-machete/3.38.1/git_machete-3.38.1-py3-none-any.whl

---
+++ packages/git-machete/git_machete-3.38.1-py3-none-any.whl/git_machete/git_config_keys.py
@@ -0,0 +1,16 @@
+SQUASH_MERGE_DETECTION = 'machete.squashMergeDetection'
+STATUS_EXTRA_SPACE_BEFORE_BRANCH_NAME = 'machete.status.extraSpaceBeforeBranchName'
+TRAVERSE_PUSH = 'machete.traverse.push'
+WORKTREE_USE_TOP_LEVEL_MACHETE_FILE = 'machete.worktree.useTopLevelMacheteFile'
+
+
+def override_fork_point_to(branch: str) -> str:
+    return f'machete.overrideForkPoint.{branch}.to'
+
+
+def override_fork_point_while_descendant_of(branch: str) -> str:
+    return f'machete.overrideForkPoint.{branch}.whileDescendantOf'
+
+
+def traverse_remote_fetch(remote: str) -> str:
+    return f'machete.traverse.fetch.{remote}'
