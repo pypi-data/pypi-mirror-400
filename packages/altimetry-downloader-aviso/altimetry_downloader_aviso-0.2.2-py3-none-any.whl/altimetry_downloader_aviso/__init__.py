5276a6db828dd794956e2a4c4f00d65ebc1f84aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 21:05:52 +0000

    Add package altimetry-downloader-aviso/0.2.2/altimetry_downloader_aviso-0.2.2-py3-none-any.whl

---
+++ packages/altimetry-downloader-aviso/altimetry_downloader_aviso-0.2.2-py3-none-any.whl/altimetry_downloader_aviso/__init__.py
@@ -0,0 +1,4 @@
+from .catalog_client.geonetwork.models.model import AvisoCatalog, AvisoProduct
+from .core import details, get, summary
+
+__all__ = ["summary", "details", "get", "AvisoProduct", "AvisoCatalog"]
