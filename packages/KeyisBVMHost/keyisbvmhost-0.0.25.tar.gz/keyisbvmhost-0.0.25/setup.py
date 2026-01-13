124694b876cf2e644bb725eb616e065f2ac563ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 04:18:53 +0000

    Add package KeyisBVMHost/0.0.25/keyisbvmhost-0.0.25.tar.gz

---
+++ packages/KeyisBVMHost/keyisbvmhost-0.0.25.tar.gz/keyisbvmhost-0.0.25/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+name = 'KeyisBVMHost'
+filesName = 'KeyisBVMHost'
+
+setup(
+    name=name,
+    version='0.0.25',
+    author="KeyisB",
+    author_email="keyisb.pip@gmail.com",
+    description=name,
+    long_description = 'GW and MMB Project libraries',
+    long_description_content_type= 'text/plain',
+    url=f"https://github.com/KeyisB/libs/tree/main/{name}",
+    include_package_data=True,
+    package_data = {}, # type: ignore
+    package_dir={'': f'{filesName}'.replace('-','_')},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.12',
+    license="MMB License v1.0",
+    install_requires = ['GNServer', 'KeyisBTools'],
+)
