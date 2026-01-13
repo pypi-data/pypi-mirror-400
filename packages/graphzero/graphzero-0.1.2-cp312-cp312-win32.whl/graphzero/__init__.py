54c83c3403b8e8e70c897f27d0213bf6152ffec1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 20:06:16 +0000

    Add package graphzero/0.1.2/graphzero-0.1.2-cp312-cp312-win32.whl

---
+++ packages/graphzero/graphzero-0.1.2-cp312-cp312-win32.whl/graphzero/__init__.py
@@ -0,0 +1,12 @@
+from .graphzero import Graph, convert_csv_to_gl
+
+# Metadata
+__version__ = "0.1.2"
+__author__ = "Krish Singaria"
+__license__ = "MIT"
+
+# Define what happens when someone does "from graphzero import *"
+__all__ = [
+    "Graph",
+    "convert_csv_to_gl"
+]
