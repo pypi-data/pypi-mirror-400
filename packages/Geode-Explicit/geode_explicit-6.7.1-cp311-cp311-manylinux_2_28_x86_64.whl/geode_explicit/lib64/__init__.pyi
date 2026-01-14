b5429e7e36f0adc05d83bca5093936f95758c805
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 09:09:58 +0000

    Add package Geode-Explicit/6.7.1/geode_explicit-6.7.1-cp311-cp311-manylinux_2_28_x86_64.whl

---
+++ packages/Geode-Explicit/geode_explicit-6.7.1-cp311-cp311-manylinux_2_28_x86_64.whl/geode_explicit/lib64/__init__.pyi
@@ -0,0 +1,4 @@
+from __future__ import annotations
+from . import geode_explicit_py_brep
+from . import geode_explicit_py_section
+__all__: list[str] = ['geode_explicit_py_brep', 'geode_explicit_py_section']
