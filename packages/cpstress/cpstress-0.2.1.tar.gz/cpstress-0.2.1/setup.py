4e0a4f38217e2655194005344542a975f4a548cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 15:10:22 +0000

    Add package cpstress/0.2.1/cpstress-0.2.1.tar.gz

---
+++ packages/cpstress/cpstress-0.2.1.tar.gz/cpstress-0.2.1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="cpstress",
+    version="0.2.1",
+    author="Srijan Verma",
+    author_email="srijan@example.com",
+    description="Cryptographically secure stress testing data generator library for competitive programming. Generates random test cases for arrays, numbers, strings, and graphs using secrets module.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sharpsalt/cpstress",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Testing",
+    ],
+    python_requires=">=3.7",
+    keywords="competitive programming test data generation random stress-testing",
+    project_urls={
+        "Bug Reports": "https://github.com/sharpsalt/cpstress/issues",
+        "Source": "https://github.com/sharpsalt/cpstress",
+    },
+)
