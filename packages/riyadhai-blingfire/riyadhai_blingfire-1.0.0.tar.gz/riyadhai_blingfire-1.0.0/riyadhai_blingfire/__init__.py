bfbde39e4db63ac63aef713db04a31dce1e9a46a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 12:07:34 +0000

    Add package riyadhai-blingfire/1.0.0/riyadhai_blingfire-1.0.0.tar.gz

---
+++ packages/riyadhai-blingfire/riyadhai_blingfire-1.0.0.tar.gz/riyadhai_blingfire-1.0.0/riyadhai_blingfire/__init__.py
@@ -0,0 +1,9 @@
+"""RiyadhAI Blingfire accelerator package.
+
+This distribution is a placeholder for a future optional native extension that
+provides fast tokenization primitives.
+"""
+
+__all__ = ["__version__"]
+
+__version__ = "1.0.0"
