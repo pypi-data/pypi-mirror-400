3098b3e3360f0b550c073d05b88de8fbdf8e8eb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 00:08:59 +0000

    Add package torchvista/0.2.8/torchvista-0.2.8-py3-none-any.whl

---
+++ packages/torchvista/torchvista-0.2.8-py3-none-any.whl/tests/models/__init__.py
@@ -0,0 +1,20 @@
+import importlib
+import os
+
+models = {}
+
+for filename in os.listdir(os.path.dirname(__file__)):
+    if filename.endswith(".py") and filename != "__init__.py":
+        modname = filename[:-3]
+        module = importlib.import_module(f".{modname}", package=__name__)
+        models[modname] = {
+            "model": getattr(module, "model"),
+            "example_input": getattr(module, "example_input"),
+            "code_contents": getattr(module, "code_contents"),
+            "error_contents": getattr(module, "error_contents", ""),
+            "collapse_modules_after_depth": getattr(module, "collapse_modules_after_depth", -1),
+            "show_non_gradient_nodes": getattr(module, "show_non_gradient_nodes", True),
+            "forced_module_tracing_depth": getattr(module, "forced_module_tracing_depth", None),
+            "show_module_attr_names": getattr(module, "show_module_attr_names", False),
+            "show_compressed_view": getattr(module, "show_compressed_view", False),
+        }
