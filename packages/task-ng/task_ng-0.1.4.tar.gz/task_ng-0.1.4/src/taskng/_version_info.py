512230783423212007004da1e7351eba4f828a2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 13:19:58 +0000

    Add package task-ng/0.1.4/task_ng-0.1.4.tar.gz

---
+++ packages/task-ng/task_ng-0.1.4.tar.gz/task_ng-0.1.4/src/taskng/_version_info.py
@@ -0,0 +1,9 @@
+"""Version information including git commit details.
+
+This file is generated at build time by scripts/generate_version.py.
+Do not edit manually.
+"""
+
+# These values are updated at build time
+GIT_COMMIT = "e9c4ef2"
+GIT_DATE = "2026-01-09 13:45:45 +0100"
