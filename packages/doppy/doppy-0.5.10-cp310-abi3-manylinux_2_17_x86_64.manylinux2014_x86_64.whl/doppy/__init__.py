b351a1c5d23209ae4b7f405a2deaf8fc38650e16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 13:20:31 +0000

    Add package doppy/0.5.10/doppy-0.5.10-cp310-abi3-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/doppy/doppy-0.5.10-cp310-abi3-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/doppy/__init__.py
@@ -0,0 +1,6 @@
+from doppy import data, netcdf, options, product, raw
+from doppy.rs import __version__
+
+from . import bench
+
+__all__ = ["raw", "options", "product", "netcdf", "bench", "data", "__version__"]
