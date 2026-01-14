9dc7e74139ea0c221faec5a702587d85970c6644
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 00:08:29 +0000

    Add package neuronswitch/2.0.1/neuronswitch-2.0.1-py3-none-any.whl

---
+++ packages/neuronswitch/neuronswitch-2.0.1-py3-none-any.whl/neuronswitch/__init__.py
@@ -0,0 +1,7 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from ultralytics.nn.modules.conv import Conv
+from ultralytics.nn.modules.block import C2f, Bottleneck, SPPF
+from neuronswitch.farida_DualYOLO import DualYOLO
+from neuronswitch.farida_DualConv import convert_backbone_to_dual_farida,activate_weight_set_farida
