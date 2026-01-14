160061a2868135e35d182d39d6349ee48c251c2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 12:07:14 +0000

    Add package velbus-aio/2026.1.2/velbus_aio-2026.1.2.tar.gz

---
+++ packages/velbus-aio/velbus_aio-2026.1.2.tar.gz/velbus_aio-2026.1.2/velbusaio/messages/forced_off.py
@@ -0,0 +1,26 @@
+"""Forced Off message class.
+
+:author: Maikel Punie <maikel.punie@gmail.com>
+"""
+
+from __future__ import annotations
+
+from velbusaio.command_registry import register
+from velbusaio.message import Message
+
+COMMAND_CODE = 0x12
+
+
+@register(COMMAND_CODE)
+class ForcedOff(Message):
+    """Forced Off message."""
+
+    def populate(self, priority, address, rtr, data):
+        """:return: None"""
+        self.needs_low_priority(priority)
+        self.needs_no_rtr(rtr)
+        self.set_attributes(priority, address, rtr)
+
+    def data_to_binary(self):
+        """:return: bytes"""
+        return bytes([COMMAND_CODE])
