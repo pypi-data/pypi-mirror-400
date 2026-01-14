0ba0dab510c317edef226d500b92260fe8c8d324
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 12:08:09 +0000

    Add package hfs-module/9.9.9/hfs_module-9.9.9-py3-none-any.whl

---
+++ packages/hfs-module/hfs_module-9.9.9-py3-none-any.whl/hfs_module/__init__.py
@@ -0,0 +1,8 @@
+# hfs_module/__init__.py
+import sys
+
+print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+print("!!! SECURITY VULNERABILITY DETECTED            !!!")
+print("!!! This package was installed from PyPI.org   !!!")
+print("!!! instead of the internal HFS repository.    !!!")
+print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
