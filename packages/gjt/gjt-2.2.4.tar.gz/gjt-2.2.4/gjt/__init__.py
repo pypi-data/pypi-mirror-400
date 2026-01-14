97915fa0f186aba77306f8b4d52a94d8647a2138
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 08:06:39 +0000

    Add package gjt/2.2.4/gjt-2.2.4.tar.gz

---
+++ packages/gjt/gjt-2.2.4.tar.gz/gjt-2.2.4/gjt/__init__.py
@@ -0,0 +1,10 @@
+from gjt.misc.connect_to_websocket import connect_to_websocket
+from gjt.misc.get_error_data import get_error_data
+from gjt.misc.get_network_data import get_network_data
+from gjt.account.login_to_account import login_to_account
+from gjt.misc.skips_calculator import skips_calculator
+from gjt.map.switch_kingdoms import switch_kingdoms
+__all__ = ["connect_to_websocket", "get_error_data", "get_network_data", "login_to_account", "skips_calculator", "switch_kingdoms"]
+__version__ = "2.2.4"
+
+
