12cbc249b2fd20e13ee4d450665bf7d709c63c48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 01:35:07 +0000

    Add package equilipy/0.2.1/equilipy-0.2.1-cp39-cp39-win_amd64.whl

---
+++ packages/equilipy/equilipy-0.2.1-cp39-cp39-win_amd64.whl/equilipy/__init__.py
@@ -0,0 +1,51 @@
+"""""" # start delvewheel patch
+def _delvewheel_patch_1_11_2():
+    import ctypes
+    import os
+    import platform
+    import sys
+    libs_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'equilipy.libs'))
+    is_conda_cpython = platform.python_implementation() == 'CPython' and (hasattr(ctypes.pythonapi, 'Anaconda_GetVersion') or 'packaged by conda-forge' in sys.version)
+    if sys.version_info[:2] >= (3, 8) and not is_conda_cpython or sys.version_info[:2] >= (3, 10):
+        if os.path.isdir(libs_dir):
+            os.add_dll_directory(libs_dir)
+    else:
+        load_order_filepath = os.path.join(libs_dir, '.load-order-equilipy-0.2.1')
+        if os.path.isfile(load_order_filepath):
+            import ctypes.wintypes
+            with open(os.path.join(libs_dir, '.load-order-equilipy-0.2.1')) as file:
+                load_order = file.read().split()
+            kernel32 = ctypes.WinDLL('kernel32', use_last_error=True)
+            kernel32.LoadLibraryExW.restype = ctypes.wintypes.HMODULE
+            kernel32.LoadLibraryExW.argtypes = ctypes.wintypes.LPCWSTR, ctypes.wintypes.HANDLE, ctypes.wintypes.DWORD
+            for lib in load_order:
+                lib_path = os.path.join(os.path.join(libs_dir, lib))
+                if os.path.isfile(lib_path) and not kernel32.LoadLibraryExW(lib_path, None, 8):
+                    raise OSError('Error loading {}; {}'.format(lib, ctypes.FormatError(ctypes.get_last_error())))
+
+
+_delvewheel_patch_1_11_2()
+del _delvewheel_patch_1_11_2
+# end delvewheel patch
+
+import equilipy.equilifort as fort
+from .variables          import * 
+from .ReadDAT            import *
+from .InputCondition     import *
+from .SystemCheck        import *
+from .Minimize           import *
+from .EquilibBatch       import *
+from .PostProcess        import *
+from .EquilibSingle      import *
+from .ScheilCooling      import *
+from .Simplex            import *
+from .ListPhases         import *
+from .PhaseSelection     import *
+from .SinglePhaseProperty import *
+from .FindTransition import *
+from .NucleoScheil import *
+# from .ParseCSDataBlockRKMP import *
+# from .ParseCSDataBlockSUBL import *
+# from .ParseCSDataBlockSUBG import *
+
+# from .ParseCS73 import *
