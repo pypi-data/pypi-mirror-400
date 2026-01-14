3d9d6a981b2d49728636231f55a7f03a41acca74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 14:09:49 +0000

    Add package tactics2d/0.1.9rc1/tactics2d-0.1.9rc1-cp311-cp311-musllinux_1_2_x86_64.whl

---
+++ packages/tactics2d/tactics2d-0.1.9rc1-cp311-cp311-musllinux_1_2_x86_64.whl/tactics2d/dataset_parser/womd_proto/__init__.py
@@ -0,0 +1,9 @@
+# Copyright (C) 2024, Tactics2D Authors. Released under the GNU GPLv3.
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+"""Womd proto module."""
+
+
+from .pb2 import scenario_pb2 as scenario_pbv2
+
+__all__ = ["scenario_pbv2"]
