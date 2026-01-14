6c971d9f95afaeea57dc6948e5c0d31b5687d3e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 08:06:39 +0000

    Add package whooshpad/0.1.2/whooshpad-0.1.2-py3-none-any.whl

---
+++ packages/whooshpad/whooshpad-0.1.2-py3-none-any.whl/whooshpad/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Whooshpad - A mobile-friendly remote control for MyWhoosh virtual cycling."""
+
+from whooshpad.server import main
+
+__version__ = "0.1.2"
+__all__ = ["main"]
