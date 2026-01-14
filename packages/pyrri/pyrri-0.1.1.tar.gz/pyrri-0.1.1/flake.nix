2f4722363e077ed967ca48a5926fd688d5cd3521
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 15:09:12 +0000

    Add package pyrri/0.1.1/pyrri-0.1.1.tar.gz

---
+++ packages/pyrri/pyrri-0.1.1.tar.gz/pyrri-0.1.1/flake.nix
@@ -0,0 +1,49 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+  outputs = { self, nixpkgs, ... }: {
+    packages.x86_64-linux = let
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
+      };
+    in {
+      pyrri = pkgs.python311Packages.buildPythonPackage rec {
+        pname = "pyrri";
+        version = "0.1.1";
+
+        src = ./.;
+
+        format = "pyproject";
+
+        buildInputs = [ pkgs.python311Packages.hatchling ];
+        propagatedBuildInputs = with pkgs.python311Packages; [
+        ];
+
+        pythonImportsCheck = [ "rri" ];
+
+        meta.mainProgram = "rri";
+      };
+      default = self.packages.x86_64-linux.pyrri;
+    };
+
+    devShells.x86_64-linux = let
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
+      };
+    in {
+      pyrri = pkgs.mkShell {
+        packages = with pkgs; [ hatch ];
+        inputsFrom = [
+          self.packages.x86_64-linux.pyrri
+        ];
+      };
+      default = self.devShells.x86_64-linux.pyrri;
+    };
+
+    hydraJobs = {
+      inherit (self)
+        packages;
+    };
+  };
+}
