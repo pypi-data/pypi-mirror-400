4b6fdbb3f530071eb7d34c138a01fb0a1d976da6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 13:20:40 +0000

    Add package ultrasound-rawdata-exchange/1.3.2/ultrasound_rawdata_exchange-1.3.2-cp312-cp312-musllinux_1_2_x86_64.whl

---
+++ packages/ultrasound-rawdata-exchange/ultrasound_rawdata_exchange-1.3.2-cp312-cp312-musllinux_1_2_x86_64.whl/ultrasound_rawdata_exchange/lib/cmake/Urx-1.3.2_static/UrxTargets-release.cmake
@@ -0,0 +1,20 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "Urx::UrxBindingsPython" for configuration "Release"
+set_property(TARGET Urx::UrxBindingsPython APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(Urx::UrxBindingsPython PROPERTIES
+  IMPORTED_COMMON_LANGUAGE_RUNTIME_RELEASE ""
+  IMPORTED_LOCATION_RELEASE "/builds/common/sw/urx/build/lib.linux-x86_64-cpython-312/ultrasound_rawdata_exchange/bindings.cpython-312-x86_64-linux-musl.so"
+  IMPORTED_NO_SONAME_RELEASE "TRUE"
+  )
+
+list(APPEND _cmake_import_check_targets Urx::UrxBindingsPython )
+list(APPEND _cmake_import_check_files_for_Urx::UrxBindingsPython "/builds/common/sw/urx/build/lib.linux-x86_64-cpython-312/ultrasound_rawdata_exchange/bindings.cpython-312-x86_64-linux-musl.so" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
