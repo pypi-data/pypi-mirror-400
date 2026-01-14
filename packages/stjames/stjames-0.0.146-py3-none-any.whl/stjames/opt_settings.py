ce63681b2ac8ebad6fff483f3079fda1b5bdd684
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 14:10:05 +0000

    Add package stjames/0.0.146/stjames-0.0.146-py3-none-any.whl

---
+++ packages/stjames/stjames-0.0.146-py3-none-any.whl/stjames/opt_settings.py
@@ -0,0 +1,24 @@
+from typing import Sequence
+
+from pydantic import PositiveFloat, PositiveInt
+
+from .base import Base
+from .constraint import Constraint
+
+
+class OptimizationSettings(Base):
+    max_steps: PositiveInt = 250
+    transition_state: bool = False
+    recalc_hess_every: int = 0  # Recalculate the hessian every `n` steps (or never with 0)
+
+    # when are we converged? (Hartree and Hartree/Å)
+    max_gradient_threshold: PositiveFloat = 7e-4
+    rms_gradient_threshold: PositiveFloat = 6e-4
+    energy_threshold: PositiveFloat = 1e-6
+
+    # for periodic systems only
+    optimize_cell: bool = False
+
+    constraints: Sequence[Constraint] = tuple()
+
+    save_intermediate_steps: bool = True
