3358ece80011f3b70971325014af23537070d124
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 06:07:18 +0000

    Add package xeto/0.1.2/xeto-0.1.2-py3-none-any.whl

---
+++ packages/xeto/xeto-0.1.2-py3-none-any.whl/main.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2026, Rick Jennings
+# Licensed under the Academic Free License version 3.0
+
+
+def main():
+    print("Hello from xeto!")
+
+
+if __name__ == "__main__":
+    main()
