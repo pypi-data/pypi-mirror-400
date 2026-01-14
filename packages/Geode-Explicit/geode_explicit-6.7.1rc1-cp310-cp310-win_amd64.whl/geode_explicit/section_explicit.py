42367dadeb3f1218dccd763adaf102fc8ab4c7d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 03:34:48 +0000

    Add package Geode-Explicit/6.7.1rc1/geode_explicit-6.7.1rc1-cp310-cp310-win_amd64.whl

---
+++ packages/Geode-Explicit/geode_explicit-6.7.1rc1-cp310-cp310-win_amd64.whl/geode_explicit/section_explicit.py
@@ -0,0 +1,11 @@
+#
+# Copyright (c) 2019 - 2025 Geode-solutions. All rights reserved.
+#
+
+import opengeode
+import geode_common
+import geode_conversion
+import geode_background
+
+from .bin.geode_explicit_py_section import *
+ExplicitSectionLibrary.initialize()
