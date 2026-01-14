cd310d039e4fefa3b6c959921b7376dd6e97104c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 06:06:56 +0000

    Add package py-gemini-watermark-remover/0.2.9/py_gemini_watermark_remover-0.2.9-py3-none-any.whl

---
+++ packages/py-gemini-watermark-remover/py_gemini_watermark_remover-0.2.9-py3-none-any.whl/gemini_watermark_remover/__init__.py
@@ -0,0 +1,3 @@
+from .watermark_remover import WatermarkRemover, WatermarkSize, process_image, process_directory
+
+__all__ = ['WatermarkRemover', 'WatermarkSize', 'process_image', 'process_directory']
