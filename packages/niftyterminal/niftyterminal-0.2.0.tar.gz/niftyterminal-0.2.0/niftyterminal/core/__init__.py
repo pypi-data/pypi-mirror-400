760dfb15762d1fa583814d5054d00bad35e073df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 04:25:18 +0000

    Add package niftyterminal/0.2.0/niftyterminal-0.2.0.tar.gz

---
+++ packages/niftyterminal/niftyterminal-0.2.0.tar.gz/niftyterminal-0.2.0/niftyterminal/core/__init__.py
@@ -0,0 +1,5 @@
+"""Core module for Nifty Terminal."""
+
+from niftyterminal.core.session import fetch, fetch_raw, NSESession, NiftyIndicesSession
+
+__all__ = ["fetch", "fetch_raw", "NSESession", "NiftyIndicesSession"]
