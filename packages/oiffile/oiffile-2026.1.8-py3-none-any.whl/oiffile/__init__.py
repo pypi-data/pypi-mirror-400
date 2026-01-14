4701e6085262fa929a2d90f1631e1ba13832c0b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 07:09:16 +0000

    Add package oiffile/2026.1.8/oiffile-2026.1.8-py3-none-any.whl

---
+++ packages/oiffile/oiffile-2026.1.8-py3-none-any.whl/oiffile/__init__.py
@@ -0,0 +1,9 @@
+# oiffile/__init__.py
+
+from .oiffile import *
+from .oiffile import __all__, __doc__, __version__
+
+# constants are repeated for documentation
+
+__version__ = __version__
+"""Oiffile version string."""
