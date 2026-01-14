c110057801ca31bbb7c89c0a00ea2783de3abfa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 06:06:34 +0000

    Add package beancount-future-transactions/0.0.7/beancount_future_transactions-0.0.7.tar.gz

---
+++ packages/beancount-future-transactions/beancount_future_transactions-0.0.7.tar.gz/beancount_future_transactions-0.0.7/README.md
@@ -0,0 +1,33 @@
+# Beancount future transactions
+
+**Attention:** The project beancount-future-transactions has been superseded by
+[beancount-extensions](https://github.com/Rudd-O/beancount-extensions).  This
+is the last release of the project as is -- upgrade your Beancount setup to
+use the new project.
+
+This is a very simple plugin for Beancount that filters out transactions with a future date, provided they are tagged with the `#future` tag.
+
+To use:
+
+* Install using your favorite Python method — probably `pip` or `python setup.py install`, although you may want to install to your user directory (`pip --user`).
+* Include the stanza `plugin "beancount_extensions.future_transactions"` in your Beancount file.
+
+That's it.
+
+From this point on, any transaction with a future tagged `#future` will not appear in your reports and queries.
+
+If you want to suppress **all** future-dated entries, add the option `suppress_all_future_entries` to your plugin stanza:
+
+```
+; beancount.bean
+
+plugin "beancount_extensions.future_transactions" "suppress_all_future_entries"
+```
+
+if you want to suppress entries taggedwith a different tag, say, `#excludenow`, then you can use the following option:
+
+```
+; beancount.bean
+
+plugin "beancount_extensions.future_transactions" "tag_to_suppress=excludenow"
+```
---
+++ packages/beancount-future-transactions/beancount_future_transactions-0.0.7.tar.gz/beancount_future_transactions-0.0.7/src/beancount_extensions/future_transactions/__init__.py
@@ -0,0 +1,57 @@
+"""A plugin that suppresses future transactions when enabled.
+"""
+
+__author__ = "Manuel Amador (Rudd-O)"
+__plugins__ = ("future_transactions_plugin",)
+__version__ = "0.0.7"
+
+import datetime
+from beancount.core import data
+
+# The name of the metadata field that indicates this is a future transaction
+# that should be filtered out if it is past today's date.
+DEFAULT_TAG = "future"
+OPT_TAG_TO_SUPPRESS = "tag_to_suppress"
+
+# Suppress all future entries or only entries tagged with OPT_TAG_TO_SUPPRESS.
+OPT_SUPPRESS_ALL_FUTURE_ENTRIES = "suppress_all_future_entries"
+
+__plugins__ = ("future_transactions_plugin",)
+
+
+# If "suppress_all_future_entries" in options_map, skips all future-dated entries
+# else only transactions tagged #future
+def future_transactions_plugin(entries, options_map, *optional_args):
+    options = {
+        OPT_SUPPRESS_ALL_FUTURE_ENTRIES: False,
+        OPT_TAG_TO_SUPPRESS: DEFAULT_TAG,
+    }
+    if optional_args:
+        for argval in optional_args[0].split(","):
+            arg, dv, val = argval.partition("=")
+            if not dv:
+                arg = argval
+                val = "true"
+            if val.lower() == "true":
+                val = True
+            elif val.lower() == "false":
+                val = False
+            options[arg] = val
+
+    errors = []
+    new_entries = []
+    today = datetime.date.today()
+
+    def skip(entry):
+        if entry.date <= today:
+            return False
+        return (options[OPT_SUPPRESS_ALL_FUTURE_ENTRIES]) or (
+            isinstance(entry, data.Transaction)
+            and options[OPT_TAG_TO_SUPPRESS] in entry.tags
+        )
+
+    for eindex, entry in enumerate(entries):
+        if not skip(entry):
+            new_entries.append(entry)
+
+    return new_entries, errors
