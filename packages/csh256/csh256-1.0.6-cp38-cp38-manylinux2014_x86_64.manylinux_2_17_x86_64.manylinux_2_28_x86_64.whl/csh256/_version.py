8e6af44286ec7b2a0f78d7334f931f2cc621e1f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 13:20:38 +0000

    Add package csh256/1.0.6/csh256-1.0.6-cp38-cp38-manylinux2014_x86_64.manylinux_2_17_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/csh256/csh256-1.0.6-cp38-cp38-manylinux2014_x86_64.manylinux_2_17_x86_64.manylinux_2_28_x86_64.whl/csh256/_version.py
@@ -0,0 +1,6 @@
+"""Version information for CSH-256"""
+
+__version__ = "1.0.6"
+__author__ = "Ibrahim Hilal Aboukila"
+__license__ = "MIT"
+__description__ = "CSH-256: A hybrid password hashing algorithm with time-cost resistance"
