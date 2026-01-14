9fed6714b7ab1b40a11bced41105cc7fe5507509
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 08:06:25 +0000

    Add package vibing/0.2.0/vibing-0.2.0-py3-none-any.whl

---
+++ packages/vibing/vibing-0.2.0-py3-none-any.whl/vibing/__init__.py
@@ -0,0 +1,22 @@
+"""Vibing: Small but useful tools for scientific publications and projects."""
+
+__version__ = "0.1.0"
+
+from vibing import (
+    calibration,
+    optimization,
+    plotting,
+    powerwell,
+    sleap_convert,
+    undistortion,
+)
+
+__all__ = [
+    "__version__",
+    "calibration",
+    "optimization",
+    "plotting",
+    "powerwell",
+    "sleap_convert",
+    "undistortion",
+]
