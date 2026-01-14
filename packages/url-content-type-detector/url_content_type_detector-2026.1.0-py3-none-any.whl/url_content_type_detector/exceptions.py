ec2f536d164832e34649a8766177310f7b85610a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 08:06:41 +0000

    Add package url-content-type-detector/2026.1.0/url_content_type_detector-2026.1.0-py3-none-any.whl

---
+++ packages/url-content-type-detector/url_content_type_detector-2026.1.0-py3-none-any.whl/url_content_type_detector/exceptions.py
@@ -0,0 +1,10 @@
+"""
+exceptions.py
+~~~~~~~~~~~~~
+
+This module contains custom exceptions for URL utilities.
+"""
+
+
+class URLUtilsError(Exception):
+    """Base exception for url_content_type_detector."""
