e1c129d9f397be4a64c02c1de2803867ee9de3d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 04:23:19 +0000

    Add package SigmaFlow/0.0.58/sigmaflow-0.0.58.tar.gz

---
+++ packages/SigmaFlow/sigmaflow-0.0.58.tar.gz/sigmaflow-0.0.58/src/sigmaflow/server/comfyui/dist/assets/nodeFilterUtil-D49mCu4H.js
@@ -0,0 +1,2 @@
+var s=Object.defineProperty;var o=(t,e)=>s(t,"name",{value:e,configurable:!0});const u=o(t=>t.constructor.nodeData?.output_node,"isOutputNode"),r=o(t=>t.filter(u),"filterOutputNodes");export{r as f,u as i};
+//# sourceMappingURL=nodeFilterUtil-D49mCu4H.js.map
