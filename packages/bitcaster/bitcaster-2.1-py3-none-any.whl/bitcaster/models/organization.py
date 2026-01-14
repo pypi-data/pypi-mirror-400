b12c247fc67ebd74c95b4c066243b0d2a6c78382
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 14:09:54 +0000

    Add package bitcaster/2.1/bitcaster-2.1-py3-none-any.whl

---
+++ packages/bitcaster/bitcaster-2.1-py3-none-any.whl/bitcaster/models/organization.py
@@ -0,0 +1,68 @@
+import logging
+from typing import TYPE_CHECKING, Any
+
+from django.db import models
+from django.db.models import QuerySet
+from django.utils.translation import gettext_lazy as _
+
+from ..constants import bitcaster
+from .mixins import BitcasterBaseModel, BitcasterBaselManager, SlugMixin
+from .user import User
+
+if TYPE_CHECKING:
+    from bitcaster.models import Channel, MessageTemplate
+
+logger = logging.getLogger(__name__)
+
+
+class OrganizationManager(BitcasterBaselManager["Organization"]):
+    def get_by_natural_key(self, slug: str) -> "Organization":
+        return self.get(slug=slug)
+
+    def local(self, **kwargs: Any) -> "QuerySet[Organization]":
+        return self.exclude(name=bitcaster.ORGANIZATION).filter(**kwargs)
+
+
+class Organization(SlugMixin, BitcasterBaseModel):
+    from_email = models.EmailField(blank=True, default="", help_text=_("default from address for emails"))
+    subject_prefix = models.CharField(
+        verbose_name=_("Subject Prefix"),
+        max_length=50,
+        default="[Bitcaster] ",
+        help_text=_("Default prefix for messages supporting subject"),
+    )
+    owner = models.ForeignKey(
+        User, verbose_name=_("Owner"), on_delete=models.PROTECT, related_name="managed_organizations"
+    )
+
+    objects = OrganizationManager()
+
+    class Meta:
+        verbose_name = _("Organization")
+        verbose_name_plural = _("Organizations")
+        ordering = ("name",)
+        constraints = [
+            models.UniqueConstraint(fields=("slug",), name="org_slug_unique"),
+            models.UniqueConstraint(fields=("slug", "owner"), name="owner_slug_unique"),
+        ]
+
+    @property
+    def users(self) -> QuerySet["User"]:
+        return User.objects.filter(roles__organization=self)
+
+    def natural_key(self) -> tuple[str]:
+        return (self.slug,)
+
+    def create_message(
+        self, name: str, channel: "Channel", defaults: "dict[str, Any] | None" = None
+    ) -> "MessageTemplate":
+        return self.messagetemplate_set.get_or_create(
+            name=name,
+            channel=channel,
+            notification=None,
+            event=None,
+            application=None,
+            project=None,
+            organization=self,
+            defaults=defaults if defaults else {},
+        )[0]
