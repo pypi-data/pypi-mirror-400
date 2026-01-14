c6ac7aad99ed870d6d47569481658b5bddaac603
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 06:07:02 +0000

    Add package bn-configuration/0.0.5/bn_configuration-0.0.5.tar.gz

---
+++ packages/bn-configuration/bn_configuration-0.0.5.tar.gz/bn_configuration-0.0.5/configuration/configuration.py
@@ -0,0 +1,69 @@
+from configparser import ConfigParser
+import os
+import logging
+import sys
+from typing import List, Optional, Dict, Any, Union
+
+log = logging.getLogger('config-reader')
+logging.basicConfig(level=logging.INFO)
+
+class MyConfig:
+    def __init__(
+        self,
+        section: str,
+        options: Optional[List[str]] = None
+    ):
+        """
+        usage examples:
+        A/ vrm = MyConfig('vrm', ['url','key','secret']) > vrm.url
+        B/ vrm = MyConfig('vrm') > vrm.get('url')
+        :param section: required, the section in the inifile; PREFIX_ in env
+        :param options: options to be set as attribute
+        """
+        self.section = section
+        self.options = options or []
+        self.ini_file_tail = '.ini'
+        self._inis = self.parse_ini()
+        self.set_options()
+
+    def __repr__(self):
+        return f'Config {self.section} {self.options_loaded}'
+
+    @property
+    def options_loaded(self):
+        return {i: len(self.__getattribute__(i)) > 0 for i in self.options}
+
+    def valid(self):
+        return all(self.options_loaded.values())
+
+    def set_options(self):
+        for option in self.options:
+            self.__setattr__(option, self.get_option(option))
+
+    def get(self, option):
+        return self.get_option(option)
+
+    def get_option(self, option):
+        var = self.env_var(option)
+        if var in os.environ:
+            return os.environ[var]
+        return self._inis.get(option, '')
+
+    def parse_ini(self):
+        def find(map):
+            for file in os.listdir(map):
+                if file.endswith(self.ini_file_tail):
+                    yield f'{map}/{file}'
+
+        root = os.path.dirname(os.path.abspath(sys.argv[0]))
+        files = [f for f in find(root)]
+        results = {}
+        if files:
+            cp = ConfigParser()
+            cp.read(files)
+            if self.section in cp:
+                results.update({option: cp[self.section][option] for option in cp[self.section]})
+        return results
+
+    def env_var(self, option=''):
+        return f'{self.section.upper()}_{option.upper()}'
---
+++ packages/bn-configuration/bn_configuration-0.0.5.tar.gz/bn_configuration-0.0.5/setup.py
@@ -0,0 +1,26 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='bn_configuration',  # only lowercase and dashes
+    version='0.0.5',
+    author='jovi',
+    author_email='jo.vinckier@bynubian.com',
+    description='configuration loader',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://gitlab.com/bynubian/bynode/python_packages/configuration',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+    install_requires=[
+        # 'requests>=2.32.4',
+        # 'python-decouple>=3.8'
+        # dependencies: other packages required
+        # eg 'numpy>=1.11.1'
+    ]
+)
