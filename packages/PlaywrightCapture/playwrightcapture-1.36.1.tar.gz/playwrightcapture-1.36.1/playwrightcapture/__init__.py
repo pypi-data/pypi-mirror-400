9731492cd968edabf1496ae3d756e3d4c1a024ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 05:09:22 +0000

    Add package PlaywrightCapture/1.36.1/playwrightcapture-1.36.1.tar.gz

---
+++ packages/PlaywrightCapture/playwrightcapture-1.36.1.tar.gz/playwrightcapture-1.36.1/playwrightcapture/__init__.py
@@ -0,0 +1,23 @@
+from .capture import Capture  # noqa
+from .capture import CaptureResponse  # noqa
+from .capture import FramesResponse  # noqa
+from .capture import SetCookieParam, Cookie  # noqa
+from .capture import TrustedTimestampSettings # noqa
+from .helpers import get_devices  # noqa
+from .exceptions import (PlaywrightCaptureException, UnknownPlaywrightDeviceType,  # noqa
+                         UnknownPlaywrightBrowser, UnknownPlaywrightDevice,
+                         InvalidPlaywrightParameter)
+
+__all__ = [
+    'Capture',
+    'CaptureResponse',
+    'FramesResponse',
+    'TrustedTimestampSettings',
+    'SetCookieParam', 'Cookie',
+    'get_devices',
+    'PlaywrightCaptureException',
+    'UnknownPlaywrightDeviceType',
+    'UnknownPlaywrightBrowser',
+    'UnknownPlaywrightDevice',
+    'InvalidPlaywrightParameter'
+]
