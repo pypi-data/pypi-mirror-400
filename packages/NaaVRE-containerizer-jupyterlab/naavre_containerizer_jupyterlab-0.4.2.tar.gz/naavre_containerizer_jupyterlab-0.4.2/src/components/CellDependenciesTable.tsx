618dfc3e955abeb0de2d2e6da55f77ac6c51fb5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 13:20:38 +0000

    Add package NaaVRE-containerizer-jupyterlab/0.4.2/naavre_containerizer_jupyterlab-0.4.2.tar.gz

---
+++ packages/NaaVRE-containerizer-jupyterlab/naavre_containerizer_jupyterlab-0.4.2.tar.gz/naavre_containerizer_jupyterlab-0.4.2/src/components/CellDependenciesTable.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import { NaaVRECatalogue } from '../naavre-common/types';
+
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableRow
+} from '@material-ui/core';
+
+interface ICellDependenciesTable {
+  items: Array<NaaVRECatalogue.WorkflowCells.IDependency>;
+}
+
+export const CellDependenciesTable: React.FC<ICellDependenciesTable> = ({
+  items
+}) => {
+  return (
+    <div>
+      <p className={'naavre-containerizer-preview'}>Dependencies</p>
+      <TableContainer
+        component={Paper}
+        className={'naavre-containerizer-table'}
+      >
+        <Table aria-label="simple table">
+          <TableBody>
+            {items.map((dep: any) => (
+              <TableRow key={`${dep.module}-${dep.name}`}>
+                <TableCell component="th" scope="row">
+                  {dep['module'] !== ''
+                    ? dep['module'] + ' • ' + dep['name']
+                    : dep['name']}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
+  );
+};
