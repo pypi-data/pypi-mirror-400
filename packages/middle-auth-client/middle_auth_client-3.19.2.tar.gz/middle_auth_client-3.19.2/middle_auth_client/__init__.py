5f08d4ac5957eb9dd9dd8fd87cb8af4f4fb9f832
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 15:10:22 +0000

    Add package middle-auth-client/3.19.2/middle_auth_client-3.19.2.tar.gz

---
+++ packages/middle-auth-client/middle_auth_client-3.19.2.tar.gz/middle_auth_client-3.19.2/middle_auth_client/__init__.py
@@ -0,0 +1,2 @@
+__version__ = '3.19.2'
+from middle_auth_client.decorators import auth_required, auth_requires_admin, auth_requires_permission, auth_requires_dataset_admin, get_usernames, user_has_permission, users_share_common_group, setPermissionLookupOverride
