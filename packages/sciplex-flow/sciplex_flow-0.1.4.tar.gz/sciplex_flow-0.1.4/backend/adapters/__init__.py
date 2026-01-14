0230279519e0061ca112a859972f1906721dfff1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 12:08:07 +0000

    Add package sciplex-flow/0.1.4/sciplex_flow-0.1.4.tar.gz

---
+++ packages/sciplex-flow/sciplex_flow-0.1.4.tar.gz/sciplex_flow-0.1.4/backend/adapters/__init__.py
@@ -0,0 +1,3 @@
+"""
+Backend adapters for Sciplex Flow.
+"""
---
+++ packages/sciplex-flow/sciplex_flow-0.1.4.tar.gz/sciplex_flow-0.1.4/cli/__main__.py
@@ -0,0 +1,5 @@
+"""Allow running with python -m sciplex_flow.cli"""
+from sciplex_flow.cli.server import main
+
+if __name__ == "__main__":
+    main()
