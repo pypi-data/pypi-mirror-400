173f9a4f3cf0f718746e7e4f91a6c6b3b00be714
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 07:09:24 +0000

    Add package cloudbeds-pms-v1-3/1.8.0/cloudbeds_pms_v1_3-1.8.0-py3-none-any.whl

---
+++ packages/cloudbeds-pms-v1-3/cloudbeds_pms_v1_3-1.8.0-py3-none-any.whl/cloudbeds_pms_v1_3/api/__init__.py
@@ -0,0 +1,26 @@
+# flake8: noqa
+
+# import apis into api package
+from cloudbeds_pms_v1_3.api.adjustment_api import AdjustmentApi
+from cloudbeds_pms_v1_3.api.allotment_blocks_api import AllotmentBlocksApi
+from cloudbeds_pms_v1_3.api.app_settings_api import AppSettingsApi
+from cloudbeds_pms_v1_3.api.authentication_api import AuthenticationApi
+from cloudbeds_pms_v1_3.api.currency_api import CurrencyApi
+from cloudbeds_pms_v1_3.api.custom_fields_api import CustomFieldsApi
+from cloudbeds_pms_v1_3.api.dashboard_api import DashboardApi
+from cloudbeds_pms_v1_3.api.emails_api import EmailsApi
+from cloudbeds_pms_v1_3.api.groups_api import GroupsApi
+from cloudbeds_pms_v1_3.api.guest_api import GuestApi
+from cloudbeds_pms_v1_3.api.hotel_api import HotelApi
+from cloudbeds_pms_v1_3.api.house_account_api import HouseAccountApi
+from cloudbeds_pms_v1_3.api.housekeeping_api import HousekeepingApi
+from cloudbeds_pms_v1_3.api.integration_api import IntegrationApi
+from cloudbeds_pms_v1_3.api.item_api import ItemApi
+from cloudbeds_pms_v1_3.api.package_api import PackageApi
+from cloudbeds_pms_v1_3.api.payment_api import PaymentApi
+from cloudbeds_pms_v1_3.api.rate_api import RateApi
+from cloudbeds_pms_v1_3.api.reservation_api import ReservationApi
+from cloudbeds_pms_v1_3.api.room_api import RoomApi
+from cloudbeds_pms_v1_3.api.taxes_and_fees_api import TaxesAndFeesApi
+from cloudbeds_pms_v1_3.api.user_api import UserApi
+
