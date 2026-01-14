d4e48c148e27b72cd9fb0819979acdfd401c0b06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 12:07:10 +0000

    Add package zope.interface/8.2/zope_interface-8.2.tar.gz

---
+++ packages/zope.interface/zope_interface-8.2.tar.gz/zope_interface-8.2/.manylinux-install.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+# Generated with zope.meta (https://zopemeta.readthedocs.io/) from:
+# https://github.com/zopefoundation/meta/tree/master/src/zope/meta/c-code
+
+set -e -x
+
+# Running inside docker
+# Set a cache directory for pip. This was
+# mounted to be the same as it is outside docker so it
+# can be persisted.
+export XDG_CACHE_HOME="/cache"
+# XXX: This works for macOS, where everything bind-mounted
+# is seen as owned by root in the container. But when the host is Linux
+# the actual UIDs come through to the container, triggering
+# pip to disable the cache when it detects that the owner doesn't match.
+# The below is an attempt to fix that, taken from bcrypt. It seems to work on
+# Github Actions.
+if [ -n "$GITHUB_ACTIONS" ]; then
+    echo Adjusting pip cache permissions
+    mkdir -p $XDG_CACHE_HOME/pip
+    chown -R $(whoami) $XDG_CACHE_HOME
+fi
+ls -ld /cache
+ls -ld /cache/pip
+
+# We need some libraries because we build wheels from scratch:
+yum -y install libffi-devel
+
+tox_env_map() {
+    case $1 in
+        *"cp310"*) echo 'py310';;
+        *"cp311"*) echo 'py311';;
+        *"cp312"*) echo 'py312';;
+        *"cp313"*) echo 'py313';;
+        *"cp314"*) echo 'py314';;
+        *"cp315"*) echo 'py315';;
+        *) echo 'py';;
+    esac
+}
+
+# Compile wheels
+for PYBIN in /opt/python/*/bin; do
+    if \
+       [[ "${PYBIN}" == *"cp310/"* ]] || \
+       [[ "${PYBIN}" == *"cp311/"* ]] || \
+       [[ "${PYBIN}" == *"cp312/"* ]] || \
+       [[ "${PYBIN}" == *"cp313/"* ]] || \
+       [[ "${PYBIN}" == *"cp314/"* ]] || \
+       [[ "${PYBIN}" == *"cp315/"* ]] ; then
+        if [[ "${PYBIN}" == *"cp315/"* ]] ; then
+            "${PYBIN}/pip" install --pre -e /io/
+            "${PYBIN}/pip" wheel /io/ --pre -w wheelhouse/
+        else
+            "${PYBIN}/pip" install -e /io/
+            "${PYBIN}/pip" wheel /io/ -w wheelhouse/
+        fi
+        if [ `uname -m` == 'aarch64' ]; then
+          cd /io/
+          ${PYBIN}/pip install tox
+          TOXENV=$(tox_env_map "${PYBIN}")
+          ${PYBIN}/tox -e ${TOXENV}
+          cd ..
+        fi
+        rm -rf /io/build /io/*.egg-info
+    fi
+done
+
+# Show what wheels we have
+echo "Fixing up the following wheels:"
+ls -l wheelhouse/zope?interface*.whl
+# Bundle external shared libraries into the wheels
+for whl in wheelhouse/zope?interface*.whl; do
+    auditwheel repair "$whl" -w /io/wheelhouse/
+done
---
+++ packages/zope.interface/zope_interface-8.2.tar.gz/zope_interface-8.2/docs/index.rst
@@ -0,0 +1,33 @@
+zope.interface documentation
+============================
+
+.. toctree::
+   :maxdepth: 2
+
+   README
+   adapter
+   human
+   verify
+   foodforthought
+   api/index
+   hacking
+   changes
+
+По-русски
+=========
+
+.. toctree::
+   :maxdepth: 2
+
+   README.ru
+   adapter.ru
+   human.ru
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
