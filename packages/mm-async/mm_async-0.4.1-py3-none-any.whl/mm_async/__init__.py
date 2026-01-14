756521a62df0740cfdd93f57b76273d4782ea8ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 00:08:04 +0000

    Add package mm-async/0.4.1/mm_async-0.4.1-py3-none-any.whl

---
+++ packages/mm-async/mm_async-0.4.1-py3-none-any.whl/mm_async/__init__.py
@@ -0,0 +1,49 @@
+"""Async HTTP client for Mattermost API."""
+
+from .client import AsyncMattermostClient
+from .exceptions import (
+    MattermostAuthError,
+    MattermostConnectionError,
+    MattermostError,
+    MattermostForbiddenError,
+    MattermostNotFoundError,
+    MattermostRateLimitError,
+    MattermostServerError,
+    MattermostValidationError,
+)
+from .constants import ClientDefaults, HttpStatus, RetryDefaults
+from .models import (
+    Channel,
+    ChannelMember,
+    Post,
+    Team,
+    TeamMember,
+    User,
+)
+
+__version__ = "0.4.1"
+
+__all__ = [
+    # Client
+    "AsyncMattermostClient",
+    # Exceptions
+    "MattermostError",
+    "MattermostAuthError",
+    "MattermostForbiddenError",
+    "MattermostNotFoundError",
+    "MattermostRateLimitError",
+    "MattermostServerError",
+    "MattermostConnectionError",
+    "MattermostValidationError",
+    # Models
+    "User",
+    "Team",
+    "Channel",
+    "Post",
+    "TeamMember",
+    "ChannelMember",
+    # Constants
+    "ClientDefaults",
+    "RetryDefaults",
+    "HttpStatus",
+]
