0e1ebb70bb029af3956a3868771696121d036a94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 07:08:46 +0000

    Add package ethan-super-ai-wrapper/0.1.2/ethan_super_ai_wrapper-0.1.2.tar.gz

---
+++ packages/ethan-super-ai-wrapper/ethan_super_ai_wrapper-0.1.2.tar.gz/ethan_super_ai_wrapper-0.1.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ethan_super_ai_wrapper",  # <--- CHANGE THIS to something unique
+    version="0.1.2",
+    description="The easiest AI wrapper for OpenAI, Anthropic, and HuggingFace",
+    author="Ethan",
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+    ],
+)
