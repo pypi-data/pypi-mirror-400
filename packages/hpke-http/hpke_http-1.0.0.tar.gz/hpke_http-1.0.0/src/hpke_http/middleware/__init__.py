e9c352af92f58a86cc1efbcd62be35aa8b9a2498
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 06:06:50 +0000

    Add package hpke-http/1.0.0/hpke_http-1.0.0.tar.gz

---
+++ packages/hpke-http/hpke_http-1.0.0.tar.gz/hpke_http-1.0.0/src/hpke_http/middleware/__init__.py
@@ -0,0 +1,9 @@
+"""
+Plug-and-play middleware for transparent HPKE encryption.
+
+Server (FastAPI):
+    from hpke_http.middleware.fastapi import HPKEMiddleware
+
+Client (aiohttp):
+    from hpke_http.middleware.aiohttp import HPKEClientSession
+"""
