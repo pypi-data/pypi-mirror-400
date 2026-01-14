ea7fe580437bb85b79b88a144f3153f814e7d697
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 09:11:01 +0000

    Add package pyovf/0.2.8/pyovf-0.2.8.tar.gz

---
+++ packages/pyovf/pyovf-0.2.8.tar.gz/pyovf-0.2.8/local_build_main.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# if ! git diff --quiet || ! git diff --cached --quiet; then
+#     commit_msg="chore: auto-commit before tagging $(date -u +"%Y-%m-%dT%H:%M:%SZ")"
+#     git add -A
+#     git commit -m "$commit_msg"
+# fi
+
+# bash delete_version_tag.sh 0.2.7
+
+# bash local-ci.sh create-tag
+
+# bash deploy_gitlab.sh
+
+# PYPI_TOKEN="" bash local-ci.sh deploy
