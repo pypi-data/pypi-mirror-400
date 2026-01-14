bf79ace033696a14cfdf84911b358fe19aea194b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 02:44:26 +0000

    Add package hoangdangkhoa/0.1.0/hoangdangkhoa-0.1.0.tar.gz

---
+++ packages/hoangdangkhoa/hoangdangkhoa-0.1.0.tar.gz/hoangdangkhoa-0.1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="hoangdangkhoa", # Tên bạn sẽ dùng để pip install
+    version="0.1.0",
+    packages=find_packages(),
+    description="Thu vien cua Khoa",
+    python_requires='>=3.6',
+)
