c8cac91cad4e8b9ea94fd858d942553fecd7ce8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 05:09:44 +0000

    Add package opnieuw/3.3.0/opnieuw-3.3.0-py3-none-any.whl

---
+++ packages/opnieuw/opnieuw-3.3.0-py3-none-any.whl/opnieuw/__init__.py
@@ -0,0 +1,11 @@
+# Opnieuw: Retries for humans
+# Copyright 2019 Channable
+#
+# Licensed under the 3-clause BSD license, see the LICENSE file in the repository root.
+
+from .exceptions import RetryException
+from .retries import retry, retry_async
+
+__all__ = ["retry_async", "retry", "RetryException"]
+
+__version__ = "3.3.0"
---
+++ packages/opnieuw/opnieuw-3.3.0-py3-none-any.whl/opnieuw/test_util.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+
+from contextlib import AbstractContextManager
+
+from .retries import BackoffCalculator, replace_backoff_calculator
+
+
+class WaitLessBackoff(BackoffCalculator):
+    def get_backoff(self) -> float | None:
+        self.backoffs += 1
+        if self.backoffs >= self.max_calls_total:
+            return None
+        return 0
+
+
+def retry_immediately(namespace: str | None = None) -> AbstractContextManager[None]:
+    """
+    Returns a contextmanager that prevents waits between retries for all `retry` and
+    `retry_async` decorators with the provided namespace. None means all decorators
+    without a provided namespace will not wait.
+    """
+    return replace_backoff_calculator(WaitLessBackoff, namespace=namespace)
