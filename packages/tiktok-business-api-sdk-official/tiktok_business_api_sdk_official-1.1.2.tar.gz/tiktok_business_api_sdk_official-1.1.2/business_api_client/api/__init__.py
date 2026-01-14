3ab8aecd804e1a962b70dfdaf491e765aebb78b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 09:09:39 +0000

    Add package tiktok-business-api-sdk-official/1.1.2/tiktok_business_api_sdk_official-1.1.2.tar.gz

---
+++ packages/tiktok-business-api-sdk-official/tiktok_business_api_sdk_official-1.1.2.tar.gz/tiktok_business_api_sdk_official-1.1.2/business_api_client/api/__init__.py
@@ -0,0 +1,33 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+"""
+ Copyright 2023 TikTok Pte. Ltd.
+
+ This source code is licensed under the MIT license found in
+ the LICENSE file in the root directory of this source tree.
+"""
+# import apis into api package
+from business_api_client.api.app_management_api import APPManagementApi
+from business_api_client.api.account_management_api import AccountManagementApi
+from business_api_client.api.ad_api import AdApi
+from business_api_client.api.ad_aco_api import AdAcoApi
+from business_api_client.api.adgroup_api import AdgroupApi
+from business_api_client.api.audience_api import AudienceApi
+from business_api_client.api.authentication_api import AuthenticationApi
+from business_api_client.api.automated_rules_api import AutomatedRulesApi
+from business_api_client.api.bc_api import BCApi
+from business_api_client.api.campaign_creation_api import CampaignCreationApi
+from business_api_client.api.comments_api import CommentsApi
+from business_api_client.api.creative_management_api import CreativeManagementApi
+from business_api_client.api.event_callback_api import EventCallbackApi
+from business_api_client.api.file_api import FileApi
+from business_api_client.api.identity_api import IdentityApi
+from business_api_client.api.measurement_api import MeasurementApi
+from business_api_client.api.recommend_tool_api import RecommendToolApi
+from business_api_client.api.reporting_api import ReportingApi
+from business_api_client.api.store_api import StoreApi
+from business_api_client.api.tool_api import ToolApi
+from business_api_client.api.video_api import VideoApi
+from business_api_client.api.catalog_api import CatalogApi
---
+++ packages/tiktok-business-api-sdk-official/tiktok_business_api_sdk_official-1.1.2.tar.gz/tiktok_business_api_sdk_official-1.1.2/setup.py
@@ -0,0 +1,35 @@
+# coding: utf-8
+
+"""
+ Copyright 2023 TikTok Pte. Ltd.
+
+ This source code is licensed under the MIT license found in
+ the LICENSE file in the root directory of this source tree.
+"""
+from setuptools import setup, find_packages  # noqa: H301
+
+NAME = "tiktok-business-api-sdk-official"
+VERSION = "1.1.2"
+
+REQUIRES = ["urllib3 >= 1.15", "six >= 1.10", "certifi", "python-dateutil"]
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description="TikTok Business API SDK",
+    author="TikTok Pte. Ltd.",
+    author_email="jason.chen@tiktok.com",
+    url="https://github.com/tiktok/tiktok-business-api-sdk",
+    keywords=["Swagger", "TikTok Business API SDK"],
+    install_requires=REQUIRES,
+    packages=find_packages(),
+    include_package_data=True,
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.4',
+)
