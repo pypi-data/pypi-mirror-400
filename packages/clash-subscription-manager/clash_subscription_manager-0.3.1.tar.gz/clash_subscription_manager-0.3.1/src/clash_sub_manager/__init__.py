06c694af5fec6a07acbcaf49d1264316c4ab78fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 03:35:56 +0000

    Add package clash-subscription-manager/0.3.1/clash_subscription_manager-0.3.1.tar.gz

---
+++ packages/clash-subscription-manager/clash_subscription_manager-0.3.1.tar.gz/clash_subscription_manager-0.3.1/src/clash_sub_manager/__init__.py
@@ -0,0 +1,20 @@
+"""Top-level package for Clash Subscription Manager."""
+
+from __future__ import annotations
+
+from importlib import metadata
+
+from .config import load_api_config
+from .proxy_selector import ClashProxySelector
+from .subscription_manager import ClashSubscriptionManager
+
+try:
+    __version__ = metadata.version("clash-subscription-manager")
+except metadata.PackageNotFoundError:  # pragma: no cover - dev installs
+    __version__ = "0.0.0"
+
+__all__ = [
+    "ClashSubscriptionManager",
+    "ClashProxySelector",
+    "load_api_config",
+]
