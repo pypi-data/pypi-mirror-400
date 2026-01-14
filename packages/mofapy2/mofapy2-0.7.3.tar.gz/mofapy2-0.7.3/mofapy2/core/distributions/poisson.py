775015316b5dd97100fcd62e2375db7eb9881545
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 14:10:11 +0000

    Add package mofapy2/0.7.3/mofapy2-0.7.3.tar.gz

---
+++ packages/mofapy2/mofapy2-0.7.3.tar.gz/mofapy2-0.7.3/mofapy2/core/distributions/poisson.py
@@ -0,0 +1,55 @@
+import numpy as np
+import scipy as s
+import scipy.stats as stats
+import math
+from .basic_distributions import Distribution
+
+from mofapy2.core.utils import *
+
+
+class Poisson(Distribution):
+    """
+    Class to define Poisson distributions.
+
+    Equations:
+    p(x|theta) = theta**x * exp(-theta) * 1/theta!
+    log p(x|a,b) = x*theta - theta - log(x!)
+    E[x] = theta
+    var[x] = theta
+    """
+
+    def __init__(self, dim, theta, E=None):
+        Distribution.__init__(self, dim)
+
+        # Initialise parameters
+        theta = np.ones(dim) * theta
+        self.params = {"theta": theta}
+
+        # Initialise expectations
+        if E is None:
+            self.updateExpectations()
+        else:
+            self.expectations = {"E": np.ones(dim) * E}
+
+        # Check that dimensionalities match
+        self.CheckDimensionalities()
+
+    def updateExpectations(self):
+        E = self.params["theta"]
+        self.expectations = {"E": E}
+
+    def density(self, x):
+        assert x.shape == self.dim, "Problem with the dimensionalities"
+        assert x.dtype == int, "x has to be an integer array"
+        theta = self.params["theta"].flatten()
+        x = x.flatten()
+        # return np.prod (stats.poisson.pmf(x,theta) )
+        return np.prod(np.divide(theta**x * np.exp(-theta), math.factorial(x)))
+
+    def loglik(self, x):
+        assert x.shape == self.dim, "Problem with the dimensionalities"
+        assert x.dtype == int, "x has to be an integer array"
+        theta = self.params["theta"].flatten()
+        x = x.flatten()
+        # return np.log( np.prod (stats.poisson.pmf(x,theta) ))
+        return np.sum(x * np.log(theta) - theta - np.log(math.factorial(x)))
