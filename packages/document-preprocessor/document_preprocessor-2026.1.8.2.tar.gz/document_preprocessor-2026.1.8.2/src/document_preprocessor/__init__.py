4d822bd4eaae7a7cbd520b277f625e0aafcea412
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 06:07:27 +0000

    Add package document-preprocessor/2026.1.8.2/document_preprocessor-2026.1.8.2.tar.gz

---
+++ packages/document-preprocessor/document_preprocessor-2026.1.8.2.tar.gz/document_preprocessor-2026.1.8.2/src/document_preprocessor/__init__.py
@@ -0,0 +1,5 @@
+__version__ = "2026.01.08.2"
+
+from .core import PreprocessorConfig, DocumentPreprocessor, DocumentProcessingError
+
+__all__ = ["PreprocessorConfig", "DocumentPreprocessor", "DocumentProcessingError"]
