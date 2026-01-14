73c9866f3287d4107f2a76cfaaeb97e83dbdc2bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 09:09:53 +0000

    Add package fcsfiles/2026.1.8/fcsfiles-2026.1.8.tar.gz

---
+++ packages/fcsfiles/fcsfiles-2026.1.8.tar.gz/fcsfiles-2026.1.8/fcsfiles/__init__.py
@@ -0,0 +1,9 @@
+# fcsfiles/__init__.py
+
+from .fcsfiles import *
+from .fcsfiles import __all__, __doc__, __version__
+
+# constants are repeated for documentation
+
+__version__ = __version__
+"""Fcsfiles version string."""
