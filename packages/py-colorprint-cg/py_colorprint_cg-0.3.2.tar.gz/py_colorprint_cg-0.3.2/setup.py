aeb24a1e76771616302f11bbded221f25a1d7ff9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 13:19:49 +0000

    Add package py-colorprint-cg/0.3.2/py_colorprint_cg-0.3.2.tar.gz

---
+++ packages/py-colorprint-cg/py_colorprint_cg-0.3.2.tar.gz/py_colorprint_cg-0.3.2/setup.py
@@ -0,0 +1,27 @@
+###########################################################
+# Colorprint- Python module that lets you print colorfully#
+# Under MIT license CoolGuy158-Git                        #
+###########################################################
+from setuptools import setup, find_packages
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name="py-colorprint-cg",
+    version="0.3.2",
+    packages=find_packages(),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="CoolGuy158-Git",
+    url="https://github.com/CoolGuy158-Git/colorprint",
+    license="MIT",
+    python_requires='>=3.7',
+    keywords="terminal color print ANSI style",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
+
