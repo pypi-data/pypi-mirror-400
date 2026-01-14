d31721314fc6db28803ddfac9d64c448dce9d601
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 02:44:36 +0000

    Add package epicscorelibs/7.0.10.99.0.0/epicscorelibs-7.0.10.99.0.0-cp314-cp314t-manylinux_2_28_x86_64.whl

---
+++ packages/epicscorelibs/epicscorelibs-7.0.10.99.0.0-cp314-cp314t-manylinux_2_28_x86_64.whl/epicscorelibs/include/compilerSpecific.h
@@ -0,0 +1,72 @@
+/*************************************************************************\
+* Copyright (c) 2008 UChicago Argonne LLC, as Operator of Argonne
+*     National Laboratory.
+* Copyright (c) 2002 The Regents of the University of California, as
+*     Operator of Los Alamos National Laboratory.
+* SPDX-License-Identifier: EPICS
+* EPICS BASE is distributed subject to a Software License Agreement found
+* in file LICENSE that is included with this distribution.
+\*************************************************************************/
+
+/*
+ * Author:
+ * Jeffrey O. Hill
+ * johill@lanl.gov
+ */
+
+#ifndef compilerSpecific_h
+#define compilerSpecific_h
+
+#ifndef __GNUC__
+#   error compiler/gcc/compilerSpecific.h is only for use with the gnu compiler
+#endif
+
+#define EPICS_ALWAYS_INLINE __inline__ __attribute__((always_inline))
+
+/* Expands to a 'const char*' which describes the name of the current function scope */
+#define EPICS_FUNCTION __PRETTY_FUNCTION__
+
+#ifdef __cplusplus
+
+/*
+ * in general we don't like ifdefs but they do allow us to check the
+ * compiler version and make the optimistic assumption that
+ * standards incompliance issues will be fixed by future compiler
+ * releases
+ */
+
+/*
+ * CXX_PLACEMENT_DELETE - defined if compiler supports placement delete
+ */
+#define CXX_PLACEMENT_DELETE
+
+#endif /* __cplusplus */
+
+/*
+ * Enable format-string checking if possible
+ */
+#if __GNUC__ * 100 + __GNUC_MINOR__  >= 404
+#define EPICS_PRINTF_STYLE(f,a) __attribute__((format(__gnu_printf__,f,a)))
+#else
+#define EPICS_PRINTF_STYLE(f,a) __attribute__((format(__printf__,f,a)))
+#endif
+
+/*
+ * Deprecation marker
+ */
+#define EPICS_DEPRECATED __attribute__((deprecated))
+
+/*
+ * Unused marker
+ */
+#define EPICS_UNUSED __attribute__((unused))
+
+/*
+ * No return marker
+ */
+#ifndef vxWorks
+// VxWorks does not mark abort() or exit() noreturn!
+#define EPICS_NORETURN __attribute__((noreturn))
+#endif
+
+#endif  /* ifndef compilerSpecific_h */
