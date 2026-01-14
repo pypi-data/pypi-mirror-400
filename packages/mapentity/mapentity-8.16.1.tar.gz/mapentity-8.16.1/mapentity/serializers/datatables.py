5b62f721795c4983c700500e7f225215849e849b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 04:23:25 +0000

    Add package mapentity/8.16.1/mapentity-8.16.1.tar.gz

---
+++ packages/mapentity/mapentity-8.16.1.tar.gz/mapentity-8.16.1/mapentity/serializers/datatables.py
@@ -0,0 +1,19 @@
+from django.db import models
+from rest_framework.serializers import ModelSerializer
+
+from mapentity.serializers import fields
+
+
+class MapentityDatatableSerializer(ModelSerializer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # patch mapping fields to use datatables format
+        mappings = self.serializer_field_mapping.copy()
+        mappings.update(
+            {
+                models.BooleanField: fields.MapentityDatatableBooleanField,
+                models.DateTimeField: fields.MapentityDatatableDateTimeField,
+                models.DateField: fields.MapentityDatatableDateField,
+            }
+        )
+        self.serializer_field_mapping = mappings
