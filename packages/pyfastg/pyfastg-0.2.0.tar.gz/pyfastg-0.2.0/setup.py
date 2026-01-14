89594315539123cc2915ea81e58a392fe053cb4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 02:45:03 +0000

    Add package pyfastg/0.2.0/pyfastg-0.2.0.tar.gz

---
+++ packages/pyfastg/pyfastg-0.2.0.tar.gz/pyfastg-0.2.0/setup.py
@@ -0,0 +1,39 @@
+# NOTE: Derived from https://github.com/biocore/qurro/blob/master/setup.py
+
+from setuptools import find_packages, setup
+
+classes = """
+    Development Status :: 3 - Alpha
+    Topic :: Software Development :: Libraries
+    Topic :: Scientific/Engineering
+    Topic :: Scientific/Engineering :: Bio-Informatics
+    Programming Language :: Python :: 3
+    Programming Language :: Python :: 3 :: Only
+"""
+classifiers = [s.strip() for s in classes.split("\n") if s]
+
+description = "Minimal Python library for parsing SPAdes FASTG files"
+
+with open("README.md") as f:
+    long_description = f.read()
+
+setup(
+    name="pyfastg",
+    version="0.2.0",
+    license="MIT",
+    description=description,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Nick Waters, Marcus Fedarko",
+    maintainer="Marcus Fedarko",
+    maintainer_email="mfedarko@umd.edu",
+    url="https://github.com/fedarko/pyfastg",
+    classifiers=classifiers,
+    packages=find_packages(),
+    install_requires=["networkx >= 2"],
+    extras_require={
+        # The black < 22.10 pin is because black 22.10 stopped supporting
+        # python 3.6
+        "dev": ["pytest", "pytest-cov", "flake8", "black < 22.10"]
+    },
+)
