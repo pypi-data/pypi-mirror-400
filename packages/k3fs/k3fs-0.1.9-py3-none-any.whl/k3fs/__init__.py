da93a6611615f88128a8c6e6844281faa4532882
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 12:08:12 +0000

    Add package k3fs/0.1.9/k3fs-0.1.9-py3-none-any.whl

---
+++ packages/k3fs/k3fs-0.1.9-py3-none-any.whl/k3fs/__init__.py
@@ -0,0 +1,59 @@
+"""
+k3fs is collection of file-system operation utilities.
+
+Usage::
+
+    >>> fwrite('/tmp/foo', "content")
+
+    >>> fread('/tmp/foo')
+    'content'
+
+    >>> 'foo' in ls_files('/tmp/')
+    True
+
+"""
+
+from importlib.metadata import version
+
+__version__ = version("k3fs")
+from .fs import (
+    FSUtilError,
+    NotMountPoint,
+    assert_mountpoint,
+    calc_checksums,
+    get_all_mountpoint,
+    get_device,
+    get_device_fs,
+    get_disk_partitions,
+    get_mountpoint,
+    get_path_fs,
+    get_path_inode_usage,
+    get_path_usage,
+    fread,
+    fwrite,
+    ls_dirs,
+    ls_files,
+    makedirs,
+    remove,
+)
+
+__all__ = [
+    "FSUtilError",
+    "NotMountPoint",
+    "assert_mountpoint",
+    "calc_checksums",
+    "get_all_mountpoint",
+    "get_device",
+    "get_device_fs",
+    "get_disk_partitions",
+    "get_mountpoint",
+    "get_path_fs",
+    "get_path_inode_usage",
+    "get_path_usage",
+    "ls_dirs",
+    "ls_files",
+    "makedirs",
+    "fread",
+    "fwrite",
+    "remove",
+]
