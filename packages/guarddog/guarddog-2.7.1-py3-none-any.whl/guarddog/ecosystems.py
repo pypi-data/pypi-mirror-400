cc669952d4a6056f38c7ba96f842b4ea722cce69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 15:09:13 +0000

    Add package guarddog/2.7.1/guarddog-2.7.1-py3-none-any.whl

---
+++ packages/guarddog/guarddog-2.7.1-py3-none-any.whl/guarddog/ecosystems.py
@@ -0,0 +1,25 @@
+from enum import Enum
+
+
+class ECOSYSTEM(Enum):
+    PYPI = "pypi"
+    NPM = "npm"
+    GO = "go"
+    GITHUB_ACTION = "github-action"
+    EXTENSION = "extension"
+
+
+def get_friendly_name(ecosystem: ECOSYSTEM) -> str:
+    match ecosystem:
+        case ECOSYSTEM.PYPI:
+            return "PyPI"
+        case ECOSYSTEM.NPM:
+            return "npm"
+        case ECOSYSTEM.GO:
+            return "go"
+        case ECOSYSTEM.GITHUB_ACTION:
+            return "GitHub Action"
+        case ECOSYSTEM.EXTENSION:
+            return "Extension"
+        case _:
+            return ecosystem.value
