5319a42e7d8d94fdd95ead6b2f86ba156af311a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 12:07:13 +0000

    Add package numcodecs-bitmap-index/0.1.2/numcodecs_bitmap_index-0.1.2.tar.gz

---
+++ packages/numcodecs-bitmap-index/numcodecs_bitmap_index-0.1.2.tar.gz/numcodecs_bitmap_index-0.1.2/tests/test_bitmap_index.py
@@ -0,0 +1,54 @@
+import numcodecs
+import numcodecs.registry
+import numpy as np
+
+
+def test_from_config():
+    codec = numcodecs.registry.get_codec(dict(id="bitmap-index"))
+    assert codec.__class__.__name__ == "BitmapIndexCodec"
+    assert codec.__class__.__module__ == "numcodecs_bitmap_index"
+
+
+def check_roundtrip(data: np.ndarray):
+    codec = numcodecs.registry.get_codec(dict(id="bitmap-index"))
+
+    encoded = codec.encode(data)
+    decoded = codec.decode(encoded)
+
+    assert decoded.dtype == data.dtype
+    assert decoded.shape == data.shape
+    assert np.all(_as_bits(decoded) == _as_bits(data))
+
+
+def test_roundtrip():
+    check_roundtrip(np.zeros(tuple()))
+    check_roundtrip(np.zeros((0,)))
+    check_roundtrip(np.arange(1000).reshape(10, 10, 10))
+    check_roundtrip(np.round(np.sqrt(np.arange(1000).reshape(10, 10, 10))))
+    check_roundtrip(
+        np.round(np.sqrt(np.arange(1000).reshape(10, 10, 10))).astype(
+            np.dtype(np.float64).newbyteorder("<")
+        )
+    )
+    check_roundtrip(
+        np.round(np.sqrt(np.arange(1000).reshape(10, 10, 10))).astype(
+            np.dtype(np.float64).newbyteorder(">")
+        )
+    )
+    check_roundtrip(np.array([np.inf, -np.inf, np.nan, -np.nan, 0.0, -0.0]))
+    check_roundtrip(
+        np.array(
+            [np.inf, -np.inf, np.nan, -np.nan, 0.0, -0.0],
+            dtype=np.dtype(np.float64).newbyteorder("<"),
+        )
+    )
+    check_roundtrip(
+        np.array(
+            [np.inf, -np.inf, np.nan, -np.nan, 0.0, -0.0],
+            dtype=np.dtype(np.float64).newbyteorder(">"),
+        )
+    )
+
+
+def _as_bits(a: np.ndarray) -> np.ndarray:
+    return a.view(a.dtype.str.replace("f", "u").replace("i", "u"))
