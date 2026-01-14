1d4c7693464af6fb1f90af66df53a73ad31a9edf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 07:09:21 +0000

    Add package isnotodd/0.1.7/isnotodd-0.1.7.tar.gz

---
+++ packages/isnotodd/isnotodd-0.1.7.tar.gz/isnotodd-0.1.7/is_not_odd.py
@@ -0,0 +1,682 @@
+def is_even(n: int) -> bool:
+    """returns True if the number is even, False otherwise"""
+    assert isinstance(n, int), "only integers can be even xd"
+    match n:
+        case 1 | -1:
+            return False
+        case 2 | -2:
+            return True
+        case 3 | -3:
+            return False
+        case 4 | -4:
+            return True
+        case 5 | -5:
+            return False
+        case 6 | -6:
+            return True
+        case 7 | -7:
+            return False
+        case 8 | -8:
+            return True
+        case 9 | -9:
+            return False
+        case 10 | -10:
+            return True
+        case 11 | -11:
+            return False
+        case 12 | -12:
+            return True
+        case 13 | -13:
+            return False
+        case 14 | -14:
+            return True
+        case 15 | -15:
+            return False
+        case 16 | -16:
+            return True
+        case 17 | -17:
+            return False
+        case 18 | -18:
+            return True
+        case 19 | -19:
+            return False
+        case 20 | -20:
+            return True
+        case 21 | -21:
+            return False
+        case 22 | -22:
+            return True
+        case 23 | -23:
+            return False
+        case 24 | -24:
+            return True
+        case 25 | -25:
+            return False
+        case 26 | -26:
+            return True
+        case 27 | -27:
+            return False
+        case 28 | -28:
+            return True
+        case 29 | -29:
+            return False
+        case 30 | -30:
+            return True
+        case 31 | -31:
+            return False
+        case 32 | -32:
+            return True
+        case 33 | -33:
+            return False
+        case 34 | -34:
+            return True
+        case 35 | -35:
+            return False
+        case 36 | -36:
+            return True
+        case 37 | -37:
+            return False
+        case 38 | -38:
+            return True
+        case 39 | -39:
+            return False
+        case 40 | -40:
+            return True
+        case 41 | -41:
+            return False
+        case 42 | -42:
+            return True
+        case 43 | -43:
+            return False
+        case 44 | -44:
+            return True
+        case 45 | -45:
+            return False
+        case 46 | -46:
+            return True
+        case 47 | -47:
+            return False
+        case 48 | -48:
+            return True
+        case 49 | -49:
+            return False
+        case 50 | -50:
+            return True
+        case 51 | -51:
+            return False
+        case 52 | -52:
+            return True
+        case 53 | -53:
+            return False
+        case 54 | -54:
+            return True
+        case 55 | -55:
+            return False
+        case 56 | -56:
+            return True
+        case 57 | -57:
+            return False
+        case 58 | -58:
+            return True
+        case 59 | -59:
+            return False
+        case 60 | -60:
+            return True
+        case 61 | -61:
+            return False
+        case 62 | -62:
+            return True
+        case 63 | -63:
+            return False
+        case 64 | -64:
+            return True
+        case 65 | -65:
+            return False
+        case 66 | -66:
+            return True
+        case 67 | -67:
+            return False
+        case 68 | -68:
+            return True
+        case 69 | -69:
+            return False
+        case 70 | -70:
+            return True
+        case 71 | -71:
+            return False
+        case 72 | -72:
+            return True
+        case 73 | -73:
+            return False
+        case 74 | -74:
+            return True
+        case 75 | -75:
+            return False
+        case 76 | -76:
+            return True
+        case 77 | -77:
+            return False
+        case 78 | -78:
+            return True
+        case 79 | -79:
+            return False
+        case 80 | -80:
+            return True
+        case 81 | -81:
+            return False
+        case 82 | -82:
+            return True
+        case 83 | -83:
+            return False
+        case 84 | -84:
+            return True
+        case 85 | -85:
+            return False
+        case 86 | -86:
+            return True
+        case 87 | -87:
+            return False
+        case 88 | -88:
+            return True
+        case 89 | -89:
+            return False
+        case 90 | -90:
+            return True
+        case 91 | -91:
+            return False
+        case 92 | -92:
+            return True
+        case 93 | -93:
+            return False
+        case 94 | -94:
+            return True
+        case 95 | -95:
+            return False
+        case 96 | -96:
+            return True
+        case 97 | -97:
+            return False
+        case 98 | -98:
+            return True
+        case 99 | -99:
+            return False
+        case 100 | -100:
+            return True
+        case 101 | -101:
+            return False
+        case 102 | -102:
+            return True
+        case 103 | -103:
+            return False
+        case 104 | -104:
+            return True
+        case 105 | -105:
+            return False
+        case 106 | -106:
+            return True
+        case 107 | -107:
+            return False
+        case 108 | -108:
+            return True
+        case 109 | -109:
+            return False
+        case 110 | -110:
+            return True
+        case 111 | -111:
+            return False
+        case 112 | -112:
+            return True
+        case 113 | -113:
+            return False
+        case 114 | -114:
+            return True
+        case 115 | -115:
+            return False
+        case 116 | -116:
+            return True
+        case 117 | -117:
+            return False
+        case 118 | -118:
+            return True
+        case 119 | -119:
+            return False
+        case 120 | -120:
+            return True
+        case 121 | -121:
+            return False
+        case 122 | -122:
+            return True
+        case 123 | -123:
+            return False
+        case 124 | -124:
+            return True
+        case 125 | -125:
+            return False
+        case 126 | -126:
+            return True
+        case 127 | -127:
+            return False
+        case 128 | -128:
+            return True
+        case 129 | -129:
+            return False
+        case 130 | -130:
+            return True
+        case 131 | -131:
+            return False
+        case 132 | -132:
+            return True
+        case 133 | -133:
+            return False
+        case 134 | -134:
+            return True
+        case 135 | -135:
+            return False
+        case 136 | -136:
+            return True
+        case 137 | -137:
+            return False
+        case 138 | -138:
+            return True
+        case 139 | -139:
+            return False
+        case 140 | -140:
+            return True
+        case 141 | -141:
+            return False
+        case 142 | -142:
+            return True
+        case 143 | -143:
+            return False
+        case 144 | -144:
+            return True
+        case 145 | -145:
+            return False
+        case 146 | -146:
+            return True
+        case 147 | -147:
+            return False
+        case 148 | -148:
+            return True
+        case 149 | -149:
+            return False
+        case 150 | -150:
+            return True
+        case 151 | -151:
+            return False
+        case 152 | -152:
+            return True
+        case 153 | -153:
+            return False
+        case 154 | -154:
+            return True
+        case 155 | -155:
+            return False
+        case 156 | -156:
+            return True
+        case 157 | -157:
+            return False
+        case 158 | -158:
+            return True
+        case 159 | -159:
+            return False
+        case 160 | -160:
+            return True
+        case 161 | -161:
+            return False
+        case 162 | -162:
+            return True
+        case 163 | -163:
+            return False
+        case 164 | -164:
+            return True
+        case 165 | -165:
+            return False
+        case 166 | -166:
+            return True
+        case 167 | -167:
+            return False
+        case 168 | -168:
+            return True
+        case 169 | -169:
+            return False
+        case 170 | -170:
+            return True
+        case 171 | -171:
+            return False
+        case 172 | -172:
+            return True
+        case 173 | -173:
+            return False
+        case 174 | -174:
+            return True
+        case 175 | -175:
+            return False
+        case 176 | -176:
+            return True
+        case 177 | -177:
+            return False
+        case 178 | -178:
+            return True
+        case 179 | -179:
+            return False
+        case 180 | -180:
+            return True
+        case 181 | -181:
+            return False
+        case 182 | -182:
+            return True
+        case 183 | -183:
+            return False
+        case 184 | -184:
+            return True
+        case 185 | -185:
+            return False
+        case 186 | -186:
+            return True
+        case 187 | -187:
+            return False
+        case 188 | -188:
+            return True
+        case 189 | -189:
+            return False
+        case 190 | -190:
+            return True
+        case 191 | -191:
+            return False
+        case 192 | -192:
+            return True
+        case 193 | -193:
+            return False
+        case 194 | -194:
+            return True
+        case 195 | -195:
+            return False
+        case 196 | -196:
+            return True
+        case 197 | -197:
+            return False
+        case 198 | -198:
+            return True
+        case 199 | -199:
+            return False
+        case 200 | -200:
+            return True
+        case 201 | -201:
+            return False
+        case 202 | -202:
+            return True
+        case 203 | -203:
+            return False
+        case 204 | -204:
+            return True
+        case 205 | -205:
+            return False
+        case 206 | -206:
+            return True
+        case 207 | -207:
+            return False
+        case 208 | -208:
+            return True
+        case 209 | -209:
+            return False
+        case 210 | -210:
+            return True
+        case 211 | -211:
+            return False
+        case 212 | -212:
+            return True
+        case 213 | -213:
+            return False
+        case 214 | -214:
+            return True
+        case 215 | -215:
+            return False
+        case 216 | -216:
+            return True
+        case 217 | -217:
+            return False
+        case 218 | -218:
+            return True
+        case 219 | -219:
+            return False
+        case 220 | -220:
+            return True
+        case 221 | -221:
+            return False
+        case 222 | -222:
+            return True
+        case 223 | -223:
+            return False
+        case 224 | -224:
+            return True
+        case 225 | -225:
+            return False
+        case 226 | -226:
+            return True
+        case 227 | -227:
+            return False
+        case 228 | -228:
+            return True
+        case 229 | -229:
+            return False
+        case 230 | -230:
+            return True
+        case 231 | -231:
+            return False
+        case 232 | -232:
+            return True
+        case 233 | -233:
+            return False
+        case 234 | -234:
+            return True
+        case 235 | -235:
+            return False
+        case 236 | -236:
+            return True
+        case 237 | -237:
+            return False
+        case 238 | -238:
+            return True
+        case 239 | -239:
+            return False
+        case 240 | -240:
+            return True
+        case 241 | -241:
+            return False
+        case 242 | -242:
+            return True
+        case 243 | -243:
+            return False
+        case 244 | -244:
+            return True
+        case 245 | -245:
+            return False
+        case 246 | -246:
+            return True
+        case 247 | -247:
+            return False
+        case 248 | -248:
+            return True
+        case 249 | -249:
+            return False
+        case 250 | -250:
+            return True
+        case 251 | -251:
+            return False
+        case 252 | -252:
+            return True
+        case 253 | -253:
+            return False
+        case 254 | -254:
+            return True
+        case 255 | -255:
+            return False
+        case 256 | -256:
+            return True
+        case 257 | -257:
+            return False
+        case 258 | -258:
+            return True
+        case 259 | -259:
+            return False
+        case 260 | -260:
+            return True
+        case 261 | -261:
+            return False
+        case 262 | -262:
+            return True
+        case 263 | -263:
+            return False
+        case 264 | -264:
+            return True
+        case 265 | -265:
+            return False
+        case 266 | -266:
+            return True
+        case 267 | -267:
+            return False
+        case 268 | -268:
+            return True
+        case 269 | -269:
+            return False
+        case 270 | -270:
+            return True
+        case 271 | -271:
+            return False
+        case 272 | -272:
+            return True
+        case 273 | -273:
+            return False
+        case 274 | -274:
+            return True
+        case 275 | -275:
+            return False
+        case 276 | -276:
+            return True
+        case 277 | -277:
+            return False
+        case 278 | -278:
+            return True
+        case 279 | -279:
+            return False
+        case 280 | -280:
+            return True
+        case 281 | -281:
+            return False
+        case 282 | -282:
+            return True
+        case 283 | -283:
+            return False
+        case 284 | -284:
+            return True
+        case 285 | -285:
+            return False
+        case 286 | -286:
+            return True
+        case 287 | -287:
+            return False
+        case 288 | -288:
+            return True
+        case 289 | -289:
+            return False
+        case 290 | -290:
+            return True
+        case 291 | -291:
+            return False
+        case 292 | -292:
+            return True
+        case 293 | -293:
+            return False
+        case 294 | -294:
+            return True
+        case 295 | -295:
+            return False
+        case 296 | -296:
+            return True
+        case 297 | -297:
+            return False
+        case 298 | -298:
+            return True
+        case 299 | -299:
+            return False
+        case 300 | -300:
+            return True
+        case 301 | -301:
+            return False
+        case 302 | -302:
+            return True
+        case 303 | -303:
+            return False
+        case 304 | -304:
+            return True
+        case 305 | -305:
+            return False
+        case 306 | -306:
+            return True
+        case 307 | -307:
+            return False
+        case 308 | -308:
+            return True
+        case 309 | -309:
+            return False
+        case 310 | -310:
+            return True
+        case 311 | -311:
+            return False
+        case 312 | -312:
+            return True
+        case 313 | -313:
+            return False
+        case 314 | -314:
+            return True
+        case 315 | -315:
+            return False
+        case 316 | -316:
+            return True
+        case 317 | -317:
+            return False
+        case 318 | -318:
+            return True
+        case 319 | -319:
+            return False
+        case 320 | -320:
+            return True
+        case 321 | -321:
+            return False
+        case 322 | -322:
+            return True
+        case 323 | -323:
+            return False
+        case 324 | -324:
+            return True
+        case 325 | -325:
+            return False
+        case 326 | -326:
+            return True
+        case 327 | -327:
+            return False
+        case 328 | -328:
+            return True
+        case 329 | -329:
+            return False
+        case 330 | -330:
+            return True
+        case 331 | -331:
+            return False
+        case 332 | -332:
+            return True
+        case 333 | -333:
+            return False
+        case 334 | -334:
+            return True
+        case 335 | -335:
+            return False
+        case 336 | -336:
+            return True
+        case 337 | -337:
+            return False
+        case 338 | -338:
+            return True
+        case _:
+            raise NotImplementedError("your number is too big (or too small), we dont know it yet")
