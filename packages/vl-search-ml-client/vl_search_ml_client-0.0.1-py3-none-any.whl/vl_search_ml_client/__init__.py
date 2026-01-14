08451b08ffd0614d2b9490e00fb5be60c9b0b481
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 16:07:18 +0000

    Add package vl-search-ml-client/0.0.1/vl_search_ml_client-0.0.1-py3-none-any.whl

---
+++ packages/vl-search-ml-client/vl_search_ml_client-0.0.1-py3-none-any.whl/vl_search_ml_client/__init__.py
@@ -0,0 +1,8 @@
+"""
+vl-search-ml-client - Reserved Package Name
+
+This package name has been reserved and is not yet available for use.
+Please check back later or contact the maintainer for more information.
+"""
+
+__version__ = "0.0.1"
