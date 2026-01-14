158bf64a3276ff351a1e632fc3d7faf89a77cccb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 03:34:46 +0000

    Add package DjPractLelo/0.1.1/djpractlelo-0.1.1.tar.gz

---
+++ packages/DjPractLelo/djpractlelo-0.1.1.tar.gz/djpractlelo-0.1.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="DjPractLelo",
+    version="0.1.1",
+    author="Dj practical wala",
+    description="My demo Python package",
+    packages=find_packages(),
+    python_requires=">=3.8",
+)
