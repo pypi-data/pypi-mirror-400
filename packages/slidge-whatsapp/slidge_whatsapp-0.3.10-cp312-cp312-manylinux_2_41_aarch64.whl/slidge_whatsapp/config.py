1c47738d7919d938eede230c013c21b91d091d36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 16:07:11 +0000

    Add package slidge-whatsapp/0.3.10/slidge_whatsapp-0.3.10-cp312-cp312-manylinux_2_41_aarch64.whl

---
+++ packages/slidge-whatsapp/slidge_whatsapp-0.3.10-cp312-cp312-manylinux_2_41_aarch64.whl/slidge_whatsapp/config.py
@@ -0,0 +1,40 @@
+"""
+Config contains plugin-specific configuration for WhatsApp, and is loaded automatically by the
+core configuration framework.
+"""
+
+from pathlib import Path
+from typing import Optional
+
+from slidge import global_config
+
+# workaround because global_config.HOME_DIR is not defined unless
+# called by slidge's main(), which is a problem for tests, docs and the
+# dedicated slidge-whatsapp setuptools entrypoint
+try:
+    DB_PATH: Optional[Path] = global_config.HOME_DIR / "whatsapp" / "whatsapp.db"
+except AttributeError:
+    DB_PATH: Optional[Path] = None  # type:ignore
+
+DB_PATH__DOC = (
+    "The path to the database used for the WhatsApp plugin. Default to "
+    "${SLIDGE_HOME_DIR}/whatsapp/whatsapp.db"
+)
+
+DB_PARAMS = "?_foreign_keys=true"
+DB_PARAMS__DOC = "Additional parameters to pass to database connection string"
+
+ALWAYS_SYNC_ROSTER = False
+ALWAYS_SYNC_ROSTER__DOC = (
+    "Whether or not to perform a full sync of the WhatsApp roster on startup"
+)
+
+ENABLE_LINK_PREVIEWS = True
+ENABLE_LINK_PREVIEWS__DOC = (
+    "Whether or not previews for links (URLs) should be generated on outgoing messages"
+)
+
+ADD_GROUP_PARTICIPANTS_TO_ROSTER = True
+ADD_GROUP_PARTICIPANTS_TO_ROSTER__DOC = (
+    "Whether or not group participants will be added to the XMPP roster"
+)
