d1c7e92d5492417e6fc766f8268f1e9ea5f1a69b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 14:09:35 +0000

    Add package tactics2d/0.1.9rc1/tactics2d-0.1.9rc1-cp39-cp39-win32.whl

---
+++ packages/tactics2d/tactics2d-0.1.9rc1-cp39-cp39-win32.whl/tactics2d/map/generator/__init__.py
@@ -0,0 +1,10 @@
+# Copyright (C) 2023, Tactics2D Authors. Released under the GNU GPLv3.
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+"""Generator module."""
+
+
+from .generate_parking_lot import ParkingLotGenerator
+from .generate_racing_track import RacingTrackGenerator
+
+__all__ = ["RacingTrackGenerator", "ParkingLotGenerator"]
