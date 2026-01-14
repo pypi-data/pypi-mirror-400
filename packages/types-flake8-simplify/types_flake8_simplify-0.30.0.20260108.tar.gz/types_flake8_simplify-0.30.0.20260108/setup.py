e608b942200f87e298d2cdd3293299d70417a3d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 02:45:09 +0000

    Add package types-flake8-simplify/0.30.0.20260108/types_flake8_simplify-0.30.0.20260108.tar.gz

---
+++ packages/types-flake8-simplify/types_flake8_simplify-0.30.0.20260108.tar.gz/types_flake8_simplify-0.30.0.20260108/setup.py
@@ -0,0 +1,4 @@
+
+from setuptools import setup
+
+setup(package_data={'flake8_simplify-stubs': ['__init__.pyi', 'constants.pyi', 'rules/__init__.pyi', 'rules/ast_assign.pyi', 'rules/ast_bool_op.pyi', 'rules/ast_call.pyi', 'rules/ast_classdef.pyi', 'rules/ast_compare.pyi', 'rules/ast_expr.pyi', 'rules/ast_for.pyi', 'rules/ast_if.pyi', 'rules/ast_ifexp.pyi', 'rules/ast_subscript.pyi', 'rules/ast_try.pyi', 'rules/ast_unary_op.pyi', 'rules/ast_with.pyi', 'utils.pyi', 'METADATA.toml', 'py.typed']})
