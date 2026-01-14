438fdfacaf3ebf8fa510da2eb09b5dbe2372f3a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 02:44:27 +0000

    Add package aiken-eng/0.1.0/aiken_eng-0.1.0.tar.gz

---
+++ packages/aiken-eng/aiken_eng-0.1.0.tar.gz/aiken_eng-0.1.0/README.md
@@ -0,0 +1,5 @@
+# aiken_eng
+This is a collection of tools used by Aiken Engineering Company.
+## plotting
+Sets the plot defaults for border, gridlines, tick marks, etc. to use with matplotlib.<br>
+`plot(x, y, x_label, y_label, title)` - a simple one-line plot interface
---
+++ packages/aiken-eng/aiken_eng-0.1.0.tar.gz/aiken_eng-0.1.0/src/aiken_eng/plotting.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+mpl.rcParams["axes.titlesize"] = "medium"
+mpl.rcParams["axes.grid"] = True
+mpl.rcParams["axes.formatter.use_mathtext"] = True
+mpl.rcParams["xtick.top"] = True
+mpl.rcParams["xtick.labelsize"] = "small"
+mpl.rcParams["xtick.direction"] = "in"
+mpl.rcParams["xtick.minor.visible"] = True
+mpl.rcParams["ytick.right"] = True
+mpl.rcParams["ytick.labelsize"] = "small"
+mpl.rcParams["ytick.direction"] = "in"
+mpl.rcParams["ytick.minor.visible"] = True
+mpl.rcParams["grid.linewidth"] = 0.5
+mpl.rcParams["legend.fontsize"] = "small"
+mpl.rcParams["legend.framealpha"] = 1.0
+
+print("aiken_plot_defaults.py imported.")
+
+
+def plot(x, y, x_label=None, y_label=None, title=None):
+    plt.plot(x, y)
+    if x_label:
+        plt.xlabel(x_label)
+    if y_label:
+        plt.ylabel(y_label)
+    if title:
+        plt.title(title)
+    plt.show()
