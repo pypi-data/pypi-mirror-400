3546f56a2c14c08c2a9953504809d361b37f1ebd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 05:09:22 +0000

    Add package campay/1.1.0/campay-1.1.0.tar.gz

---
+++ packages/campay/campay-1.1.0.tar.gz/campay-1.1.0/README.md
@@ -0,0 +1,189 @@
+# [CamPay](https://www.campay.net/) Python SDK
+
+Python SDK for CamPay Payment Gateway
+
+CamPay is a Fintech service of the company TAKWID
+GROUP which launched its financial services in Cameroon
+from January 2021.
+
+We provide businesses and institutions with solutions for
+collecting and transferring money online, via primarily
+Mobile Money(MTN and Orange).
+
+With CamPay, simplify the purchasing experience for
+your customers thanks to our mobile money
+payment solutions, accessible via your website
+and/or mobile application.
+
+
+## Summary
+
+  - [Getting Started](#getting-started)
+  - [Running the samples](#running-the-samples)
+  - [Deployment](#deployment)
+
+## Getting Started
+
+These instructions will get you started with the CamPay SDK for development and testing purposes. See deployment
+for notes on how to deploy the project on a live system.
+
+### Prerequisites
+
+ - Create an account on [CamPay](https://www.campay.net/) platform
+ - Register an application under your account.
+ - Expand your registered application to get access to your API keys
+
+### Installing
+
+   ```python
+        pip install campay
+   ```
+
+## Running the samples
+
+  - Initialize the library with credentials. 
+    ```python
+        from campay.sdk import Client as CamPayClient
+
+        campay = CamPayClient({
+            "app_username" : "PASTE YOUR APP_USERNAME HERE",
+            "app_password" : "PASTE YOUR APP_PASSWORD HERE",
+            "environment" : "DEV" #use "DEV" for demo mode or "PROD" for live mode
+        })
+    ```
+
+### To collect payments from your client - DIRECTLY
+
+   **OPTION 1**: This option initiates a transaction and waits( or block the running code) for the transaction to complete before returning a response. 
+
+   ```python
+         collect = campay.collect({
+            "amount": "5", #The amount you want to collect
+            "currency": "XAF",
+            "from": "2376xxxxxxxx", #Phone number to request amount from. Must include country code
+            "description": "some description",
+            "external_reference": "", #Reference from the system initiating the transaction.
+         })
+
+         print(collect)
+         #{"reference": "bcedde9b-62a7-4421-96ac-2e6179552a1a", "external_reference":"12345678", "status": "SUCCESSFUL", "amount": 5, "currency": "XAF", "operator": "MTN", "code": "CP201027T00005", "operator_reference":  "1880106956" }
+         
+   ```
+   > status can be SUCCESSFUL or FAILED
+
+   **OPTION 2**: This option initiates a transaction and immediately returns the initiated transaction reference for you to use to check the status of the transaction.
+
+   ```python
+         collect = campay.initCollect({
+            "amount": "5", #The amount you want to collect
+            "currency": "XAF",
+            "from": "2376xxxxxxxx", #Phone number to request amount from. Must include country code
+            "description": "some description",
+            "external_reference": "", #Reference from the system initiating the transaction.
+         })
+
+         print(collect)
+         #{"reference": "bcedde9b-62a7-4421-96ac-2e6179552a1a", "ussd_code": "*126# for MTN or #150*50# for ORANGE", "operator": "mtn or orange" }
+         
+   ```
+   > The default status for initiated transactions is PENDING
+
+### To check a transaction status.
+
+   You need the initiated transaction **reference** to use this function.
+
+   ```python
+         campay_status = campay.get_transaction_status({
+            "reference": "bcedde9b-62a7-1234-96ac-2e6179552a1a", #The reference returned from campay.initCollect
+         })
+
+         print(campay_status)
+         #{"reference": "bcedde9b-62a7-4421-96ac-2e6179552a1a", "external_reference":"12345678", "status": "SUCCESSFUL", "amount": 5, "currency": "XAF", "operator": "MTN", "code": "CP201027T00005", "operator_reference":  "1880106956" }
+         
+   ```
+   > status can be PENDING, SUCCESSFUL or FAILED
+
+### To collect payments from your client - using PAYMENT LINKS
+
+   ```python
+         payment_link = campay.get_payment_link({
+            "amount": "5",
+            "currency": "XAF",
+            "description": "some description",
+            "external_reference": "12345678",
+            "from":"2376xxxxxxxx",
+            "first_name":"John",
+            "last_name":"Doe",
+            "email":"",
+            "redirect_url": "https://mysite.com/",
+            "failure_redirect_url":"https://failed.com/",
+            "payment_options":"MOMO,CARD"
+         })
+
+         print(payment_link)
+         #{"status": "SUCCESSFUL", "link": "https://www.campay.com/pay/with/link/", "reference": "bcedde9b-62a7-4421-96ac-2e6179552a1a"}
+         '''
+         Redirect your customer to the returned payment link. 
+         You can use the "reference" to verify the status of the transaction. If check the transaction status and client has not initiated a transaction on the payment link, you will get a response telling you the transaction is not found.
+         '''
+         
+   ```
+   > status can be SUCCESSFUL or FAILED
+
+
+### To disburse
+   > Please enable API withdrawal under app settings before trying this request
+   
+   ```python
+        disburse = campay.disburse({
+            "amount": "5", #The amount you want to disburse
+            "currency": "XAF",
+            "to": "2376xxxxxxxx", #Phone number to disburse amount to. Must include country code
+            "description": "some description",
+            "external_reference": "", #Reference from the system initiating the transaction.
+        })
+
+        print(disburse)
+        #{"reference": "bcedde9b-62a7-4421-96ac-2e6179552a1a", "external_reference":"12345678", "status": "SUCCESSFUL", "amount": 5, "currency": "XAF", "operator": "MTN", "code": "CP201027T00005", "operator_reference":  "1880106956" }
+
+   ```
+   > status can be SUCCESSFUL or FAILED
+
+### To Get application balance.
+
+   ```python
+        balance = campay.get_balance()
+
+        print(balance)
+        #{"total_balance": 0, "mtn_balance": 0, "orange_balance": 0, "currency": "XAF"}
+   ```
+<!-- 
+### Transfer Airtime
+   > Please enable API withdrawal under app settings before trying this request
+   
+   ```python
+        airtime = campay.transfer_airtime({
+            "amount": "100", #The amount you want to transfer
+            "to": "2376xxxxxxxx", #Phone number to receive airtime. Must include country code
+            "external_reference": "", #Reference from the system initiating the transaction.
+        })
+
+        print(airtime)
+        #{"reference": "bcedde9b-62a7-4421-96ac-2e6179552a1a", "external_reference":"12345678", "status": "SUCCESSFUL", "amount": 5, "currency": "XAF", "operator": "MTN", "code": "CP201027U00005", "operator_reference":  "1880106956" }
+
+   ```
+   > status can be SUCCESSFUL or FAILED
+-->
+
+## Deployment
+
+Change the environment of the library introduction to PROD
+
+  ```python
+        from campay.sdk import Client
+        campay = Client({
+            "app_username" : "PASTE YOUR APP_USERNAME HERE",
+            "app_password" : "PASTE YOUR APP_PASSWORD HERE",
+            "environment" : "PROD" #use "DEV" for demo mode or "PROD" for live mode
+        })
+  ```
---
+++ packages/campay/campay-1.1.0.tar.gz/campay-1.1.0/src/campay/sdk.py
@@ -0,0 +1,551 @@
+import requests
+import json
+import uuid
+import time
+
+import warnings
+warnings.filterwarnings('ignore', message='Unverified HTTPS request')
+
+class Client():
+
+	def __init__(self, kwargs):
+		self.app_username = kwargs["app_username"]
+		self.app_password = kwargs["app_password"]
+		self.environment = kwargs["environment"] #DEV, PROD
+		self.host = "https://demo.campay.net" if self.environment == 'DEV' else "https://www.campay.net"
+		self.debug = True if self.environment == 'DEV' else False
+
+		# print(kwargs)
+
+	def get_token(self):
+		#Get Access token
+
+		token_headers = {
+			"Content-Type":"application/json"
+		}
+		
+		data = {
+			"username":self.app_username,
+			"password":self.app_password
+		}
+
+		json_data = json.dumps(data)
+		
+		got_json_response = False
+		try:
+			token_response = requests.post(self.host+'/api/token/', data=json_data, headers=token_headers, verify=False, timeout=10)
+			json_token_response = token_response.json()
+			got_json_response = True
+		except:
+			pass
+
+		if got_json_response:
+		
+			token_response_response_status_code = int(token_response.status_code)
+
+			if token_response_response_status_code == 200:
+				token = json_token_response['token']
+				is_successful = True
+			else:
+				token = None
+				is_successful = False
+
+				if self.debug:
+					print(">>>>>>>>>>>>>>>>>>: ", json_token_response)
+			
+			return {"token":token, "is_successful":is_successful}
+
+		else:
+			return {"token":None, "is_successful":False}
+
+	
+
+	def collect(self, values):
+		if self.debug:
+			print(">>>>>>>>>>>>>>>>>>: ", "Collecting...")
+
+		token = self.get_token()["token"]
+		
+		if token:
+
+			#### Request collect
+			collect_data = {
+				"amount":str(values["amount"]),
+				"currency": str(values["currency"]),
+				"from":str(values["from"]),
+				"description":str(values["description"]),
+				"external_reference":str(values["external_reference"])
+			}
+			collect_payload = json.dumps(collect_data)
+			collect_headers = {
+				'Authorization': 'Token '+token,
+				'Content-Type': 'application/json'
+			}
+			
+			got_json_response = False
+			try:
+				collect_response = requests.post(self.host+'/api/collect/', data=collect_payload, headers=collect_headers, verify=False)
+				collect_response_json = collect_response.json()
+				got_json_response = True
+			except:
+				pass
+			
+			if got_json_response:
+				if self.debug:
+					print(">>>>>>>>>>>>>>>>>>: ", collect_response_json)
+					
+				collect_response_status_code = int(collect_response.status_code)
+				if collect_response_status_code == 200:
+					reference = collect_response_json['reference']
+					print(">>>>>>>>>>>>>>>>>>: Confirm on phone...")
+					is_successful = True
+				else:
+					reference = None
+					is_successful = False
+
+				if is_successful:
+					####Check Transaction Status
+					status_headers = {
+						'Authorization': 'Token '+token,
+						'Content-Type': 'application/json',
+					}
+					
+					status = "PENDING"
+					while status == "PENDING":
+						time.sleep(3)
+						
+						got_json_response = False
+						try:
+							status_response = requests.get(self.host+"/api/transaction/"+reference, headers=status_headers, verify=False)
+							status_response_json = status_response.json()
+							got_json_response = True
+						except:
+							pass
+						
+						if got_json_response:
+							status_response_status_code = int(status_response.status_code)
+							if status_response_status_code == 200:
+								status = status_response_json['status']
+								
+								if status != "PENDING":
+									if self.debug:
+										print(">>>>>>>>>>>>>>>>>>: ", status_response_json)
+									return status_response_json
+							else:
+								pass
+
+				else:
+					message = collect_response_json["message"]
+					if self.debug:
+						print(">>>>>>>>>>>>>>>>>>: ", message)
+					return {"status":"FAILED", "message": message}
+
+			else:
+				message = "Collect error"
+				if self.debug:
+					print(">>>>>>>>>>>>>>>>>>: ", message)
+				return {"status":"FAILED", "message":message}
+
+		else:
+			message = "Token error. Please check your App Username and Pass password. Also check your environment"
+			if self.debug:
+				print(">>>>>>>>>>>>>>>>>>: ", message)
+			return {"status":"FAILED", "message":message}
+
+
+
+	def initCollect(self, values):
+		if self.debug:
+			print(">>>>>>>>>>>>>>>>>>: ", "Collecting...")
+
+		token = self.get_token()["token"]
+		
+		if token:
+
+			#### Request collect
+			collect_data = {
+				"amount":str(values["amount"]),
+				"currency": str(values["currency"]),
+				"from":str(values["from"]),
+				"description":str(values["description"]),
+				"external_reference":str(values["external_reference"])
+			}
+			collect_payload = json.dumps(collect_data)
+			collect_headers = {
+				'Authorization': 'Token '+token,
+				'Content-Type': 'application/json'
+			}
+			
+			got_json_response = False
+			try:
+				collect_response = requests.post(self.host+'/api/collect/', data=collect_payload, headers=collect_headers, verify=False)
+				collect_response_json = collect_response.json()
+				got_json_response = True
+			except:
+				pass
+			
+			if got_json_response:
+				if self.debug:
+					print(">>>>>>>>>>>>>>>>>>: ", collect_response_json)
+					
+				collect_response_status_code = int(collect_response.status_code)
+				if collect_response_status_code == 200:
+					print(">>>>>>>>>>>>>>>>>>: Confirm on phone...")
+					return collect_response_json
+				else:
+
+					message = collect_response_json["message"]
+					if self.debug:
+						print(">>>>>>>>>>>>>>>>>>: ", message)
+
+					return {"status":"FAILED", "message": message}
+
+			else:
+				message = "Collect error"
+				if self.debug:
+					print(">>>>>>>>>>>>>>>>>>: ", message)
+				return {"status":"FAILED", "message":message}
+
+		else:
+			message = "Token error. Please check your App Username and Pass password. Also check your environment"
+			if self.debug:
+				print(">>>>>>>>>>>>>>>>>>: ", message)
+			return {"status":"FAILED", "message":message}
+
+
+
+
+
+	def disburse(self, values):
+		if self.debug:
+			print(">>>>>>>>>>>>>>>>>>: ", "Disbursing...")
+		token = self.get_token()["token"]
+		if token:
+
+			#### Request withdraw
+			withdraw_data = {
+				"amount":str(values["amount"]),
+				"currency": str(values["currency"]),
+				"to":str(values["to"]),
+				"description":str(values["description"]),
+				"external_reference":str(values["external_reference"])
+			}
+			withdraw_payload = json.dumps(withdraw_data)
+			withdraw_headers = {
+				'Authorization': 'Token '+token,
+				'Content-Type': 'application/json'
+			}
+			
+			got_json_response = False
+			try:
+				withdraw_response = requests.post(self.host+'/api/withdraw/', data=withdraw_payload, headers=withdraw_headers, verify=False)
+				withdraw_response_json = withdraw_response.json()
+				got_json_response = True
+			except:
+				pass
+			
+			if got_json_response:
+				if self.debug:
+					print(">>>>>>>>>>>>>>>>>>: ", withdraw_response_json)
+					
+				withdraw_response_status_code = int(withdraw_response.status_code)
+				if withdraw_response_status_code == 200:
+					reference = withdraw_response_json['reference']
+					is_successful = True
+				else:
+					reference = None
+					is_successful = False
+
+				if is_successful:
+					####Check Transaction Status
+					status_headers = {
+						'Authorization': 'Token '+token,
+						'Content-Type': 'application/json',
+					}
+					
+					status = "PENDING"
+					while status == "PENDING":
+						time.sleep(3)
+						
+						got_json_response = False
+						try:
+							status_response = requests.get(self.host+"/api/transaction/"+reference, headers=status_headers, verify=False)
+							status_response_json = status_response.json()
+							got_json_response = True
+						except:
+							pass
+						
+						if got_json_response:
+							status_response_status_code = int(status_response.status_code)
+							if status_response_status_code == 200:
+								status = status_response_json['status']
+								
+								if status != "PENDING":
+									if self.debug:
+										print(">>>>>>>>>>>>>>>>>>: ", status_response_json)
+									return status_response_json
+							else:
+								pass
+
+				else:
+					message = withdraw_response_json["message"]
+					if self.debug:
+						print(">>>>>>>>>>>>>>>>>>: ", message)
+					return {"status":"FAILED", "message": message}
+
+			else:
+				message = "Disburse error"
+				if self.debug:
+					print(">>>>>>>>>>>>>>>>>>: ", message)
+				return {"status":"FAILED", "message":message}
+
+		else:
+			message = "Token error. Please check your App Username and Pass password. Also check your environment"
+			if self.debug:
+				print(">>>>>>>>>>>>>>>>>>: ", message)
+			return {"status":"FAILED", "message":message}
+
+
+
+	def get_balance(self):
+		if self.debug:
+			print(">>>>>>>>>>>>>>>>>>: ", "Getting balance...")
+		token = self.get_token()["token"]
+		if token:
+			status_headers = {
+				'Authorization': 'Token '+token,
+				'Content-Type': 'application/json',
+			}
+							
+			got_json_response = False
+			try:
+				response = requests.get(self.host+"/api/balance/", headers=status_headers, verify=False)
+				response_json = response.json()
+				got_json_response = True
+			except:
+				pass
+
+			if got_json_response:
+				if self.debug:
+					print(">>>>>>>>>>>>>>>>>>: ", response_json)
+				return response_json
+				
+			else:
+				message = "Balance error"
+				if self.debug:
+					print(">>>>>>>>>>>>>>>>>>: ", message)
+				return {"status":"FAILED", "message":message}
+
+		else:
+			message = "Token error. Please check your App Username and Pass password. Also check your environment"
+			if self.debug:
+				print(">>>>>>>>>>>>>>>>>>: ", message)
+			return {"status":"FAILED", "message":message}
+
+
+
+	def get_payment_link(self, values):
+		if self.debug:
+			print(">>>>>>>>>>>>>>>>>>: ", "Getting payment link...")
+
+		token = self.get_token()["token"]
+		
+		if token:
+
+			#### Request collect
+			collect_data = {
+				"amount":str(values["amount"]),
+				"currency": str(values["currency"]),
+				"description":str(values["description"]),
+				"external_reference":str(values["external_reference"]),
+				"redirect_url":str(values["redirect_url"]),
+				#
+				"from":str(values["from"]),
+				"first_name":str(values["first_name"]),
+				"last_name":str(values["last_name"]),
+				"email":str(values["email"]),
+				"failure_redirect_url":str(values["failure_redirect_url"]),
+				"payment_options":str(values["payment_options"])
+			}
+			collect_payload = json.dumps(collect_data)
+			collect_headers = {
+				'Authorization': 'Token '+token,
+				'Content-Type': 'application/json'
+			}
+			
+			got_json_response = False
+			try:
+				collect_response = requests.post(self.host+'/api/get_payment_link/', data=collect_payload, headers=collect_headers, verify=False)
+				collect_response_json = collect_response.json()
+				got_json_response = True
+			except:
+				pass
+			
+			if got_json_response:
+				if self.debug:
+					print(">>>>>>>>>>>>>>>>>>: ", collect_response_json)
+					
+				collect_response_status_code = int(collect_response.status_code)
+				if collect_response_status_code == 200:
+					link = collect_response_json['link']
+					reference = collect_response_json['reference']
+					if self.debug:
+						print(">>>>>>>>>>>>>>>>>>: Redirect your customer to the payment link. Use the 'reference' to check the transaction status")
+					is_successful = True
+				else:
+					link = None
+					reference = None
+					is_successful = False
+
+				if is_successful:
+					####Check Transaction Status
+					return {"status":"SUCCESSFUL", "link": link, "reference": reference}
+
+				else:
+					message = collect_response_json["message"]
+					if self.debug:
+						print(">>>>>>>>>>>>>>>>>>: ", message)
+					return {"status":"FAILED", "message": message}
+
+			else:
+				message = "Collect error"
+				if self.debug:
+					print(">>>>>>>>>>>>>>>>>>: ", message)
+				return {"status":"FAILED", "message":message}
+
+		else:
+			message = "Token error. Please check your App Username and Pass password. Also check your environment"
+			if self.debug:
+				print(">>>>>>>>>>>>>>>>>>: ", message)
+			return {"status":"FAILED", "message":message}
+
+
+
+	# def transfer_airtime(self, values):
+	# 	if self.debug:
+	# 		print(">>>>>>>>>>>>>>>>>>: ", "Airtime Transfer...")
+	# 	token = self.get_token()["token"]
+	# 	if token:
+
+	# 		#### Request airtime
+	# 		airtime_data = {
+	# 			"amount":str(values["amount"]),
+	# 			"to":str(values["to"]),
+	# 			"external_reference":str(values["external_reference"])
+	# 		}
+	# 		airtime_payload = json.dumps(airtime_data)
+	# 		airtime_headers = {
+	# 			'Authorization': 'Token '+token,
+	# 			'Content-Type': 'application/json'
+	# 		}
+			
+	# 		got_json_response = False
+	# 		try:
+	# 			airtime_response = requests.post(self.host+'/api/utilities/airtime/transfer/', data=airtime_payload, headers=airtime_headers, verify=False)
+	# 			airtime_response_json = airtime_response.json()
+	# 			got_json_response = True
+	# 		except:
+	# 			pass
+			
+	# 		if got_json_response:
+	# 			if self.debug:
+	# 				print(">>>>>>>>>>>>>>>>>>: ", airtime_response_json)
+					
+	# 			airtime_response_status_code = int(airtime_response.status_code)
+	# 			if airtime_response_status_code == 200:
+	# 				reference = airtime_response_json['reference']
+	# 				is_successful = True
+	# 			else:
+	# 				reference = None
+	# 				is_successful = False
+
+	# 			if is_successful:
+	# 				####Check Transaction Status
+	# 				status_headers = {
+	# 					'Authorization': 'Token '+token,
+	# 					'Content-Type': 'application/json',
+	# 				}
+					
+	# 				status = "PENDING"
+	# 				while status == "PENDING":
+	# 					time.sleep(3)
+						
+	# 					got_json_response = False
+	# 					try:
+	# 						status_response = requests.get(self.host+"/api/utilities/transaction/"+reference+"/", headers=status_headers, verify=False)
+	# 						status_response_json = status_response.json()
+	# 						got_json_response = True
+	# 					except:
+	# 						pass
+						
+	# 					if got_json_response:
+	# 						status_response_status_code = int(status_response.status_code)
+	# 						if status_response_status_code == 200:
+	# 							status = status_response_json['status']
+								
+	# 							if status != "PENDING":
+	# 								if self.debug:
+	# 									print(">>>>>>>>>>>>>>>>>>: ", status_response_json)
+	# 								return status_response_json
+	# 						else:
+	# 							pass
+
+	# 			else:
+	# 				message = airtime_response_json["message"]
+	# 				if self.debug:
+	# 					print(">>>>>>>>>>>>>>>>>>: ", message)
+	# 				return {"status":"FAILED", "message": message}
+
+	# 		else:
+	# 			message = "Disburse error"
+	# 			if self.debug:
+	# 				print(">>>>>>>>>>>>>>>>>>: ", message)
+	# 			return {"status":"FAILED", "message":message}
+
+	# 	else:
+	# 		message = "Token error. Please check your App Username and Pass password. Also check your environment"
+	# 		if self.debug:
+	# 			print(">>>>>>>>>>>>>>>>>>: ", message)
+	# 		return {"status":"FAILED", "message":message}
+
+
+
+	def get_transaction_status(self, values):
+		if self.debug:
+			print(">>>>>>>>>>>>>>>>>>: ", "Getting Transaction status...")
+		token = self.get_token()["token"]
+		if token:
+			try:
+				reference = values["reference"]
+			except:
+				return {"status":"", "message":"Transaction Reference is required"}
+			
+			status_headers = {
+				'Authorization': 'Token '+token,
+				'Content-Type': 'application/json',
+			}
+							
+			got_json_response = False
+			try:
+				response = requests.get(self.host+f"/api/transaction/{reference}/", headers=status_headers, verify=False)
+				response_json = response.json()
+				got_json_response = True
+			except:
+				pass
+
+			if got_json_response:
+				if self.debug:
+					print(">>>>>>>>>>>>>>>>>>: ", response_json)
+				return response_json
+				
+			else:
+				message = "Request error"
+				if self.debug:
+					print(">>>>>>>>>>>>>>>>>>: ", message)
+				return {"status":"", "message":message}
+
+		else:
+			message = "Token error. Please check your App Username and Pass password. Also check your environment"
+			if self.debug:
+				print(">>>>>>>>>>>>>>>>>>: ", message)
+			return {"status":"", "message":message}
