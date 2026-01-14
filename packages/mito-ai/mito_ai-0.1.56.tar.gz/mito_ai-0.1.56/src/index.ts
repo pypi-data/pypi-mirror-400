1380c71105a6bae16eae148d15daa9b5e18d0069
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 00:08:37 +0000

    Add package mito-ai/0.1.56/mito_ai-0.1.56.tar.gz

---
+++ packages/mito-ai/mito_ai-0.1.56.tar.gz/mito_ai-0.1.56/src/index.ts
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) Saga Inc.
+ * Distributed under the terms of the GNU Affero General Public License v3.0 License.
+ */
+
+import AiChatPlugin from './Extensions/AiChat/AiChatPlugin';
+import ContextManagerPlugin from './Extensions/ContextManager/ContextManagerPlugin';
+import ErrorMimeRendererPlugin from './Extensions/ErrorMimeRenderer/ErrorMimeRendererPlugin';
+import ToolbarButtonsPlugin from './Extensions/ToolbarButtons/ToolbarButtonsPlugin';
+import AppDeployPlugin from './Extensions/AppDeploy/AppDeployPlugin';
+import StreamlitPreviewPlugin from './Extensions/AppPreview/StreamlitPreviewPlugin';
+import { emptyCellPlaceholder } from './Extensions/emptyCell/EmptyCellPlugin';
+import { statusItem } from './Extensions/status';
+import SettingsManagerPlugin from './Extensions/SettingsManager/SettingsManagerPlugin';
+import { versionCheckPlugin } from './Extensions/VersionCheck';
+import NotebookFooterPlugin from './Extensions/NotebookFooter';
+import mitoThemePlugin from './Extensions/MitoThemeLight';
+import ManageAppsPlugin from "./Extensions/AppManager/ManageAppsPlugin"
+
+// This is the main entry point to the mito-ai extension. It must export all of the top level
+// extensions that we want to load.
+export default [
+  AiChatPlugin,
+  ErrorMimeRendererPlugin,
+  ContextManagerPlugin,
+  AppDeployPlugin,
+  StreamlitPreviewPlugin,
+  ToolbarButtonsPlugin,
+  emptyCellPlaceholder,
+  statusItem,
+  SettingsManagerPlugin,
+  versionCheckPlugin,
+  NotebookFooterPlugin,
+  ManageAppsPlugin,
+  mitoThemePlugin
+];
