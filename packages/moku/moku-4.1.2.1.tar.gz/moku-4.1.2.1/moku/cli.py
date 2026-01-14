7fc978660ca0fdf86c6d88d32ee980f195b3286f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 03:36:05 +0000

    Add package moku/4.1.2.1/moku-4.1.2.1.tar.gz

---
+++ packages/moku/moku-4.1.2.1.tar.gz/moku-4.1.2.1/moku/cli.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+
+def main():
+    print(
+        "This has been replaced with the fully-featured Moku Command Line Utility `mokucli`. Please download from https://liquidinstruments.com/software/utilities/"
+    )
+
+
+# Compatible with direct run and distutils binary packaging
+if __name__ == "__main__":
+    main()
