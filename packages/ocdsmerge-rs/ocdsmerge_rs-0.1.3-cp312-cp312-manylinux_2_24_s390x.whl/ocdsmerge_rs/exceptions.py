7614f2aed06ab2a1f408f465bc11540bee7bfbf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 02:45:45 +0000

    Add package ocdsmerge-rs/0.1.3/ocdsmerge_rs-0.1.3-cp312-cp312-manylinux_2_24_s390x.whl

---
+++ packages/ocdsmerge-rs/ocdsmerge_rs-0.1.3-cp312-cp312-manylinux_2_24_s390x.whl/ocdsmerge_rs/exceptions.py
@@ -0,0 +1,25 @@
+from .ocdsmerge_rs import (
+    DuplicateIdValueWarning,
+    InconsistentTypeError,
+    MergeError,
+    MergeWarning,
+    MissingDateKeyError,
+    NonObjectReleaseError,
+    NonStringDateValueError,
+    NullDateValueError,
+    OutOfOrderReleaseError,
+    RepeatedDateValueWarning,
+)
+
+__all__ = [
+    "DuplicateIdValueWarning",
+    "InconsistentTypeError",
+    "MergeError",
+    "MergeWarning",
+    "MissingDateKeyError",
+    "NonObjectReleaseError",
+    "NonStringDateValueError",
+    "NullDateValueError",
+    "OutOfOrderReleaseError",
+    "RepeatedDateValueWarning",
+]
