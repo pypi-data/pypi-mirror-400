a327232747010d54e88a20e522747ce42bb957ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 00:09:00 +0000

    Add package torchvista/0.2.8/torchvista-0.2.8.tar.gz

---
+++ packages/torchvista/torchvista-0.2.8.tar.gz/torchvista-0.2.8/tests/examples_generator.py
@@ -0,0 +1,31 @@
+from .models import models
+from torchvista import tracer
+from pathlib import Path
+
+PROJECT_ROOT = Path(__file__).resolve().parents[1]
+DOCS_DIR = PROJECT_ROOT / "docs"
+
+
+def test_all_models():
+    for name, items in models.items():
+        model = items["model"]
+        example_input = items["example_input"]
+        code_contents = items["code_contents"]
+        kwargs = {}
+        if items["collapse_modules_after_depth"] >= 0:
+            kwargs["collapse_modules_after_depth"] = items["collapse_modules_after_depth"]
+        kwargs["show_non_gradient_nodes"] = items["show_non_gradient_nodes"]
+        kwargs["forced_module_tracing_depth"] = items["forced_module_tracing_depth"]
+        kwargs["show_module_attr_names"] = items["show_module_attr_names"]
+        kwargs["show_compressed_view"] = items["show_compressed_view"]
+        # error_contents = items["error_contents"]
+        print(f"Testing {name}...")
+        
+        graph_html, exception = tracer._get_demo_html_str(model, example_input, code_contents, **kwargs)
+        print('exception:', exception)
+        output_path = DOCS_DIR / f"{name}.html"
+        output_path.write_text(graph_html)
+        print(f"Saved output to {output_path}")
+
+if __name__ == "__main__":
+    test_all_models()
---
+++ packages/torchvista/torchvista-0.2.8.tar.gz/torchvista-0.2.8/tests/models/ComplexInputOutput.py
@@ -0,0 +1,65 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class DataHolder:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = [(y)]
+
+class MyModule(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.linear = nn.Linear(5, 12)
+
+    def forward(self, x):
+        a = x['tensor_1']
+        b = x['inner']['tensor_2']
+        c = (a * b) / a[0]
+        d = self.linear(F.relu(a) - b)
+        return DataHolder(c, d)
+        
+
+model = MyModule()
+example_input = {
+    'tensor_1': torch.randn(2, 5),
+    'inner': {
+        'tensor_2': torch.randn(2, 5)
+    }
+}
+
+code_contents = """\
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvista import trace_model
+
+class DataHolder:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = [(y)]
+
+class MyModule(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.linear = nn.Linear(5, 12)
+
+    def forward(self, x):
+        a = x['tensor_1']
+        b = x['inner']['tensor_2']
+        c = (a * b) / a[0]
+        d = self.linear(F.relu(a) - b)
+        return DataHolder(c, d)
+        
+
+model = MyModule()
+example_input = {
+    'tensor_1': torch.randn(2, 5),
+    'inner': {
+        'tensor_2': torch.randn(2, 5)
+    }
+}
+
+trace_model(model, example_input)
+
+"""
