6b84fe562d62b6760eed0f34fba893aecbfe6caf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 09:11:01 +0000

    Add package ix-notifiers/1.0.8/ix_notifiers-1.0.8-py3-none-any.whl

---
+++ packages/ix-notifiers/ix_notifiers-1.0.8-py3-none-any.whl/ix_notifiers/constants.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+""" Constants declarations """
+
+# These get overwritten at build time. See build.sh
+VERSION = '1.0.8'
+BUILD = '2252920518'
+NAME = 'ix-notifiers'
