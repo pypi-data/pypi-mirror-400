a743592afe2afd3f56ed96b3b7a86d8090ed4c09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 06:06:48 +0000

    Add package djangoldp-ds4go/1.0.3/djangoldp_ds4go-1.0.3.tar.gz

---
+++ packages/djangoldp-ds4go/djangoldp_ds4go-1.0.3.tar.gz/djangoldp_ds4go-1.0.3/djangoldp_ds4go/models/category.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from djangoldp import fields
+from djangoldp_i18n.views import I18nLDPViewSet
+
+from djangoldp_ds4go.models.__base_named_model import baseNamedModel
+
+
+class Category(baseNamedModel):
+    parent_category = fields.ForeignKey(
+        "self", on_delete=models.SET_NULL, blank=True, null=True
+    )
+
+    class Meta(baseNamedModel.Meta):
+        verbose_name = _("Category")
+        verbose_name_plural = _("Categories")
+        container_path = "categories"
+
+        rdf_type = "ds4go:Category"
+        serializer_fields = baseNamedModel.Meta.serializer_fields + [
+            "parent_category",
+        ]
+
+        view_set = I18nLDPViewSet
