e9e5fdbbc3687ffb21156315647dd499366c8145
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 13:20:32 +0000

    Add package funboost/53.7/funboost-53.7.tar.gz

---
+++ packages/funboost/funboost-53.7.tar.gz/funboost-53.7/funboost/core/helper_funs.py
@@ -0,0 +1,100 @@
+import copy
+import pytz
+import time
+import uuid
+from funboost.utils.uuid7 import uuid7
+import datetime
+from funboost.core.funboost_time import FunboostTime, fast_get_now_time_str
+
+
+def get_publish_time(paramsx: dict):
+    """
+    :param paramsx:
+    :return:
+    """
+    return paramsx.get('extra', {}).get('publish_time', None)
+
+
+def get_publish_time_format(paramsx: dict):
+    """
+    :param paramsx:
+    :return:
+    """
+    return paramsx.get('extra', {}).get('publish_time_format', None)
+
+
+def delete_keys_and_return_new_dict(dictx: dict, keys: list = None):
+    dict_new = copy.deepcopy(dictx)  # 主要是去掉一级键 publish_time，浅拷贝即可。新的消息已经不是这样了。
+    keys = ['publish_time', 'publish_time_format', 'extra'] if keys is None else keys
+    for dict_key in keys:
+        try:
+            dict_new.pop(dict_key)
+        except KeyError:
+            pass
+    return dict_new
+
+
+def block_python_main_thread_exit():
+    """
+
+    https://funboost.readthedocs.io/zh-cn/latest/articles/c10.html#runtimeerror-cannot-schedule-new-futures-after-interpreter-shutdown
+
+    主要是用于 python3.9以上 定时任务报错，  定时任务报错 RuntimeError: cannot schedule new futures after interpreter shutdown
+    如果主线程结束了，apscheduler就会报这个错，加上这个while 1 ： time.sleep(100) 目的就是阻止主线程退出。
+    """
+    while 1:
+        time.sleep(100)
+
+
+run_forever = block_python_main_thread_exit
+
+
+class MsgGenerater:
+    @staticmethod
+    def generate_task_id(queue_name:str) -> str:
+        """
+        UUIDv7 是 时间有序（time-ordered） 的 UUID，新一代 UUID 规范（RFC 9562，已标准化），
+        专门为数据库/分布式系统设计。一句话总结：
+        UUIDv7 = “像 UUID 一样全局唯一 + 像雪花 ID 一样按时间递增”
+        """
+        # return f'{queue_name}_result:{uuid.uuid4()}'
+        uuid7_obj =  uuid7()
+        return str(uuid7_obj) # uuid7 对数据库顺序更友好
+
+
+    @staticmethod
+    def generate_publish_time() -> float:
+        return round(time.time(),4)
+
+
+    @staticmethod
+    def generate_publish_time_format() -> str:
+        # return FunboostTime().get_str()  # 性能不好
+        # return get_now_time_str_by_tz()  # 2秒100万次
+        return fast_get_now_time_str() # 0.4秒100万次
+
+
+    @classmethod
+    def generate_pulish_time_and_task_id(cls,queue_name:str,task_id=None):
+        extra_params = {'task_id': task_id or cls.generate_task_id(queue_name), 
+                        'publish_time': cls.generate_publish_time(),  # 时间戳秒
+                        'publish_time_format': cls.generate_publish_time_format() # 时间字符串 例如 2025-12-25 10:00:00
+                        
+                        }
+        return extra_params
+
+
+
+if __name__ == '__main__':
+
+    from funboost import FunboostCommonConfig
+
+    print(FunboostTime())
+    for i in range(1000000):
+        # time.time()
+        MsgGenerater.generate_publish_time_format()
+        # FunboostTime().get_str()
+
+        # datetime.datetime.now(tz=pytz.timezone(FunboostCommonConfig.TIMEZONE)).strftime(FunboostTime.FORMATTER_DATETIME_NO_ZONE)
+
+    print(FunboostTime())
