72d5712b00908554c9892fcb0ac3738c37b89099
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 15:09:31 +0000

    Add package cnry-mcp-server-qdrant/0.0.2/cnry_mcp_server_qdrant-0.0.2.tar.gz

---
+++ packages/cnry-mcp-server-qdrant/cnry_mcp_server_qdrant-0.0.2.tar.gz/cnry_mcp_server_qdrant-0.0.2/src/mcp_server_qdrant/main.py
@@ -0,0 +1,33 @@
+import argparse
+
+
+def main():
+    """
+    Main entry point for the mcp-server-qdrant script defined
+    in pyproject.toml. It runs the MCP server with a specific transport
+    protocol.
+    """
+
+    # Parse the command-line arguments to determine the transport protocol.
+    parser = argparse.ArgumentParser(description="mcp-server-qdrant")
+    parser.add_argument(
+        "--transport",
+        choices=["stdio", "sse", "streamable-http"],
+        default="stdio",
+    )
+    parser.add_argument(
+        "--port",
+        default="8000",
+        type=int,
+    )
+    args = parser.parse_args()
+
+    # Import is done here to make sure environment variables are loaded
+    # only after we make the changes.
+    from mcp_server_qdrant.server import mcp
+
+    extra_args = dict()
+    if args.transport != "stdio":
+        extra_args = {"port": args.port}
+
+    mcp.run(transport=args.transport, **extra_args)
