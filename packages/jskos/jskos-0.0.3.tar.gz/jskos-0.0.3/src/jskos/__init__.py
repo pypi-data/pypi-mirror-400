66f31d96493deb11f415837c073962cea8abf43e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 12:07:40 +0000

    Add package jskos/0.0.3/jskos-0.0.3.tar.gz

---
+++ packages/jskos/jskos-0.0.3.tar.gz/jskos-0.0.3/src/jskos/__init__.py
@@ -0,0 +1,27 @@
+"""A data model for JSKOS."""
+
+from .api import (
+    KOS,
+    Concept,
+    ConceptBundle,
+    ConceptScheme,
+    Item,
+    Mapping,
+    ProcessedConcept,
+    ProcessedKOS,
+    Resource,
+    read,
+)
+
+__all__ = [
+    "KOS",
+    "Concept",
+    "ConceptBundle",
+    "ConceptScheme",
+    "Item",
+    "Mapping",
+    "ProcessedConcept",
+    "ProcessedKOS",
+    "Resource",
+    "read",
+]
