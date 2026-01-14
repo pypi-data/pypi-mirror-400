fca992d66b26646a04791aa03496f1f33b4feb71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 07:08:47 +0000

    Add package cyares/0.3.1/cyares-0.3.1.tar.gz

---
+++ packages/cyares/cyares-0.3.1.tar.gz/cyares-0.3.1/tests/test_utils.py
@@ -0,0 +1,16 @@
+from socket import htonl, htons
+
+from cyares.channel import __htonl, __htons  # type: ignore
+
+
+def test_cyares_htons() -> None:
+    x = __htons(0xF4F5)
+    assert hex(x) == hex(htons(0xF4F5))
+    assert __htons(0xFEED) == htons(0xFEED)
+    assert __htons(0xBEEF) == htons(0xBEEF)
+    assert __htons(0x1166) == htons(0x1166)
+
+
+def test_cyares_htonl() -> None:
+    assert __htonl(0xFEEDBEEF) == htonl(0xFEEDBEEF)
+    assert __htonl(0x11663322) == htonl(0x11663322)
