875cab3250a7799be31c3d27f603cfdb3e9c51b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 14:10:05 +0000

    Add package stjames/0.0.146/stjames-0.0.146.tar.gz

---
+++ packages/stjames/stjames-0.0.146.tar.gz/stjames-0.0.146/stjames/__init__.py
@@ -0,0 +1,25 @@
+# ruff: noqa: I001
+
+from .calculation import *
+from .atom import *
+from .periodic_cell import *
+from .molecule import *
+from .workflows import *
+
+from .scf_settings import *
+from .opt_settings import *
+from .compute_settings import *
+from .thermochem_settings import *
+from .settings import *
+
+from .method import *
+from .basis_set import *
+from .task import *
+from .correction import *
+from .solvent import *
+from .mode import *
+from .status import *
+from .constraint import *
+from .message import *
+from .types import *
+from .engine import *
---
+++ packages/stjames/stjames-0.0.146.tar.gz/stjames-0.0.146/stjames/solvent.py
@@ -0,0 +1,48 @@
+from .base import Base, LowercaseStrEnum
+
+
+class Solvent(LowercaseStrEnum):
+    WATER = "water"
+    NITROMETHANE = "nitromethane"
+    NITROBENZENE = "nitrobenzene"
+    TOLUENE = "toluene"
+    BENZENE = "benzene"
+    CHLOROBENZENE = "chlorobenzene"
+    CARBONTETRACHLORIDE = "carbontetrachloride"
+    DICHLOROETHANE = "dichloroethane"
+    DICHLOROMETHANE = "dichloromethane"
+    CHLOROFORM = "chloroform"
+    DIETHYLETHER = "diethylether"
+    DIISOPROPYLETHER = "diisopropylether"
+    DIMETHYLSULFOXIDE = "dimethylsulfoxide"
+    TETRAHYDROFURAN = "tetrahydrofuran"
+    CYCLOHEXANE = "cyclohexane"
+    OCTANE = "octane"
+    ACETICACID = "aceticacid"
+    HEXANE = "hexane"
+    ETHYLACETATE = "ethylacetate"
+    ACETONE = "acetone"
+    ACETONITRILE = "acetonitrile"
+    METHANOL = "methanol"
+    ETHANOL = "ethanol"
+    ISOPROPANOL = "isopropanol"
+    OCTANOL = "octanol"
+    DIMETHYLACETAMIDE = "dimethylacetamide"
+    DIMETHYLFORMAMIDE = "dimethylformamide"
+    N_METHYLPYRROLIDONE = "n_methylpyrrolidone"
+    ETHYLENE_GLYCOL = "ethylene_glycol"
+
+
+class SolventModel(LowercaseStrEnum):
+    PCM = "pcm"
+    CPCM = "cpcm"
+    ALPB = "alpb"
+    COSMO = "cosmo"
+    GBSA = "gbsa"
+    CPCMX = "cpcmx"
+    SMD = "SMD"
+
+
+class SolventSettings(Base):
+    solvent: Solvent
+    model: SolventModel
