461373ddbf7754e8bc12fdaf14708b787f389fd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 00:08:14 +0000

    Add package hydra-router/0.12.23/hydra_router-0.12.23.tar.gz

---
+++ packages/hydra-router/hydra_router-0.12.23.tar.gz/hydra_router-0.12.23/hydra_router/__init__.py
@@ -0,0 +1,20 @@
+# hydra_router/__init__.py
+#
+#   Hydra Router
+#    Author: Nadim-Daniel Ghaznavi
+#    Copyright: (c) 2025-2026 Nadim-Daniel Ghaznavi
+#    GitHub: https://github.com/NadimGhaznavi/hydra_router
+#    Website: https://hydra-router.readthedocs.io/en/latest
+#    License: GPL 3.0
+
+"""
+Hydra Router - ZeroMQ-based client-server communication framework.
+
+This package provides HydraServer and HydraClient classes for simple
+REQ/REP messaging patterns using ZeroMQ.
+"""
+
+__version__ = "0.12.23"
+
+# Classes will be available after implementation
+__all__ = ["HydraServer", "HydraClient"]
