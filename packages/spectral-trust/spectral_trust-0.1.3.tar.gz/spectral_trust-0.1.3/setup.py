323c78dbafe92a6d72bae0bc470c251d383d77f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 00:08:53 +0000

    Add package spectral-trust/0.1.3/spectral_trust-0.1.3.tar.gz

---
+++ packages/spectral-trust/spectral_trust-0.1.3.tar.gz/spectral_trust-0.1.3/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / "README.md").read_text(encoding="utf-8")
+
+setup(
+    name="spectral_trust",
+    version="0.1.3",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    install_requires=[
+        "numpy",
+        "pandas",
+        "matplotlib",
+        "seaborn",
+        "scipy",
+        "torch>=2.0.0",
+        "transformers>=4.30.0",
+        "tqdm",
+        "accelerate"
+    ],
+    entry_points={
+        "console_scripts": [
+            "gsp-cli=spectral_trust.cli:main",
+        ],
+    },
+    author="Valentin Noël",
+    author_email="val.noel@proton.me",
+    description="Spectral diagnostics for trust in LLMs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    python_requires=">=3.8",
+)
