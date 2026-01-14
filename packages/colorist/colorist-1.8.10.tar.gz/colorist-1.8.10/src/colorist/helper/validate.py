70daf4f896f3b2349efe45cdff411f58ccb3e47d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 05:09:42 +0000

    Add package colorist/1.8.10/colorist-1.8.10.tar.gz

---
+++ packages/colorist/colorist-1.8.10.tar.gz/colorist-1.8.10/src/colorist/helper/validate.py
@@ -0,0 +1,26 @@
+# Copyright 2022 – present, Jakob Bagterp. BSD 3-Clause license and refer to LICENSE file.
+
+import re
+
+
+def is_valid_vga_value(value: int) -> bool:
+    return 0 <= value <= 255
+
+
+def is_valid_rgb_value(value: int) -> bool:
+    return 0 <= value <= 255
+
+
+def is_valid_hsl_hue(hue: float) -> bool:
+    return 0 <= hue <= 360
+
+
+def is_valid_percentage(value: float) -> bool:
+    return 0 <= value <= 100
+
+
+HEX_SYNTAX_PATTERN = re.compile(r"^#?([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$")
+
+
+def is_valid_hex_value(hex: str) -> bool:
+    return bool(HEX_SYNTAX_PATTERN.match(hex))
