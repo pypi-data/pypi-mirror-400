9ec4cf536eac6d73713700c6ff9b9764882342d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 00:08:31 +0000

    Add package pollistack/0.1.2/pollistack-0.1.2.tar.gz

---
+++ packages/pollistack/pollistack-0.1.2.tar.gz/pollistack-0.1.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pollistack",
+    version="0.1.2",
+    packages=find_packages(),
+    install_requires=[
+        "httpx>=0.23.0",
+    ],
+    author="PolliStack Team",
+    description="Python SDK for PolliStack Agent Engine",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/pollistack/pollistack",
+    python_requires=">=3.7",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
