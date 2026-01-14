9836d9aed4e291e988e0224c189e61e53099818b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 07:09:17 +0000

    Add package celery-types-ng/0.25.5/celery_types_ng-0.25.5-py3-none-any.whl

---
+++ packages/celery-types-ng/celery_types_ng-0.25.5-py3-none-any.whl/celery-stubs/worker/consumer/heart.pyi
@@ -0,0 +1,22 @@
+from typing import Any, ClassVar
+
+from celery.bootsteps import Step
+
+__all__ = ("Heart",)
+
+class Heart(Step):
+    name: ClassVar[str]  # pyright: ignore[reportIncompatibleVariableOverride]
+
+    def __init__(
+        self,
+        c: Any,
+        without_heartbeat: bool = False,
+        heartbeat_interval: float | None = None,
+        **kwargs: Any,
+    ) -> None: ...
+    def create(self, parent: Any) -> Any: ...
+    def start(self, c: Any) -> None: ...
+    def stop(self, c: Any) -> None: ...
+    def shutdown(self, c: Any) -> None: ...
+    def include_if(self, parent: Any) -> bool: ...
+    def info(self, obj: Any) -> dict[str, Any]: ...  # type: ignore[override]  # pyright: ignore[reportIncompatibleMethodOverride]
