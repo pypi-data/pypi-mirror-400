368c94089078797fcf40e7183260009fd16bc44a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 08:06:37 +0000

    Add package clacks-gnu-middleware/0.0.1/clacks_gnu_middleware-0.0.1-py3-none-any.whl

---
+++ packages/clacks-gnu-middleware/clacks_gnu_middleware-0.0.1-py3-none-any.whl/clacks_gnu_middleware/__init__.py
@@ -0,0 +1,9 @@
+"""
+clacks_gnu_middleware
+A FastAPI middleware to handle the GNU Headers (Memorial Headers) of the Clacks (Going Postal Terry Pratchett) for "sending people home".
+"""
+
+__version__ = "0.0.1"
+__author__ = "Tobias Heimig-Elschner <Tobias@heimig.de>"
+__license__ = "GPL-3.0-or-later"
+__url__ = "https://gitlab.com/THE-Git/clacks-gnu-middleware/"
