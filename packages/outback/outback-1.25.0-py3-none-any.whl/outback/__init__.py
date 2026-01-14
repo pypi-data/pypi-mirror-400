682502ee9550dbedaf78da4bed19fb5493832c17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 01:35:08 +0000

    Add package outback/1.25.0/outback-1.25.0-py3-none-any.whl

---
+++ packages/outback/outback-1.25.0-py3-none-any.whl/outback/__init__.py
@@ -0,0 +1,8 @@
+"""Outback - Python utilities for .NET package dependency analysis and management."""
+
+try:
+    from .__version__ import __version__
+except ImportError:
+    __version__ = "0.0.0.dev0"
+
+__all__ = []
