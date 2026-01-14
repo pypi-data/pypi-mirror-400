cfa11457b4eb77cc1a2fe10a3bbabe0287b50850
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 09:09:51 +0000

    Add package PyKLU/0.1.1/pyklu-0.1.1-cp314-cp314-win_amd64.whl

---
+++ packages/PyKLU/pyklu-0.1.1-cp314-cp314-win_amd64.whl/PyKLU/klu_interf.h
@@ -0,0 +1,26 @@
+// PyKLU/klu_interf.h
+// PyKLU – Python bindings for SuiteSparse KLU
+// Copyright (C) 2015-2025 CERN
+// Licensed under the LGPL-2.1-or-later. See LICENSE for details.
+
+#ifndef __KLUINTERF
+#define __KLUINTERF
+
+#include <stdio.h>
+#include "klu.h"
+
+typedef struct{
+	int m;
+	klu_common Common;
+	klu_symbolic *Symbolic ;
+    klu_numeric *Numeric ;
+} lu_state;
+
+lu_state* construct_superlu(int m, int n, int nnz, double* Acsc_data_ptr, 
+		int* Acsc_indices_ptr, int* Acsc_indptr_ptr);
+
+void lusolve(lu_state* lus, double* BX, int nrhs);
+
+void lu_destroy(lu_state* lus);
+
+#endif
