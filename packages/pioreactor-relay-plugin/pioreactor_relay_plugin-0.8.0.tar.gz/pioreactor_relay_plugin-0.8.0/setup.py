37802005b2d94d30856fbf1e85be88cedb826c96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 15:10:01 +0000

    Add package pioreactor-relay-plugin/0.8.0/pioreactor_relay_plugin-0.8.0.tar.gz

---
+++ packages/pioreactor-relay-plugin/pioreactor_relay_plugin-0.8.0.tar.gz/pioreactor_relay_plugin-0.8.0/setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import annotations
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+setup(
+    name="pioreactor-relay-plugin",
+    version="0.8.0",
+    license="MIT",
+    description="Turn the PWM channels into a simple on/off relay for additional hardware.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author_email="cam@pioreactor.com",
+    author="Kelly Tran, Pioreactor",
+    url="https://github.com/CamDavidsonPilon/pioreactor-relay-plugin",
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points={"pioreactor.plugins": "pioreactor_relay_plugin = pioreactor_relay_plugin"},
+)
