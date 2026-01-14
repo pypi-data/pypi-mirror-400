711ceb8462832b05d72caf6aa62e9cc0199624f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 16:07:05 +0000

    Add package benchling-api-client/2.0.415/benchling_api_client-2.0.415-py3-none-any.whl

---
+++ packages/benchling-api-client/benchling_api_client-2.0.415-py3-none-any.whl/benchling_api_client/v2/beta/models/benchling_app_manifest_feature_canvas_locations.py
@@ -0,0 +1,23 @@
+from enum import Enum
+from functools import lru_cache
+from typing import cast
+
+from ..extensions import Enums
+
+
+class BenchlingAppManifestFeatureCanvasLocations(Enums.KnownString):
+    ENTRY = "ENTRY"
+    ENTRY_TEMPLATE = "ENTRY_TEMPLATE"
+
+    def __str__(self) -> str:
+        return str(self.value)
+
+    @staticmethod
+    @lru_cache(maxsize=None)
+    def of_unknown(val: str) -> "BenchlingAppManifestFeatureCanvasLocations":
+        if not isinstance(val, str):
+            raise ValueError(
+                f"Value of BenchlingAppManifestFeatureCanvasLocations must be a string (encountered: {val})"
+            )
+        newcls = Enum("BenchlingAppManifestFeatureCanvasLocations", {"_UNKNOWN": val}, type=Enums.UnknownString)  # type: ignore
+        return cast(BenchlingAppManifestFeatureCanvasLocations, getattr(newcls, "_UNKNOWN"))
