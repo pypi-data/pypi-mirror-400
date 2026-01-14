8eb04d26a99d562e27a5437e5e0c4c6619563165
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 03:35:39 +0000

    Add package Geode-Explicit/6.7.0/geode_explicit-6.7.0-cp311-cp311-win_amd64.whl

---
+++ packages/Geode-Explicit/geode_explicit-6.7.0-cp311-cp311-win_amd64.whl/geode_explicit/section_explicit.pyi
@@ -0,0 +1,8 @@
+from __future__ import annotations
+import geode_background as geode_background
+import geode_common as geode_common
+import geode_conversion as geode_conversion
+from geode_explicit.bin.geode_explicit_py_section import ExplicitSectionLibrary
+from geode_explicit.bin.geode_explicit_py_section import SectionExplicitModeler
+import opengeode as opengeode
+__all__: list[str] = ['ExplicitSectionLibrary', 'SectionExplicitModeler', 'geode_background', 'geode_common', 'geode_conversion', 'opengeode']
