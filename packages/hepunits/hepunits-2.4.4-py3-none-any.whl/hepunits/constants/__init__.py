4cee5d7634b0d5732772ced2f8ac326a658b2f49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 13:20:50 +0000

    Add package hepunits/2.4.4/hepunits-2.4.4-py3-none-any.whl

---
+++ packages/hepunits/hepunits-2.4.4-py3-none-any.whl/hepunits/constants/__init__.py
@@ -0,0 +1,59 @@
+# Licensed under a 3-clause BSD style license, see LICENSE.
+
+from . import constants
+from .constants import (
+    Avogadro,
+    c_light,
+    c_light_sq,
+    e_SI,
+    e_sq,
+    electronvolt,
+    eminus,
+    eplus,
+    h_Planck,
+    half_pi,
+    hbar,
+    hbar_Planck,
+    hbarc,
+    hbarc_sq,
+    joule,
+    k_Boltzmann,
+    kelvin,
+    m,
+    mole,
+    pi,
+    pi_sq,
+    s,
+    two_pi,
+)
+
+__all__ = (
+    "Avogadro",
+    "c_light",
+    "c_light_sq",
+    "constants",
+    "e_SI",
+    "e_sq",
+    "electronvolt",
+    "eminus",
+    "eplus",
+    "h_Planck",
+    "half_pi",
+    "hbar",
+    "hbar_Planck",
+    "hbarc",
+    "hbarc_sq",
+    "joule",
+    "k_Boltzmann",
+    "kelvin",
+    "m",
+    "mole",
+    "pi",
+    "pi_sq",
+    "s",
+    "two_pi",
+)
+
+
+def __dir__() -> list[str]:
+    return list(__all__)
