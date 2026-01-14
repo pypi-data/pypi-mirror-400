8d1dcfe3d046d470057cf53570227ec9b336e6e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 13:20:44 +0000

    Add package ultrasound-rawdata-exchange/1.3.2/ultrasound_rawdata_exchange-1.3.2-cp312-cp312-win_amd64.whl

---
+++ packages/ultrasound-rawdata-exchange/ultrasound_rawdata_exchange-1.3.2-cp312-cp312-win_amd64.whl/ultrasound_rawdata_exchange/lib/cmake/Urx-1.3.2_static/UrxTargets-release.cmake
@@ -0,0 +1,19 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "Urx::UrxBindingsPython" for configuration "Release"
+set_property(TARGET Urx::UrxBindingsPython APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(Urx::UrxBindingsPython PROPERTIES
+  IMPORTED_COMMON_LANGUAGE_RUNTIME_RELEASE ""
+  IMPORTED_LOCATION_RELEASE "D:/CI/builds/common/sw/urx/build/lib.win-amd64-cpython-312/ultrasound_rawdata_exchange/bindings.cp312-win_amd64.pyd"
+  )
+
+list(APPEND _cmake_import_check_targets Urx::UrxBindingsPython )
+list(APPEND _cmake_import_check_files_for_Urx::UrxBindingsPython "D:/CI/builds/common/sw/urx/build/lib.win-amd64-cpython-312/ultrasound_rawdata_exchange/bindings.cp312-win_amd64.pyd" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
