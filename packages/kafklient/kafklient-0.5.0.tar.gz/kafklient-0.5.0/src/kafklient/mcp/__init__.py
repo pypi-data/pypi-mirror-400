6cbf6345576224655b11ea9213e5edc6c09ec971
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 04:25:03 +0000

    Add package kafklient/0.5.0/kafklient-0.5.0.tar.gz

---
+++ packages/kafklient/kafklient-0.5.0.tar.gz/kafklient-0.5.0/src/kafklient/mcp/__init__.py
@@ -0,0 +1,13 @@
+from .client import kafka_client_transport
+from .server import Server, kafka_server_transport, run_server, run_server_async
+from .session import inprocess_client_session, kafka_client_session
+
+__all__ = [
+    "kafka_client_transport",
+    "kafka_client_session",
+    "inprocess_client_session",
+    "kafka_server_transport",
+    "run_server_async",
+    "run_server",
+    "Server",
+]
