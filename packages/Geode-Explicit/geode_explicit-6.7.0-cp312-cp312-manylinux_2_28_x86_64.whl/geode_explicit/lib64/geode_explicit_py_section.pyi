9af5e99e2dc023a476e8c575ea9bb54134b0c1a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 03:35:36 +0000

    Add package Geode-Explicit/6.7.0/geode_explicit-6.7.0-cp312-cp312-manylinux_2_28_x86_64.whl

---
+++ packages/Geode-Explicit/geode_explicit-6.7.0-cp312-cp312-manylinux_2_28_x86_64.whl/geode_explicit/lib64/geode_explicit_py_section.pyi
@@ -0,0 +1,20 @@
+"""
+Geode-Explicit Python binding for Section
+"""
+from __future__ import annotations
+import opengeode.lib64.opengeode_py_mesh
+import opengeode.lib64.opengeode_py_model
+__all__: list[str] = ['ExplicitSectionLibrary', 'SectionExplicitModeler']
+class ExplicitSectionLibrary:
+    @staticmethod
+    def initialize() -> None:
+        ...
+class SectionExplicitModeler:
+    def __init__(self) -> None:
+        ...
+    def add_curve(self, arg0: opengeode.lib64.opengeode_py_mesh.EdgedCurve2D) -> None:
+        ...
+    def add_section(self, arg0: opengeode.lib64.opengeode_py_model.Section) -> None:
+        ...
+    def build(self) -> opengeode.lib64.opengeode_py_model.Section:
+        ...
