2c429d00b128ede2e48a772a5ece3766a64e350f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 02:44:35 +0000

    Add package epicscorelibs/7.0.10.99.0.0/epicscorelibs-7.0.10.99.0.0-cp314-cp314-manylinux_2_28_x86_64.whl

---
+++ packages/epicscorelibs/epicscorelibs-7.0.10.99.0.0-cp314-cp314-manylinux_2_28_x86_64.whl/epicscorelibs/_config.py
@@ -0,0 +1,20 @@
+# Generated file
+from collections import OrderedDict
+toolchain_macros = OrderedDict([('__cplusplus', '201703L'),
+             ('__GNUC__', '14'),
+             ('__GNUC_MINOR__', '2'),
+             ('__GNUC_PATCHLEVEL__', '1'),
+             ('__clang__', None),
+             ('__clang_version__', None),
+             ('_MSC_VER', None),
+             ('_MSC_FULL_VER', None),
+             ('__GLIBC__', '2'),
+             ('__GLIBCXX__', '20250110'),
+             ('_GLIBCXX_USE_CXX11_ABI', '1'),
+             ('__UCLIBC__', None),
+             ('__UCLIBC_MAJOR__', None),
+             ('__UCLIBC_MINOR__', None),
+             ('__UCLIBC_SUBLEVEL__', None),
+             ('_CPPLIB_VER', None),
+             ('_LIBCPP_VERSION', None)])
+cxxdefs = [('_GLIBCXX_USE_CXX11_ABI', '1')]
