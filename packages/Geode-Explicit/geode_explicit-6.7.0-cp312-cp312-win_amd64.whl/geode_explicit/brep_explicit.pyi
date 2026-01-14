f0e8ef9660338f600a6daaf1820d4ebb0d8c0914
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 03:35:38 +0000

    Add package Geode-Explicit/6.7.0/geode_explicit-6.7.0-cp312-cp312-win_amd64.whl

---
+++ packages/Geode-Explicit/geode_explicit-6.7.0-cp312-cp312-win_amd64.whl/geode_explicit/brep_explicit.pyi
@@ -0,0 +1,10 @@
+from __future__ import annotations
+import geode_background as geode_background
+import geode_common as geode_common
+import geode_conversion as geode_conversion
+from geode_explicit.bin.geode_explicit_py_brep import BRepExplicitModeler
+from geode_explicit.bin.geode_explicit_py_brep import BRepVolumicInserter
+from geode_explicit.bin.geode_explicit_py_brep import ExplicitBRepLibrary
+import opengeode as opengeode
+import opengeode_inspector as opengeode_inspector
+__all__: list[str] = ['BRepExplicitModeler', 'BRepVolumicInserter', 'ExplicitBRepLibrary', 'geode_background', 'geode_common', 'geode_conversion', 'opengeode', 'opengeode_inspector']
