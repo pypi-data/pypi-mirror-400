1a2ddde19a0b046bf80d774acaaaeaf4119c8a61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 09:09:43 +0000

    Add package OpenGeode-core/15.30.13/opengeode_core-15.30.13-cp39-cp39-win_amd64.whl

---
+++ packages/OpenGeode-core/opengeode_core-15.30.13-cp39-cp39-win_amd64.whl/opengeode/gdal.pyi
@@ -0,0 +1,4 @@
+from __future__ import annotations
+from opengeode.bin.opengeode_py_gdal import initialize_gdal
+from opengeode.bin.opengeode_py_gdal import ostream_redirect
+__all__: list[str] = ['initialize_gdal', 'ostream_redirect']
