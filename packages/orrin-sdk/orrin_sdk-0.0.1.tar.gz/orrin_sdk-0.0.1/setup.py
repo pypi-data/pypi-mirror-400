ee9d76b0130eedc529a500cf02ab69b067ebccd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 05:09:49 +0000

    Add package orrin-sdk/0.0.1/orrin_sdk-0.0.1.tar.gz

---
+++ packages/orrin-sdk/orrin_sdk-0.0.1.tar.gz/orrin_sdk-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="orrin-sdk",
+    version="0.0.1",
+    description="Orrin SDK for developers",
+    packages=find_packages(),
+    python_requires=">=3.9",
+)
