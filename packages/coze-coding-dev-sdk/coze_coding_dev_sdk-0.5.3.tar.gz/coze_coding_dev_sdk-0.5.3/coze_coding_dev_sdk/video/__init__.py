53bb1cd20b4237a7c077a164c06a5ca04ea03d64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 14:09:57 +0000

    Add package coze-coding-dev-sdk/0.5.3/coze_coding_dev_sdk-0.5.3.tar.gz

---
+++ packages/coze-coding-dev-sdk/coze_coding_dev_sdk-0.5.3.tar.gz/coze_coding_dev_sdk-0.5.3/coze_coding_dev_sdk/video/__init__.py
@@ -0,0 +1,19 @@
+from .client import VideoGenerationClient
+from .models import (
+    VideoConfig,
+    VideoGenerationRequest,
+    VideoGenerationTask,
+    TextContent,
+    ImageURLContent,
+    ImageURL
+)
+
+__all__ = [
+    "VideoGenerationClient",
+    "VideoConfig",
+    "VideoGenerationRequest",
+    "VideoGenerationTask",
+    "TextContent",
+    "ImageURLContent",
+    "ImageURL"
+]
