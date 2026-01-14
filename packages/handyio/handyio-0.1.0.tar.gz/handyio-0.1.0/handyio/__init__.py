b20cc3230a20ae49db587808bd7ca5bbf551aa1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 09:10:12 +0000

    Add package handyio/0.1.0/handyio-0.1.0.tar.gz

---
+++ packages/handyio/handyio-0.1.0.tar.gz/handyio-0.1.0/handyio/__init__.py
@@ -0,0 +1,13 @@
+from .io_utils import (
+    save_list_to_txt,
+    load_list_from_txt,
+    save_dict_to_txt,
+    load_dict_from_txt,
+)
+
+__all__ = [
+    "save_list_to_txt",
+    "load_list_from_txt",
+    "save_dict_to_txt",
+    "load_dict_from_txt",
+]
