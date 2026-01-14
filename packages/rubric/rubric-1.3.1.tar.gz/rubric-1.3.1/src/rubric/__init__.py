0cfd1d759776d588c4f901724e5147e6515169d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 02:45:18 +0000

    Add package rubric/1.3.1/rubric-1.3.1.tar.gz

---
+++ packages/rubric/rubric-1.3.1.tar.gz/rubric-1.3.1/src/rubric/__init__.py
@@ -0,0 +1,36 @@
+from rubric.rubric import Rubric
+from rubric.types import (
+    CountFn,
+    Criterion,
+    CriterionReport,
+    EvaluationReport,
+    LengthPenalty,
+    PenaltyType,
+    ThinkingOutputDict,
+    ToGradeInput,
+)
+from rubric.utils import (
+    compute_length_penalty,
+    normalize_to_grade_input,
+    parse_thinking_output,
+    word_count,
+)
+
+__version__ = "1.3.1"
+__all__ = [
+    "CountFn",
+    "Criterion",
+    "CriterionReport",
+    "EvaluationReport",
+    "LengthPenalty",
+    "PenaltyType",
+    "Rubric",
+    "ThinkingOutputDict",
+    "ToGradeInput",
+    "compute_length_penalty",
+    "normalize_to_grade_input",
+    "parse_thinking_output",
+    "word_count",
+]
+__name__ = "rubric"
+__author__ = "The LLM Data Company"
