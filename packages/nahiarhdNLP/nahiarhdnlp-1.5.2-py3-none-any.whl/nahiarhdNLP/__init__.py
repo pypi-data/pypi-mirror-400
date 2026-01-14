25098ead92029ec6e99c6394c8a5efafc2261d47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 03:35:40 +0000

    Add package nahiarhdNLP/1.5.2/nahiarhdnlp-1.5.2-py3-none-any.whl

---
+++ packages/nahiarhdNLP/nahiarhdnlp-1.5.2-py3-none-any.whl/nahiarhdNLP/__init__.py
@@ -0,0 +1,14 @@
+"""
+nahiarhdNLP - Advanced Indonesian Natural Language Processing Library
+"""
+
+# Import main modules
+from . import datasets, preprocessing
+
+# Version info
+__version__ = "1.5.2"
+__author__ = "Raihan Hidayatullah Djunaedi"
+__email__ = "raihanhd.dev@gmail.com"
+
+# Export main modules
+__all__ = ["preprocessing", "datasets"]
