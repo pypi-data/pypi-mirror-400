1ff769408fd4ffe953e3aa44d2ac6e0a231ceae5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 03:34:55 +0000

    Add package diverse-seq/2026.1.7/diverse_seq-2026.1.7-cp314-cp314-win_amd64.whl

---
+++ packages/diverse-seq/diverse_seq-2026.1.7-cp314-cp314-win_amd64.whl/diverse_seq/__init__.py
@@ -0,0 +1,21 @@
+"""diverse_seq: a tool for sampling diverse biological sequences"""
+
+import typing
+import warnings
+
+warnings.filterwarnings("ignore", message='.+Cannot register "UNREGISTER".+')
+
+if typing.TYPE_CHECKING:
+    from cogent3.core.alignment import SequenceCollection
+
+__version__ = "2026.1.7"
+
+
+def load_sample_data() -> "SequenceCollection":
+    """load sample data"""
+    from cogent3 import load_aligned_seqs  # noqa: PLC0415
+
+    from .util import get_sample_data_path  # noqa: PLC0415
+
+    path = get_sample_data_path()
+    return load_aligned_seqs(path, moltype="dna").degap()
