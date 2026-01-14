6747aaa72e6f2c4f459807d2ad65ce5449df75e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 14:09:58 +0000

    Add package empirica/1.3.0/empirica-1.3.0.tar.gz

---
+++ packages/empirica/empirica-1.3.0.tar.gz/empirica-1.3.0/empirica/investigation/__init__.py
@@ -0,0 +1,9 @@
+"""
+Investigation Module
+
+Provides pluggable investigation capabilities for Empirica CASCADE.
+"""
+
+from .investigation_plugin import InvestigationPlugin
+
+__all__ = ['InvestigationPlugin']
