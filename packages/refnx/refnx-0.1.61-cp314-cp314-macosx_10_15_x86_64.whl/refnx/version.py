c1b563062e4aeec10f6145f9c10a19faf312cf6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 02:44:33 +0000

    Add package refnx/0.1.61/refnx-0.1.61-cp314-cp314-macosx_10_15_x86_64.whl

---
+++ packages/refnx/refnx-0.1.61-cp314-cp314-macosx_10_15_x86_64.whl/refnx/version.py
@@ -0,0 +1,9 @@
+
+# THIS FILE IS GENERATED FROM REFNX SETUP.PY
+short_version = '0.1.61'
+version = '0.1.61'
+full_version = '0.1.61'
+git_revision = '2bc276a5938f845cb6b03de9c9331017ad7a3b1d'
+release = True
+if not release:
+    version = full_version
