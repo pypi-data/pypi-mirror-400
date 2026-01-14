60a75cba70fedd667e01820f9f204ae4ffb6c55e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 03:34:52 +0000

    Add package chaine/4.0.0b4/chaine-4.0.0b4-cp313-cp313-win32.whl

---
+++ packages/chaine/chaine-4.0.0b4-cp313-cp313-win32.whl/chaine/optimization/__init__.py
@@ -0,0 +1,10 @@
+from chaine.optimization import utils
+from chaine.optimization.spaces import (
+    APSearchSpace,
+    AROWSearchSpace,
+    L2SGDSearchSpace,
+    LBFGSSearchSpace,
+    PASearchSpace,
+    SearchSpace,
+)
+from chaine.optimization.trial import OptimizationTrial
