e3209bc3cf7089abb0b9a1b3f31eb61ccf4d95c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 05:10:52 +0000

    Add package pypas-cli/0.5.4/pypas_cli-0.5.4.tar.gz

---
+++ packages/pypas-cli/pypas_cli-0.5.4.tar.gz/pypas_cli-0.5.4/src/pypas/main.py
@@ -0,0 +1,240 @@
+from pathlib import Path
+from typing import List
+
+import typer
+from rich.prompt import Confirm
+
+from pypas import Config, Exercise, User, console, sysutils
+from pypas.lib.decorators import (
+    auth_required,
+    check_exercise_version,
+    check_pypas_version,
+    inside_exercise,
+)
+
+app = typer.Typer(
+    add_completion=False,
+    help='pypas ⚘ Python Practical Assignments',
+    pretty_exceptions_enable=False,
+)
+
+
+# https://peq.es/8ff0b0
+@app.callback(invoke_without_command=True)
+def default(
+    ctx: typer.Context,
+    version: bool = typer.Option(
+        False,
+        '--version',
+        show_default=False,
+        help='Show pypas-cli installed version.',
+    ),
+):
+    if version:
+        sysutils.handle_package_version()
+        sysutils.show_package_info()
+    # https://typer.tiangolo.com/tutorial/commands/context/#exclusive-executable-callback
+    elif ctx.invoked_subcommand is None:
+        typer.echo(ctx.get_help())
+
+
+@app.command()
+@check_pypas_version
+def get(
+    exercise_slug: str = typer.Argument(help='Slug of exercise'),
+    public: bool = typer.Option(False, '--public', '-p', help='Get public exercise'),
+):
+    """Get (download) exercise."""
+    if (exercise := Exercise(exercise_slug)).folder_exists():
+        console.warning(f'Folder ./{exercise.folder} already exists!')
+        console.info(
+            '[italic]If continue, files coming from server will [red]OVERWRITE[/red] your existing files'
+        )
+        if not Confirm.ask('Continue', default=False):
+            return
+    if Config.local_config_exists():
+        console.warning('Current folder seems to be a pypas exercise.')
+        console.info(
+            '[italic]If continue, files coming from server will [red]MESS[/red] your existing files'
+        )
+        if not Confirm.ask('Continue', default=False):
+            return
+    config = Config()
+    if exercise.download(public or config.get('token')):  # type: ignore
+        exercise.unzip()
+        console.info(f'Exercise is available at [note]./{exercise.folder}[/note] [success]✔')
+
+
+@app.command()
+@inside_exercise
+@check_pypas_version
+def doc():
+    """Open documentation for exercise."""
+    exercise = Exercise.from_config()
+    exercise.open_docs()
+
+
+@app.command()
+@inside_exercise
+@check_pypas_version
+def update(
+    force: bool = typer.Option(
+        False, '--force', '-f', help='Force update and omit backup of existing files'
+    ),
+):
+    """Update exercise."""
+    # Backup patterns follow fnmatch syntax: https://docs.python.org/3/library/fnmatch.html
+    if (exercise := Exercise.from_config()).is_up_to_date():
+        console.success(
+            f"You're on the latest version of [i]{exercise}[/i]: [note]{exercise.version}[/note]"
+        )
+        return
+    config = Config()
+    if exercise.download(config.get('token')):  # type: ignore
+        dir = exercise.unzip(to_tmp_dir=True)
+        exercise.update(src_dir=dir, backup=not force)
+
+
+@app.command()
+@check_pypas_version
+def auth(token: str = typer.Argument(default='', help='Access token')):
+    """Authenticate at pypas.es."""
+    if not token:
+        config = Config()
+        if saved_token := config.get('token'):
+            console.info(f'Token: [note]{saved_token}[/note]')
+            user = User(saved_token)  # type: ignore
+            if info := user.auth_info():
+                console.info(f'Username: {info["username"]}')
+                console.info(f'Context: {info["context"]}')
+        else:
+            console.warning('You are not authenticated yet.')
+        return
+    if User(token).authenticate():
+        config = Config()
+        config.save(token=token)
+
+
+@app.command()
+def upgrade():
+    """Upgrade pypas-cli from PyPI."""
+    sysutils.handle_upgrade_pypas()
+
+
+@app.command()
+@inside_exercise
+@check_pypas_version
+def zip(verbose: bool = typer.Option(False, '--verbose', '-v', help='Increase verbosity.')):
+    """Compress exercise contents."""
+    # Excluded patterns follow fnmatch syntax: https://docs.python.org/3/library/fnmatch.html
+    exercise = Exercise.from_config()
+    zipfile = exercise.zip(verbose=verbose)
+    size, str_size = sysutils.get_file_size(zipfile)
+    console.info(f'Compressed exercise is available at: [note]{zipfile}[/note] [dim]({str_size})')
+
+
+@app.command()
+@auth_required
+@inside_exercise
+@check_pypas_version
+def put():
+    """Put (upload) exercise."""
+    config = Config()
+    if nested_config_path := config.find_nested_config(relative_to_cwd=True):
+        console.warning(
+            f'Another exercise seems to be nested inside current folder: ./{nested_config_path}'
+        )
+        console.info('[italic]If continue, upload will [red]BREAK[/red] testing[/italic]')
+        if not Confirm.ask('Continue', default=False):
+            return
+    exercise = Exercise.from_config()
+    zipfile = exercise.zip(to_tmp_dir=True)
+    exercise.upload(zipfile, config['token'])  # type: ignore
+
+
+@app.command(context_settings={'ignore_unknown_options': True})
+@inside_exercise
+@check_exercise_version(confirm=True, confirm_suffix='testing')
+@check_pypas_version(confirm=True, confirm_suffix='testing')
+def test(
+    args: List[str] = typer.Argument(None, help='Arguments passed to test tool'),
+):
+    """Test exercise."""
+    exercise = Exercise.from_config()
+    exercise.test(args or [])
+
+
+@app.command()
+@check_pypas_version
+def log(
+    frame: str = typer.Option('', '--frame', '-f', help='Filter by frame.'),
+    verbose: bool = typer.Option(False, '--verbose', '-v', help='Increase verbosity.'),
+):
+    """Log of uploaded assignments."""
+    config = Config()
+    Exercise.log(config.get('token'), frame, verbose)  # type: ignore
+
+
+@app.command()
+@check_pypas_version
+def list(
+    frame: str = typer.Option('', '--frame', '-f', help='Filter by frame.'),
+    primary_topic: str = typer.Option('', '--ptopic', '-p', help='Filter by primary topic.'),
+    secondary_topic: str = typer.Option('', '--stopic', '-s', help='Filter by secondary topic.'),
+):
+    """List exercises. Topic in format <primary>/<secondary>"""
+    config = Config()
+    Exercise.list(config.get('token'), frame, primary_topic, secondary_topic)  # type: ignore
+
+
+@app.command()
+@check_pypas_version
+def unauth():
+    """Unauthenticate from pypas.es (clear token)."""
+    config = Config()
+    config.save(token='')
+    console.success('You have been successfully unauthenticated')
+
+
+@app.command()
+@check_pypas_version
+def run():
+    """Run exercise with given args."""
+    exercise = Exercise.from_config()
+    exercise.run()
+
+
+@app.command()
+@check_pypas_version
+def pull(
+    item_slug: str = typer.Argument(help='Slug of exercise or frame.'),
+):
+    """Pull (download) specific assignment or all frame assignments."""
+    if (dst_folder := Path(item_slug)).exists():
+        console.warning(f'Folder ./{dst_folder} already exists!')
+        console.info(
+            '[italic]If continue, files coming from server will [red]OVERWRITE[/red] your existing files'
+        )
+        if not Confirm.ask('Continue', default=False):
+            return
+    config = Config()
+    if file := Exercise.pull(item_slug, config.get('token')):  # type: ignore
+        folder = sysutils.unzip(file, extract_to=dst_folder)
+        console.info(f'Assignment(s) are available at [note]./{folder.name}[/note] [success]✔')
+
+
+@app.command()
+@inside_exercise
+@check_pypas_version
+def exercise(
+    add_release_notes: bool = typer.Option(
+        False, '--release-notes', '-r', help='Add release notes.'
+    ),
+):
+    """Show exercise information."""
+    exercise = Exercise.from_config()
+    exercise.show_info(add_release_notes=add_release_notes)
+
+
+if __name__ == '__main__':
+    app()
