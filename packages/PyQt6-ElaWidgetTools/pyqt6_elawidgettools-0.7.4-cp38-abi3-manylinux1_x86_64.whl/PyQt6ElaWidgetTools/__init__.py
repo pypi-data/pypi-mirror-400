24b8757817ffafa451822a1c49dc50e755c030a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 06:06:52 +0000

    Add package PyQt6-ElaWidgetTools/0.7.4/pyqt6_elawidgettools-0.7.4-cp38-abi3-manylinux1_x86_64.whl

---
+++ packages/PyQt6-ElaWidgetTools/pyqt6_elawidgettools-0.7.4-cp38-abi3-manylinux1_x86_64.whl/PyQt6ElaWidgetTools/__init__.py
@@ -0,0 +1,21 @@
+from PyQt6 import QtCore, QtWidgets, QtGui
+from .ElaWidgetTools import *
+
+
+def __wrapsingleton(T):
+
+    class __:
+        def __getattr__(self, name):
+            return getattr(T.getInstance(), name)
+
+    return __()
+
+
+eTheme: ElaTheme = __wrapsingleton(ElaTheme)
+eApp: ElaApplication = __wrapsingleton(ElaApplication)
+
+
+def ElaThemeColor(
+    themeMode: ElaThemeType.ThemeMode, themeColor: ElaThemeType.ThemeColor
+):
+    return eTheme.getThemeColor(themeMode, themeColor)
