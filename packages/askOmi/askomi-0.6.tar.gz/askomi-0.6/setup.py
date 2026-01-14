3b804602080cbcfbe96f1f47288bad26b47a8845
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 09:10:41 +0000

    Add package askOmi/0.6/askomi-0.6.tar.gz

---
+++ packages/askOmi/askomi-0.6.tar.gz/askomi-0.6/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='askOmi',
+    version='0.6',
+    packages=find_packages(),
+    install_requires=[
+        'google-generativeai',
+    ],  # list any dependencies
+    author='Why Should I reveal',
+    description='Hi Cutie',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+)
