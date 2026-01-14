7914225a2d2696c39f429300d13fa51f91e66d90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 04:24:38 +0000

    Add package admin-scripts/0.7.0/admin_scripts-0.7.0.tar.gz

---
+++ packages/admin-scripts/admin_scripts-0.7.0.tar.gz/admin_scripts-0.7.0/README.md
@@ -0,0 +1,14 @@
+# [Admin Scripts](http://admin-scripts.hive.pt)
+
+Set of scripts for general administration tasks.
+
+## License
+
+Admin Scripts is currently licensed under the [Apache License, Version 2.0](http://www.apache.org/licenses/).
+
+## Build Automation
+
+[![Build Status](https://app.travis-ci.com/hivesolutions/admin-scripts.svg?branch=master)](https://travis-ci.com/github/hivesolutions/admin-scripts)
+[![Coverage Status](https://coveralls.io/repos/hivesolutions/admin-scripts/badge.svg?branch=master)](https://coveralls.io/r/hivesolutions/admin-scripts?branch=master)
+[![PyPi Status](https://img.shields.io/pypi/v/admin-scripts.svg)](https://pypi.python.org/pypi/admin-scripts)
+[![License](https://img.shields.io/badge/license-Apache%202.0-blue.svg)](https://www.apache.org/licenses/)
---
+++ packages/admin-scripts/admin_scripts-0.7.0.tar.gz/admin_scripts-0.7.0/src/admin_scripts/extra/__init__.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Hive Administration Scripts
+# Copyright (c) 2008-2025 Hive Solutions Lda.
+#
+# This file is part of Hive Administration Scripts.
+#
+# Hive Administration Scripts is free software: you can redistribute it and/or modify
+# it under the terms of the Apache License as published by the Apache
+# Foundation, either version 2.0 of the License, or (at your option) any
+# later version.
+#
+# Hive Administration Scripts is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# Apache License for more details.
+#
+# You should have received a copy of the Apache License along with
+# Hive Administration Scripts. If not, see <http://www.apache.org/licenses/>.
+
+__copyright__ = "Copyright (c) 2008-2025 Hive Solutions Lda."
+""" The copyright for the module """
+
+__license__ = "Apache License, Version 2.0"
+""" The license for the module """
+
+from . import css
+from . import js
+from . import lib
+from . import log
+from . import shell
+
+from .css import uniqify, simplify_hex_colors, css_slimmer
+from .js import javascript_minify
+from .lib import handle_ignore, normalize_path, configuration
+from .log import STDOUT, STDERR, echo, warn, error, has_errors
+from .shell import shell_exec
