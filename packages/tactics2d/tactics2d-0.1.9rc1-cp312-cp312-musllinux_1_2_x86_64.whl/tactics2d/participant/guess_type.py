dde6f1c3a6f008eb9d2bd9bdd0729f0c356213d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 14:09:49 +0000

    Add package tactics2d/0.1.9rc1/tactics2d-0.1.9rc1-cp312-cp312-musllinux_1_2_x86_64.whl

---
+++ packages/tactics2d/tactics2d-0.1.9rc1-cp312-cp312-musllinux_1_2_x86_64.whl/tactics2d/participant/guess_type.py
@@ -0,0 +1,63 @@
+# Copyright (C) 2023, Tactics2D Authors. Released under the GNU GPLv3.
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+"""Guess type implementation."""
+
+
+import os
+
+import joblib
+import numpy as np
+
+from tactics2d.participant.trajectory.trajectory import Trajectory
+
+
+class GuessType:
+    """This class provides a set of SVM classifiers to roughly guess the type of a traffic participant based on different features.
+
+    The training process of the SVM classifiers are in the ./utils folder.
+    """
+
+    def __init__(self):
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        self.trajectory_clf = joblib.load(os.path.join(current_dir, "trajectory_classifier.m"))
+
+    def guess_by_size(self, size_info: tuple, hint_type: str):
+        """Guess the type of the participant by the size information with SVM model.
+
+        This method is usually used to distinguish different type of vehicles.
+
+        [TODO]: To be implemented.
+
+        Args:
+            size_info (tuple): _description_
+            hint_type (str): _description_
+        """
+        return
+
+    def guess_by_trajectory(self, trajectory: Trajectory) -> str:
+        """Guess the type of the participant by the trajectory with SVM model.
+
+        This method is recommend for distinguishing the pedestrians from the cyclists.
+
+        Args:
+            trajectory (Trajectory): _description_
+            hint_type (str): _description_
+
+        Returns:
+            _type_: _description_
+        """
+        history_speed = np.array([state.speed for state in trajectory.history_states.values()])
+        history_heading = np.array([state.heading for state in trajectory.history_states.values()])
+        speed_max = np.max(history_speed)
+        speed_min = np.min(history_speed)
+        speed_mean = np.mean(history_speed)
+        speed_std = np.std(history_speed)
+        heading_changing_std = (
+            np.std(history_heading[1:] - history_heading[:-1]) if len(history_heading) > 1 else 0
+        )
+
+        X = np.array([[speed_min, speed_max, speed_mean, speed_std, heading_changing_std]])
+        y_predict = self.trajectory_clf.predict(X)
+
+        return y_predict[0]
