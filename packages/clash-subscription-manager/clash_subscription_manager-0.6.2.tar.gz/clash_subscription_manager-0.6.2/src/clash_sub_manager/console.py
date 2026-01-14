6757564765607c9c4e8ae66bd06fbf014882031e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 03:35:58 +0000

    Add package clash-subscription-manager/0.6.2/clash_subscription_manager-0.6.2.tar.gz

---
+++ packages/clash-subscription-manager/clash_subscription_manager-0.6.2.tar.gz/clash_subscription_manager-0.6.2/src/clash_sub_manager/console.py
@@ -0,0 +1,13 @@
+"""Terminal formatting helpers."""
+
+
+class Colors:
+    """Simple ANSI color helpers used across CLIs."""
+
+    RED = '\033[0;31m'
+    GREEN = '\033[0;32m'
+    YELLOW = '\033[1;33m'
+    BLUE = '\033[0;34m'
+    MAGENTA = '\033[0;35m'
+    CYAN = '\033[0;36m'
+    NC = '\033[0m'
