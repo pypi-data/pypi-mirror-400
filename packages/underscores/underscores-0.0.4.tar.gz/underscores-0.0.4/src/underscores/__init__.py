f2abf8b68d33b8fd624e99ef4c039411bb795177
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 13:20:44 +0000

    Add package underscores/0.0.4/underscores-0.0.4.tar.gz

---
+++ packages/underscores/underscores-0.0.4.tar.gz/underscores-0.0.4/src/underscores/__init__.py
@@ -0,0 +1,32 @@
+"""
+`_` allows you to write Python code using only `_`.
+
+Importing this module will register the `_` encoding with the Python interpreter,
+which should be done automatically by the `_.pth` file in the `site-packages` directory.
+See the [README](https://github.com/lemonyte/_) for more information.
+
+## Usage
+
+The following script is a "Hello, world!" program which can be executed by the Python interpreter:
+
+```python
+# coding: _
+_ ____ _ _____ _ ____ __ _ _ ___ ______ ____ _ ____ _ ___ _ ____ __ ___ _ __ _
+_____ _ _ ______ _____ _ ___ _ _ _ ___ _____ ______ _ ____ _ _ _ ____ _ _ _ ____
+_ ____ _ __ __ ___ _ _ ______ ___ _ ____ __ ______ _ ____ _ ____ _ ____ __ _ _
+____ _ _ _ ___ _____ _____ _ _ ______ ____ _ _ ______ _____ _ __ _ ______
+```
+
+You can generate a script with `_`:
+
+```python
+print('print("Hello, world!")'.encode("_").decode("utf-8"))
+```
+"""
+
+from importlib.metadata import version
+
+from underscores._ import _
+
+__version__ = version("underscores")
+__all__ = ("_",)
