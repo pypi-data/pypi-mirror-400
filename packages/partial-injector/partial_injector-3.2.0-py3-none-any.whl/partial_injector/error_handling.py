495def7c6793cf55c398518cde5f9b5e7a65350d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 00:08:42 +0000

    Add package partial-injector/3.2.0/partial_injector-3.2.0-py3-none-any.whl

---
+++ packages/partial-injector/partial_injector-3.2.0-py3-none-any.whl/partial_injector/error_handling.py
@@ -0,0 +1,8 @@
+import traceback
+
+class PartialContainerException(Exception):
+    def __init__(self, message):
+        self.message = message
+
+    def __str__(self):
+        return self.message
