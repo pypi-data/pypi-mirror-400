0097669aff1022632b7c63f1d9679a7bd7349fc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 04:23:31 +0000

    Add package samplemaker-sparrow/5.4.6/samplemaker_sparrow-5.4.6-cp310-cp310-manylinux_2_26_aarch64.manylinux_2_28_aarch64.whl

---
+++ packages/samplemaker-sparrow/samplemaker_sparrow-5.4.6-cp310-cp310-manylinux_2_26_aarch64.manylinux_2_28_aarch64.whl/samplemaker/resources/__init__.py
@@ -0,0 +1,6 @@
+"""
+Resources module for samplemaker.
+
+This module provides access to resource files and compiled extensions,
+including the boopy C++ extension for Boolean polygon operations.
+"""
