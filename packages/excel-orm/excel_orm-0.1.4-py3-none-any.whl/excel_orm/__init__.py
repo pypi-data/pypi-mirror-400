88a7169fa2fd429ef027b605d6f4282fecd03dec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 05:10:42 +0000

    Add package excel-orm/0.1.4/excel_orm-0.1.4-py3-none-any.whl

---
+++ packages/excel-orm/excel_orm-0.1.4-py3-none-any.whl/excel_orm/__init__.py
@@ -0,0 +1,23 @@
+from .base import RowBase
+from .column import (
+    Column,
+    ColumnSpec,
+    bool_column,
+    date_column,
+    int_column,
+    text_column,
+)
+from .orm import ExcelFile, PivotSheetSpec, SheetSpec
+
+__all__ = [
+    "Column",
+    "ColumnSpec",
+    "ExcelFile",
+    "PivotSheetSpec",
+    "RowBase",
+    "SheetSpec",
+    "bool_column",
+    "date_column",
+    "int_column",
+    "text_column",
+]
