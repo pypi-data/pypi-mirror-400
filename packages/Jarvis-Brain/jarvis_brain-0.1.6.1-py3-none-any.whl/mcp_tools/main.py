ed06d6b6893ec3f5c73067b671d549800580383b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 04:23:23 +0000

    Add package Jarvis-Brain/0.1.6.1/jarvis_brain-0.1.6.1-py3-none-any.whl

---
+++ packages/Jarvis-Brain/jarvis_brain-0.1.6.1-py3-none-any.whl/mcp_tools/main.py
@@ -0,0 +1,31 @@
+from fastmcp import FastMCP
+
+from mcp_tools.dp_tools import *
+from tools.browser_manager import browser_manager
+
+mcp = FastMCP("Jarvis Brain Mcp Tools")
+
+# 根据环境变量加载模块
+enabled_modules = os.getenv("MCP_MODULES", "TeamNode-Dp").split(",")
+base_cwd = os.getenv("BASE_CWD", os.path.expanduser('~'))
+
+if "TeamNode-Dp" in enabled_modules:
+    # 页面管理
+    register_close_tab(mcp, browser_manager)
+    register_switch_tab(mcp, browser_manager)
+    register_get_new_tab(mcp, browser_manager)
+    # 功能
+    register_visit_url(mcp, browser_manager)
+    register_get_html(mcp, browser_manager)
+    register_check_selector(mcp, browser_manager)
+
+if "JarvisNode" in enabled_modules:
+    register_assert_waf(mcp, browser_manager)
+
+
+def main():
+    mcp.run(transport="stdio")
+
+
+if __name__ == '__main__':
+    main()
