701876eb00539c5c980a4da7ed45da239b6e95be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 12:08:11 +0000

    Add package pyguard-args/0.2.0/pyguard_args-0.2.0-py3-none-any.whl

---
+++ packages/pyguard-args/pyguard_args-0.2.0-py3-none-any.whl/pyguard/guard.py
@@ -0,0 +1,118 @@
+from collections import defaultdict
+from inspect import BoundArguments
+from typing import Any, Optional
+
+from pyguard import validators
+from pyguard.exceptions import GuardConfigurationException, GuardValidationError
+from pyguard.utils import is_hint_optional
+
+Validator = validators.Validator
+
+
+class Guard:
+    __registered_validators__: dict[str, type["Validator"]] = {}
+
+    @classmethod
+    def register_validator(cls, keyword: str):
+        def decorator(validator: type["Validator"]):
+            if not hasattr(cls, "__registered_validators__") or cls.__registered_validators__ is None:
+                cls.__registered_validators__ = {}
+
+            if keyword in cls.__registered_validators__:
+                raise GuardConfigurationException(f"{keyword} is already registered")
+
+            cls.__registered_validators__[keyword] = validator
+            return validator
+
+        return decorator
+
+    @classmethod
+    def get_validator(cls, keyword: str) -> Optional[type["Validator"]]:
+        validator = cls.__registered_validators__.get(keyword, None)
+        if not validator:
+            raise GuardConfigurationException(f"Validator {keyword} is not registered")
+        return validator
+
+    @classmethod
+    def _validate_argument(
+        cls,
+        configuration: dict[str, Any],
+        argument: str,
+        value: Any,
+        bound: BoundArguments,
+    ) -> list[str]:
+        errors = []
+
+        for rule_name, expected_config in configuration.items():
+            validator_class = Guard.get_validator(rule_name)
+
+            expected_value = expected_config
+            custom_error_message = None
+
+            if isinstance(expected_config, tuple):
+                # Handle tuple expected values for LengthValidator
+                if validator_class == validators.LengthValidator:
+                    if isinstance(expected_config[0], tuple):
+                        expected_value, custom_error_message = expected_config
+                    else:
+                        expected_value = expected_config
+                else:
+                    expected_value, custom_error_message = expected_config
+
+            validator = validator_class(name=argument, expected=expected_value, bound=bound)
+
+            if error := validator.validate(value=value):
+                errors.append(custom_error_message or error)
+        return errors
+
+    @classmethod
+    def validate_arguments(
+        cls, function_name: str, bound: BoundArguments, hints: dict[str, Any], guard_config: dict[str, Any]
+    ) -> None:
+        validation_errors = defaultdict(list)
+
+        for argument, value in bound.arguments.items():
+            argument_configuration = guard_config.get(argument, {})
+            argument_hints = hints.get(argument)
+
+            if argument_hints and not is_hint_optional(argument_hints):
+                argument_configuration["required"] = True
+
+            # If hints are applied, automatically add type validation
+            if "type" not in argument_configuration and argument_hints is not None:
+                argument_configuration["type"] = argument_hints
+
+            if argument_errors := cls._validate_argument(argument_configuration, argument, value, bound):
+                validation_errors[argument] = argument_errors
+
+        if validation_errors:
+            raise GuardValidationError(function_name=function_name, errors=validation_errors)
+
+
+def register_default_validators():
+    """
+    Register default validators
+    """
+    Guard.register_validator("lt")(validators.LessThan)
+    Guard.register_validator("lte")(validators.LessThanOrEqual)
+    Guard.register_validator("gt")(validators.GreaterThan)
+    Guard.register_validator("gte")(validators.GreaterThanOrEqual)
+    Guard.register_validator("choices")(validators.ChoicesValidator)
+    Guard.register_validator("type")(validators.TypeValidator)
+    Guard.register_validator("required")(validators.RequiredValidator)
+    Guard.register_validator("keys")(validators.RequiredKeyValidator)
+    Guard.register_validator("schema")(validators.SchemaValidator)
+    Guard.register_validator("length")(validators.LengthValidator)
+    Guard.register_validator("regex")(validators.RegexValidator)
+    Guard.register_validator("email")(validators.EmailValidator)
+    Guard.register_validator("url")(validators.URLValidator)
+    Guard.register_validator("startswith")(validators.StartsWithValidator)
+    Guard.register_validator("endswith")(validators.EndsWithValidator)
+    Guard.register_validator("contains")(validators.ContainsValidator)
+    Guard.register_validator("ip")(validators.IPAddressValidator)
+    Guard.register_validator("uuid")(validators.UUIDValidator)
+    Guard.register_validator("lowercase")(validators.LowercaseValidator)
+    Guard.register_validator("uppercase")(validators.UppercaseValidator)
+
+
+register_default_validators()
