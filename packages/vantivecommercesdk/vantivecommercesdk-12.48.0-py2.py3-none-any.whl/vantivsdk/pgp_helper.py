9f77b1d0202bd3a7a827425e62a921e4630b4f5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 12:08:07 +0000

    Add package vantivecommercesdk/12.48.0/vantivecommercesdk-12.48.0-py2.py3-none-any.whl

---
+++ packages/vantivecommercesdk/vantivecommercesdk-12.48.0-py2.py3-none-any.whl/vantivsdk/pgp_helper.py
@@ -0,0 +1,101 @@
+import subprocess
+from subprocess import check_output
+from subprocess import CalledProcessError
+
+import os
+
+from . import (utils)
+
+class PgpHelper(object):
+
+  # Encrypt a file.
+  def encryptFile(self, recipient, toBeEncryptedFilepath, outputFilepath):
+    # Call gpg command line to encrypt the file.
+    try:
+      check_output(["gpg",
+      "--batch",
+      "--yes",
+      "--always-trust",
+      "--no-secmem-warning",
+      "--armor",
+      "--trust-model", "always",
+      "--output", outputFilepath,
+      "--recipient", recipient,
+      "--encrypt", toBeEncryptedFilepath])
+      # Check for error code.
+      print("\"%s\" has been encrypted to \"%s\"." % (toBeEncryptedFilepath, outputFilepath))
+    except CalledProcessError as err:
+      raise utils.VantivException("Encrypting the file has failed!\n%s" % err.output)
+
+
+  # Handle gpg encryption when the output filename is the same as the input filename.
+  def encryptFileSameName(self, recipient, toBeEncryptedFilepath):
+    try:
+      temp = 'pgp.vantiv'
+      self.encryptFile(recipient, toBeEncryptedFilepath, temp)
+      writer = open(toBeEncryptedFilepath, 'wb')
+      reader = open(temp, 'rb')
+      writer.write(reader.read())
+      writer.close()
+      reader.close()
+      os.remove(temp)
+    except CalledProcessError as err:
+      raise utils.VantivException("Encrypting the file to the output with the same name has failed!\n%s" % err.output)
+
+
+  # Decrypt an encrypted file.
+  def decryptFile(self, passphrase, encryptedFilepath, outputFilepath):
+    # Call gpg command line to decrypt the file.
+    try:
+      check_output(["gpg",
+      "--batch",
+      "--yes",
+      "--no-secmem-warning",
+      "--no-mdc-warning",
+      "--output", outputFilepath,
+      "--passphrase", passphrase,
+      "--decrypt", encryptedFilepath])
+      # Check for error code.
+      print("\"%s\" has been decrypted to \"%s\"." % (encryptedFilepath, outputFilepath))
+    except CalledProcessError as err:
+      raise utils.VantivException("Decrypting the file has failed!\n%s" % err.output)
+
+
+  # Add Vantiv public key into merchants' keyrings.
+  def importVantivPublicKey(self, publicKeyFilePath):
+    # Call gpg command line to import public key.
+    try:
+      check_output(["gpg",
+      "--import", publicKeyFilePath])
+      #Check for error code.
+      print("Successfully added Vantiv public key!")
+    except CalledProcessError as err:
+      raise utils.VantivException("Adding Vantiv public key has failed with error code is %s.\n" % err.output)
+
+def encryptPayload(toBeEncryptedString, path):
+    byte_array = bytearray(toBeEncryptedString, 'utf-8')
+      # Convert the object to a JSON string
+    try:
+      # Call gpg command line to encrypt the string
+      process = subprocess.Popen([
+        "gpg",
+        "--batch",
+        "--yes",
+        "--always-trust",
+        "--no-secmem-warning",
+        "--armor",
+        "--trust-model", "always",
+        "--recipient-file", path,
+        "--encrypt"
+      ], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+      # Pass the string to be encrypted to the gpg process
+      encrypted_string, err = process.communicate(input=byte_array)
+
+      if process.returncode != 0:
+        raise subprocess.CalledProcessError(process.returncode, process.args, output=err)
+
+      return encrypted_string.decode()
+    except subprocess.CalledProcessError as err:
+      raise utils.VantivException(
+        "Encrypting the payload has failed. Please check the Encryption key or key path, is correct!\n%s" % err.output)
