214351e48af3d50dfb00a813c997a86954c76ed6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 03:35:30 +0000

    Add package lib-clockifybot/3.0.0/lib_clockifybot-3.0.0.tar.gz

---
+++ packages/lib-clockifybot/lib_clockifybot-3.0.0.tar.gz/lib_clockifybot-3.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+
+def read_requirements(file):
+    with open(file) as f:
+        return f.read().splitlines()
+
+
+setup(
+    name="lib_clockifybot",
+    version="3.0.0",
+    author="retr0err0r - veininvein",
+    packages=find_packages(),
+    install_requires=read_requirements("requirements.txt"),
+)
