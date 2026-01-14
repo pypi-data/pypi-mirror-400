db42178ead02b398ab9809709fa9828102792e34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 14:09:39 +0000

    Add package python-imager/1.4.48/python_imager-1.4.48.tar.gz

---
+++ packages/python-imager/python_imager-1.4.48.tar.gz/python_imager-1.4.48/src/pythonImager/resolution.py
@@ -0,0 +1,19 @@
+from screeninfo import get_monitors
+
+monitors = [m for m in get_monitors()]
+
+class resolution:
+    resolutions = [(m.width, m.height) for m in monitors]
+    try: index = [m.is_primary for m in monitors].index(True)
+    except: index = 0
+    resolution = resolutions[index]
+    def update() -> None:
+        resolution.index = (resolution.index+1)%len(resolution.resolutions)
+        resolution.resolution = resolution.resolutions[resolution.index]
+    def update_resses() -> None:
+        monitors = [m for m in get_monitors()]
+        resolution.resolutions = [(m.width, m.height) for m in monitors]
+        resolution.index = [m.is_primary for m in monitors].index(True)
+        resolution.resolution = resolution.resolutions[resolution.index]
+    def percentile(x, y) -> list[int]:
+        return [resolution.resolution[0]/100*x, resolution.resolution[1]/100*y]
