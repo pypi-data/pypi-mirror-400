1019c56ef9b240f642f24abeea515016eede63f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 12:07:10 +0000

    Add package colorstreak/2.1.1/colorstreak-2.1.1.tar.gz

---
+++ packages/colorstreak/colorstreak-2.1.1.tar.gz/colorstreak-2.1.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='colorstreak',
+    version='2.1.0',
+    description='Terminal color formatting for Python, with logging capabilities',
+    author='Fernando Leon Franco',
+    packages=find_packages(),
+    install_requires=[],
+)
