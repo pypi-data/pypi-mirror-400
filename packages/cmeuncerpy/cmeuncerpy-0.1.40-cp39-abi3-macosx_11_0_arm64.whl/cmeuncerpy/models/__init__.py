695bc42dcc403c9bae1563fd8ef15c87df89353b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 00:08:28 +0000

    Add package cmeuncerpy/0.1.40/cmeuncerpy-0.1.40-cp39-abi3-macosx_11_0_arm64.whl

---
+++ packages/cmeuncerpy/cmeuncerpy-0.1.40-cp39-abi3-macosx_11_0_arm64.whl/cmeuncerpy/models/__init__.py
@@ -0,0 +1,4 @@
+from cmeuncerpy.models.LinearRegression import LinearRegression
+from cmeuncerpy.models.NeuralNetworkRegression import NeuralNetworkRegression
+
+__all__ = ["LinearRegression", "NeuralNetworkRegression"]
