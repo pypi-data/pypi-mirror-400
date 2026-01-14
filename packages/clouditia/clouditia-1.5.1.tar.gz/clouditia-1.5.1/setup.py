d134ddd24c75f5b4169dbdd1145cf8fa4a7c7d73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 15:09:17 +0000

    Add package clouditia/1.5.1/clouditia-1.5.1.tar.gz

---
+++ packages/clouditia/clouditia-1.5.1.tar.gz/clouditia-1.5.1/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+"""
+Clouditia SDK - Execute Python and Shell code on remote GPU sessions.
+
+This is a compatibility shim for older pip versions that don't support
+pyproject.toml. Modern pip versions will use pyproject.toml directly.
+"""
+
+from setuptools import setup
+
+if __name__ == "__main__":
+    setup()
