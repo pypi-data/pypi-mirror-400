52eeec8d3f4bc7c82f41f7339cf1708096c6ac0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 08:06:47 +0000

    Add package gptdiff/0.5.2/gptdiff-0.5.2-py3-none-any.whl

---
+++ packages/gptdiff/gptdiff-0.5.2-py3-none-any.whl/gptdiff/__init__.py
@@ -0,0 +1,3 @@
+from .gptdiff import generate_diff, smartapply, load_project_files, build_environment, save_files
+
+__all__ = ['generate_diff', 'smartapply', 'load_project_files', 'build_environment', 'save_files']
