bf20e8ce57159e03ed50892950b9ea215e90cd29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 02:44:13 +0000

    Add package pulp-ostree-client/2.6.0/pulp_ostree_client-2.6.0.tar.gz

---
+++ packages/pulp-ostree-client/pulp_ostree_client-2.6.0.tar.gz/pulp_ostree_client-2.6.0/pulpcore/client/pulp_ostree/api/__init__.py
@@ -0,0 +1,14 @@
+# flake8: noqa
+
+# import apis into api package
+from pulpcore.client.pulp_ostree.api.content_commits_api import ContentCommitsApi
+from pulpcore.client.pulp_ostree.api.content_configs_api import ContentConfigsApi
+from pulpcore.client.pulp_ostree.api.content_content_api import ContentContentApi
+from pulpcore.client.pulp_ostree.api.content_objects_api import ContentObjectsApi
+from pulpcore.client.pulp_ostree.api.content_refs_api import ContentRefsApi
+from pulpcore.client.pulp_ostree.api.content_summaries_api import ContentSummariesApi
+from pulpcore.client.pulp_ostree.api.distributions_ostree_api import DistributionsOstreeApi
+from pulpcore.client.pulp_ostree.api.remotes_ostree_api import RemotesOstreeApi
+from pulpcore.client.pulp_ostree.api.repositories_ostree_api import RepositoriesOstreeApi
+from pulpcore.client.pulp_ostree.api.repositories_ostree_versions_api import RepositoriesOstreeVersionsApi
+
