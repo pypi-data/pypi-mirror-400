7c3231b4e3cbe59eaac5d0705454ea8f5e137109
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 13:20:40 +0000

    Add package typer2ui/0.12.0/typer2ui-0.12.0.tar.gz

---
+++ packages/typer2ui/typer2ui-0.12.0.tar.gz/typer2ui-0.12.0/typer2ui/__init__.py
@@ -0,0 +1,51 @@
+"""Typer-UI: Automatically generate desktop GUIs for Typer CLI applications."""
+
+from .spec_builder import build_app_spec
+from .state import State
+from .ui_app import UiApp, UICommand
+from .output import ui, text, dx
+from .data_source import DataSource
+from .ui_blocks import (
+    UiBlock,
+    Container,
+    Text,
+    Md,
+    Table,
+    Row,
+    Column,
+    Button,
+    Link,
+    TextInput,
+    Alert,
+    Confirm,
+    Tab,
+    Tabs,
+    DataTable,
+)
+
+__version__ = "0.12.0"
+__all__ = [
+    "build_app_spec",
+    "State",
+    "UiApp",
+    "UICommand",
+    "ui",
+    "text",
+    "dx",
+    "DataSource",
+    "UiBlock",
+    "Container",
+    "Text",
+    "Md",
+    "Table",
+    "DataTable",
+    "Row",
+    "Column",
+    "Button",
+    "Link",
+    "TextInput",
+    "Alert",
+    "Confirm",
+    "Tab",
+    "Tabs",
+]
