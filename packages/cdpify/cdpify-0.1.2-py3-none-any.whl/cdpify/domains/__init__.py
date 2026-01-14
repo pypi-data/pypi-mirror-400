f316a17a940854e4cd121059d140f6f76d203429
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 03:35:16 +0000

    Add package cdpify/0.1.2/cdpify-0.1.2-py3-none-any.whl

---
+++ packages/cdpify/cdpify-0.1.2-py3-none-any.whl/cdpify/domains/__init__.py
@@ -0,0 +1,89 @@
+"""Generated CDP domains."""
+
+from .page import PageClient
+from .eventbreakpoints import EventBreakpointsClient
+from .systeminfo import SystemInfoClient
+from .deviceorientation import DeviceOrientationClient
+from .target import TargetClient
+from .animation import AnimationClient
+from .domdebugger import DOMDebuggerClient
+from .console import ConsoleClient
+from .performance import PerformanceClient
+from .accessibility import AccessibilityClient
+from .io import IOClient
+from .dom import DOMClient
+from .cast import CastClient
+from .browser import BrowserClient
+from .network import NetworkClient
+from .fetch import FetchClient
+from .audits import AuditsClient
+from .heapprofiler import HeapProfilerClient
+from .domstorage import DOMStorageClient
+from .media import MediaClient
+from .tracing import TracingClient
+from .webauthn import WebAuthnClient
+from .storage import StorageClient
+from .schema import SchemaClient
+from .layertree import LayerTreeClient
+from .emulation import EmulationClient
+from .webaudio import WebAudioClient
+from .profiler import ProfilerClient
+from .runtime import RuntimeClient
+from .tethering import TetheringClient
+from .overlay import OverlayClient
+from .serviceworker import ServiceWorkerClient
+from .memory import MemoryClient
+from .input import InputClient
+from .security import SecurityClient
+from .debugger import DebuggerClient
+from .indexeddb import IndexedDBClient
+from .backgroundservice import BackgroundServiceClient
+from .css import CSSClient
+from .log import LogClient
+from .cachestorage import CacheStorageClient
+from .domsnapshot import DOMSnapshotClient
+
+__all__ = [
+    "PageClient",
+    "EventBreakpointsClient",
+    "SystemInfoClient",
+    "DeviceOrientationClient",
+    "TargetClient",
+    "AnimationClient",
+    "DOMDebuggerClient",
+    "ConsoleClient",
+    "PerformanceClient",
+    "AccessibilityClient",
+    "IOClient",
+    "DOMClient",
+    "CastClient",
+    "BrowserClient",
+    "NetworkClient",
+    "FetchClient",
+    "AuditsClient",
+    "HeapProfilerClient",
+    "DOMStorageClient",
+    "MediaClient",
+    "TracingClient",
+    "WebAuthnClient",
+    "StorageClient",
+    "SchemaClient",
+    "LayerTreeClient",
+    "EmulationClient",
+    "WebAudioClient",
+    "ProfilerClient",
+    "RuntimeClient",
+    "TetheringClient",
+    "OverlayClient",
+    "ServiceWorkerClient",
+    "MemoryClient",
+    "InputClient",
+    "SecurityClient",
+    "DebuggerClient",
+    "IndexedDBClient",
+    "BackgroundServiceClient",
+    "CSSClient",
+    "LogClient",
+    "CacheStorageClient",
+    "DOMSnapshotClient",
+]
