040014d211278d5c64627ed9277ec987129524c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 14:09:44 +0000

    Add package ailoy-py/0.2.5/ailoy_py-0.2.5-cp310-abi3-win_amd64.whl

---
+++ packages/ailoy-py/ailoy_py-0.2.5-cp310-abi3-win_amd64.whl/ailoy/__init__.py
@@ -0,0 +1,17 @@
+# ruff: noqa: F401
+# ruff: noqa: I001
+
+
+# start delvewheel patch
+def _delvewheel_patch_1_11_2():
+    import os
+    if os.path.isdir(libs_dir := os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'ailoy_py.libs'))):
+        os.add_dll_directory(libs_dir)
+
+
+_delvewheel_patch_1_11_2()
+del _delvewheel_patch_1_11_2
+# end delvewheel patch
+
+from ._core import *  # noqa: F403
+import ailoy._patches
